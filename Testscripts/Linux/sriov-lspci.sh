e03062259c946a3506b259a4e1143c8a04b99c50
Simon Xiao <sixiao@microsoft.com>
2018-05-29 14:25:18 -0700

    New test case: SRIOV, verify the VF entry from lspci output

---
+++ Testscripts/Linux/sriov-lspci.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# 
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+#
+# Sample script to run sysbench.
+# In this script, we want to bench-mark device IO performance on a mounted folder.
+# You can adapt this script to other situations easily like for stripe disks as RAID0.
+# The only thing to keep in mind is that each different configuration you're testing
+# must log its output to a different directory.
+#
+
+LogMsg()
+{
+	echo "[$(date +"%x %r %Z")] ${1}"
+	echo "[$(date +"%x %r %Z")] ${1}" >> "./TestExecution.log"
+}
+
+UpdateTestState()
+{
+	echo "${1}" > ./TestState.log
+}
+
+############################################################
+#	Main body
+############################################################
+
+LogMsg "*********INFO: Starting test execution ... *********"
+lspci=$(lspci)
+LogMsg $lspci
+
+lspci | grep -i "Mellanox"
+
+if [[ "$?" == "0" ]];
+then
+	UpdateTestState "PASS"
+else
+	UpdateTestState "FAIL"
+fi
+
+LogMsg "*********INFO: Script execution completed. *********"
+exit 0
