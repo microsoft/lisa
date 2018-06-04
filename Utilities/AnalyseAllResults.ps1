b7b51cbe49076f6c575505880718d47c6d7734e8
Shital Savekar <iamshital@users.noreply.github.com>
2018-06-04 16:18:47 -0700

    Fixed comment

--- Utilities/AnalyseAllResults.ps1
+++ Utilities/AnalyseAllResults.ps1
@@ -6,7 +6,7 @@
     # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 .DESCRIPTION
-    This script authenticates PS sessing using Azure principal account.
+    This script checks contents of the ./report/*-junit.xml files and exit with zero or non-zero exit code.
 
 .PARAMETER 
 
