390ccaf3d7ed7e85fc065e1bfd86f22959aec973
Ju-Hyoung Lee <juhlee@microsoft.com>
2018-07-27 14:43:13 -0700

    Fixed code with shellchecker

--- Testscripts/Linux/report-ntttcp-and-tcping.sh
+++ Testscripts/Linux/report-ntttcp-and-tcping.sh
@@ -8,7 +8,6 @@
 	lagscope_log_prefix="lagscope-ntttcp-p"
 	test_threads_collection="$2"
 	max_server_threads=64
-	result_parsed=""
 	result_file="${log_folder}/report.log"
 
 	if [[ $log_folder == ""  ]]; then
@@ -18,19 +17,19 @@
 
 	function get_throughput() {
 		throuput="Empty"
-		while read line
+		while read -r line
 		do
 			if [[ "$line" == *"throughput"* ]]; then
 				if [[ "$line" == *"Gbps"* ]]; then
-					temp1=`echo "$line" | awk -F'\t:' '{print $2}' | sed 's/Gbps//'`
+					temp1=$(echo "$line" | awk -F'\t:' '{print $2}' | sed 's/Gbps//')
 				elif [[ "$line" == *"Mbps"* ]]; then
-					temp1m=`echo "$line" | awk -F'\t:' '{print $2}' | sed 's/Mbps//'`
+					temp1m=$(echo "$line" | awk -F'\t:' '{print $2}' | sed 's/Mbps//')
 					temp1=$(echo "scale=3; $temp1m/1000" | bc)
 				fi
-				echo $temp1
+				echo "$temp1"
 				throuput="NotEmpty"
 			fi
-		done < $1
+		done < "$1"
 
 		if [ "$throuput" == "Empty" ]; then
 			echo 0
@@ -38,49 +37,49 @@
 	}
 
 	function get_cyclesperbyte() {
-		while read line
+		while read -r line
 		do
 			if [[ "$line" == *"cycles/byte"* ]]; then
 				echo "$line" | awk -F'\t:' '{print $2}'
 			fi
-		done < $1
+		done < "$1"
 	}
 
 	function get_latency() {
 		average="Empty"
-		while read line
+		while read -r line
 		do
 			if [[ "$line" == *"Average"* ]]; then
-				temp2=`echo "$line" | awk -F'=' '{print $4}' | awk -F'us' '{print $1}'`
-				echo $temp2
+				temp2=$(echo "$line" | awk -F'=' '{print $4}' | awk -F'us' '{print $1}')
+				echo "$temp2"
 				average="NotEmpty"
 			fi
-		done < $1
+		done < "$1"
 		
 		if [ "$average" == "Empty" ]; then
 			echo 0
 		fi
 	}
 
-	echo "#test_connections throughput_gbps cycles/bytes    average_tcp_latency" > $result_file
+	echo "#test_connections throughput_gbps cycles/bytes    average_tcp_latency" > "$result_file"
 	i=0
 	
 	for current_test_threads in $test_threads_collection; do
 		#current_test_threads=${test_threads_collection[$i]}
-		if [ $current_test_threads -lt $max_server_threads ]; then
+		if [ "$current_test_threads" -lt "$max_server_threads" ]; then
 			num_threads_P=$current_test_threads
 			num_threads_n=1
 		else
 			num_threads_P=$max_server_threads
-			num_threads_n=$(($current_test_threads / $num_threads_P))
+			num_threads_n=$((current_test_threads / num_threads_P))
 		fi
 
 		ntttcp_log_file="$log_folder/${ntttcp_log_prefix}${num_threads_P}X${num_threads_n}.log"
 		lagscope_log_file="$log_folder/${lagscope_log_prefix}${num_threads_P}X${num_threads_n}.log"
 
-		throughput=$(get_throughput $ntttcp_log_file)
-		cyclesperbytes=$(get_cyclesperbyte $ntttcp_log_file)
-		latency=$(get_latency $lagscope_log_file)
+		throughput=$(get_throughput "$ntttcp_log_file")
+		cyclesperbytes=$(get_cyclesperbyte "$ntttcp_log_file")
+		latency=$(get_latency "$lagscope_log_file")
 
 		if  [ "x$throughput" == "x" ]; then
 			throughput=0
@@ -94,9 +93,9 @@
 			latency=0
 		fi
 
-		printf "%4s  %8.2f %8.2f %8.2f\n" ${current_test_threads} ${throughput} ${cyclesperbytes} ${latency} >> $result_file
+		printf "%4s  %8.2f %8.2f %8.2f\\n" "${current_test_threads}" "${throughput}" "${cyclesperbytes}" "${latency}" >> "$result_file"
 
-		i=$(($i + 1))
+		i=$((i + 1))
 	done
 
-	cat $result_file
+	cat "$result_file"
--- Testscripts/Linux/run-ntttcp-and-tcping.sh
+++ Testscripts/Linux/run-ntttcp-and-tcping.sh
@@ -46,44 +46,44 @@
 
 	function get_tx_bytes() {
 		# RX bytes:66132495566 (66.1 GB)  TX bytes:3067606320236 (3.0 TB)
-		Tx_bytes=`ifconfig $eth_name | grep "TX bytes"   | awk -F':' '{print $3}' | awk -F' ' ' {print $1}'`
+		Tx_bytes=$(ifconfig $eth_name | grep "TX bytes"   | awk -F':' '{print $3}' | awk -F' ' ' {print $1}')
 
 		if [ "x$Tx_bytes" == "x" ]; then
 			#TX packets 223558709  bytes 15463202847 (14.4 GiB)
-			Tx_bytes=`ifconfig $eth_name| grep "TX packets"| awk '{print $5}'`
+			Tx_bytes=$(ifconfig $eth_name| grep "TX packets"| awk '{print $5}')
 		fi
-		echo $Tx_bytes
+		echo "$Tx_bytes"
 	}
 
 	function get_tx_pkts() {
 		# TX packets:543924452 errors:0 dropped:0 overruns:0 carrier:0
-		Tx_pkts=`ifconfig $eth_name | grep "TX packets" | awk -F':' '{print $2}' | awk -F' ' ' {print $1}'`
+		Tx_pkts=$(ifconfig $eth_name | grep "TX packets" | awk -F':' '{print $2}' | awk -F' ' ' {print $1}')
 
 		if [ "x$Tx_pkts" == "x" ]; then
 			#TX packets 223558709  bytes 15463202847 (14.4 GiB)
-			Tx_pkts=`ifconfig $eth_name| grep "TX packets"| awk '{print $3}'`
+			Tx_pkts=$(ifconfig $eth_name| grep "TX packets"| awk '{print $3}')
 		fi
 		echo $Tx_pkts
 	}
 
-	mkdir $log_folder
+	mkdir "$log_folder"
 	ssh $server_username@$server_ip "ulimit -n 20480"
 	ssh $server_username@$server_ip "mkdir $log_folder"
 
-	rm -rf $eth_log
-	echo "#test_connections    throughput_gbps    average_packet_size" > $eth_log
+	rm -rf "$eth_log"
+	echo "#test_connections    throughput_gbps    average_packet_size" > "$eth_log"
 
 	previous_tx_bytes=$(get_tx_bytes)
 	previous_tx_pkts=$(get_tx_pkts)
 	i=0
 	for current_test_threads in $test_threads_collection; do
 		#current_test_threads=${test_threads_collection[$i]}
-		if [ $current_test_threads -lt $max_server_threads ]; then
+		if [ "$current_test_threads" -lt "$max_server_threads" ]; then
 			num_threads_P=$current_test_threads
 			num_threads_n=1
 		else
 			num_threads_P=$max_server_threads
-			num_threads_n=$(($current_test_threads / $num_threads_P))
+			num_threads_n=$((current_test_threads / num_threads_P))
 		fi
 
 		echo "======================================"
@@ -104,22 +104,21 @@
 		ssh $server_username@$server_ip "dstat -dam > ./$log_folder/dstat-receiver-p${num_threads_P}X${num_threads_n}.log" &
 
 		ssh $server_username@$server_ip "pkill -f mpstat"
-		ssh $server_username@$server_ip "mpstat -P ALL 1 ${test_run_duration} > \
-        ./$log_folder/mpstat-receiver-p${num_threads_P}X${num_threads_n}.log" &
+		ssh $server_username@$server_ip "mpstat -P ALL 1 ${test_run_duration} > ./$log_folder/mpstat-receiver-p${num_threads_P}X${num_threads_n}.log" &
 
 		ulimit -n 204800
 		sleep 2
-		sar -n DEV 1 ${test_run_duration} > "./$log_folder/sar-sender-p${num_threads_P}X${num_threads_n}.log" &
+		sar -n DEV 1 "${test_run_duration}" > "./$log_folder/sar-sender-p${num_threads_P}X${num_threads_n}.log" &
 		dstat -dam > "./$log_folder/dstat-sender-p${num_threads_P}X${num_threads_n}.log" &
-		mpstat -P ALL 1 ${test_run_duration} > "./$log_folder/mpstat-sender-p${num_threads_P}X${num_threads_n}.log" &
-		lagscope -s$server_ip -t ${test_run_duration} -V > "./$log_folder/lagscope-ntttcp-p${num_threads_P}X${num_threads_n}.log" &
-		ntttcp -s${server_ip} -P $num_threads_P -n $num_threads_n -t ${test_run_duration}  > \
+		mpstat -P ALL 1 "${test_run_duration}" > "./$log_folder/mpstat-sender-p${num_threads_P}X${num_threads_n}.log" &
+		lagscope -s$server_ip -t "${test_run_duration}" -V > "./$log_folder/lagscope-ntttcp-p${num_threads_P}X${num_threads_n}.log" &
+		ntttcp -s${server_ip} -P $num_threads_P -n $num_threads_n -t "${test_run_duration}"  > \
         "./$log_folder/ntttcp-sender-p${num_threads_P}X${num_threads_n}.log"
 
 		current_tx_bytes=$(get_tx_bytes)
 		current_tx_pkts=$(get_tx_pkts)
-		bytes_new=$(($current_tx_bytes-$previous_tx_bytes))
-		pkts_new=$(($current_tx_pkts-$previous_tx_pkts))
+		bytes_new=$((current_tx_bytes-previous_tx_bytes))
+		pkts_new=$((current_tx_pkts-previous_tx_pkts))
 		avg_pkt_size=$(echo "scale=2;$bytes_new/$pkts_new/1024" | bc)
 		throughput=$(echo "scale=2;$bytes_new/$test_run_duration*8/1024/1024/1024" | bc)
 		previous_tx_bytes=$current_tx_bytes
@@ -127,10 +126,10 @@
 
 		echo "throughput (gbps): $throughput"
 		echo "average packet size: $avg_pkt_size"
-		printf "%4s  %8.2f  %8.2f\n" ${current_test_threads} $throughput $avg_pkt_size >> $eth_log
+		printf "%4s  %8.2f  %8.2f\\n" "${current_test_threads}" "$throughput" "$avg_pkt_size" >> "$eth_log"
 
 		echo "current test finished. wait for next one... "
-		i=$(($i + 1))
+		i=$((i + 1))
 		sleep 5
 	done
 
