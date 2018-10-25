616d8de00007c521310f6138b9245dd12360c526
Chris Valean <cvalean@cloudbasesolutions.com>
2018-10-25 15:09:22 +0300

    Revert "Hyper-V: Added 24 Storage tests"

--- Libraries/Test-Framework.psm1
+++ Libraries/Test-Framework.psm1
@@ -540,7 +540,18 @@ function Run-Test {
              -Timeout $timeout
         $resultArr += $testResult
     }
-    
+
+    $currentTestResult.TestResult = GetFinalResultHeader -resultarr $resultArr
+    if ($DeployVMPerEachTest -or $ExecuteTeardown) {
+        LogMsg "VM CLEANUP ~~~~~~~~~~~~~~~~~~~~~~~"
+        $optionalParams = @{}
+        if ($testParameters["SkipVerifyKernelLogs"] -eq "True" -or (-not $DeployVMPerEachTest)) {
+            $optionalParams["SkipVerifyKernelLogs"] = $True
+        }
+        DoTestCleanUp -CurrentTestResult $CurrentTestResult -TestName $currentTestData.testName `
+             -ResourceGroups $isDeployed @optionalParams
+    }
+
     if ($testPlatform -eq "Hyperv" -and $CurrentTestData.CleanupScript) {
         foreach ($VM in $AllVMData) {
             if (Get-VM -Name $VM.RoleName -ComputerName `
@@ -558,17 +569,6 @@ function Run-Test {
                     $VM.HyperVHost
             }
         }
-    }
-
-    $currentTestResult.TestResult = GetFinalResultHeader -resultarr $resultArr
-    if ($DeployVMPerEachTest -or $ExecuteTeardown) {
-        LogMsg "VM CLEANUP ~~~~~~~~~~~~~~~~~~~~~~~"
-        $optionalParams = @{}
-        if ($testParameters["SkipVerifyKernelLogs"] -eq "True" -or (-not $DeployVMPerEachTest)) {
-            $optionalParams["SkipVerifyKernelLogs"] = $True
-        }
-        DoTestCleanUp -CurrentTestResult $CurrentTestResult -TestName $currentTestData.testName `
-             -ResourceGroups $isDeployed @optionalParams
     }
   
     return $currentTestResult
