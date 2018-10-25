8efe0af85c0a5a91273c920bf76f6b1043855992
sireeshapotnuru <potnuru.sireesha@wipro.com>
2018-10-25 19:56:32 +0530

    Added new Secureboot testcase

---
+++ Testscripts/Windows/CLEANUP-Backup-DISK.ps1
@@ -0,0 +1,56 @@
+﻿# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache License.
+<#
+.Synopsis
+    This script creates a disk to be used for backup and restore tests
+.Description
+    This script will create a new VHD double the size of the VHD in the
+    given vm. The VHD will be mounted to a new partiton, initialized and
+    formatted with NTFS
+#>
+$ErrorActionPreference = "Stop"
+function Main {
+    try {
+        $testResult = $null
+        $captureVMData = $allVMData
+        $VMName = $captureVMData.RoleName
+        $HvServer= $captureVMData.HyperVhost
+        # Change the working directory to where we need to be
+        Set-Location $WorkingDirectory
+        $backupdiskpath = (Get-VMHost).VirtualHardDiskPath + "\" + $VMName + "_VSS_DISK.vhdx"
+        $tempFile = (Get-VMHost).VirtualHardDiskPath + "\" + $VMName + "_DRIVE_LETTER.txt"
+        # This is used to set the $global:driveletter variable
+        Get-DriveLetter $VMName $HvServer
+        if ($global:driveletter) {
+            Dismount-VHD -Path $backupDiskPath -ErrorAction SilentlyContinue
+            if (-not $?) {
+                LogErr "Dismounting VHD has failed"
+            }
+            Remove-Item $backupdiskpath -Force -ErrorAction SilentlyContinue
+            if (-not $?) {
+                LogErr "Could not remove backup disk"
+            }
+            Remove-Item $tempFile -Force -ErrorAction SilentlyContinue
+            if (-not $?) {
+                LogErr "Could not remove temporary file"
+            }
+            LogMsg "Cleanup completed!"
+            $testResult=$resultPass
+        }
+        else {
+            LogErr "Drive letter isn't set"
+        }
+    } catch {
+        $ErrorMessage =  $_.Exception.Message
+        $ErrorLine = $_.InvocationInfo.ScriptLineNumber
+        LogErr "$ErrorMessage at line: $ErrorLine"
+    } finally {
+        if (!$testResult) {
+            $testResult = $resultAborted
+        }
+        $resultArr += $testResult
+    }
+    $currentTestResult.TestResult = GetFinalResultHeader -resultarr $resultArr
+    return $currentTestResult.TestResult
+}
+Main
---
+++ Testscripts/Windows/SETUP-Backup-DISK.ps1
@@ -0,0 +1,72 @@
+﻿# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache License.
+<#
+.Synopsis
+    This script creates a disk to be used for backup and restore tests
+.Description
+    This scrip will create a new VHD double the size of the VHD in the
+    given vm. The VHD will be mounted to a new partiton, initialized and
+    formatted with NTFS
+#>
+$ErrorActionPreference = "Stop"
+function Main {
+    try {
+        $testResult = $null
+        $captureVMData = $allVMData
+        $VMName = $captureVMData.RoleName
+        # Change the working directory to where we need to be
+        Set-Location $WorkingDirectory
+        $utilsScript = ".\Utilities\STOR-VSS-Utils.ps1"
+        $backupdisksize = 2*$(Get-VMHardDiskDrive -VMName $VMName |get-vhd)[0].size
+        $backupdiskpath = (Get-VMHost).VirtualHardDiskPath + "\" + $VMName + "_VSS_DISK.vhdx"
+        $driveletter = Get-ChildItem function:[g-y]: -n | Where-Object { !(test-path $_) } | Get-random
+        $originaldriveletter = $driveletter
+        [char]$driveletter = $driveletter.Replace(":","")
+        if ([string]::IsNullOrEmpty($driveletter)) {
+            throw "Setup: The driveletter variable is empty!"
+        }
+        $maxRetryCount=2
+        $currentRetryCount = 0
+        while ($currentRetryCount -lt $maxRetryCount){
+            if (Test-Path ($backupdiskpath)) {
+                LogMsg "Disk already exists. Deleting old disk and creating new disk."
+                Dismount-VHD $backupdiskpath
+                Remove-Item $backupdiskpath
+            }
+            New-VHD -Path $backupdiskpath -Size $backupdisksize
+            Mount-VHD -Path $backupdiskpath
+            if($?) {
+                break
+            }
+            $currentRetryCount++
+        }
+        if ($currentRetryCount -eq $maxRetryCount) {
+            throw "Mounting VHD Failed"
+        }
+        $backupdisk = Get-VHD -Path $backupdiskpath
+        Initialize-Disk $backupdisk.DiskNumber
+        $diskpartition = New-Partition -DriveLetter $driveletter -DiskNumber $backupdisk.DiskNumber -UseMaximumSize
+        $volume = Format-Volume -FileSystem NTFS -Confirm:$False -Force -Partition $diskpartition
+        LogMsg "Disk initialized with volume $volume $diskpartition"
+        New-PSDrive -Name $driveletter -PSProvider FileSystem -Root $originaldriveletter -Description "VSS"
+        $filePath = (Get-VMHost).VirtualHardDiskPath + "\" + "$VMName" + "_DRIVE_LETTER.txt"
+        if(Test-Path ($filePath)) {
+            LogMsg "Removing existing file."
+            Remove-Item $filePath
+        }
+        Write-Output "$originaldriveletter" >> $filePath
+        $testResult=$resultPass
+    } catch {
+        $ErrorMessage =  $_.Exception.Message
+        $ErrorLine = $_.InvocationInfo.ScriptLineNumber
+        LogErr "$ErrorMessage at line: $ErrorLine"
+    } finally {
+        if (!$testResult) {
+            $testResult = $resultAborted
+        }
+        $resultArr += $testResult
+    }
+    $currentTestResult.TestResult = GetFinalResultHeader -resultarr $resultArr
+    return $currentTestResult.TestResult
+}
+Main
