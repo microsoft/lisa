833c92fa75d9e67d8d8ba7dcae8ce99779aec21a
Simon Xiao <sixiao@microsoft.com>
2018-11-21 10:12:44 -0800

    Update WorkingSpaceManagement.psm1
    Remove the trailing whitespace

--- Libraries/WorkingSpaceManagement.psm1
+++ Libraries/WorkingSpaceManagement.psm1
@@ -9,7 +9,7 @@
 	PS modules for LISAv2 test automation.
 	When LISAv2 is running, some files may be created with multiple layer of folders;
 	in such cases, the total length of the file path may be too long for Windows to handle.
-	So, LISAv2 will try to find a folder with short folder path as its working directory. 
+	So, LISAv2 will try to find a folder with short folder path as its working directory.
 
 .PARAMETER
 	<Parameters>
