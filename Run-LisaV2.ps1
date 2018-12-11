microsoft/lisa

commit 087bfca718cc72d5d9a27790d7f71c6e35b5255d

LISAv2 code cleanup (#518)
* Adjust the script flow of Run-LisaV2.ps1

* Remove parameter of "CoreCountExceededTimeout"

This parameter is used only once in Create-AllResourceGroupDeployments().
Change this to a local const.

* Rename 'Create-ResultSummary' to 'New-ResultSummary'

Powershell compliants about the naming.
VERBOSE: The 'Create-ResultSummary' command in the LogProcessing' module was imported, but because its name does not include an approved verb, it might be difficult to
find. The suggested alternative verbs are "New".

Fix it in this patch.

* Fix trailing whitespace

Run-LisaV2.ps1
@@ -68,7 +68,6 @@ Param(
 	[string] $CustomLIS,
 
 	#[Optional] Parameters for changing framework behavior.
-	[string] $CoreCountExceededTimeout,
 	[int]    $TestIterations,
 	[string] $TiPSessionId,
 	[string] $TiPCluster,
@@ -146,6 +145,14 @@ try {
 	# Validate the test parameters.
 	Validate-Parameters
 
+	# Validate all the XML files and then import test cases from them for test
+	Validate-XmlFiles -ParentFolder $WorkingDirectory
+	$TestConfigurationXmlFile = "$WorkingDirectory\TestConfiguration.xml"
+	Import-TestCases $WorkingDirectory $TestConfigurationXmlFile
+	# Inject default / custom replaceable test parameters to TestConfiguration.xml
+	$ReplaceableTestParameters = [xml](Get-Content -Path "$WorkingDirectory\XML\Other\ReplaceableTestParameters.xml")
+	Inject-CustomTestParameters $CustomParameters $ReplaceableTestParameters $TestConfigurationXmlFile
+
 	# Handle the Secrets file
 	if ($env:Azure_Secrets_File) {
 		$XMLSecretFile = $env:Azure_Secrets_File
@@ -173,18 +180,6 @@ try {
 		Write-LogErr "Failed to update configuration files. '-XMLSecretFile [FilePath]' is not provided."
 	}
 
-	Validate-XmlFiles -ParentFolder $WorkingDirectory
-
-	$TestConfigurationXmlFile = "$WorkingDirectory\TestConfiguration.xml"
-	Import-TestCases $WorkingDirectory $TestConfigurationXmlFile
-
-	#This function will inject default / custom replaceable test parameters to TestConfiguration.xml
-	$ReplaceableTestParameters = [xml](Get-Content -Path "$WorkingDirectory\XML\Other\ReplaceableTestParameters.xml")
-	Inject-CustomTestParameters $CustomParameters $ReplaceableTestParameters $TestConfigurationXmlFile
-
-	$xmlConfig = [xml](Get-Content $TestConfigurationXmlFile)
-	$xmlConfig.Save("$TestConfigurationXmlFile")
-
 	# Create report folder
 	$reportFolder = "$pwd/Report"
 	if(!(Test-Path $reportFolder)) {
@@ -193,19 +188,24 @@ try {
 	$TestReportXml = Join-Path "$reportFolder" "LISAv2_TestReport_$TestID.xml"
 
 	$command = ".\AutomationManager.ps1 -xmlConfigFile '$TestConfigurationXmlFile' -cycleName 'TC-$TestID' -RGIdentifier '$RGIdentifier' -TestReportXmlPath $TestReportXml"
-	if ( $CustomKernel) { $command += " -CustomKernel '$CustomKernel'" }
+	# Override VM #
 	if ( $OverrideVMSize ) { $command += " -OverrideVMSize $OverrideVMSize" }
 	if ( $EnableAcceleratedNetworking ) { $command += " -EnableAcceleratedNetworking" }
+	if ( $UseManagedDisks ) {	$command += " -UseManagedDisks" }
+	# Override Kernel or LIS #
+	if ( $CustomKernel) { $command += " -CustomKernel '$CustomKernel'" }
+	if ( $CustomLIS) { $command += " -CustomLIS $CustomLIS" }
+	# Override Test Behaviors #
+	if ( $TestIterations -gt 1 ) { $command += " -TestIterations $TestIterations" }
 	if ( $ForceDeleteResources ) { $command += " -ForceDeleteResources" }
 	if ( $DoNotDeleteVMs ) { $command += " -DoNotDeleteVMs" }
 	if ( $DeployVMPerEachTest ) { $command += " -DeployVMPerEachTest" }
-	if ( $CustomLIS) { $command += " -CustomLIS $CustomLIS" }
-	if ( $CoreCountExceededTimeout ) { $command += " -CoreCountExceededTimeout $CoreCountExceededTimeout" }
-	if ( $TestIterations -gt 1 ) { $command += " -TestIterations $TestIterations" }
+	# Override for TiP #
 	if ( $TiPSessionId) { $command += " -TiPSessionId $TiPSessionId" }
 	if ( $TiPCluster) { $command += " -TiPCluster $TiPCluster" }
-	if ($UseManagedDisks) {	$command += " -UseManagedDisks" }
-	if ($XMLSecretFile) { $command += " -XMLSecretFile '$XMLSecretFile'" }
+	# Attaching Secret file if provided #
+	if ( $XMLSecretFile ) { $command += " -XMLSecretFile '$XMLSecretFile'" }
+
 	Write-LogInfo $command
 
 	Invoke-Expression -Command $command

AutomationManager.ps1
@@ -44,7 +44,6 @@ param (
 # Experimental Feature
 [switch] $UseManagedDisks,
 
-[int] $CoreCountExceededTimeout = 3600,
 [int] $TestIterations = 1,
 [string] $TiPSessionId="",
 [string] $TiPCluster="",
@@ -315,8 +314,6 @@ Set-Variable -Name CurrentTestData -Value $CurrentTestData -Scope Global
 Set-Variable -Name preserveKeyword -Value "preserving" -Scope Global
 Set-Variable -Name TiPSessionId -Value $TiPSessionId -Scope Global
 Set-Variable -Name TiPCluster -Value $TiPCluster -Scope Global
-
-Set-Variable -Name CoreCountExceededTimeout -Value $CoreCountExceededTimeout -Scope Global
 
 Set-Variable -Name resultPass -Value "PASS" -Scope Global
 Set-Variable -Name resultFail -Value "FAIL" -Scope Global

Libraries/Azure.psm1
@@ -364,17 +364,18 @@ Function Create-AllResourceGroupDeployments($setupType, $xmlConfig, $Distro, [st
             $validateStartTime = Get-Date
             Write-LogInfo "Checking the subscription usage..."
             $readyToDeploy = $false
+            $coreCountExceededTimeout = 3600
             while (!$readyToDeploy) {
                 $readyToDeploy = Validate-SubscriptionUsage -subscriptionID $xmlConfig.config.$TestPlatform.General.SubscriptionID -RGXMLData $RG
                 $validateCurrentTime = Get-Date
                 $elapsedWaitTime = ($validateCurrentTime - $validateStartTime).TotalSeconds
-                if ( (!$readyToDeploy) -and ($elapsedWaitTime -lt $CoreCountExceededTimeout)) {
+                if ( (!$readyToDeploy) -and ($elapsedWaitTime -lt $coreCountExceededTimeout)) {
                     $waitPeriod = Get-Random -Minimum 1 -Maximum 10 -SetSeed (Get-Random)
-                    Write-LogInfo "Timeout in approx. $($CoreCountExceededTimeout - $elapsedWaitTime) seconds..."
+                    Write-LogInfo "Timeout in approx. $($coreCountExceededTimeout - $elapsedWaitTime) seconds..."
                     Write-LogInfo "Waiting $waitPeriod minutes..."
                     sleep -Seconds ($waitPeriod * 60)
                 }
-                if ( $elapsedWaitTime -gt $CoreCountExceededTimeout ) {
+                if ( $elapsedWaitTime -gt $coreCountExceededTimeout ) {
                     break
                 }
             }

Libraries/LogProcessing.psm1
@@ -86,7 +86,7 @@ Function Write-LogWarn($text)
 	Write-Log "WARN" $text
 }
 
-Function Create-ResultSummary($testResult, $checkValues, $testName, $metaData)
+Function New-ResultSummary($testResult, $checkValues, $testName, $metaData)
 {
 	if ( $metaData )
 	{

Testscripts/Windows/BVT-VERIFY-DEPLOYMENT-PROVISION.ps1
@@ -3,38 +3,38 @@
 
 function Main {
 	try {
-		$CurrentTestResult.TestSummary += Create-ResultSummary -testResult "PASS" -metaData "FirstBoot" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
+		$CurrentTestResult.TestSummary += New-ResultSummary -testResult "PASS" -metaData "FirstBoot" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
 		Write-LogInfo "Check 1: Checking call tracess again after 30 seconds sleep"
 		Start-Sleep 30
 		$noIssues = Check-KernelLogs -allVMData $allVMData
 		if ($noIssues) {
-			$CurrentTestResult.TestSummary += Create-ResultSummary -testResult "PASS" -metaData "FirstBoot : Call Trace Verification" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
+			$CurrentTestResult.TestSummary += New-ResultSummary -testResult "PASS" -metaData "FirstBoot : Call Trace Verification" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
 			$RestartStatus = Restart-AllDeployments -allVMData $allVMData
 			if($RestartStatus -eq "True") {
-				$CurrentTestResult.TestSummary += Create-ResultSummary -testResult "PASS" -metaData "Reboot" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
+				$CurrentTestResult.TestSummary += New-ResultSummary -testResult "PASS" -metaData "Reboot" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
 				Write-LogInfo "Check 2: Checking call tracess again after Reboot > 30 seconds sleep"
 				Start-Sleep 30
 				$noIssues = Check-KernelLogs -allVMData $allVMData
 				if ($noIssues) {
-					$CurrentTestResult.TestSummary += Create-ResultSummary -testResult "PASS" -metaData "Reboot : Call Trace Verification" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
+					$CurrentTestResult.TestSummary += New-ResultSummary -testResult "PASS" -metaData "Reboot : Call Trace Verification" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
 					Write-LogInfo "Test Result : PASS."
 					$testResult = "PASS"
 				}
 				else {
-					$CurrentTestResult.TestSummary += Create-ResultSummary -testResult "FAIL" -metaData "Reboot : Call Trace Verification" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
+					$CurrentTestResult.TestSummary += New-ResultSummary -testResult "FAIL" -metaData "Reboot : Call Trace Verification" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
 					Write-LogInfo "Test Result : FAIL."
 					$testResult = "FAIL"
 				}
 			}
 			else {
-				$CurrentTestResult.TestSummary += Create-ResultSummary -testResult "FAIL" -metaData "Reboot" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
+				$CurrentTestResult.TestSummary += New-ResultSummary -testResult "FAIL" -metaData "Reboot" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
 				Write-LogInfo "Test Result : FAIL."
 				$testResult = "FAIL"
 			}
 
 		}
 		else {
-			$CurrentTestResult.TestSummary += Create-ResultSummary -testResult "FAIL" -metaData "FirstBoot : Call Trace Verification" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
+			$CurrentTestResult.TestSummary += New-ResultSummary -testResult "FAIL" -metaData "FirstBoot : Call Trace Verification" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
 			Write-LogInfo "Test Result : FAIL."
 			$testResult = "FAIL"
 		}

Testscripts/Windows/DPDK-TESTCASE-DRIVER.ps1
@@ -354,7 +354,7 @@ collect_VM_properties
 			$testResult = "Aborted"
 		}
 		$resultArr += $testResult
-		$currentTestResult.TestSummary +=  Create-ResultSummary -testResult $testResult -metaData "DPDK-TEST" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
+		$currentTestResult.TestSummary +=  New-ResultSummary -testResult $testResult -metaData "DPDK-TEST" -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName
 	}
 
 	$currentTestResult.TestResult = Get-FinalResultHeader -resultarr $resultArr

Testscripts/Windows/NESTED-HYPERV-NTTTCP-DIFFERENT-L1-NAT.ps1
@@ -629,13 +629,13 @@ function Main () {
 					$averageTcpLatency = $splits[3]
 					$metadata = "Connections=$testConnections"
 					$connResult = "throughput=$throughputGbps`Gbps cyclePerBytet=$cyclePerByte Avg_TCP_lat=$averageTcpLatency"
-					$currentTestResult.TestSummary +=  Create-ResultSummary -testResult $connResult -metaData $metaData -checkValues $checkValues -testName $currentTestData.testName
+					$currentTestResult.TestSummary +=  New-ResultSummary -testResult $connResult -metaData $metaData -checkValues $checkValues -testName $currentTestData.testName
 					if ([string]$throughputGbps -eq "0.00") {
 						$testResult = $resultFail
 						$uploadResults = $false
 					}
 				} catch {
-					$currentTestResult.TestSummary +=  Create-ResultSummary -testResult "Error in parsing logs." -metaData "NTTTCP" -checkValues $checkValues -testName $currentTestData.testName
+					$currentTestResult.TestSummary +=  New-ResultSummary -testResult "Error in parsing logs." -metaData "NTTTCP" -checkValues $checkValues -testName $currentTestData.testName
 				}
 			}
 

Testscripts/Windows/NESTED-HYPERV-NTTTCP-DIFFERENT-L1-PUBLIC-BRIDGE.ps1
@@ -357,7 +357,7 @@ function Main () {
 					$averageTcpLatency = $splits[3]
 					$metadata = "Connections=$testConnections"
 					$connResult = "throughput=$throughputGbps`Gbps cyclePerBytet=$cyclePerByte Avg_TCP_lat=$averageTcpLatency"
-					$currentTestResult.TestSummary +=  Create-ResultSummary -testResult $connResult -metaData $metaData -checkValues $checkValues -testName $currentTestData.testName
+					$currentTestResult.TestSummary +=  New-ResultSummary -testResult $connResult -metaData $metaData -checkValues $checkValues -testName $currentTestData.testName
 					if ([string]$throughputGbps -eq "0.00")
 					{
 						$testResult = $resultFail
@@ -366,7 +366,7 @@ function Main () {
 				}
 				catch
 				{
-					$currentTestResult.TestSummary +=  Create-ResultSummary -testResult "Error in parsing logs." -metaData "NTTTCP" -checkValues $checkValues -testName $currentTestData.testName
+					$currentTestResult.TestSummary +=  New-ResultSummary -testResult "Error in parsing logs." -metaData "NTTTCP" -checkValues $checkValues -testName $currentTestData.testName
 				}
 			}
 

commit 56027c8d5d934df076438e706ffc4e26755406da

lisav2: fix azure secrets load
This patch fixes the azure secrets load failure.
Without this patch, lisav2 fails with error:
[azure] Select-AzureRmSubscription : Please provide a valid tenant or a
valid
[azure] subscription.
[azure] At C:\LISAv2\FGNS14927\AutomationManager.ps1:417 char:27
[azure] + ... scription = Select-AzureRmSubscription -SubscriptionId
$AzureSetup.Su ...
[azure] +
~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
[azure]     + CategoryInfo          : CloseError: (:)
[Set-AzureRmContext], ArgumentEx
[azure]    ception
[azure]     + FullyQualifiedErrorId :
Microsoft.Azure.Commands.Profile.SetAzureRMConte
[azure]    xtCommand

The problem was introduced by commit:
https://github.com/LIS/LISAv2/commit/087bfca718cc72d5d9a27790d7f71c6e35b5255d

Run-LisaV2.ps1
@@ -142,16 +142,9 @@ try {
 	foreach ($var in $GlobalVariables) {
 		[void](Set-Variable -Name $var.Name -Value $var.Value -Scope Local -ErrorAction SilentlyContinue)
 	}
+
 	# Validate the test parameters.
 	Validate-Parameters
-
-	# Validate all the XML files and then import test cases from them for test
-	Validate-XmlFiles -ParentFolder $WorkingDirectory
-	$TestConfigurationXmlFile = "$WorkingDirectory\TestConfiguration.xml"
-	Import-TestCases $WorkingDirectory $TestConfigurationXmlFile
-	# Inject default / custom replaceable test parameters to TestConfiguration.xml
-	$ReplaceableTestParameters = [xml](Get-Content -Path "$WorkingDirectory\XML\Other\ReplaceableTestParameters.xml")
-	Inject-CustomTestParameters $CustomParameters $ReplaceableTestParameters $TestConfigurationXmlFile
 
 	# Handle the Secrets file
 	if ($env:Azure_Secrets_File) {
@@ -179,6 +172,14 @@ try {
 	} else {
 		Write-LogErr "Failed to update configuration files. '-XMLSecretFile [FilePath]' is not provided."
 	}
+
+	# Validate all the XML files and then import test cases from them for test
+	Validate-XmlFiles -ParentFolder $WorkingDirectory
+	$TestConfigurationXmlFile = "$WorkingDirectory\TestConfiguration.xml"
+	Import-TestCases $WorkingDirectory $TestConfigurationXmlFile
+	# Inject default / custom replaceable test parameters to TestConfiguration.xml
+	$ReplaceableTestParameters = [xml](Get-Content -Path "$WorkingDirectory\XML\Other\ReplaceableTestParameters.xml")
+	Inject-CustomTestParameters $CustomParameters $ReplaceableTestParameters $TestConfigurationXmlFile
 
 	# Create report folder
 	$reportFolder = "$pwd/Report"