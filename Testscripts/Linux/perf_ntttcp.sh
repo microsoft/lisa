microsoft/lisa

commit 38b12807664ab04d2024ae535e99d455e8465faf

Partial update bash scripts for Shell Check rules (#546)

Testscripts/Linux/perf_ntttcp.sh
@@ -20,15 +20,12 @@
 
 CONSTANTS_FILE="./constants.sh"
 UTIL_FILE="./utils.sh"
-ICA_TESTRUNNING="TestRunning"           # The test is running
-ICA_TESTCOMPLETED="TestCompleted"       # The test completed successfully
 ICA_TESTABORTED="TestAborted"           # Error during the setup of the test
-ICA_TESTFAILED="TestFailed"                     # Error occurred during the test
 touch ./ntttcpTest.log
 
 LogMsg()
 {
-	echo `date "+%b %d %Y %T"` : "${1}"	# Add the time stamp to the log message
+	echo $(date "+%b %d %Y %T") : "${1}"	# Add the time stamp to the log message
 	echo "${1}" >> ./ntttcpTest.log
 }
 
@@ -50,14 +47,14 @@ UpdateTestState()
 	exit 10
 }
 
-if [ ! ${server} ]; then
+if [ ! "${server}" ]; then
 	errMsg="Please add/provide value for server in constants.sh. server=<server ip>"
 	LogMsg "${errMsg}"
 	echo "${errMsg}" >> ./summary.log
 	UpdateTestState $ICA_TESTABORTED
 	exit 1
 fi
-if [ ! ${client} ]; then
+if [ ! "${client}" ]; then
 	errMsg="Please add/provide value for client in constants.sh. client=<client ip>"
 	LogMsg "${errMsg}"
 	echo "${errMsg}" >> ./summary.log
@@ -65,7 +62,7 @@ if [ ! ${client} ]; then
 	exit 1
 fi
 
-if [ ! ${testDuration} ]; then
+if [ ! "${testDuration}" ]; then
 	errMsg="Please add/provide value for testDuration in constants.sh. testDuration=60"
 	LogMsg "${errMsg}"
 	echo "${errMsg}" >> ./summary.log
@@ -73,7 +70,7 @@ if [ ! ${testDuration} ]; then
 	exit 1
 fi
 
-if [ ! ${nicName} ]; then
+if [ ! "${nicName}" ]; then
 	errMsg="Please add/provide value for nicName in constants.sh. nicName=eth0/bond0"
 	LogMsg "${errMsg}"
 	echo "${errMsg}" >> ./summary.log
@@ -83,7 +80,7 @@ fi
 #Make & build ntttcp on client and server Machine
 
 LogMsg "Configuring client ${client}..."
-ssh ${client} ". $UTIL_FILE && install_ntttcp"
+ssh "${client}" ". $UTIL_FILE && install_ntttcp"
 if [ $? -ne 0 ]; then
 	LogMsg "Error: ntttcp installation failed in ${client}.."
 	UpdateTestState "TestAborted"
@@ -91,7 +88,7 @@ if [ $? -ne 0 ]; then
 fi
 
 LogMsg "Configuring server ${server}..."
-ssh ${server} ". $UTIL_FILE && install_ntttcp"
+ssh "${server}" ". $UTIL_FILE && install_ntttcp"
 if [ $? -ne 0 ]; then
 	LogMsg "Error: ntttcp installation failed in ${server}.."
 	UpdateTestState "TestAborted"
@@ -104,8 +101,8 @@ if [[ $(detect_linux_distribution) == coreos ]]; then
 	mpstat_cmd="docker run --network host lisms/toolbox mpstat"
 	dstat_cmd="docker run --network host lisms/toolbox dstat"
 	sar_cmd="docker run --network host lisms/toolbox sar"
-	ssh root@${server} ". $UTIL_FILE && Delete_Containers"
-	ssh root@${client} ". $UTIL_FILE && Delete_Containers"
+	ssh root@"${server}" ". $UTIL_FILE && Delete_Containers"
+	ssh root@"${client}" ". $UTIL_FILE && Delete_Containers"
 else
 	ntttcp_cmd="ntttcp"
 	lagscope_cmd="lagscope"
@@ -114,63 +111,63 @@ else
 	sar_cmd="sar"
 fi
 
-bc_cmd=`echo $(Get_BC_Command)`
+bc_cmd=$(echo $(Get_BC_Command))
 log_folder="ntttcp-${testType}-test-logs"
 max_server_threads=64
 
 Get_Throughput()
 {
 	throughput=0
-	throughput=$(cat ${1} | grep throughput | tail -1 | tr ":" " " | awk '{ print $NF }')
+	throughput=$(cat "${1}" | grep throughput | tail -1 | tr ":" " " | awk '{ print $NF }')
 	if [[ $throughput =~ "Gbps" ]];
 	then
-		throughput=$(echo $throughput | sed 's/Gbps//')
+		throughput=$(echo "$throughput" | sed 's/Gbps//')
 	elif [[ $throughput =~ "Mbps" ]];
 	then
-		throughput=$(echo "scale=5; $(echo $throughput | sed 's/Mbps//')/1024" | ${bc_cmd})
+		throughput=$(echo "scale=5; $(echo "$throughput" | sed 's/Mbps//')/1024" | ${bc_cmd})
 	elif [[ $throughput =~ "Kbps" ]];
 	then
-		throughput=$(echo "scale=5; $(echo $throughput | sed 's/Kbps//')/1024/1024" | ${bc_cmd})
+		throughput=$(echo "scale=5; $(echo "$throughput" | sed 's/Kbps//')/1024/1024" | ${bc_cmd})
 	elif [[ $throughput =~ "bps" ]];
 	then
-		throughput=$(echo "scale=5; $(echo $throughput | sed 's/Kbps//')/1024/1024/1024" | ${bc_cmd})
+		throughput=$(echo "scale=5; $(echo "$throughput" | sed 's/Kbps//')/1024/1024/1024" | ${bc_cmd})
 	else
 		throughput=0
 	fi
-	throughput=`printf %.2f $throughput`
-	echo $throughput
+	throughput=$(printf %.2f $throughput)
+	echo "$throughput"
 }
 
 Get_Average_Latency()
 {
 	avglatency=0
-	avglatency=$(cat ${1} | grep Average | sed 's/.* //')
+	avglatency=$(cat "${1}" | grep Average | sed 's/.* //')
 	if [[ $avglatency =~ "us" ]];
 	then
-		avglatency=$(echo $avglatency | sed 's/us//')
+		avglatency=$(echo "$avglatency" | sed 's/us//')
 	elif [[ $avglatency =~ "ms" ]];
 	then
-		avglatency=$(echo "scale=5; $(echo $avglatency | sed 's/ms//')/1000" | ${bc_cmd})
+		avglatency=$(echo "scale=5; $(echo "$avglatency" | sed 's/ms//')/1000" | ${bc_cmd})
 	elif [[ $avglatency =~ "sec" ]];
 	then
-		avglatency=$(echo "scale=5; $(echo $avglatency | sed 's/sec//')/1000/1000" | ${bc_cmd})
+		avglatency=$(echo "scale=5; $(echo "$avglatency" | sed 's/sec//')/1000/1000" | ${bc_cmd})
 	else
 		avglatency=0
 	fi
-	avglatency=`printf %.3f $avglatency`
-	echo $avglatency
+	avglatency=$(printf %.3f $avglatency)
+	echo "$avglatency"
 }
 
 Get_Cyclesperbytes()
 {
 	cyclesperbytes=0
-	cyclesperbytes=$(cat ${1} | grep cycles/byte | tr ":" " " | awk '{ print $NF }')
+	cyclesperbytes=$(cat "${1}" | grep cycles/byte | tr ":" " " | awk '{ print $NF }')
 	if [[ ! $cyclesperbytes ]];
 	then
         cyclesperbytes=0
 	fi
-	cyclesperbytes=`printf %.2f $cyclesperbytes`
-	echo $cyclesperbytes
+	cyclesperbytes=$(printf %.2f $cyclesperbytes)
+	echo "$cyclesperbytes"
 }
 
 Run_Ntttcp()
@@ -178,19 +175,19 @@ Run_Ntttcp()
 	i=0
 	data_loss=0
 	
-	ssh ${server} "mkdir -p $log_folder"
-	ssh ${client} "mkdir -p $log_folder"
+	ssh "${server}" "mkdir -p $log_folder"
+	ssh "${client}" "mkdir -p $log_folder"
 	result_file="${log_folder}/report.csv"
 	if [[ $testType == "udp" ]];
 	then
 		bufferLength=$(($bufferLength/1024))
-		echo "test_connections,tx_throughput_in_Gbps,rx_throughput_in_Gbps,datagram_loss_in_%" > $result_file
+		echo "test_connections,tx_throughput_in_Gbps,rx_throughput_in_Gbps,datagram_loss_in_%" > "$result_file"
 		core_mem_set_cmd="sysctl -w net.core.rmem_max=67108864; sysctl -w net.core.rmem_default=67108864; sysctl -w net.core.wmem_default=67108864; sysctl -w net.core.wmem_max=67108864"
-		ssh ${server} "${core_mem_set_cmd}"
-		ssh ${client} "${core_mem_set_cmd}"
+		ssh "${server}" "${core_mem_set_cmd}"
+		ssh "${client}" "${core_mem_set_cmd}"
 	else
 		testType="tcp"
-		echo "test_connections,throughput_in_Gbps,cycles/byte,avglatency_in_us" > $result_file
+		echo "test_connections,throughput_in_Gbps,cycles/byte,avglatency_in_us" > "$result_file"
 	fi
 
 	for current_test_threads in "${testConnections[@]}"; do
@@ -216,7 +213,7 @@ Run_Ntttcp()
 			run_msg="Running ${testType} Test: $current_test_threads connections : $num_threads_P X $num_threads_n"
 			server_ntttcp_cmd="ulimit -n 204800 && ${ntttcp_cmd} -P ${num_threads_P} -t ${testDuration} -e -W 1 -C 1"
 			client_ntttcp_cmd="ulimit -n 204800 && ${ntttcp_cmd} -s${server} -P ${num_threads_P} -n ${num_threads_n} -t ${testDuration} -W 1 -C 1"
-			ssh ${server} "for i in {1..$testDuration}; do ss -ta | grep ESTA | grep -v ssh | wc -l >> ./$log_folder/tcp-connections-p${num_threads_P}X${num_threads_n}.log; sleep 1; done" &
+			ssh "${server}" "for i in {1..$testDuration}; do ss -ta | grep ESTA | grep -v ssh | wc -l >> ./$log_folder/tcp-connections-p${num_threads_P}X${num_threads_n}.log; sleep 1; done" &
 
 		fi
 
@@ -226,23 +223,23 @@ Run_Ntttcp()
 		tx_ntttcp_log_file="$log_folder/ntttcp-${tx_log_prefix}"
 		tx_lagscope_log_file="$log_folder/lagscope-${tx_log_prefix}"
 		rx_ntttcp_log_file="$log_folder/ntttcp-${rx_log_prefix}"
-		Kill_Process ${server} ntttcp
+		Kill_Process "${server}" ntttcp
 		LogMsg "ServerCmd: $server_ntttcp_cmd > ./$log_folder/ntttcp-${rx_log_prefix}"
-		ssh ${server} "${server_ntttcp_cmd}" > "./$log_folder/ntttcp-${rx_log_prefix}" &
-		Kill_Process ${server} lagscope
-		ssh ${server} "${lagscope_cmd} -r" &
-		Kill_Process ${server} dstat
-		ssh ${server} "${dstat_cmd} -dam" > "./$log_folder/dstat-${rx_log_prefix}" &
-		Kill_Process ${server} mpstat
-		ssh ${server} "${mpstat_cmd} -P ALL 1 ${testDuration}" > "./$log_folder/mpstat-${rx_log_prefix}" &
+		ssh "${server}" "${server_ntttcp_cmd}" > "./$log_folder/ntttcp-${rx_log_prefix}" &
+		Kill_Process "${server}" lagscope
+		ssh "${server}" "${lagscope_cmd} -r" &
+		Kill_Process "${server}" dstat
+		ssh "${server}" "${dstat_cmd} -dam" > "./$log_folder/dstat-${rx_log_prefix}" &
+		Kill_Process "${server}" mpstat
+		ssh "${server}" "${mpstat_cmd} -P ALL 1 ${testDuration}" > "./$log_folder/mpstat-${rx_log_prefix}" &
 
 		sleep 2
-		${sar_cmd} -n DEV 1 ${testDuration} > "./$log_folder/sar-${tx_log_prefix}" &
+		${sar_cmd} -n DEV 1 "${testDuration}" > "./$log_folder/sar-${tx_log_prefix}" &
 		${dstat_cmd} -dam > "./$log_folder/dstat-${tx_log_prefix}" &
-		${mpstat_cmd} -P ALL 1 ${testDuration} > "./$log_folder/mpstat-${tx_log_prefix}" &
-		${lagscope_cmd} -s${server} -t ${testDuration} -V > "./$log_folder/lagscope-${tx_log_prefix}" &
+		${mpstat_cmd} -P ALL 1 "${testDuration}" > "./$log_folder/mpstat-${tx_log_prefix}" &
+		${lagscope_cmd} -s"${server}" -t "${testDuration}" -V > "./$log_folder/lagscope-${tx_log_prefix}" &
 		LogMsg "ClientCmd: ${client_ntttcp_cmd} > ./${log_folder}/ntttcp-${tx_log_prefix}"
-		ssh ${client} "${client_ntttcp_cmd}" > "./${log_folder}/ntttcp-${tx_log_prefix}"
+		ssh "${client}" "${client_ntttcp_cmd}" > "./${log_folder}/ntttcp-${tx_log_prefix}"
 
 		LogMsg "Parsing results for $current_test_threads connections"
 		sleep 10
@@ -253,9 +250,9 @@ Run_Ntttcp()
 		rx_cyclesperbytes=$(Get_Cyclesperbytes "$rx_ntttcp_log_file")
 		if [[ $tx_throughput == "0.00" ]];
 		then
-			data_loss=`printf %.2f 0`
+			data_loss=$(printf %.2f 0)
 		else
-			data_loss=`printf %.2f $(echo "scale=5; 100*(($tx_throughput-$rx_throughput)/$tx_throughput)" | ${bc_cmd})`
+			data_loss=$(printf %.2f $(echo "scale=5; 100*(($tx_throughput-$rx_throughput)/$tx_throughput)" | ${bc_cmd}))
 		fi
 
 		LogMsg "Test Results: "
@@ -266,13 +263,13 @@ Run_Ntttcp()
 		LogMsg "DataLoss in %: $data_loss"
 		if [[ $testType == "udp" ]];
 		then
-			echo "$current_test_threads,$tx_throughput,$rx_throughput,$data_loss" >> $result_file
+			echo "$current_test_threads,$tx_throughput,$rx_throughput,$data_loss" >> "$result_file"
 		else
 			testType="tcp"
-			echo "$current_test_threads,$tx_throughput,$tx_cyclesperbytes,$avg_latency" >> $result_file
+			echo "$current_test_threads,$tx_throughput,$tx_cyclesperbytes,$avg_latency" >> "$result_file"
 		fi
 		LogMsg "current test finished. wait for next one... "
-		i=$(($i + 1))
+		i=$(("$i" + 1))
 		sleep 5
 	done
 }
@@ -281,11 +278,11 @@ Run_Ntttcp()
 LogMsg "Now running ${testType} test using NTTTCP"
 Run_Ntttcp
 
-Kill_Process ${client} dstat
-Kill_Process ${server} lagscope
-Kill_Process ${server} dstat
-Kill_Process ${server} mpstat
-column -s, -t $result_file > ./$log_folder/report.log
-cp $log_folder/* .
+Kill_Process "${client}" dstat
+Kill_Process "${server}" lagscope
+Kill_Process "${server}" dstat
+Kill_Process "${server}" mpstat
+column -s, -t "$result_file" > ./"$log_folder"/report.log
+cp "$log_folder"/* .
 cat report.log
 SetTestStateCompleted

Testscripts/Linux/BVT-CORE-RELOAD-MODULES.sh
@@ -32,55 +32,54 @@ HYPERV_MODULES=(hv_vmbus hv_netvsc hv_storvsc hv_utils hv_balloon hid_hyperv hyp
 skip_modules=()
 config_path="/boot/config-$(uname -r)"
 
-vmbus_included=`grep CONFIG_HYPERV=y $config_path`
-if [ $vmbus_included ]; then
+vmbus_included=$(grep CONFIG_HYPERV=y "$config_path")
+if [ "$vmbus_included" ]; then
     skip_modules+=("hv_vmbus")
     LogMsg "Info: Skiping hv_vmbus module as it is built-in."
 fi
 
-netvsc_includes=`grep CONFIG_HYPERV_NET=y $config_path`
-if [ $netvsc_includes ]; then
+netvsc_includes=$(grep CONFIG_HYPERV_NET=y "$config_path")
+if [ "$netvsc_includes" ]; then
     skip_modules+=("hv_netvsc")
     LogMsg "Info: Skiping hv_netvsc module as it is built-in."
 fi
 
-storvsc_included=`grep CONFIG_HYPERV_STORAGE=y $config_path`
-if [ $storvsc_included ]; then
+storvsc_included=$(grep CONFIG_HYPERV_STORAGE=y "$config_path")
+if [ "$storvsc_included" ]; then
     skip_modules+=("hv_storvsc")
     LogMsg "Info: Skiping hv_storvsc module as it is built-in."
 fi
 
-utils_includes=`grep CONFIG_HYPERV_UTILS=y $config_path`
-if [ $utils_includes ]; then
+utils_includes=$(grep CONFIG_HYPERV_UTILS=y "$config_path")
+if [ "$utils_includes" ]; then
     skip_modules+=("hv_utils")
     LogMsg "Info: Skiping hv_utils module as it is built-in."
 fi
 
-balloon_includes=`grep CONFIG_HYPERV_BALLOON=y $config_path`
-if [ $balloon_includes ]; then
+balloon_includes=$(grep CONFIG_HYPERV_BALLOON=y "$config_path")
+if [ "$balloon_includes" ]; then
     skip_modules+=("hv_balloon")
     LogMsg "Info: Skiping hv_balloon module as it is built-in."
 fi
 
-hid_includes=`grep CONFIG_HID_HYPERV_MOUSE=y $config_path`
-if [ $hid_includes ]; then
+hid_includes=$(grep CONFIG_HID_HYPERV_MOUSE=y "$config_path")
+if [ "$hid_includes" ]; then
     skip_modules+=("hid_hyperv")
     LogMsg "Info: Skiping hid_hyperv module as it is built-in."
 fi
 
-keyboard_includes=`grep CONFIG_HYPERV_KEYBOARD=y $config_path`
-if [ $keyboard_includes ]; then
+keyboard_includes=$(grep CONFIG_HYPERV_KEYBOARD=y "$config_path")
+if [ "$keyboard_includes" ]; then
     skip_modules+=("hyperv_keyboard")
     LogMsg "Info: Skiping hyperv_keyboard module as it is built-in."
 fi
 
-fb_includes=`grep CONFIG_FB_HYPERV=y $config_path`
-if [ $fb_includes ]; then
+fb_includes=$(grep CONFIG_FB_HYPERV=y "$config_path")
+if [ "$fb_includes" ]; then
     skip_modules+=("hyperv_fb")
     LogMsg "Info: Skiping hyperv_fb module as it is built-in."
 fi
 
-temp_list=()
 # Remove each module in HYPERV_MODULES from skip_modules
 for module in "${HYPERV_MODULES[@]}"; do
     skip=""
@@ -103,7 +102,7 @@ VerifyModules()
         MODULES=~/modules.txt
         lsmod | grep "hv_*" > $MODULES
         lsmod | grep "hyperv" >> $MODULES
-        if ! grep -q $module $MODULES; then
+        if ! grep -q "$module" "$MODULES"; then
             msg="Error: $module not loaded"
             LogErr "${msg}"
             SetTestStateFailed

Testscripts/Linux/BVT-CORE-VERIFY-LIS-MODULES-VERSION.sh
@@ -36,20 +36,18 @@ if [[ ( $hv_string == "" ) || ! ( $hv_string == *"hv_vmbus:"*"Vmbus version:"* )
 fi
 
 skip_modules=()
-vmbus_included=`grep CONFIG_HYPERV=y /boot/config-$(uname -r)`
-if [ $vmbus_included ]; then
+vmbus_included=$(grep CONFIG_HYPERV=y /boot/config-$(uname -r))
+if [ "$vmbus_included" ]; then
     skip_modules+=("hv_vmbus")
     LogMsg "Info: Skiping hv_vmbus module as it is built-in."
 fi
 
-storvsc_included=`grep CONFIG_HYPERV_STORAGE=y /boot/config-$(uname -r)`
-if [ $storvsc_included ]; then
+storvsc_included=$(grep CONFIG_HYPERV_STORAGE=y /boot/config-$(uname -r))
+if [ "$storvsc_included" ]; then
     skip_modules+=("hv_storvsc")
     LogMsg "Info: Skiping hv_storvsc module as it is built-in."
 fi
 
-# Declare temporary array
-temp_list=()
 # Remove each module in HYPERV_MODULES from skip_modules
 for module in "${HYPERV_MODULES[@]}"; do
     skip=""

Testscripts/Linux/CORE-CPUVerifyOnline.sh
@@ -10,7 +10,6 @@
 #	as each core can't be offline except vcpu0 for a successful test pass.
 
 CONSTANTS_FILE="constants.sh"
-nonCPU0inter=0
 
 . utils.sh || {
     echo "unable to source utils.sh!"
@@ -41,8 +40,8 @@ for ((cpu=1 ; cpu<=$cpu_count ; cpu++)) ;do
     __file_path="/sys/devices/system/cpu/cpu$cpu/online"
     if [ -e "$__file_path" ]; then
         echo 0 > $__file_path > /dev/null 2>&1
-        val=`cat $__file_path`
-        if [ $val -ne 0 ]; then
+        val=$(cat $__file_path)
+        if [ "$val" -ne 0 ]; then
             LogMsg "CPU core ${cpu} can't be offline."
         else
             LogErr "CPU ${cpu} can be offline!"

Testscripts/Linux/CORE-ConfigTimeSync.sh
@@ -140,7 +140,7 @@ ConfigUbuntu()
         service chrony stop
         if [ $? -ne 0 ]; then
             LogErr "Unable to stop chrony"
-            UpdateTestState $ICA_TESTFAILED
+            SetTestStateFailed
             exit 0
         fi
 

Testscripts/Linux/CORE-InitrdModulesCheck.sh
@@ -10,10 +10,10 @@ CopyImage()
     fi
 
     mkdir /root/initr
-    cp $1 /root/initr/boot.img
+    cp "$1" /root/initr/boot.img
     cd /root/initr/
 
-    img_type=`file boot.img`
+    img_type=$(file boot.img)
     LogMsg "The image type is: $img_type"
 }
 
@@ -22,14 +22,14 @@ SearchModules()
     LogMsg "Searching for modules..."
     [[ -d "/root/initr/usr/lib/modules" ]] && abs_path="/root/initr/usr/lib/modules/" || abs_path="/root/initr/lib/modules/"
     for module in "${hv_modules[@]}"; do
-        grep -i $module $abs_path*/modules.dep
+        grep -i "$module" $abs_path*/modules.dep
         if [ $? -eq 0 ]; then
             LogMsg "Module $module was found in initrd."
             echo "Module $module was found in initrd." >> /root/summary.log
         else
             LogMsg "Module $module was NOT found."
             echo "Module $module was NOT found." >> /root/summary.log
-            grep -i $module $abs_path*/modules.dep >> /root/summary.log
+            grep -i "$module" $abs_path*/modules.dep >> /root/summary.log
             SetTestStateFailed
             exit 1
         fi
@@ -65,18 +65,18 @@ fi
 # Rebuild array to exclude built-in modules
 skip_modules=()
 
-vmbusIncluded=`grep CONFIG_HYPERV=y /boot/config-$(uname -r)`
-if [ $vmbusIncluded ]; then
+vmbusIncluded=$(grep CONFIG_HYPERV=y /boot/config-$(uname -r))
+if [ "$vmbusIncluded" ]; then
     skip_modules+=("hv_vmbus.ko")
     LogMsg "hv_vmbus module is built-in. Skipping module. "
 fi
-storvscIncluded=`grep CONFIG_HYPERV_STORAGE=y /boot/config-$(uname -r)`
-if [ $storvscIncluded ]; then
+storvscIncluded=$(grep CONFIG_HYPERV_STORAGE=y /boot/config-$(uname -r))
+if [ "$storvscIncluded" ]; then
     skip_modules+=("hv_storvsc.ko")
     LogMsg "hv_storvsc module is built-in. Skipping module. "
 fi
-netvscIncluded=`grep CONFIG_HYPERV_NET=y /boot/config-$(uname -r)`
-if [ $netvscIncluded ]; then
+netvscIncluded=$(grep CONFIG_HYPERV_NET=y /boot/config-$(uname -r))
+if [ "$netvscIncluded" ]; then
     skip_modules+=("hv_netvsc.ko")
     LogMsg "hv_netvsc module is built-in. Skipping module. "
 fi
@@ -124,22 +124,22 @@ esac
 if [ -f /boot/initramfs-0-rescue* ]; then
     img=/boot/initramfs-0-rescue*
 else
-  if [ -f "/boot/initrd-`uname -r`" ]; then
-    img="/boot/initrd-`uname -r`"
+  if [ -f "/boot/initrd-$(uname -r)" ]; then
+    img="/boot/initrd-$(uname -r)"
   fi
 
-  if [ -f "/boot/initramfs-`uname -r`.img" ]; then
-    img="/boot/initramfs-`uname -r`.img"
+  if [ -f "/boot/initramfs-$(uname -r).img" ]; then
+    img="/boot/initramfs-$(uname -r).img"
   fi
 
-  if [ -f "/boot/initrd.img-`uname -r`" ]; then
-    img="/boot/initrd.img-`uname -r`"
+  if [ -f "/boot/initrd.img-$(uname -r)" ]; then
+    img="/boot/initrd.img-$(uname -r)"
   fi
 fi
 
 UpdateSummary "The initrd test image is: $img"
 
-CopyImage $img
+CopyImage "$img"
 
 LogMsg "Unpacking the image..."
 
@@ -147,7 +147,7 @@ case $img_type in
     *ASCII*cpio*)
         cpio -id -F boot.img &> out.file
         skip_block_size=$(cat out.file | awk '{print $1}')
-        dd if=boot.img of=finalInitrd.img bs=512 skip=$skip_block_size
+        dd if=boot.img of=finalInitrd.img bs=512 skip="$skip_block_size"
         /usr/lib/dracut/skipcpio finalInitrd.img |zcat| cpio -id --no-absolute-filenames
         if [ $? -eq 0 ]; then
             LogMsg "Successfully unpacked the image."

Testscripts/Linux/DM_HotAdd_Verify_udev.sh
@@ -30,13 +30,13 @@ for udevfile in $(find /etc/udev/ /lib/udev/ -name "*.rules*"); do # search for 
     match_count=0
     for i in "${items[@]}"
     do
-        grep $i $udevfile > /dev/null # grep for the udev rule
+        grep "$i" "$udevfile" > /dev/null # grep for the udev rule
         sts=$?
         if [ 0 -eq ${sts} ]; then
-             match_count=`expr $match_count + 1`
+             match_count=$(expr $match_count + 1)
         fi
     done
-    if [ ${#items[@]} -eq $match_count ]; then
+    if [ ${#items[@]} -eq "$match_count" ]; then
         filelist=("${filelist[@]}" $udevfile) # populate an array with the results
     fi
 done
@@ -48,7 +48,7 @@ if [ ${#filelist[@]} -gt 0 ]; then # check if we found anything
         LogMsg "Following DM udev files were found:"
         # list the files
         for rulefile in "${filelist[@]}"; do
-            LogMsg $rulefile
+            LogMsg "$rulefile"
         done
     else
         UpdateSummary "Hot-Add udev rule present: Success"

Testscripts/Linux/DetectLinuxDistro.sh
@@ -11,13 +11,13 @@
 #  FEDORA
 DetectDistro()
 {
-while echo $1 | grep ^- > /dev/null; do
-    eval $( echo $1 | sed 's/-//g' | tr -d '\012')=$2
+while echo "$1" | grep ^- > /dev/null; do
+    eval $( echo "$1" | sed 's/-//g' | tr -d '\012')="$2"
     shift
     shift
 done
         if [ -e /etc/debian_version ]; then
-		        tmp=`cat /etc/*-release`
+		        tmp=$(cat /etc/*-release)
                 if [[ "$tmp" == *Ubuntu* ]]; then
                     echo "UBUNTU"
                     exitVal=0
@@ -26,9 +26,9 @@ done
                     exitVal=0
                 fi
         elif [ -e /etc/redhat-release ]; then
-                tmp=`cat /etc/redhat-release`
+                tmp=$(cat /etc/redhat-release)
                 if [ -e /etc/oracle-release ]; then
-                    tmp=`cat /etc/oracle-release`
+                    tmp=$(cat /etc/oracle-release)
                     if [[ "$tmp" == *Oracle* ]]; then
                         echo "ORACLELINUX"
                         exitVal=0
@@ -50,7 +50,7 @@ done
                     exitVal=1
                 fi
         elif [ -e /etc/SuSE-release ]; then
-                tmp=`cat /etc/SuSE-release`
+                tmp=$(cat /etc/SuSE-release)
                 if [[ "$tmp" == *Enterprise* ]]; then
                     echo "SLES"
                     exitVal=0
@@ -61,7 +61,7 @@ done
                     echo "Unknown"
                 fi
         elif [ -e /etc/os-release ]; then
-                tmp=`cat /etc/os-release`
+                tmp=$(cat /etc/os-release)
                 if [[ "$tmp" == *coreos* ]]; then
                     echo "COREOS"
                     exitVal=0
@@ -72,7 +72,7 @@ done
                     echo "Unknown"         
                 fi
         elif [ -e /usr/share/clear/version ]; then
-                tmp=`cat /usr/share/clear/version`
+                tmp=$(cat /usr/share/clear/version)
                 echo "CLEARLINUX"
                 exitVal=0
         fi

commit 2f29b19caa4563e91cfbb76ae7067da78f4b40cd

perf_ntttcp: fix bug introduced in PR #546

Testscripts/Linux/perf_ntttcp.sh
@@ -269,7 +269,7 @@ Run_Ntttcp()
 			echo "$current_test_threads,$tx_throughput,$tx_cyclesperbytes,$avg_latency" >> "$result_file"
 		fi
 		LogMsg "current test finished. wait for next one... "
-		i=$(("$i" + 1))
+		i=$(($i + 1))
 		sleep 5
 	done
 }