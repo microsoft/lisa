484779f3687914a75bd499a611297be3fbf9d217
Adrian Vladu <avladu@cloudbasesolutions.com>
2019-01-14 14:12:53 +0200

    utilities: move Add-AzureAccountFromSecretsFile to Azure module

--- Utilities/AddAzureRmAccountFromSecretsFile.ps1
+++ Utilities/AddAzureRmAccountFromSecretsFile.ps1
@@ -40,44 +40,4 @@ Get-ChildItem (Join-Path $rootPath "Libraries") -Recurse | `
     Where-Object { $_.FullName.EndsWith(".psm1") } | `
     ForEach-Object { Import-Module $_.FullName -Force -Global -DisableNameChecking }
 
-if ( $customSecretsFilePath ) {
-    $secretsFile = $customSecretsFilePath
-    Write-LogInfo "Using provided secrets file: $secretsFile"
-}
-if ($env:Azure_Secrets_File) {
-    $secretsFile = $env:Azure_Secrets_File
-    Write-LogInfo "Using secrets file: $secretsFile, defined in environments."
-}
-if ( ($null -eq $secretsFile) -or ($secretsFile -eq [string]::Empty)) {
-    Write-LogErr "ERROR: The Secrets file is not being set."
-    Raise-Exception ("XML Secrets file not provided")
-}
-
-#---------------------------------------------------------[Script Start]--------------------------------------------------------
-
-if ( Test-Path $secretsFile ) {
-    Write-LogInfo "$secretsFile found."
-    Write-LogInfo "------------------------------------------------------------------"
-    Write-LogInfo "Authenticating Azure PS session.."
-    $XmlSecrets = [xml](Get-Content $secretsFile)
-    $ClientID = $XmlSecrets.secrets.SubscriptionServicePrincipalClientID
-    $TenantID = $XmlSecrets.secrets.SubscriptionServicePrincipalTenantID
-    $Key = $XmlSecrets.secrets.SubscriptionServicePrincipalKey
-    $pass = ConvertTo-SecureString $key -AsPlainText -Force
-    $mycred = New-Object System.Management.Automation.PSCredential ($ClientID, $pass)
-    $subIDSplitted = ($XmlSecrets.secrets.SubscriptionID).Split("-")
-    $subIDMasked = "$($subIDSplitted[0])-xxxx-xxxx-xxxx-$($subIDSplitted[4])"
-
-    $null = Add-AzureRmAccount -ServicePrincipal -Tenant $TenantID -Credential $mycred
-    $selectedSubscription = Select-AzureRmSubscription -SubscriptionId $XmlSecrets.secrets.SubscriptionID
-    if ( $selectedSubscription.Subscription.Id -eq $XmlSecrets.secrets.SubscriptionID ) {
-        Write-LogInfo "Current Subscription : $subIDMasked."
-    } else {
-        Write-LogInfo "There was an error when selecting $subIDMasked."
-    }
-    Write-LogInfo "------------------------------------------------------------------"
-}
-else {
-    Write-LogErr "Secret file $secretsFile does not exist"
-    Raise-Exception ("XML Secrets file not provided")
-}
+Add-AzureAccountFromSecretsFile -CustomSecretsFilePath $customSecretsFilePath
