f4e8eb7ecc09e1ebecca4bf1fd8b722d5bb41869
LiliDeng <v-lide@microsoft.com>
2019-03-18 16:45:11 +0800

    Fix test case return unexpected output. (#38)

--- Testscripts/Windows/DiskHotAddRemove-Parallel.ps1
+++ Testscripts/Windows/DiskHotAddRemove-Parallel.ps1
@@ -4,18 +4,18 @@ param([String] $TestParams, [object] $AllVMData)
 $ErrorActionPreference = "Stop"
 function Main {
     param (
-        $TestParams,$AllVMData
+        $TestParams, $AllVMData
     )
     $currentTestResult = Create-TestResultObject
     $resultArr = @()
     try {
         $testResult = $null
-        $diskSizeinGB=$TestParams.DiskSize
+        $diskSizeinGB = $TestParams.DiskSize
         $allDiskNames = @()
         $VirtualMachine = Get-AzureRmVM -ResourceGroupName $AllVMData.ResourceGroupName -Name $AllVMData.RoleName
         $diskCount = (Get-AzureRmVMSize -Location $AllVMData.Location | Where-Object {$_.Name -eq $AllVMData.InstanceSize}).MaxDataDiskCount
         Write-LogInfo "Parallel Addition of Data Disks to the VM "
-        While($count -lt $diskCount) {
+        While ($count -lt $diskCount) {
             $count += 1
             $diskName = "disk"+ $count.ToString()
             $allDiskNames += $diskName
@@ -23,55 +23,52 @@ function Main {
             $diskConfig = New-AzureRmDiskConfig -SkuName $StorageType -Location $AllVMData.Location -CreateOption Empty -DiskSizeGB $diskSizeinGB
             $dataDisk = New-AzureRmDisk -DiskName $diskName -Disk $diskConfig -ResourceGroupName $AllVMData.ResourceGroupName
             Write-LogInfo "Adding an empty data disk of size $diskSizeinGB GB"
-            $sts=Add-AzureRmVMDataDisk -VM $VirtualMachine -Name $diskName -CreateOption Attach -ManagedDiskId $dataDisk.Id -Lun $count
-            if($sts.ProvisioningState -eq "Succeeded") {
+            $sts = Add-AzureRmVMDataDisk -VM $VirtualMachine -Name $diskName -CreateOption Attach -ManagedDiskId $dataDisk.Id -Lun $count
+            if ($sts.ProvisioningState -eq "Succeeded") {
                 Write-LogInfo "Successfully created an empty data disk of size $diskSizeinGB GB"
-            }
-            else {
+            } else {
                 $testResult = $resultFail
                 throw "Failed to create an empty data disk of size $diskSizeinGB GB"
             }
         }
         Write-LogInfo "Number of data disks added to the VM $count"
-        Update-AzureRMVM -VM $VirtualMachine -ResourceGroupName $AllVMData.ResourceGroupName
+        Update-AzureRMVM -VM $VirtualMachine -ResourceGroupName $AllVMData.ResourceGroupName | Out-Null
         Write-LogInfo "Successfully added $diskCount empty data disks to the VM"
         Write-LogInfo "Verifying if data disk is added to the VM: Running fdisk on remote VM"
         $fdiskOutput = Run-LinuxCmd -username $user -password $password -ip $AllVMData.PublicIP -port $AllVMData.SSHPort -command "/sbin/fdisk -l | grep /dev/sd" -runAsSudo
-        foreach($line in ($fdiskOutput.Split([Environment]::NewLine))) {
-            if($line -imatch "Disk /dev/sd[^ab]:" -and [int64]($line.Split()[4]) -eq (([int64]($diskSizeinGB) * [int64]1073741824))){
+        foreach ($line in ($fdiskOutput.Split([Environment]::NewLine))) {
+            if ($line -imatch "Disk /dev/sd[^ab]:" -and [int64]($line.Split()[4]) -eq (([int64]($diskSizeinGB) * [int64]1073741824))){
                 Write-LogInfo "Data disk is successfully mounted to the VM: $line"
                 $verifiedDiskCount += 1
             }
         }
         Write-LogInfo "Number of data disks verified inside VM $verifiedDiskCount"
-        if($verifiedDiskCount -ge $diskCount) {
+        if ($verifiedDiskCount -ge $diskCount) {
             Write-LogInfo "Data disks added to the VM are successfully verified inside VM"
-        }
-        else {
+        } else {
             $testResult=$resultFail
             throw "Data disks added to the VM failed to verify inside VM"
         }
         Write-LogInfo "Parallel Removal of Data Disks from the VM"
-        $sts=Remove-AzureRmVMDataDisk -VM $VirtualMachine -DataDiskNames $allDiskNames
-        if($sts.ProvisioningState -eq "Succeeded") {
+        $sts = Remove-AzureRmVMDataDisk -VM $VirtualMachine -DataDiskNames $allDiskNames
+        if ($sts.ProvisioningState -eq "Succeeded") {
             Write-LogInfo "Successfully removed the data disk from the VM"
-        }
-        else {
+        } else {
             throw "Failed to remove data disk"
         }
-        Update-AzureRMVM -VM $VirtualMachine -ResourceGroupName $AllVMData.ResourceGroupName
+        Update-AzureRMVM -VM $VirtualMachine -ResourceGroupName $AllVMData.ResourceGroupName | Out-Null
         Write-LogInfo "Verifying if data disks are removed from the VM: Running fdisk on remote VM"
         $fdiskFinalOutput = Run-LinuxCmd -username $user -password $password -ip  $AllVMData.PublicIP -port $AllVMData.SSHPort -command "/sbin/fdisk -l | grep /dev/sd" -runAsSudo
-        foreach($line in ($fdiskFinalOutput.Split([Environment]::NewLine))) {
+        foreach ($line in ($fdiskFinalOutput.Split([Environment]::NewLine))) {
             if($line -imatch "Disk /dev/sd[^ab]:" -and [int64]($line.Split()[4]) -eq (([int64]($diskSizeinGB) * [int64]1073741824))) {
                 $testResult=$resultFail
                 throw "Data disk is NOT removed from the VM at $line"
             }
         }
         Write-LogInfo "Successfully verified that all data disks are removed from the VM"
-        $testResult=$resultPass
+        $testResult = $resultPass
     } catch {
-        $ErrorMessage =  $_.Exception.Message
+        $ErrorMessage = $_.Exception.Message
         $ErrorLine = $_.InvocationInfo.ScriptLineNumber
         Write-LogErr "$ErrorMessage at line: $ErrorLine"
     } finally {
--- Testscripts/Windows/DiskHotAddRemove-Serial.ps1
+++ Testscripts/Windows/DiskHotAddRemove-Serial.ps1
@@ -4,69 +4,59 @@ param([String] $TestParams, [object] $AllVMData)
 $ErrorActionPreference = "Stop"
 function Main {
     param (
-        $TestParams,$AllVMData
+        $TestParams, $AllVMData
     )
     # Create test result
     $currentTestResult = Create-TestResultObject
     $resultArr = @()
     try {
         $testResult = $null
-        $diskSizeinGB=$TestParams.DiskSize
+        $diskSizeinGB = $TestParams.DiskSize
         $VirtualMachine = Get-AzureRmVM -ResourceGroupName $AllVMData.ResourceGroupName -Name $AllVMData.RoleName
         $diskCount = (Get-AzureRmVMSize -Location $AllVMData.Location | Where-Object {$_.Name -eq $AllVMData.InstanceSize}).MaxDataDiskCount
         Write-LogInfo "Serial Addition and Removal of Data Disks"
-        While($count -lt $diskCount)
-        {
+        While ($count -lt $diskCount) {
             $count += 1
             $verifiedDiskCount = 0
             $diskName = "disk"+ $count.ToString()
             $storageType = 'Premium_LRS'
             $diskConfig = New-AzureRmDiskConfig -SkuName $storageType -Location $AllVMData.Location -CreateOption Empty -DiskSizeGB $diskSizeinGB
             $dataDisk = New-AzureRmDisk -DiskName $diskName -Disk $diskConfig -ResourceGroupName $AllVMData.ResourceGroupName
-            $sts1=Add-AzureRmVMDataDisk -VM $VirtualMachine -Name $diskName -CreateOption Attach -ManagedDiskId $dataDisk.Id -Lun $count
-            if($sts1.ProvisioningState -eq "Succeeded") {
+            $sts1 = Add-AzureRmVMDataDisk -VM $VirtualMachine -Name $diskName -CreateOption Attach -ManagedDiskId $dataDisk.Id -Lun $count
+            if ($sts1.ProvisioningState -eq "Succeeded") {
                 Write-LogInfo "Successfully created an empty data disk of size $diskSizeinGB GB"
-            }
-            else {
+            } else {
                 $testResult = $resultFail
                 throw "Failed to create an empty data disk of size $diskSizeinGB GB"
             }
-            Update-AzureRMVM -VM $VirtualMachine -ResourceGroupName $AllVMData.ResourceGroupName
+            Update-AzureRMVM -VM $VirtualMachine -ResourceGroupName $AllVMData.ResourceGroupName | Out-Null
             Write-LogInfo "Verifying if data disk is added to the VM: Running fdisk on remote VM"
             $fdiskOutput = Run-LinuxCmd -username $user -password $password -ip $AllVMData.PublicIP -port $AllVMData.SSHPort -command "/sbin/fdisk -l | grep /dev/sd" -runAsSudo
-            foreach($line in ($fdiskOutput.Split([Environment]::NewLine)))
-            {
-                if($line -imatch "Disk /dev/sd[^ab]:" -and [int64]($line.Split()[4]) -eq (([int64]($diskSizeinGB) * [int64]1073741824)))
-                {
+            foreach ($line in ($fdiskOutput.Split([Environment]::NewLine))) {
+                if($line -imatch "Disk /dev/sd[^ab]:" -and [int64]($line.Split()[4]) -eq (([int64]($diskSizeinGB) * [int64]1073741824))) {
                     Write-LogInfo "Data disk is successfully mounted to the VM: $line"
                     $verifiedDiskCount += 1
                 }
             }
-            if($verifiedDiskCount -eq 1)
-            {
+            if ($verifiedDiskCount -eq 1) {
                 Write-LogInfo "Data disk added to the VM is successfully verified inside VM"
-            }
-            else
-            {
+            } else {
                 $testResult = $resultFail
                 throw "Data disk added to the VM failed to verify inside VM"
             }
             Write-LogInfo "Removing the Data Disks from the VM"
             $sts3 = Remove-AzureRmVMDataDisk -VM $VirtualMachine -DataDiskNames $diskName
-            if($sts3.ProvisioningState -eq "Succeeded") {
+            if ($sts3.ProvisioningState -eq "Succeeded") {
                 Write-LogInfo "Successfully removed data disk"
-            }
-            else {
+            } else {
                 throw "Failed to remove data disk"
             }
-            Update-AzureRMVM -VM $VirtualMachine -ResourceGroupName $AllVMData.ResourceGroupName
+            Update-AzureRMVM -VM $VirtualMachine -ResourceGroupName $AllVMData.ResourceGroupName | Out-Null
             Write-LogInfo "Successfully removed the data disk from the VM"
             Write-LogInfo "Verifying if data disk is removed from the VM: Running fdisk on remote VM"
             $fdiskFinalOutput = Run-LinuxCmd -username $user -password $password -ip $AllVMData.PublicIP -port $AllVMData.SSHPort -command "/sbin/fdisk -l | grep /dev/sd" -runAsSudo
-            foreach($line in ($fdiskFinalOutput.Split([Environment]::NewLine)))
-            {
-                if($line -imatch "Disk /dev/sd[^ab]:" -and [int64]($line.Split()[4]) -eq (([int64]($diskSizeinGB) * [int64]1073741824)))
-                {
+            foreach ($line in ($fdiskFinalOutput.Split([Environment]::NewLine))) {
+                if($line -imatch "Disk /dev/sd[^ab]:" -and [int64]($line.Split()[4]) -eq (([int64]($diskSizeinGB) * [int64]1073741824))) {
                     $testResult = $resultFail
                     throw "Data disk is NOT removed from the VM at $line"
                 }
@@ -74,9 +64,9 @@ function Main {
             Write-LogInfo "Successfully verified that data disk is removed from the VM"
         }
         Write-LogInfo "Successfully added and removed $diskCount number of data disks"
-        $testResult=$resultPass
+        $testResult = $resultPass
     } catch {
-        $ErrorMessage =  $_.Exception.Message
+        $ErrorMessage = $_.Exception.Message
         $ErrorLine = $_.InvocationInfo.ScriptLineNumber
         Write-LogErr "$ErrorMessage at line: $ErrorLine"
     } finally {
