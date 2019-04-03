f949ec1fe668eeb4c3ad18d972dee81d71e95e0a
Adrian Vladu <avladu@cloudbasesolutions.com>
2019-04-03 13:21:46 +0300

    kernelInstall: cascade failure to lis install and sriov config

--- Libraries/CommonFunctions.psm1
+++ Libraries/CommonFunctions.psm1
@@ -920,7 +920,7 @@ Function Set-CustomConfigInVMs($CustomKernel, $CustomLIS, $EnableSRIOV, $AllVMDa
 			$retValue = $false
 		}
 	}
-	if ($CustomLIS) {
+	if ($retValue -and $CustomLIS) {
 		# LIS is only available Redhat, CentOS and Oracle image which uses Redhat kernel.
 		if(@("REDHAT", "ORACLELINUX", "CENTOS").contains($global:detectedDistro)) {
 			Write-LogInfo "Custom LIS: $CustomLIS will be installed on all machines..."
@@ -935,7 +935,7 @@ Function Set-CustomConfigInVMs($CustomKernel, $CustomLIS, $EnableSRIOV, $AllVMDa
 			$retValue = $false
 		}
 	}
-	if ($EnableSRIOV) {
+	if ($retValue -and $EnableSRIOV) {
 		$SRIOVStatus = Enable-SRIOVInAllVMs -allVMData $AllVMData -TestProvider $TestProvider
 		if (!$SRIOVStatus) {
 			Write-LogErr "Failed to enable Accelerated Networking. Aborting tests."
