5c9d8ef6662b7419ebb3a57aca1e0f2e8001f6bd
Lili Deng (Wicresoft North America Ltd) <v-lide@microsoft.com>
2019-05-25 00:44:56 -0700

    Enable filesystem case against RHEL 8.

--- Libraries/TestHelpers.psm1
+++ Libraries/TestHelpers.psm1
@@ -36,17 +36,12 @@ Function New-ResultSummary($testResult, $checkValues, $testName, $metaData)
 }
 
 Function Get-FinalResultHeader($resultArr){
-	if (($resultArr -imatch "FAIL" ) -or ($resultArr -imatch "Aborted"))
-	{
-		$result = "FAIL"
-		if ($resultArr -imatch "Aborted")
-		{
-			$result = "Aborted"
-		}
-	}
-	else
-	{
-		$result = "PASS"
+	switch ($resultArr) {
+		{($_ -imatch "FAIL")} { $result = $global:ResultFail; break}
+		{($_ -imatch "Abort")} { $result = $global:ResultAborted; break}
+		{($_ -imatch "Skip")} { $result = $global:ResultSkipped; break}
+		{($_ -imatch "PASS")} { $result = $global:ResultPass; break}
+		default { $result = $global:ResultFail }
 	}
 	return $result
 }
