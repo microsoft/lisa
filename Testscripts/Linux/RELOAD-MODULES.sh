34503255bdc67b79f1cf7435894cbcd14c334e6d
Chris Valean <v-chvale@microsoft.com>
2019-05-29 18:45:43 +0300

    RELOAD-MODULES: changes to LIS drivers handling (#274)
    * Update RELOAD-MODULES.sh
    
    1. add check that hv_utils cannot be unloaded, modprobe will return that the module is in use and cannot be unloaded.
    2. remove hid_hyperv reload from loop, we care about the netvsc driver. reloading multiple lis drivers was the original design, as other modules might have been unloaded. nowadays, that is no longer the case.
    
    * Update RELOAD-MODULES.sh
    
    * Update RELOAD-MODULES.sh

--- Testscripts/Linux/RELOAD-MODULES.sh
+++ Testscripts/Linux/RELOAD-MODULES.sh
@@ -7,7 +7,7 @@
 # Description:
 #    This script will first check the existence of LIS modules.
 #    Then it will reload the modules in a loop in order to stress the system.
-#    It also checks that hyperv_fb cannot be unloaded.
+#    It also checks that hv_utils and hyperv_fb cannot be unloaded.
 #    When done it will bring up the eth0 interface and check again
 #    for the presence of modules.
 #
@@ -118,11 +118,17 @@ VerifyModules()
 # Main script body
 #
 #######################################################################
+if (printf '%s\n' "${HYPERV_MODULES[@]}" | grep -xq "hv_utils"); then
+    if modprobe -r hv_utils; then
+        LogErr "hv_utils can be disabled, this is unexpected behavior."
+        SetTestStateFailed
+        exit 0
+    fi
+fi
+
 if (printf '%s\n' "${HYPERV_MODULES[@]}" | grep -xq "hyperv_fb"); then
-    modprobe -r hyperv_fb
-    if [[ $? -eq 0 ]]; then
-        msg="Error: hyperv_fb could be disabled!"
-        LogMsg "${msg}"
+    if modprobe -r hyperv_fb; then
+        LogErr "hyperv_fb can be disabled, this is unexpected behavior."
         SetTestStateFailed
         exit 0
     fi
@@ -138,23 +144,8 @@ do
         modprobe hv_netvsc
         sleep 1
     fi
-
-    if (printf '%s\n' "${HYPERV_MODULES[@]}" | grep -xq "hv_utils"); then
-        modprobe -r hv_utils
-        sleep 1
-        modprobe hv_utils
-        sleep 1
-    fi
-
-    if (printf '%s\n' "${HYPERV_MODULES[@]}" | grep -xq "hid_hyperv"); then
-        modprobe -r hid_hyperv
-        sleep 1
-        modprobe hid_hyperv
-        sleep 1
-    fi
-
     pass=$((pass+1))
-    LogMsg $pass
+    LogMsg "Reload iteration ${pass}"
 done
 
 END=$(date +%s)
