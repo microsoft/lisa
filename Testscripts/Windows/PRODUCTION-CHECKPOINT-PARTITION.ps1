dcac3709ede4e62b1c0cbbbabbf526b3d32f79ef
Hongxiong Xian (Wicresoft North America Ltd) <v-hoxian@microsoft.com>
2019-07-03 02:39:57 -0700

    PRODUCTION-CHECKPOINT-PARTITION.ps1: Traverse all disks combination

--- Testscripts/Windows/PRODUCTION-CHECKPOINT-PARTITION.ps1
+++ Testscripts/Windows/PRODUCTION-CHECKPOINT-PARTITION.ps1
@@ -81,13 +81,13 @@ function Main {
         $vm = Get-VM -Name $VMName -ComputerName $HvServer
         # Check if we can set the Production Checkpoint as default
         if ($vm.CheckpointType -ne "ProductionOnly"){
-            Set-VM -Name $VMName -ComputerName $HvServer -CheckpointType ProductionOnly
+            Set-VM -Name $VMName -ComputerName $HvServer -CheckpointType ProductionOnly | Out-Null
         }
         $random = Get-Random -minimum 1024 -maximum 4096
         $snapshot = "TestSnapshot_$random"
 
         Write-LogInfo "Info : creating Production Checkpoint ${snapshot} of VM ${VMName}"
-        Checkpoint-VM -Name $VMName -SnapshotName $snapshot -ComputerName $HvServer
+        Checkpoint-VM -Name $VMName -SnapshotName $snapshot -ComputerName $HvServer | Out-Null
         if (-not $?)
         {
             throw "Could not create Production checkpoint with $snapshot"
@@ -107,12 +107,12 @@ function Main {
              throw "Cannot create file ${testfile2} in /mnt/2"
         }
         Write-LogInfo "Info : Restoring Production Checkpoint ${snapshot}"
-        Restore-VMSnapshot -VMName $VMName -Name $snapshot -ComputerName $HvServer -Confirm:$false
+        Restore-VMSnapshot -VMName $VMName -Name $snapshot -ComputerName $HvServer -Confirm:$false | Out-Null
 
         #
         # Starting the VM
         #
-        Start-VM $VMName -ComputerName $HvServer
+        Start-VM $VMName -ComputerName $HvServer | Out-Null
 
         #
         # Waiting for the VM to run again and respond to SSH - port 22
@@ -135,27 +135,68 @@ function Main {
                 $availableDisks += $disk.Trim()
             }
         }
-        $mount_1st_disk = $availableDisks[0]
-        $mount_2nd_disk = $availableDisks[1]
-        Run-LinuxCmd -username $user -password $password -ip $Ipv4 -port $VMPort `
-            -command "mount /dev/${mount_1st_disk}1 /mnt/1; mount /dev/${mount_1st_disk}2 /mnt/2" -runAsSudo
+
+        # The disk swap, dual mount and resource disk make mount/umount complex
+        # It has to traverse all disks combination if DUALMOUNT is true
+        $disk_combination = @()
         if ($TestParams.DUALMOUNT) {
-            Run-LinuxCmd -username $user -password $password -ip $Ipv4 -port $VMPort `
-                -command "mount /dev/${mount_2nd_disk}1 /mnt/1; mount /dev/${mount_2nd_disk}2 /mnt/2" -runAsSudo
+            $rest = New-Object Collections.ArrayList
+            $availableDisks[1..($availableDisks.Length - 1)] | foreach { $rest.Add($_) | Out-Null }
+            $prefix = $availableDisks[0]
+            $(while ($rest) {
+                foreach ($suffix in $rest) {
+                    $disk_combination += $prefix
+                    $disk_combination += $suffix
+                }
+                $prefix = $rest[0]
+                $rest.RemoveAt(0)
+            })
+            $disk_combination += $disk_combination[-1..($disk_combination.Length-2)]
+        } else {
+            $disk_combination = $availableDisks
         }
-        $sts1 = Check-FileInLinuxGuest -VMPassword $password -VMPort $VMPort -VMUserName $user -Ipv4 $Ipv4 -fileName "/mnt/1/${testfile1}"
-        $sts2 = Check-FileInLinuxGuest -VMPassword $password -VMPort $VMPort -VMUserName $user -Ipv4 $Ipv4 -fileName "/mnt/2/${testfile1}"
-        if (-not $sts1 -or -not $sts2)
-        {
+
+        $testResultTmp = $resultFail
+        for ($i = 0; $i -le $disk_combination.Length; $i++) {
+            $mount_1st_disk = $disk_combination[$i]
+            Run-LinuxCmd -username $user -password $password -ip $Ipv4 -port $VMPort `
+                -command "mount /dev/${mount_1st_disk}1 /mnt/1; mount /dev/${mount_1st_disk}2 /mnt/2" -runAsSudo
+            if ($TestParams.DUALMOUNT) {
+                $i++
+                $mount_2nd_disk = $disk_combination[$i]
+                Run-LinuxCmd -username $user -password $password -ip $Ipv4 -port $VMPort `
+                    -command "mount /dev/${mount_2nd_disk}1 /mnt/1; mount /dev/${mount_2nd_disk}2 /mnt/2" -runAsSudo
+            }
+            $sts1 = Check-FileInLinuxGuest -VMPassword $password -VMPort $VMPort -VMUserName $user -Ipv4 $Ipv4 -fileName "/mnt/1/${testfile1}"
+            $sts2 = Check-FileInLinuxGuest -VMPassword $password -VMPort $VMPort -VMUserName $user -Ipv4 $Ipv4 -fileName "/mnt/2/${testfile1}"
+            if (-not $sts1 -or -not $sts2)
+            {
+                Write-LogWarn "TestFile1 is not present, try other disk"
+                if ($TestParams.DUALMOUNT) {
+                    Run-LinuxCmd -username $user -password $password -ip $Ipv4 -port $VMPort `
+                        -command "umount /dev/${mount_2nd_disk}1; umount /dev/${mount_2nd_disk}2" -runAsSudo
+                }
+                Run-LinuxCmd -username $user -password $password -ip $Ipv4 -port $VMPort `
+                    -command "umount /dev/${mount_1st_disk}1; umount /dev/${mount_1st_disk}2" -runAsSudo
+                continue
+            } else {
+                $testResultTmp = $resultPass
+                break
+            }
+        }
+
+        if( $testResultTmp -eq $resultPass){
+            $sts1 = Check-FileInLinuxGuest -VMPassword $password -VMPort $VMPort -VMUserName $user -Ipv4 $Ipv4 -fileName "/mnt/1/${testfile2}"
+            $sts2 = Check-FileInLinuxGuest -VMPassword $password -VMPort $VMPort -VMUserName $user -Ipv4 $Ipv4 -fileName "/mnt/2/${testfile2}"
+            if ($sts1 -or $sts2) {
+                Write-LogErr "TestFile2 is present,it should not be present on the VM"
+                $testResult = $resultFail
+            }
+        } else {
             Write-LogErr "TestFile1 is not present, it should be present on the VM"
             $testResult = $resultFail
         }
-        $sts1 = Check-FileInLinuxGuest -VMPassword $password -VMPort $VMPort -VMUserName $user -Ipv4 $Ipv4 -fileName "/mnt/1/${testfile2}"
-        $sts2 = Check-FileInLinuxGuest -VMPassword $password -VMPort $VMPort -VMUserName $user -Ipv4 $Ipv4 -fileName "/mnt/2/${testfile2}"
-        if ($sts1 -or $sts2) {
-            Write-LogErr "TestFile2 is present,it should not be present on the VM"
-            $testResult = $resultFail
-        }
+
         #
         # Delete the snapshot
         #
@@ -168,7 +209,7 @@ function Main {
         Run-LinuxCmd -username $user -password $password -ip $Ipv4 -port $VMPort `
             -command "umount /dev/${mount_1st_disk}1; umount /dev/${mount_1st_disk}2" -runAsSudo
 
-        Remove-VMSnapshot -VMName $VMName -Name $snapshot -ComputerName $HvServer
+        Remove-VMSnapshot -VMName $VMName -Name $snapshot -ComputerName $HvServer | Out-Null
 
         if( $testResult -ne $resultFail){
             Write-LogInfo "Info : Only the first file is present. Test succeeded"
