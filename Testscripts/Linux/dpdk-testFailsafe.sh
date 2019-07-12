9c2793a37d0e0c1acf34d4a6304e902f77383bc0
v-tonx <v-tonx@microsoft.com>
2019-07-11 20:02:41 -0700

    VERIFY-DPDK-FAILSAFE-DURING-TRAFFIC - fix perf results TestMode and TestCaseName

--- Testscripts/Linux/dpdk-testFailsafe.sh
+++ Testscripts/Linux/dpdk-testFailsafe.sh
@@ -127,10 +127,15 @@ function Testfailsafe_Parser() {
 		SetTestStateAborted
 		exit 1
 	fi
+	if [ -z "${MODES}" ]; then
+		MODES="txonly"
+		LogMsg "MODES parameter not found in environment; using default ${MODES}"
+	fi
 
 	local csv_file=$(Create_Csv)
-	echo "dpdk_version,phase,fwdrx_pps_avg,fwdtx_pps_avg" > "${csv_file}"
+	echo "dpdk_version,test_mode,phase,fwdrx_pps_avg,fwdtx_pps_avg" > "${csv_file}"
 	local dpdk_version=$(Get_DPDK_Version "${LIS_HOME}/${DPDK_DIR}")
+	local test_mode=${MODES}
 
 	local trimmed_prefix="trimmed-forwarder"
 	local trimmed_log="${LOG_DIR}/${trimmed_prefix}.log"
@@ -161,7 +166,7 @@ function Testfailsafe_Parser() {
 		else
 			phase="overall"
 		fi
-		echo "${dpdk_version},${phase},${fwdrx_pps_avg},${fwdtx_pps_avg}" >> "${csv_file}"
+		echo "${dpdk_version},${test_mode},${phase},${fwdrx_pps_avg},${fwdtx_pps_avg}" >> "${csv_file}"
 	done
 
 	column -s, -t "${csv_file}"
