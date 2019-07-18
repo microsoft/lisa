359cbe2daff266ec7a37dfb15e3af72bb757db4e
Stefan Lupsa <slupsa@cloudbasesolutions.com>
2019-07-18 15:01:39 +0300

    VERIFY-DEPLOYMENT-PROVISION: Add optional vCpu count check

--- Testscripts/Windows/VERIFY-DEPLOYMENT-PROVISION.ps1
+++ Testscripts/Windows/VERIFY-DEPLOYMENT-PROVISION.ps1
@@ -1,10 +1,15 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the Apache License.
 
-param([object] $AllVmData, [object] $CurrentTestData, [object] $TestProvider)
+param([object] $AllVmData, [object] $CurrentTestData, [object] $TestProvider, [object] $TestParams)
 
 function Main {
-	param([object] $allVMData, [object] $CurrentTestData, [object] $TestProvider)
+	param(
+		[object] $allVMData,
+		[object] $CurrentTestData,
+		[object] $TestProvider,
+		[object] $TestParams
+	)
 	try {
 		$CurrentTestResult = Create-TestResultObject
 		$CurrentTestResult.TestSummary += New-ResultSummary -testResult "PASS" `
@@ -27,8 +32,31 @@ function Main {
 					$CurrentTestResult.TestSummary += New-ResultSummary -testResult "PASS" `
 						-metaData "Reboot : Call Trace Verification" -checkValues "PASS,FAIL,ABORTED" `
 						-testName $currentTestData.testName
-					Write-LogInfo "Test Result : PASS."
-					$testResult = "PASS"
+					# If vCpu parameter is empty, skip the check and pass the test.
+					if ($TestParams.vCpu) {
+						Write-LogInfo "Check vCpu: Checking number of vCpu in VM"
+						$vmCpuCount = Run-LinuxCmd -username $user -password $password -ip $allVMData.PublicIP `
+							-port $allVMData.SSHPort -command "nproc" -ignoreLinuxExitCode
+						Write-LogInfo "VM CPU Count: $vmCpuCount"
+						if ($vmCpuCount -ne $TestParams.vCpu) {
+							Write-LogErr "Check expected vcpu: $($TestParams.vCpu) actual: ${vmCpuCount}"
+							$CurrentTestResult.TestSummary += New-ResultSummary -testResult "FAIL" `
+								-metaData "vCpu : Check expected vcpu" -checkValues "PASS,FAIL,ABORTED" `
+								-testName $currentTestData.testName
+							Write-LogInfo "Test Result : FAIL."
+							$testResult = "FAIL"
+						} else {
+							Write-LogInfo "Check expected vcpu: $($TestParams.vCpu) actual: ${vmCpuCount}"
+							$CurrentTestResult.TestSummary += New-ResultSummary -testResult "PASS" `
+								-metaData "vCpu : Check expected vcpu" -checkValues "PASS,FAIL,ABORTED" `
+								-testName $currentTestData.testName
+							Write-LogInfo "Test Result : PASS."
+							$testResult = "PASS"
+						}
+					} else {
+						Write-LogInfo "Test Result : PASS."
+						$testResult = "PASS"
+					}
 				} else {
 					$CurrentTestResult.TestSummary += New-ResultSummary -testResult "FAIL" `
 						-metaData "Reboot : Call Trace Verification" -checkValues "PASS,FAIL,ABORTED" `
@@ -65,4 +93,5 @@ function Main {
 	return $currentTestResult
 }
 
-Main -allVMData $AllVmData -CurrentTestData $CurrentTestData -TestProvider $TestProvider
+Main -AllVmData $AllVmData -CurrentTestData $CurrentTestData -TestProvider $TestProvider `
+	-TestParams (ConvertFrom-StringData $TestParams.Replace(";","`n"))
