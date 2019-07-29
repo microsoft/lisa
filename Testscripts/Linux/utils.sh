73b543193b6f21d4db5fe68d57140303e3dd5c8a
Hongxiong Xian (Wicresoft North America Ltd) <v-hoxian@microsoft.com>
2019-07-28 23:28:27 -0700

    STOR-VHDX-RESIZE-GROWFS.ps1: Get OS disk with common function

--- Testscripts/Linux/utils.sh
+++ Testscripts/Linux/utils.sh
@@ -3586,7 +3586,7 @@ function get_OSdisk() {
 	for driveName in /dev/sd*[^0-9];
 	do
 		# Get the OS disk based on "Linux filesystem" string or BootFlag(*) of a partition
-		fdisk -l $driveName | grep -i "Linux filesystem\|/dev/sd[a-z][0-9]\+[ ]*\*" > /dev/null
+		fdisk -l $driveName 2> /dev/null | grep -i "Linux filesystem\|/dev/sd[a-z][0-9]\+[ ]*\*" > /dev/null
 		if [ 0 -eq $? ]; then
 			os_disk=$(echo $driveName | awk -v FS=/ '{print $NF}')
 			break
