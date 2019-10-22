74182fb1b4c01224241dc82739b98a2f7c59eb10
juhlee-microsoft <juhlee@microsoft.com>
2019-10-21 17:06:37 -0700

    device name path changed
    Below command returns two lines now.
    
    find /sys/devices/* -name "*${synthetic_interface}" | grep "pci" | sed 's/\// /g'| awk '{print $12}'
    
    The actual path shows like below with the command, find /sys/devices/* -name "*eth0"
    /sys/devices/LNXSYSTM:00/LNXSYBUS:00/ACPI0004:00/VMBUS:00/0ce8bfb4-39c0-4431-9830-49a806c94fad/pci9830:00/9830:00:02.0/net/eth1/upper_eth0
    /sys/devices/LNXSYSTM:00/LNXSYBUS:00/ACPI0004:00/VMBUS:00/000d3aa2-81af-000d-3aa2-81af000d3aa2/net/eth0
    
    This should be 'print $11'

--- Testscripts/Linux/SR-IOV-Utils.sh
+++ Testscripts/Linux/SR-IOV-Utils.sh
@@ -80,7 +80,7 @@ VerifyVF()
             synthetic_MAC=$(ip link show ${synthetic_interface} | grep ether | awk '{print $2}')
             vf_interface=$(grep -il ${synthetic_MAC} /sys/class/net/*/address | grep -v $synthetic_interface | sed 's/\// /g' | awk '{print $4}')
         else
-            vf_interface=$(find /sys/devices/* -name "*${synthetic_interface}" | grep "pci" | sed 's/\// /g' | awk '{print $12}')
+            vf_interface=$(find /sys/devices/* -name "*${synthetic_interface}" | grep "pci" | sed 's/\// /g' | awk '{print $11}')
         fi
     fi
 
