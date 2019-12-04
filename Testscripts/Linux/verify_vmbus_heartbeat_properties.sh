921694d882c54584a5eca98ed193c2dbf2f74ee7
juhlee-microsoft <juhlee@microsoft.com>
2019-12-04 12:50:57 -0800

    Extend to other distro

--- Testscripts/Linux/verify_vmbus_heartbeat_properties.sh
+++ Testscripts/Linux/verify_vmbus_heartbeat_properties.sh
@@ -22,7 +22,7 @@ UtilsInit
 # Install python 2.7 if missing
 python_path=$(which python)
 if [[ $? -gt 0 ]]; then
-	yum_install python2
+	install_package python2
 	ln -s /usr/bin/python2.7 /usr/bin/python
 fi
 
