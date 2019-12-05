745ade1bf789e5267552e9374aa84fd98f85cb41
juhlee-microsoft <juhlee@microsoft.com>
2019-12-05 15:33:59 -0800

    Update VERIFY-LIS-MODULES-VERSION.sh
    Update VERIFY-LIS-MODULES-VERSION.sh
    
    Update VERIFY-LIS-MODULES-VERSION.sh
    
    Update VERIFY-LIS-MODULES-VERSION.sh
    
    Update VERIFY-LIS-MODULES-VERSION.sh
    
    Update VERIFY-LIS-MODULES-VERSION.sh

--- Testscripts/Linux/VERIFY-LIS-MODULES-VERSION.sh
+++ Testscripts/Linux/VERIFY-LIS-MODULES-VERSION.sh
@@ -12,7 +12,7 @@
 #
 ########################################################################
 
-exit_code="0"
+exit_code=0
 expected_lis_version=''
 min_supported_distro_version="7.3"
 min_supported_LIS_version="4.3.0"
@@ -33,7 +33,7 @@ function check_version_greater_equal() { test "$(echo "$@" | tr " " "\n" | sort 
 # Check if vmbus string is recorded in dmesg
 hv_string=$(dmesg | grep "Vmbus version:")
 if [[ ( $hv_string == "" ) || ! ( $hv_string == *"hv_vmbus:"*"Vmbus version:"* ) ]]; then
-    LogErr "Error! Could not find the VMBus protocol string in dmesg."
+    LogErr "Could not find the VMBus protocol string in dmesg. Test stopped here."
     SetTestStateAborted
     exit 0
 fi
@@ -42,6 +42,7 @@ skip_modules=()
 config_path="/boot/config-$(uname -r)"
 if [[ $(detect_linux_distribution) == clear-linux-os ]]; then
     config_path="/usr/lib/kernel/config-$(uname -r)"
+    LogMsg "Set the configuration path to $config_path"
 fi
 
 declare -A config_modulesDic
@@ -59,7 +60,7 @@ do
     module_included=$(grep $key "$config_path")
     if [ "$module_included" ]; then
         skip_modules+=("${config_modulesDic[$key]}")
-        LogMsg "Info: Skiping ${config_modulesDic[$key]} module as it is built-in."
+        LogMsg "Skipping the built-in modules, ${config_modulesDic[$key]}"
     fi
 done
 
@@ -72,22 +73,32 @@ for module in "${HYPERV_MODULES[@]}"; do
     [[ -n $skip ]] || tempList+=("$module")
 done
 HYPERV_MODULES=("${tempList[@]}")
+LogMsg "Target module names: ${HYPERV_MODULES[*]}"
+
+if [ ! $HYPERV_MODULES ]; then
+    LogErr "Target module is empty or null"
+    exit_code=$((exit_code+1))
+fi
 
 if which rpm 2>/dev/null;then
     rpmAvailable=true
+    isLISInstalled=$(rpm -qa | grep microsoft-hyper-v 2>/dev/null)
+    LogMsg "The current LIS installation state: $isLISInstalled"
 else
     rpmAvailable=false
+    isLISInstalled=''
+    LogMsg "The current LIS installation state: none"
 fi
-
-isLISInstalled=$(rpm -qa | grep microsoft-hyper-v 2>/dev/null)
+LogMsg "RPM availability in the system: $rpmAvailable"
 
 if [ ! -z "$isLISInstalled" ]; then
-    expected_lis_version=$(dmesg | grep 'Vmbus LIS version' | awk -F ':' '{print $3}' | tr -d [:blank:])
+    expected_lis_version=$(dmesg | grep -i 'Vmbus LIS version' | awk -F ':' '{print $3}' | tr -d [:blank:])
     if check_version_greater_equal $DISTRO_VERSION $min_supported_distro_version ; then
         HYPERV_MODULES+=('pci_hyperv')
         pci_module=$(lsmod | grep pci_hyperv)
         if [ -z $pci_module ]; then
             modprobe pci_hyperv
+            LogMsg "pci_hyperv module loaded"
         fi
     fi
     if [[ $DISTRO_VERSION =~ 7\.3 ]] || [[ $DISTRO_VERSION =~ 7\.4 ]] ; then
@@ -96,6 +107,7 @@ if [ ! -z "$isLISInstalled" ]; then
             mlx4_module=$(lsmod | grep mlx4_en)
             if [ -z $mlx4_module ]; then
                 modprobe mlx4_en
+                LogMsg "mlx4_en module loaded"
             fi
         fi
     fi
@@ -107,44 +119,43 @@ for module in "${HYPERV_MODULES[@]}"; do
 
     # Check to see if the module is loaded
     if [[ $load_status =~ $module ]]; then
-        if [ "$rpmAvailable" = true ] ; then
-            if [ ! -z "$isLISInstalled" ]; then
-                version=$(modinfo "$module" | grep version: | head -1 | awk '{print $2}')
-                LogMsg "$module module: ${version}"
-                if [ "$module" == "mlx4_en" ] ;then
-                    if [ "$MLNX_VERSION" != "$version" ] ;then
-                        LogErr "ERROR: Status: $module $version doesnot match with build version $MLNX_VERSION"
-                        exit_code="1"
-                    fi
-                    continue
-                fi
-                if [ "$expected_lis_version" != "$version" ] ;then
-                    LogErr "ERROR: Status: $module $version doesnot match with build version $expected_lis_version"
-                    exit_code="1"
+        LogMsg "LIS nmodule $module loaded successfully"
+        if [ "$rpmAvailable" = true ] && [ ! -z "$isLISInstalled" ]; then
+            version=$(modinfo "$module" | grep version: | head -1 | awk '{print $2}')
+            LogMsg "$module module version: ${version}"
+            if [ "$module" == "mlx4_en" ] ;then
+                if [ "$MLNX_VERSION" != "$version" ] ;then
+                    LogErr "Status: $module $version did not match to the build one, $MLNX_VERSION"
+                    exit_code=$((exit_code+1))
                 fi
                 continue
             fi
+            if [ "$expected_lis_version" != "$version" ] ;then
+                LogErr "Status: $module $version did not match to the build one, $expected_lis_version"
+                exit_code=$((exit_code+1))
+            fi
+            continue
         fi
-        
+
         version=$(modinfo "$module" | grep vermagic: | awk '{print $2}')
         if [[ "$version" == "$(uname -r)" ]]; then
-            LogMsg "Found a kernel matching version for $module module: ${version}"
+            LogMsg "Found the matching kernel version of $module module: ${version}"
         else
-            LogErr "Error: LIS module $module doesn't match the kernel build version!"
-            exit_code="1"
+            LogErr "LIS module $module did not match the kernel build version!"
+            exit_code=$((exit_code+1))
         fi
     else
-         LogErr "Error: LIS module $module is not loaded"
-         exit_code="1"
+         LogErr "LIS module $module was not loaded"
+         exit_code=$((exit_code+1))
     fi
 done
 
-if [ "1" -eq "$exit_code" ]; then
-    LogMsg "Exiting with state: TestAborted."
-    SetTestStateAborted
-else
-    LogMsg "Exiting with state: TestCompleted."
+if [ 0 -eq $exit_code ]; then
+    LogMsg "Exiting with state: $__LIS_TESTCOMPLETED."
     SetTestStateCompleted
+    exit 0
+else
+    LogMsg "Exiting with state: $__LIS_TESTABORTED."
+    SetTestStateAborted
+    exit 1
 fi
-
-exit 0
