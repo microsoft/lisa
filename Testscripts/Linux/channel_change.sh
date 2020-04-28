806d36b5c20a4d1b1c7e676c9ca381f363117f2e
juhlee-microsoft <juhlee@microsoft.com>
2020-04-28 14:18:45 -0700

    Initial check in draft files

---
+++ Testscripts/Linux/channel_change.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache License.
+# This script will set up CPU offline feature with vmbus interrupt channel re-assignment.
+# This feature will be enabled the kernel version 5.7+
+# Select a CPU number where does not associate to vmbus channels; /sys/bus/vmbus/devices/<device ID>/channels/<channel ID>/cpu.
+# Set 1 to online file, echo 1 > /sys/devices/system/cpu/cpu<number>/online
+# Verify the dmesg log like ‘smpboot: Booting Node xx Processor x APIC 0xXX’
+# Set 0 to online file, echo 0 > /sys/devices/system/cpu/cpu<number>/online
+# Verify the dmesg log like ‘smpboot: CPU x is now offline’
+# Select a CPU number where associates to vmbus channels.
+# Set 1 to online file, echo 1 > /sys/devices/system/cpu/cpu<number>/online
+# Verify the command error: Device or resource busy
+# Set 0 to online file, echo 0 > /sys/devices/system/cpu/cpu<number>/online
+# Verify the command error: Device or resource busy
+########################################################################################################
+# Source utils.sh
+. utils.sh || {
+	echo "Error: unable to source utils.sh!"
+	echo "TestAborted" >state.txt
+	exit 0
+}
+# Source constants file and initialize most common variables
+UtilsInit
+
+# Constants/Globals
+# Get distro information
+GetDistro
+
+function Main() {
+
+	if [[ $repo_url != "" ]]; then
+		LogMsg "CPU offline and vmbus interrupt reassignement requires kernel build in the VM until the version 5.7"
+		update_repos
+
+		# Install common packages
+		req_pkg="gcc make flex bison git"
+		install_package $req_pkg
+		LogMsg "$?: Installed the common required packages; $req_pkg"
+
+		case $DISTRO in
+			redhat_7|centos_7|redhat_8|centos_8)
+				req_pkg="elfutils-libelf-devel ncurses-devel bc elfutils-libelf-devel openssl-devel grub2"
+				;;
+			suse*|sles*)
+				req_pkg="ncurses-devel libelf-dev"
+				;;
+			ubuntu*)
+				req_pkg="build-essential fakeroot libncurses5-dev libssl-dev ccache"
+				;;
+			*)
+				LogErr "$DISTRO does not support hibernation"
+				SetTestStateFailed
+				exit 0
+				;;
+		esac
+		install_package $req_pkg
+		LogMsg "$?: Installed required packages, $req_pkg"
+
+		# Start kernel compilation
+		LogMsg "Clone and compile new kernel from $repo_url to /usr/src/linux"
+		git clone $repo_url /usr/src/linux
+		LogMsg "$?: Cloned the kernel source repo in /usr/src/linux"
+
+		cd /usr/src/linux/
+
+		git checkout $repo_branch
+		LogMsg "$?: Changed to $repo_branch"
+
+		cp /boot/config*-azure /usr/src/linux/.config
+		LogMsg "$?: Copied the default config file from /boot"
+
+		yes '' | make oldconfig
+		LogMsg "$?: Did oldconfig make file"
+
+		make -j $(getconf _NPROCESSORS_ONLN)
+		LogMsg "$?: Compiled the source codes"
+
+		make modules_install
+		LogMsg "$?: Installed new kernel modules"
+
+		make install
+		LogMsg "$?: Install new kernel"
+
+		cd
+
+		# Append the test log to the main log files.
+		cat /usr/src/linux/TestExecution.log >> ~/TestExecution.log
+		cat /usr/src/linux/TestExecutionError.log >> ~/TestExecutionError.log
+	fi
+
+	sed -i -e "s/GRUB_HIDDEN_TIMEOUT=*.*/GRUB_HIDDEN_TIMEOUT=30/g" /etc/default/grub.d/50-cloudimg-settings.cfg
+	LogMsg "$?: Updated GRUB_HIDDEN_TIMEOUT value with 30"
+
+	sed -i -e "s/GRUB_TIMEOUT=*.*/GRUB_TIMEOUT=30/g" /etc/default/grub.d/50-cloudimg-settings.cfg
+	LogMsg "$?: Updated GRUB_TIMEOUT value with 30"
+
+	update-grub2
+	LogMsg "$?: Ran update-grub2"
+
+	echo "job_completed=0" >> ~/constants.sh
+	LogMsg "Main job function completed"
+}
+
+# main body
+Main
+cp ~/TestExecution.log ~/Setup-TestExecution.log
+cp ~/TestExecutionError.log ~/Setup-TestExecutionError.log
+SetTestStateCompleted
+exit 0
---
+++ Testscripts/Windows/CPU-Offline.ps1
@@ -0,0 +1,147 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache License.
+<#
+.Synopsis
+	CPU offline feature testing with vmbus interrupt channel reassignment
+
+.Description
+	CPU offline and verify no error in dmesg/syslog
+	CPU offline all except one
+#>
+
+param([object] $AllVmData, [string]$TestParams)
+
+function Main {
+	param($AllVMData, $TestParams)
+	$currentTestResult = Create-TestResultObject
+	try {
+		$testResult = $resultFail
+
+		#region Generate constants.sh
+		# We need to add extra parameters to constants.sh file apart from parameter properties defined in XML.
+		# Hence, we are generating constants.sh file again in test script.
+
+		Write-LogInfo "Generating constants.sh ..."
+		$constantsFile = "$LogDir\constants.sh"
+		foreach ($TestParam in $CurrentTestData.TestParameters.param) {
+			Add-Content -Value "$TestParam" -Path $constantsFile
+			Write-LogInfo "$TestParam added to constants.sh"
+		}
+
+		Write-LogInfo "constants.sh created successfully..."
+		#endregion
+
+		#region Upload files to master VM
+		foreach ($VMData in $AllVMData) {
+			Copy-RemoteFiles -uploadTo $VMData.PublicIP -port $VMData.SSHPort -files "$constantsFile,$($CurrentTestData.files)" -username $user -password $password -upload
+			Write-LogInfo "Copied the script files to the VM"
+		}
+		#endregion
+
+		# New kernel build for CPU channel change and vmbus interrupt re-assignment
+		Run-LinuxCmd -ip $AllVMData.PublicIP -port $AllVMData.SSHPort -username $user -password $password -command "./CPUOfflineKernelBuild.sh" -RunInBackground -runAsSudo -ignoreLinuxExitCode:$true | Out-Null
+		Write-LogInfo "Executing CPUOfflineKernelBuild script inside VM"
+
+		# Wait for kernel compilation completion. 60 min timeout
+		$timeout = New-Timespan -Minutes 60
+		$sw = [diagnostics.stopwatch]::StartNew()
+		while ($sw.elapsed -lt $timeout){
+			$vmCount = $AllVMData.Count
+			Wait-Time -seconds 15
+			$state = Run-LinuxCmd -ip $VMData.PublicIP -port $VMData.SSHPort -username $user -password $password "cat ~/state.txt"
+			if ($state -eq "TestCompleted") {
+				$kernelCompileCompleted = Run-LinuxCmd -ip $VMData.PublicIP -port $VMData.SSHPort -username $user -password $password "cat ~/constants.sh | grep setup_completed=0"
+				if ($kernelCompileCompleted -ne "setup_completed=0") {
+					Write-LogErr "CPUOfflineKernelBuild.sh finished on $($VMData.RoleName) but setup was not successful!"
+				} else {
+					Write-LogInfo "CPUOfflineKernelBuild.sh finished on $($VMData.RoleName)"
+					$vmCount--
+				}
+				break
+			} elseif ($state -eq "TestSkipped") {
+				Write-LogInfo "CPUOfflineKernelBuild.sh finished with SKIPPED state!"
+				$resultArr = $resultSkipped
+				$currentTestResult.TestResult = Get-FinalResultHeader -resultarr $resultArr
+				return $currentTestResult.TestResult
+			} elseif ($state -eq "TestFailed") {
+				Write-LogErr "CPUOfflineKernelBuild.sh didn't finish successfully!"
+				$resultArr = $resultFail
+				$currentTestResult.TestResult = Get-FinalResultHeader -resultarr $resultArr
+				return $currentTestResult.TestResult
+			} elseif ($state -eq "TestAborted") {
+				Write-LogInfo "CPUOfflineKernelBuild.sh finished with Aborted state!"
+				$resultArr = $resultAborted
+				$currentTestResult.TestResult = Get-FinalResultHeader -resultarr $resultArr
+				return $currentTestResult.TestResult
+			} else {
+				Write-LogInfo "CPUOfflineKernelBuild.sh is still running in the VM!"
+			}
+		}
+		if ($vmCount -le 0){
+			Write-LogInfo "CPUOfflineKernelBuild.sh is done successfully"
+		} else {
+			Throw "CPUOfflineKernelBuild.sh didn't finish in the VM!"
+		}
+
+		# Running CPU channel change
+		Run-LinuxCmd -ip $AllVMData.PublicIP -port $AllVMData.SSHPort -username $user -password $password -command "./channel_change.sh" -RunInBackground -runAsSudo -ignoreLinuxExitCode:$true | Out-Null
+		Write-LogInfo "Executed channel_change script inside VM"
+
+		# Wait for kernel compilation completion. 60 min timeout
+		$timeout = New-Timespan -Minutes 60
+		$sw = [diagnostics.stopwatch]::StartNew()
+		while ($sw.elapsed -lt $timeout){
+			$vmCount = $AllVMData.Count
+			Wait-Time -seconds 15
+			$state = Run-LinuxCmd -ip $VMData.PublicIP -port $VMData.SSHPort -username $user -password $password "cat ~/state.txt"
+			if ($state -eq "TestCompleted") {
+				$kernelCompileCompleted = Run-LinuxCmd -ip $VMData.PublicIP -port $VMData.SSHPort -username $user -password $password "cat ~/constants.sh | grep setup_completed=0"
+				if ($kernelCompileCompleted -ne "setup_completed=0") {
+					Write-LogErr "channel_change.sh finished on $($VMData.RoleName) but setup was not successful!"
+				} else {
+					Write-LogInfo "channel_change.sh finished on $($VMData.RoleName)"
+					$vmCount--
+				}
+				break
+			} elseif ($state -eq "TestSkipped") {
+				Write-LogInfo "channel_change.sh finished with SKIPPED state!"
+				$resultArr = $resultSkipped
+				$currentTestResult.TestResult = Get-FinalResultHeader -resultarr $resultArr
+				return $currentTestResult.TestResult
+			} elseif ($state -eq "TestFailed") {
+				Write-LogErr "channel_change.sh didn't finish successfully!"
+				$resultArr = $resultFail
+				$currentTestResult.TestResult = Get-FinalResultHeader -resultarr $resultArr
+				return $currentTestResult.TestResult
+			} elseif ($state -eq "TestAborted") {
+				Write-LogInfo "channel_change.sh finished with Aborted state!"
+				$resultArr = $resultAborted
+				$currentTestResult.TestResult = Get-FinalResultHeader -resultarr $resultArr
+				return $currentTestResult.TestResult
+			} else {
+				Write-LogInfo "channel_change.sh is still running in the VM!"
+			}
+		}
+		if ($vmCount -le 0){
+			Write-LogInfo "channel_change.sh is done"
+		} else {
+			Throw "channel_change.sh didn't finish in the VM!"
+		}
+
+		$testResult = $resultPass
+	} catch {
+		$ErrorMessage =  $_.Exception.Message
+		$ErrorLine = $_.InvocationInfo.ScriptLineNumber
+		Write-LogErr "EXCEPTION : $ErrorMessage at line: $ErrorLine"
+	} finally {
+		if (!$testResult) {
+			$testResult = $resultAborted
+		}
+		$resultArr = $testResult
+	}
+
+	$currentTestResult.TestResult = Get-FinalResultHeader -resultarr $resultArr
+	return $currentTestResult
+}
+
+Main -AllVmData $AllVmData -CurrentTestData $CurrentTestData
