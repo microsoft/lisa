0dc6aae4bf6e58b3bf9cac1fdbd680cdbe324f50
Baihua Lu <balu@microsoft.com>
2020-04-30 17:37:14 +0800

    Add some descriptions for the benchmarks of lkp test results.

--- Testscripts/Windows/PERF-LKP.ps1
+++ Testscripts/Windows/PERF-LKP.ps1
@@ -15,35 +15,57 @@ $LKP_LOG_FILE = "lkp_log.tar"
 $LKP_LOG_DIR = "lkp_log"
 $VERTICAL_FORMAT_TESTS = @("perf-bench-numa-mem", "pmbench", "aim9")
 $MICROBENCHMARK_TEST_NAMES = @("perf-bench-sched-pipe", "perf-bench-numa-mem", "pmbench")
+$BENCHMARK_DESC = @{
+    "hackbench" = "It's main job is to create a specified number of pairs of schedulable entities `
+                  (either threads or traditional processes) which communicate via either sockets `
+                  or pipes and time how long it takes for each pair to send data back and forth. `
+                  100%,1600%,50% as below means the percentage of the threads number and the cpus number";
+    "unixbench" = "The purpose of UnixBench is to provide a basic indicator of the performance of `
+                  a Unix-like system; These test results are then compared to the scores from a `
+                  baseline system to produce an index value. `
+                  '1' as below means the threads number is the same with the cpus number.";
+    "aim9" = "The AIM Independent Resource Benchmark exercises and times each component of a UNIX `
+                  computer system, independently. The benchmark uses 58 subtests to generate absolute processing rates,`
+                  in operations per second, for subsystems, I/O transfers, function calls, and UNIX system calls.";
+    "perf-bench-sched-pipe" = "The loop number is 10000000. It has 'process' and 'threads' mode.";
+    "perf-bench-numa-mem" = "NUMA scheduling and MM benchmarks. The size of processed memory is 300M `
+                  The threads number is 2. ";
+    "pmbench" = "pmbench is a micro-benchmark that profiles system paging performance by measuring `
+                  latencies of each memory access throughout the run and reporting the statistics of measured latencies.`
+                  The benchmark creates a free_mem * 75% GiB memory map and then accesses only the first 128MB with runtime `
+                  is 300s. 0-256% as below means the percentage of the accesses that took between 0~256 ns and all the accesses."}
+$FORMAT_ALIGN_SIZE = 30
 
 function Get-BenchmarkResult ($testName, $jsonResult) {
     switch ($testName) {
         "hackbench" {
-            $resultTitle = "{0,-30} {1,-30}" -f "throughput", "workload"
-            $result = "{0,-30} {1,-30}" -f ${jsonResult}.'hackbench.throughput'[0], ${jsonResult}.'hackbench.workload'[0]
+            $job_desc = "{0,-$FORMAT_ALIGN_SIZE}" -f "(IPC-mode-threads)"
+            $result_desc = "(throughput)"
+            $result = ${jsonResult}.'hackbench.throughput'[0]
         }
         "perf-bench-sched-pipe" {
-            $resultTitle = "ops_per_sec"
+            $job_desc = "{0,-$FORMAT_ALIGN_SIZE}" -f "(loops-mode)"
+            $result_desc = "(ops per second)"
             $result = ${jsonResult}.'perf-bench-sched-pipe.ops_per_sec'[0]
         }
         "unixbench" {
-            $resultTitle = "{0,-30} {1,-30}" -f "score", "workload"
-            $result = "{0,-30} {1,-30}" -f ${jsonResult}.'unixbench.score'[0], ${jsonResult}.'unixbench.workload'[0]
+            $job_desc = "{0,-$FORMAT_ALIGN_SIZE}" -f "(threads-runtime-testname)"
+            $result_desc = "(system benchmarks index score)"
+            $result = ${jsonResult}.'unixbench.score'[0]
         }
         default {
             Write-LogErr "unknow test name of ${testName}"
             return $null,$null
         }
     }
-    return $resultTitle, $result
+    return $job_desc, $result_desc, $result
 }
 
 function Get-VerticalFormat-TestResult ($currentTestResult, $currentTestData, $testName) {
     try {
         $LogPath = Join-Path $LogDir\$LKP_LOG_DIR $testName
         if (-not (Test-Path -Path $LogPath)) {
-            Write-LogErr "EXCEPTION : $LKP_LOG_DIR doesn't exist"
-            return $resultAborted
+            throw- "$LKP_LOG_DIR\$testName doesn't exist"
         }
 
         $items = Get-ChildItem $LogPath
@@ -55,13 +77,6 @@ function Get-VerticalFormat-TestResult ($currentTestResult, $currentTestData, $t
                 return $resultPass
             }
 
-            $metaData = "{0,-30}" -f "$_ Job"
-            $result = $null
-            $item = "{0,-30} {1,30}" -f $metaData, $result
-            Add-Content -Value $item -Path "$LogDir\$LKP_RESULTS"
-            $CurrentTestResult.TestSummary += New-ResultSummary -testResult $result -metaData $metaData `
-                    -checkValues "PASS,FAIL,SKIP" -testName $CurrentTestData.testName
-
             (Get-Content $resultFile) | ConvertFrom-Json > tmp.txt
             $content = Get-Content ".\tmp.txt"
             $content | ForEach-Object {
@@ -70,7 +85,7 @@ function Get-VerticalFormat-TestResult ($currentTestResult, $currentTestData, $t
                     $metaData = "    $($_.split(':')[0])"
                     $result = $_.split(":")[1].split("{}")[1]
 
-                    $item = "{0,-30} {1,30}" -f $metaData, $result
+                    $item = "{0,-$FORMAT_ALIGN_SIZE} {1,$FORMAT_ALIGN_SIZE}" -f $metaData, $result
                     Add-Content -Value $item -Path "$LogDir\$LKP_RESULTS"
                     $CurrentTestResult.TestSummary += New-ResultSummary -testResult $result -metaData $metaData `
                             -checkValues "PASS,FAIL,SKIP" -testName $CurrentTestData.testName
@@ -92,12 +107,10 @@ function Get-VerticalFormat-TestResult ($currentTestResult, $currentTestData, $t
 function Get-HorizontalFormat-TestResult ($currentTestResult, $currentTestData, $testName) {
     try {
         $isResultHead = $true
-        $metaDataTitle = "{0,-30}" -f "Jobs"
 
         $LogPath = Join-Path $LogDir\$LKP_LOG_DIR $testName
         if (-not (Test-Path -Path $LogPath)) {
-            Write-LogErr "EXCEPTION : $LKP_LOG_DIR\$testName doesn't exist"
-            return $resultAborted
+            throw "$LKP_LOG_DIR\$testName doesn't exist"
         }
 
         $items = Get-ChildItem $LogPath
@@ -109,18 +122,18 @@ function Get-HorizontalFormat-TestResult ($currentTestResult, $currentTestData, 
                 return $resultPass
             }
             $jsonResult = (Get-Content $resultFile) | ConvertFrom-Json
-            $metaData = "{0,-30}" -f $_
-            $resultTitle, $result = Get-BenchmarkResult -testName $testName -jsonResult $jsonResult
+            $metaData = "{0,-$FORMAT_ALIGN_SIZE}" -f $_
+            $job_desc, $result_desc, $result = Get-BenchmarkResult -testName $testName -jsonResult $jsonResult
 
             if ($isResultHead) {
                 $isResultHead = $false
-                $item = "{0,-30} {1,30}" -f $metaDataTitle, $resultTitle
+                $item = "{0,-$FORMAT_ALIGN_SIZE} {1,$FORMAT_ALIGN_SIZE}" -f $job_desc, $result_desc
                 Add-Content -Value $item -Path "$LogDir\$LKP_RESULTS"
-                $CurrentTestResult.TestSummary += New-ResultSummary -testResult $resultTitle -metaData $metaDataTitle `
+                $CurrentTestResult.TestSummary += New-ResultSummary -testResult $result_desc -metaData $job_desc `
                         -checkValues "PASS,FAIL,SKIP" -testName $CurrentTestData.testName
             }
 
-            $item = "{0,-30} {1,30}" -f $metaData, $result
+            $item = "{0,-$FORMAT_ALIGN_SIZE} {1,$FORMAT_ALIGN_SIZE}" -f $metaData, $result
             Add-Content -Value $item -Path "$LogDir\$LKP_RESULTS"
             $CurrentTestResult.TestSummary += New-ResultSummary -testResult $result -metaData $metaData `
                         -checkValues "PASS,FAIL,SKIP" -testName $CurrentTestData.testName
@@ -154,9 +167,16 @@ function Get-TestResult ($currentTestResult, $currentTestData) {
         }
 
         $testNameSet | ForEach-Object {
-            $metaData = "{0,-30}" -f $_.ToUpper()
-            $result = $null
-            $item = "{0,-30} {1,30}" -f $metaData, $result
+            $metaData = "`n`tBenchmark Name"
+            $result = $_
+            $item = "$metaData : $result"
+            Add-Content -Value $item -Path "$LogDir\$LKP_RESULTS"
+            $CurrentTestResult.TestSummary += New-ResultSummary -testResult $result -metaData $metaData `
+                    -checkValues "PASS,FAIL,SKIP" -testName $CurrentTestData.testName
+
+            $metaData = "Description"
+            $result = "$($BENCHMARK_DESC[$_])`n"
+            $item = "$metaData : $result"
             Add-Content -Value $item -Path "$LogDir\$LKP_RESULTS"
             $CurrentTestResult.TestSummary += New-ResultSummary -testResult $result -metaData $metaData `
                     -checkValues "PASS,FAIL,SKIP" -testName $CurrentTestData.testName
