d0a55931feb4e087fd0d8d38a20a5c5436b70e1a
juhlee-microsoft <juhlee@microsoft.com>
2020-05-13 17:08:25 -0700

    Fixed the format error

--- Testscripts/Linux/CPUOfflineKernelBuild.sh
+++ Testscripts/Linux/CPUOfflineKernelBuild.sh
@@ -18,7 +18,7 @@
 # Source utils.sh
 . utils.sh || {
 	echo "Error: unable to source utils.sh!"
-	echo "TestAborted" >state.txt
+	echo "TestAborted" > state.txt
 	exit 0
 }
 # Source constants file and initialize most common variables
