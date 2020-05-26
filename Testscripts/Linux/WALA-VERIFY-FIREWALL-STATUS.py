82d275776bf69cc324ec7bddc89187e43ae4e940
Lili Deng <lildeng@microsoft.com>
2020-05-26 12:00:13 +0800

    Fix logic of WALA FireWall test case.

--- Testscripts/Linux/WALA-VERIFY-FIREWALL-STATUS.py
+++ Testscripts/Linux/WALA-VERIFY-FIREWALL-STATUS.py
@@ -2,23 +2,106 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the Apache License.
 from azuremodules import *
+import os
+import pwd
+import re
+import subprocess
+import sys
+
+if sys.version_info[0]== 3:
+    import http.client as httpclient
+elif sys.version_info[0] == 2:
+    import httplib as httpclient
+
+FIREWALL_USER = os.environ["SUDO_USER"]
+EXECUTION_USER = "root"
+WIRESERVER_ENDPOINT_FILE = '/var/lib/waagent/WireServerEndpoint'
+VERSIONS_PATH = '/?comp=versions'
+AGENT_CONFIG_FILE = '/etc/waagent.conf'
+OS_ENABLE_FIREWALL_RX = r'OS.EnableFirewall\s*=\s*(\S+)'
+
+
+def is_firewall_enabled():
+    with open(AGENT_CONFIG_FILE, 'r') as config_fh:
+        for line in config_fh.readlines():
+            if not line.startswith('#'):
+                update_match = re.match(OS_ENABLE_FIREWALL_RX, line, re.IGNORECASE)
+                if update_match:
+                    return update_match.groups()[0].lower() == 'y'
+
+    # The firewall is enabled by default.
+    return True
+
+
+def run(*args):
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    rc = p.wait()
+    if rc != 0:
+        return False, None
+    else:
+        o = list(map(lambda s: s.decode('utf-8').strip(), p.stdout.read()))
+        return True, o
 
 
 def RunTest():
     UpdateState("TestRunning")
-    RunLog.info("Checking firewall status using iptables...")
-    output = Run("iptables -L > iptables.txt")
-    output = Run("cat iptables.txt")
-    dropRulesCount = GetStringMatchCount("iptables.txt", "policy DROP")
-    dropItemsCount = GetStringMatchCount("iptables.txt", "DROP")
-
-    if (dropRulesCount <= 0 and dropItemsCount <= 0) :
-        RunLog.info('No iptables DROP rules found, and Firewall is disabled. iptables output is: %s', output)
+    if not is_firewall_enabled():
+        RunLog.info("The firewall is not enabled, skipping checks")
         ResultLog.info('PASS')
         UpdateState("TestCompleted")
-    else :
-        RunLog.info('A few iptables DROP rules found, looks like firewall is enabled. iptables output is: %s', output)
+        return
+
+    try:
+        with open(WIRESERVER_ENDPOINT_FILE, 'r') as f:
+            wireserver_ip = f.read()
+    except Exception as e:
+        RunLog.error("unable to read wireserver ip: {0}".format(e))
         ResultLog.error('FAIL')
         UpdateState("TestCompleted")
+        return
+
+    try:
+        uid = pwd.getpwnam(FIREWALL_USER)[2]
+        os.seteuid(uid)
+    except Exception as e:
+        RunLog.error("Error -- failed to switch users: {0}".format(e))
+        ResultLog.error('FAIL')
+        UpdateState("TestCompleted")
+        return
+
+    try:
+        client = httpclient.HTTPConnection(wireserver_ip, timeout=1)
+    except Exception as e:
+        RunLog.error("Error -- failed to create HTTP connection: {0}".format(e))
+        ResultLog.error('FAIL')
+        UpdateState("TestCompleted")
+        return
+
+    try:
+        client.request('GET', VERSIONS_PATH)
+        success = True
+    except Exception as e:
+        success = False
+
+    if success:
+        RunLog.error("Error -- user could connect to wireserver")
+        ResultLog.error('FAIL')
+        UpdateState("TestCompleted")
+        return
+
+    RunLog.info("Success -- user access to wireserver is blocked")
+    ResultLog.info('PASS')
+
+    # Set current user back, otherwise no permission to write state.txt file
+    try:
+        uid = pwd.getpwnam(EXECUTION_USER)[2]
+        os.seteuid(uid)
+    except Exception as e:
+        RunLog.error("Error -- failed to switch users: {0}".format(e))
+        ResultLog.error('FAIL')
+        UpdateState("TestCompleted")
+        return
+
+    UpdateState("TestCompleted")
 
 RunTest()
