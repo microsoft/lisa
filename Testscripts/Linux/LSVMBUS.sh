642f003f46c7fd0d70a3bccaff46d28cbffb1293
Ju-Hyoung Lee <juhlee@microsoft.com>
2020-05-27 15:25:12 -0700

    Update LSVMBUS.sh

--- Testscripts/Linux/LSVMBUS.sh
+++ Testscripts/Linux/LSVMBUS.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the Apache License.
+# TEST ONLY
 
 tokens=("Operating system shutdown" "Time Synchronization" "Heartbeat"
         "Data Exchange" "mouse" "keyboard"
