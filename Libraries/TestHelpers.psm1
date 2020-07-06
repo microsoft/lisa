523d34385cbfd2268ec8e12e083c9a5623709ad6
Liz Zhang <lizzha@microsoft.com>
2020-07-05 18:12:12 -0700

    Fix issue

--- Libraries/TestHelpers.psm1
+++ Libraries/TestHelpers.psm1
@@ -372,7 +372,7 @@ Function Run-LinuxCmd([string] $username, [string] $password, [string] $ip, [str
 		}
 	}
 	if ($global:sshPrivateKey) {
-		Write-LogDbg ".\Tools\plink.exe -ssh -t -i ppkfile -P $port $username@$ip $logCommand"
+		Write-LogDbg ".\Tools\plink.exe -ssh -t -i $sshKey -P $port $username@$ip $logCommand"
 	} else {
 		Write-LogDbg ".\Tools\plink.exe -ssh -t -pw $password -P $port $username@$ip $logCommand"
 	}
@@ -390,7 +390,7 @@ Function Run-LinuxCmd([string] $username, [string] $password, [string] $ip, [str
 			{ `
 				$username = $args[1]; $password = $args[2]; $ip = $args[3]; $port = $args[4]; $jcommand = $args[5]; $sshKey = $args[6];`
 				Set-Location $args[0]; `
-				if ($sshKey) { .\Tools\plink.exe -ssh -C -v -i $sshKey -P $port $username@$ip $jcommand; } `
+				if ($sshKey) { Write-Output "y" | .\Tools\plink.exe -ssh -C -v -i $sshKey -P $port $username@$ip $jcommand; } `
 				else { .\Tools\plink.exe -ssh -C -v -pw $password -P $port $username@$ip $jcommand; } `
 			} `
 			-ArgumentList $currentDir, $username, $password, $ip, $port, $linuxCommand, $sshKey
