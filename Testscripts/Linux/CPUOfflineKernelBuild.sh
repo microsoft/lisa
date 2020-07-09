2084ff98d7323832dd8400412e31ccfb9ff175f9
Ju-Hyoung Lee <juhlee@microsoft.com>
2020-07-08 23:19:05 -0700

    New test development - CPU-OFFLINE-STORAGE-WORKLOAD (#894)
    * Initial draft of new test development
    
        1. Defined a new test in xml file, CPU-OFFLINE-STORAGE-WORKLOAD.
        2. Added steps for disk addition in Azure VM, and workload script in the PS1 script body.
        3. Relocated the VM restart step to the end.
        4. Added steps for disk addition in Bash script.
    Adjust the run-time flow
        1. Changed fio command with different job numbers
        2. Increased the wait-time from 15 to 30 seconds
        3. Added the log print
    
    * Fixed from PR comments
    
    Added LogMsg line for $ret printing
    Remved the duplicated variable definition.
    
    * Added kernel compilation fix for redhat/centos
    
    RHEL/CentOS have different disk layout. It blocks the customized kernel compilation.
    Moved fles to common required package as SUSE need it too.

--- Testscripts/Linux/CPUOfflineKernelBuild.sh
+++ Testscripts/Linux/CPUOfflineKernelBuild.sh
@@ -22,12 +22,57 @@ function Main() {
 
 	basedir=$(pwd)
 
+	# RHEL/CentOS need an extra disk for customized kernel compilation
+	if [[ $storage == "yes" ]] || [[ $DISTRO == *"redhat"* ]] || [[ $DISTRO == *"centos"* ]]; then
+		# Set up new disk
+		for key in n p 1 2048 '' t 1 p w
+		do
+			echo $key >> keys.txt
+		done
+		LogMsg "Generated the keys.txt file for fdisk commanding"
+
+		cat keys.txt | fdisk /dev/sdc
+		LogMsg "$?: Executed fdisk command"
+
+		partprobe
+		LogMsg "$?: Updated the kernel with the change"
+
+		ret=$(ls /dev/sd*)
+		LogMsg "$?: Listed out /dev/sd* - $ret"
+
+		mkfs -t ext4 /dev/sdc1
+		LogMsg "$?: Wrote a file system to the partition"
+
+		mkdir -p $basedir/data
+		chmod 777 $basedir/data
+		LogMsg "$?: Created a new directory"
+
+		mount /dev/sdc1 $basedir/data
+		LogMsg "$?: Mounted the disk partition to the data directory"
+
+		ext_uuid=$(blkid | grep -i ext4 | awk '{print $2}' | tr -d " " | sed 's/"//g' | tail -n 1)
+		LogMsg "$?: Found the disk space UUID: $ext_uuid"
+		if [[ -z "$ext_uuid" ]];then
+			LogErr "New space disk UUID is empty. Abort the test."
+			SetTestStateAborted
+			exit 0
+		fi
+
+		chmod 766 /etc/fstab
+
+		echo $ext_uuid $basedir/data ext4 defaults,nofail 1 2 >> /etc/fstab
+		LogMsg "$?: Updated /etc/fstab file with uuid information"
+		ret=$(cat /etc/fstab)
+		LogMsg "$ret"
+		LogMsg "$?: Displayed the contents in /etc/fstab"
+	fi
+
 	if [[ $repo_url != "" ]]; then
 		LogMsg "CPU offline and vmbus interrupt reassignement requires kernel build in the VM until the version 5.7"
 		update_repos
 
 		# Install common packages
-		req_pkg="gcc make bison git"
+		req_pkg="gcc make bison git flex"
 		install_package $req_pkg
 		LogMsg "$?: Installed the required common packages; $req_pkg"
 
@@ -39,7 +84,7 @@ function Main() {
 				req_pkg="ncurses-devel libopenssl-devel libelf-devel"
 				;;
 			ubuntu*)
-				req_pkg="build-essential fakeroot libncurses5-dev libssl-dev ccache flex dkms"
+				req_pkg="build-essential fakeroot libncurses5-dev libssl-dev ccache dkms"
 				if [[ "${DISTRO_VERSION}" == "16.04" ]]; then
 					req_pkg="${req_pkg} bc libelf-dev"
 				fi
@@ -54,28 +99,42 @@ function Main() {
 		LogMsg "$?: Installed required packages, $req_pkg"
 
 		# Start kernel compilation
+		# RHEL & CentOS need extra space for kernel repo due to different disk partitioning.
 		LogMsg "Clone and compile new kernel from $repo_url"
-		git clone $repo_url linux
+		if [[ $DISTRO == *"redhat"* ]] || [[ $DISTRO == *"centos"* ]]; then
+			git clone $repo_url data/linux
+			cd data/linux
+		else
+			git clone $repo_url linux
+			cd linux
+		fi
 		LogMsg "$?: Cloned the kernel source repo"
-
-		cd linux
 
 		git checkout $repo_branch
 		LogMsg "$?: Changed to $repo_branch"
 
 		config_file="/boot/config-$(uname -r)"
 
-		cp $config_file $basedir/linux/.config
+		if [[ $DISTRO == *"redhat"* ]] || [[ $DISTRO == *"centos"* ]]; then
+			cp $config_file $basedir/data/linux/.config
+		else
+			cp $config_file $basedir/linux/.config
+		fi
 		LogMsg "$?: Copied the default config file from /boot"
 
 		if [[ $DISTRO == "redhat_8" ]]; then
 			# comment out those 2 parameters in RHEL 8.x
-			sed -i -e "s/CONFIG_SYSTEM_TRUSTED_KEY*.*/#CONFIG_SYSTEM_TRUSTED_KEY/g" ~/.config
-			sed -i -e "s/CONFIG_MODULE_SIG_KEY*.*/#CONFIG_MODULE_SIG_KEY/g" ~/.config
+			sed -i -e "s/CONFIG_SYSTEM_TRUSTED_KEY*.*/#CONFIG_SYSTEM_TRUSTED_KEY/g" .config
+			sed -i -e "s/CONFIG_MODULE_SIG_KEY*.*/#CONFIG_MODULE_SIG_KEY/g" .config
 		fi
 
-		yes '' | make oldconfig
-		LogMsg "Did make oldconfig file"
+		if [[ $DISTRO == *"redhat"* ]] || [[ $DISTRO == *"centos"* ]]; then
+			yes '' | make prepare
+			LogMsg "Did make prepare"
+		else
+			yes '' | make oldconfig
+			LogMsg "Did make oldconfig"
+		fi
 
 		make -j $(getconf _NPROCESSORS_ONLN)
 		LogMsg "Compiled the source codes"
@@ -91,10 +150,17 @@ function Main() {
 			LogMsg "$?: Ran update-grub2"
 		fi
 
-		cp $basedir/TestExecution.log $basedir/Setup-TestExecution.log
-		cat ./TestExecution.log >> $basedir/Setup-TestExecution.log
-		cp $basedir/TestExecutionError,log $basedir/Setup-TestExecutionError.log
-		cat ./TestExecutionError.log >> $basedir/Setup-TestExecutionError.log
+		if [ -f ./TestExecution.log ]; then
+			cp $basedir/TestExecution.log $basedir/Setup-TestExecution.log
+			chmod 766 $basedir/Setup-TestExecution.log
+			cat ./TestExecution.log >> $basedir/Setup-TestExecution.log
+		fi
+		if [ -f ./TestExecutionError.log ]; then
+			cp $basedir/TestExecutionError.log $basedir/Setup-TestExecutionError.log
+			chmod 766 $basedir/Setup-TestExecutionError.log
+			cat ./TestExecutionError.log >> $basedir/Setup-TestExecutionError.log
+		fi
+		cd $basedir
 	fi
 
 	echo "setup_completed=0" >> $basedir/constants.sh
--- Testscripts/Windows/CPU-Offline.ps1
+++ Testscripts/Windows/CPU-Offline.ps1
@@ -12,6 +12,8 @@
 	Reboot VM and repeat above steps for a few times, if this is stress mode.
 	Handle the offline cpu assignment to the vmbus channel in negative test.
 	It's optional to have no VM reboot.
+	TODO: Find utilization, throughput or latency values parsing from workload output
+	and compare those results during CPU offline/vmbus interrupt change.
 #>
 
 param([object] $AllVmData, [string]$TestParams)
@@ -28,6 +30,8 @@ function Main {
 
 	try {
 		$testResult = $resultFail
+		$isStorageWorkloadEnable = 0
+		$azureSyncSecond = 30
 
 		# Find the local test script
 		foreach ($TestScript in $CurrentTestData.files) {
@@ -51,9 +55,48 @@ function Main {
 				# Overwrite if vm_reboot parameter is set
 				$vm_reboot = [string]($TestParam.Replace("vm_reboot=", "").Trim('"'))
 			}
+			if ($TestParam -imatch "storage=yes") {
+				# Overwrite if storage is set
+				$isStorageWorkloadEnable = 1
+			}
 		}
 		Write-LogInfo "constants.sh created successfully..."
 		#endregion
+
+		if ($isStorageWorkloadEnable -eq 1) {
+			Write-LogDbg "Prepare a disk space for VM $($AllVMData[0].RoleName) in RG $($AllVMData[0].ResourceGroupName)."
+			# region Prepare the disk space in the target VM
+			$rgName = $AllVMData[0].ResourceGroupName
+			$vmName = $AllVMData[0].RoleName
+			$location = $AllVMData[0].Location
+			$storageType = 'StandardSSD_LRS'
+			$dataDiskName = $vmName + '_datadisk1'
+			#endregion
+
+			#region Add a new swap disk to Azure VM
+			$diskConfig = New-AzDiskConfig -SkuName $storageType -Location $location -CreateOption Empty -DiskSizeGB 1024
+			$dataDisk1 = New-AzDisk -DiskName $dataDiskName -Disk $diskConfig -ResourceGroupName $rgName
+
+			$vm = Get-AzVM -Name $AllVMData[0].RoleName -ResourceGroupName $rgName
+			Start-Sleep -s $azureSyncSecond
+			$vm = Add-AzVMDataDisk -VM $vm -Name $dataDiskName -CreateOption Attach -ManagedDiskId $dataDisk1.Id -Lun 1
+			Start-Sleep -s $azureSyncSecond
+
+			$ret_val = Update-AzVM -VM $vm -ResourceGroupName $rgName
+			Write-LogInfo "Updated the VM with a new data disk"
+			Write-LogInfo "Waiting for $azureSyncSecond seconds for configuration sync"
+			# Wait for disk sync with Azure host
+			Start-Sleep -s $azureSyncSecond
+
+			# Verify the new data disk addition
+			if ($ret_val.IsSuccessStatusCode) {
+				Write-LogInfo "Successfully add a new disk to the Resource Group, $rgName"
+			} else {
+				Write-LogErr "Failed to add a new disk to the Resource Group, $rgname"
+				throw "Failed to add a new disk"
+			}
+			#endregion
+		}
 
 		#region Upload files to master VM
 		foreach ($VMData in $AllVMData) {
@@ -64,7 +107,7 @@ function Main {
 
 		# ##################################################################################
 		# New kernel build for CPU channel change and vmbus interrupt re-assignment
-		Run-LinuxCmd -ip $AllVMData.PublicIP -port $AllVMData.SSHPort -username $user -password $password -command "./CPUOfflineKernelBuild.sh" -RunInBackground -runAsSudo -ignoreLinuxExitCode:$true | Out-Null
+		Run-LinuxCmd -ip $AllVMData.PublicIP -port $AllVMData.SSHPort -username $user -password $password -command "bash CPUOfflineKernelBuild.sh" -RunInBackground -runAsSudo -ignoreLinuxExitCode:$true | Out-Null
 		Write-LogInfo "Executing CPUOfflineKernelBuild script inside VM"
 
 		# Wait for kernel compilation completion. 60 min timeout
@@ -75,7 +118,7 @@ function Main {
 			Wait-Time -seconds 15
 			$state = Run-LinuxCmd -ip $VMData.PublicIP -port $VMData.SSHPort -username $user -password $password -command "cat ~/state.txt"
 			if ($state -eq "TestCompleted") {
-				$kernelCompileCompleted = Run-LinuxCmd -ip $VMData.PublicIP -port $VMData.SSHPort -username $user -password $password -command "cat ~/constants.sh | grep setup_completed=0" -runAsSudo
+				$kernelCompileCompleted = Run-LinuxCmd -ip $VMData.PublicIP -port $VMData.SSHPort -username $user -password $password -command "cat ~/constants.sh | grep setup_completed=0"
 				if ($kernelCompileCompleted -ne "setup_completed=0") {
 					Write-LogErr "CPUOfflineKernelBuild.sh finished on $($VMData.RoleName) but setup was not successful!"
 				} else {
@@ -99,24 +142,55 @@ function Main {
 		if ($vmCount -le 0){
 			Write-LogInfo "CPUOfflineKernelBuild.sh is done successfully"
 		} else {
-			Throw "CPUOfflineKernelBuild.sh didn't finish in the VM!"
+			throw "CPUOfflineKernelBuild.sh didn't finish in the VM!"
+		}
+
+		# Load the target VM
+		Write-LogInfo "Rebooting VM!"
+		$TestProvider.RestartAllDeployments($AllVMData)
+
+		# Run the workload if defined
+		# setting up the storage workload script
+		if ($isStorageWorkloadEnable -eq 1) {
+			$workCommand = @"
+source utils.sh
+SetTestStateRunning
+for jn in 1 2 3 4 5 6 7 8 9 10
+do
+fio --size=1G --name=workload --direct=1 --ioengine=libaio --filename=./data/fiodata --overwrite=1 --readwrite=readwrite --bs=1M --iodepth=128 --numjobs=`$jn --runtime=300 --output-format=json+ --output=./data/workload.json
+rm -f ./data/fiodata
+sync
+echo 3 > /proc/sys/vm/drop_caches
+done
+SetTestStateCompleted
+"@
+			Set-Content "$LogDir\workCommand.sh" $workCommand
+
+			$setupcommand = @"
+source utils.sh
+update_repos
+install_package "fio"
+"@
+			Set-Content "$LogDir\setup.sh" $setupcommand
+			#endregion
+
+			#region Upload files to VM
+			foreach ($VMData in $AllVMData) {
+				Copy-RemoteFiles -uploadTo $VMData.PublicIP -port $VMData.SSHPort -files "$constantsFile,$($CurrentTestData.files),$LogDir\*.sh" -username $user -password $password -upload
+				Write-LogInfo "Copied the script files to the VM"
+				Run-LinuxCmd -ip $VMData.PublicIP -port $VMData.SSHPort -username $user -password $password -command "bash setup.sh" -runAsSudo
+			}
+			#endregion
 		}
 
 		for ($loopCount = 1;$loopCount -le $max_stress_count;$loopCount++) {
-			if (($vm_reboot -eq "yes") -or ($loopCount -eq 1)) {
-				# ##################################################################################
-				# Reboot VM
-				Write-LogInfo "Rebooting VM! - Loop Count: $loopCount"
-				$TestProvider.RestartAllDeployments($AllVMData)
-			} else {
-				# ##################################################################################
-				# No reboot but wait for 60 seconds for settling down
-				Write-LogInfo "Loop Count: $loopCount"
-				Start-Sleep -second 60
+			if ($isStorageWorkloadEnable -eq 1) {
+				Write-LogInfo "Running workload command in the background job"
+				Run-LinuxCmd -ip $AllVMData[0].PublicIP -port $AllVMData[0].SSHPort -username $user -password $password -command "bash workCommand.sh" -RunInBackground -runAsSudo -ignoreLinuxExitCode:$true | Out-Null
 			}
 			# Feature test and stress test case with $local_script
 			# Running the local test script
-			Run-LinuxCmd -ip $AllVMData.PublicIP -port $AllVMData.SSHPort -username $user -password $password -command "./$local_script" -RunInBackground -runAsSudo -ignoreLinuxExitCode:$true | Out-Null
+			Run-LinuxCmd -ip $AllVMData[0].PublicIP -port $AllVMData[0].SSHPort -username $user -password $password -command "./$local_script" -RunInBackground -runAsSudo -ignoreLinuxExitCode:$true | Out-Null
 			Write-LogInfo "Executed $local_script script inside VM"
 
 			# Wait for kernel compilation completion. 60 min timeout
@@ -124,10 +198,10 @@ function Main {
 			$sw = [diagnostics.stopwatch]::StartNew()
 			while ($sw.elapsed -lt $timeout){
 				$vmCount = $AllVMData.Count
-				Wait-Time -seconds 15
+				Wait-Time -seconds 30
 				$state = Run-LinuxCmd -ip $VMData.PublicIP -port $VMData.SSHPort -username $user -password $password -command "cat ~/state.txt"
 				if ($state -eq "TestCompleted") {
-					$channelChangeCompleted = Run-LinuxCmd -ip $VMData.PublicIP -port $VMData.SSHPort -username $user -password $password -command "cat ~/constants.sh | grep job_completed=0" -runAsSudo
+					$channelChangeCompleted = Run-LinuxCmd -ip $VMData.PublicIP -port $VMData.SSHPort -username $user -password $password -command "cat ~/constants.sh | grep job_completed=0"
 					if ($channelChangeCompleted -ne "job_completed=0") {
 						throw "$local_script finished on $($VMData.RoleName) but job was not successful!"
 					} else {
@@ -154,11 +228,25 @@ function Main {
 				Throw "$local_script didn't finish in the VM!"
 			}
 
+			if ($vm_reboot -eq "yes") {
+				# ##################################################################################
+				# Reboot VM
+				Write-LogInfo "Rebooting VM! - Loop Count: $loopCount"
+				$TestProvider.RestartAllDeployments($AllVMData)
+			} else {
+				# ##################################################################################
+				# No reboot but wait for 60 seconds for settling down
+				Write-LogInfo "Loop Count: $loopCount"
+				Start-Sleep -second 60
+			}
+
 			# Revert state.txt and remove job_completed=0
-			$state = Run-LinuxCmd -ip $VMData.PublicIP -port $VMData.SSHPort -username $user -password $password -command "cat /dev/null > ~/state.txt" -runAsSudo
-			$state = Run-LinuxCmd -ip $VMData.PublicIP -port $VMData.SSHPort -username $user -password $password -command "sed -i -e 's/job_completed=0//g' ~/constants.sh" -runAsSudo
+			$state = Run-LinuxCmd -ip $VMData.PublicIP -port $VMData.SSHPort -username $user -password $password -command "chmod 766 state.txt" -runAsSudo
+			$state = Run-LinuxCmd -ip $VMData.PublicIP -port $VMData.SSHPort -username $user -password $password -command "cat /dev/null > state.txt" -runAsSudo
+			$state = Run-LinuxCmd -ip $VMData.PublicIP -port $VMData.SSHPort -username $user -password $password -command "sed -i -e 's/job_completed=0//g' constants.sh" -runAsSudo
 		}
 		$testResult = $resultPass
+		Write-LogInfo "The test passed"
 	} catch {
 		$ErrorMessage =  $_.Exception.Message
 		$ErrorLine = $_.InvocationInfo.ScriptLineNumber
