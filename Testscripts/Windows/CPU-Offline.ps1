472ae2d01b74c6018ec184ae131e5ba2f36055cb
Ju-Hyoung Lee <juhlee@microsoft.com>
2020-07-13 13:38:59 -0700

    Update CPU-Offline.ps1

--- Testscripts/Windows/CPU-Offline.ps1
+++ Testscripts/Windows/CPU-Offline.ps1
@@ -182,10 +182,9 @@ SetTestStateCompleted
 			$targetIPAddress = $AllVMData[1].InternalIP
 			$workCommand = @"
 source utils.sh
-nc=$(nproc)
 for jn in 1 2 3 4 5 6 7 8 9 10
 do
-iperf3 -c $targetIPAddress -t 60 -P $nc >> workload.json
+iperf3 -c $targetIPAddress -t 60 -P 32 >> workload.json
 done
 "@
 			Set-Content "$LogDir\workCommand.sh" $workCommand
