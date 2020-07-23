41f89ef139f2020ab813aca8af01ff95a32be14a
Prashant Agrawal <pra@microsoft.com>
2020-07-23 13:43:31 -0700

    Fix SSH key read issue

--- Libraries/TestHelpers.psm1
+++ Libraries/TestHelpers.psm1
@@ -733,7 +733,7 @@ Function Get-LISAv2Tools($XMLSecretFile) {
 	# Copy required binary files to working folder
 	$CurrentDirectory = Get-Location
 	$CmdArray = @('7za.exe','dos2unix.exe','gawk','jq','plink.exe','pscp.exe', `
-					'kvp_client32','kvp_client64','nc.exe','lz4.exe')
+					'kvp_client32','kvp_client64','nc.exe','lz4.exe', 'sbinfo')
 
 	if ($XMLSecretFile) {
 		$WebClient = New-Object System.Net.WebClient
@@ -763,7 +763,7 @@ Function Get-SSHKey ($XMLSecretFile) {
 	if ($XMLSecretFile) {
 		$WebClient = New-Object System.Net.WebClient
 		$xmlSecret = [xml](Get-Content $XMLSecretFile)
-		$privateSSHKey = $xmlSecret.secrets.sshPrivateKey.InnerText
+		$privateSSHKey = $xmlSecret.secrets.sshPrivateKey
 		if ($privateSSHKey) {
 			$sshKeyPath = $privateSSHKey
 		}
