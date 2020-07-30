5a30dd7e2a339891541f6750e8607e541ac72954
Abhishek Marathe <abmarath@microsoft.com>
2020-07-30 16:23:55 -0700

    PERF-XDP-TCP-NTTTCP: New testcase to verify xdp throughput performance

--- Testscripts/Linux/perf_ntttcp.sh
+++ Testscripts/Linux/perf_ntttcp.sh
@@ -54,7 +54,12 @@ if [ ! "${client}" ]; then
 	UpdateTestState $ICA_TESTABORTED
 	exit 1
 fi
-
+if [ ! "${testServerIP}" ];then
+	LogMsg "testServerIP not defined. Using server ip: ${server} for testing"
+	testServerIP=$server
+else
+	LogMsg "testServerIP is ${testServerIP}. Ntttcp sender and receiver will use this ip"
+fi
 if [ ! "${testDuration}" ]; then
 	errMsg="Please add/provide value for testDuration in constants.sh. testDuration=60"
 	LogMsg "${errMsg}"
@@ -215,7 +220,7 @@ Get_VFName()
 	# -K changed into --show-nic-packets, -I changed into --show-dev-interrupts in master branch
 	if [ $currentVersion ] && ( [ $currentVersion \> "1.4.0" ] || [ $currentVersion == "1.4.0" ] || [ $currentVersion == "master" ] ); then
 		vf_interface=$(ssh "$ip" lshw -c network -businfo | grep -i "Virtual Function" | awk '{print $2}')
-		if [ $vf_interface ]; then
+		if [[ $vf_interface ]]; then
 			if [[ $currentVersion == "master" ]]; then
 				ntttcp_cmd="$ntttcp_cmd --show-nic-packets $vf_interface --show-dev-interrupts mlx"
 			else
@@ -238,11 +243,11 @@ Run_Ntttcp()
 	data_loss=0
 	Kill_Process "${server}" ntttcp
 	Kill_Process "${client}" ntttcp
-	
+
 	# Disable firewalld
 	Run_SSHCommand "${client}" "service firewalld stop"
 	Run_SSHCommand "${server}" "service firewalld stop"
-	
+
 	Run_SSHCommand "${server}" "mkdir -p ${log_folder}"
 	Run_SSHCommand "${client}" "mkdir -p ${log_folder}"
 	result_file="${log_folder}/report.csv"
@@ -283,22 +288,22 @@ Run_Ntttcp()
 			tx_log_prefix="sender-${testType}-${bufferLength}k-p${num_threads_P}X${num_threads_n}.log"
 			rx_log_prefix="receiver-${testType}-${bufferLength}k-p${num_threads_P}X${num_threads_n}.log"
 			run_msg="Running ${testType} ${bufferLength}k Test: $current_test_threads connections : $num_threads_P X $num_threads_n X $client_count clients"
-			server_ntttcp_cmd="ulimit -n 204800 && ${ntttcp_cmd} -u -b ${bufferLength}k -P ${num_threads_P} -t ${testDuration} -e -W 1 -C 1"
+			server_ntttcp_cmd="ulimit -n 204800 && ${ntttcp_cmd} -r${testServerIP} -u -b ${bufferLength}k -P ${num_threads_P} -t ${testDuration} -e -W 1 -C 1"
 			if [[ "$mode" == "multi-clients" ]];
 			then
 				server_ntttcp_cmd+=" -M"
 			fi
-			client_ntttcp_cmd="ulimit -n 204800 && ${ntttcp_cmd} -s${server} -u -b ${bufferLength}k -P ${num_threads_P} -n ${num_threads_n} -t ${testDuration} -W 1 -C 1"
+			client_ntttcp_cmd="ulimit -n 204800 && ${ntttcp_cmd} -s${testServerIP} -u -b ${bufferLength}k -P ${num_threads_P} -n ${num_threads_n} -t ${testDuration} -W 1 -C 1"
 		else
 			tx_log_prefix="sender-${testType}-p${num_threads_P}X${num_threads_n}.log"
 			rx_log_prefix="receiver-${testType}-p${num_threads_P}X${num_threads_n}.log"
 			run_msg="Running ${testType} Test: $current_test_threads connections : $num_threads_P X $num_threads_n X $client_count clients"
-			server_ntttcp_cmd="ulimit -n 204800 && ${ntttcp_cmd} -P ${num_threads_P} -t ${testDuration} -e -W 1 -C 1"
+			server_ntttcp_cmd="ulimit -n 204800 && ${ntttcp_cmd} -r${testServerIP} -P ${num_threads_P} -t ${testDuration} -e -W 1 -C 1"
 			if [[ "$mode" == "multi-clients" ]];
 			then
 				server_ntttcp_cmd+=" -M"
 			fi
-			client_ntttcp_cmd="ulimit -n 204800 && ${ntttcp_cmd} -s${server} -P ${num_threads_P} -n ${num_threads_n} -t ${testDuration} -W 1 -C 1"
+			client_ntttcp_cmd="ulimit -n 204800 && ${ntttcp_cmd} -s${testServerIP} -P ${num_threads_P} -n ${num_threads_n} -t ${testDuration} -W 1 -C 1"
 			Run_SSHCommand "${server}" "for i in {1..$testDuration}; do ss -ta | grep ESTA | grep -v ssh | wc -l >> ${log_folder}/tcp-connections-p${num_threads_P}X${num_threads_n}.log; sleep 1; done" &
 		fi
 
