51d98e0f46db4190672e819425eecaa119528ee4
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2020-08-07 16:56:08 -0700

    Hide class and IDs of `ActionStatus` enum
    Using the Enum module’s functional API we can hide the implementation
    details that it is technically a class and that the members are
    internally represented as integers, providing a clearer abstraction.

--- lisa/core/actionStatus.py
+++ lisa/core/actionStatus.py
@@ -1,14 +1,17 @@
 from enum import Enum
 
-
-class ActionStatus(Enum):
-    UNINITIALIZED = 1
-    INITIALIZING = 2
-    INITIALIZED = 3
-    WAITING = 4
-    RUNNING = 5
-    SUCCESS = 6
-    FAILED = 7
-    STOPPING = 8
-    STOPPED = 9
-    UNKNOWN = 10
+ActionStatus = Enum(
+    "ActionStatus",
+    [
+        "UNINITIALIZED",
+        "INITIALIZING",
+        "INITIALIZED",
+        "WAITING",
+        "RUNNING",
+        "SUCCESS",
+        "FAILED",
+        "STOPPING",
+        "STOPPED",
+        "UNKNOWN",
+    ],
+)
