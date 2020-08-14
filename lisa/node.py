5c2668566c994391969621c7cceb3aff5a829f41
Chi Song <27178119+squirrelsc@users.noreply.github.com>
2020-08-14 18:19:37 +0800

    remove factory patterns from code
    Use pythonic function to replace factory.

--- lisa/node.py
+++ lisa/node.py
@@ -278,44 +278,38 @@ class Node:
         self.shell.close()
 
 
-class NodeFactory:
-    @staticmethod
-    def create_from_config(
-        identifier: str, config: Dict[str, object]
-    ) -> Optional[Node]:
-        node_type = cast(str, config.get(constants.TYPE))
-        node = None
-        if node_type is None:
-            raise LisaException("type of node shouldn't be None")
-        if node_type in [
-            constants.ENVIRONMENTS_NODES_LOCAL,
-            constants.ENVIRONMENTS_NODES_REMOTE,
-        ]:
-            is_default = cast(bool, config.get(constants.IS_DEFAULT, False))
-            node = Node.create(identifier, node_type=node_type, is_default=is_default)
-            if node.is_remote:
-                fields = [
-                    constants.ENVIRONMENTS_NODES_REMOTE_ADDRESS,
-                    constants.ENVIRONMENTS_NODES_REMOTE_PORT,
-                    constants.ENVIRONMENTS_NODES_REMOTE_PUBLIC_ADDRESS,
-                    constants.ENVIRONMENTS_NODES_REMOTE_PUBLIC_PORT,
-                    constants.ENVIRONMENTS_NODES_REMOTE_USERNAME,
-                    constants.ENVIRONMENTS_NODES_REMOTE_PASSWORD,
-                    constants.ENVIRONMENTS_NODES_REMOTE_PRIVATEKEYFILE,
-                ]
-                parameters: Dict[str, Any] = dict()
-                for key in config:
-                    if key in fields:
-                        parameters[key] = cast(str, config[key])
-                node.set_connection_info(**parameters)
-        return node
+def from_config(identifier: str, config: Dict[str, object]) -> Optional[Node]:
+    node_type = cast(str, config.get(constants.TYPE))
+    node = None
+    if node_type is None:
+        raise LisaException("type of node shouldn't be None")
+    if node_type in [
+        constants.ENVIRONMENTS_NODES_LOCAL,
+        constants.ENVIRONMENTS_NODES_REMOTE,
+    ]:
+        is_default = cast(bool, config.get(constants.IS_DEFAULT, False))
+        node = Node.create(identifier, node_type=node_type, is_default=is_default)
+        if node.is_remote:
+            fields = [
+                constants.ENVIRONMENTS_NODES_REMOTE_ADDRESS,
+                constants.ENVIRONMENTS_NODES_REMOTE_PORT,
+                constants.ENVIRONMENTS_NODES_REMOTE_PUBLIC_ADDRESS,
+                constants.ENVIRONMENTS_NODES_REMOTE_PUBLIC_PORT,
+                constants.ENVIRONMENTS_NODES_REMOTE_USERNAME,
+                constants.ENVIRONMENTS_NODES_REMOTE_PASSWORD,
+                constants.ENVIRONMENTS_NODES_REMOTE_PRIVATEKEYFILE,
+            ]
+            parameters: Dict[str, Any] = dict()
+            for key in config:
+                if key in fields:
+                    parameters[key] = cast(str, config[key])
+            node.set_connection_info(**parameters)
+    return node
 
-    @staticmethod
-    def create_from_spec(
-        spec: Dict[str, object], node_type: str = constants.ENVIRONMENTS_NODES_REMOTE
-    ) -> Node:
-        is_default = cast(bool, spec.get(constants.IS_DEFAULT, False))
-        node = Node.create(
-            "spec", spec=spec, node_type=node_type, is_default=is_default
-        )
-        return node
+
+def from_spec(
+    spec: Dict[str, object], node_type: str = constants.ENVIRONMENTS_NODES_REMOTE
+) -> Node:
+    is_default = cast(bool, spec.get(constants.IS_DEFAULT, False))
+    node = Node.create("spec", spec=spec, node_type=node_type, is_default=is_default)
+    return node
