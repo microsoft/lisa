63545b5ec43ca59eb8ed9ae99c282010520247ac
Prashant Agrawal <prashant.a.vjti@gmail.com>
2020-09-09 23:02:54 -0700

    Get sbinfo from packages.mircosoft.com at runtime (#1012)
    Co-authored-by: Prashant Agrawal <pra@microsoft.com>

--- Libraries/TestHelpers.psm1
+++ Libraries/TestHelpers.psm1
@@ -750,7 +750,7 @@ Function Get-LISAv2Tools($XMLSecretFile) {
 	# Copy required binary files to working folder
 	$CurrentDirectory = Get-Location
 	$CmdArray = @('7za.exe','dos2unix.exe','gawk','jq','plink.exe','pscp.exe', `
-					'kvp_client32','kvp_client64','nc.exe','lz4.exe','sbinfo')
+					'kvp_client32','kvp_client64','nc.exe','lz4.exe')
 
 	if ($XMLSecretFile) {
 		$WebClient = New-Object System.Net.WebClient
