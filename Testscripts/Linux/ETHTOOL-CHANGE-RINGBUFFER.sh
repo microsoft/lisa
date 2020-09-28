2c18b010a1b7315023532a87379f7d498e4827e8
Johnson George <johgeorg@microsoft.com>
2020-09-28 13:56:06 -0700

    Fix for change ringbuffer to support multiple interface

--- Testscripts/Linux/ETHTOOL-CHANGE-RINGBUFFER.sh
+++ Testscripts/Linux/ETHTOOL-CHANGE-RINGBUFFER.sh
@@ -13,7 +13,6 @@
 #    If the new values were set then the test is passed
 #
 #############################################################################
-declare -i __iterator=0
 
 # Source utils.sh
 . utils.sh || {
@@ -38,40 +37,43 @@ if ! GetSynthNetInterfaces; then
     exit 0
 fi
 
-# Check if kernel support ring settings from ethtool
-sts=$(ethtool -g "${SYNTH_NET_INTERFACES[@]}" 2>&1)
-if [[ "$sts" = *"Operation not supported"* ]]; then
-    LogMsg "$sts"
-    LogMsg "Operation not supported. Test Skipped."
-    SetTestStateAborted
-    exit 0
-fi
+for ifc in "${SYNTH_NET_INTERFACES[@]}";do
+    LogMsg "Processing ${ifc} ..."
+    # Check if kernel support ring settings from ethtool
+    sts=$(ethtool -g "${ifc}" 2>&1)
+    if [[ "$sts" = *"Operation not supported"* ]]; then
+        LogMsg "$sts"
+        LogMsg "Operation not supported. Test Skipped."
+        SetTestStateSkipped
+        exit 0
+    fi
 
-# Take the initial values
-rx_value=$(echo "$sts" | grep RX: | sed -n 2p | grep -o '[0-9]*')
-tx_value=$(echo "$sts" | grep TX: | sed -n 2p | grep -o '[0-9]*')
-LogMsg "RX: ${rx_value} | TX: ${tx_value}."
+    # Take the initial values
+    rx_value=$(echo "$sts" | grep RX: | sed -n 2p | grep -o '[0-9]*')
+    tx_value=$(echo "$sts" | grep TX: | sed -n 2p | grep -o '[0-9]*')
+    LogMsg "RX: ${rx_value} | TX: ${tx_value}."
 
-# Try to change RX and TX with new values
-if ! ethtool -G "${SYNTH_NET_INTERFACES[@]}" rx "$rx" tx "$tx"; then
-    LogMsg "Cannot change RX and TX values."
-    SetTestStateFailed
-    exit 0
-fi
+    # Try to change RX and TX with new values
+    if ! ethtool -G "${ifc}" rx "$rx" tx "$tx"; then
+        LogMsg "Cannot change RX and TX values."
+        SetTestStateFailed
+        exit 0
+    fi
 
-# Take the values after changes
-new_sts=$(ethtool -g "${SYNTH_NET_INTERFACES[$__iterator]}" 2>&1)
-rx_modified=$(echo "$new_sts" | grep RX: | sed -n 2p | grep -o '[0-9]*')
-tx_modified=$(echo "$new_sts" | grep TX: | sed -n 2p | grep -o '[0-9]*')
-LogMsg "RX_modified: $rx_modified | TX_modified: $tx_modified."
+    # Take the values after changes
+    new_sts=$(ethtool -g "${ifc}" 2>&1)
+    rx_modified=$(echo "$new_sts" | grep RX: | sed -n 2p | grep -o '[0-9]*')
+    tx_modified=$(echo "$new_sts" | grep TX: | sed -n 2p | grep -o '[0-9]*')
+    LogMsg "RX_modified: $rx_modified | TX_modified: $tx_modified."
 
-# Compare provided values with values after changes
-if [ "$rx_modified" == "$rx" ] && [ "$tx_modified" == "$tx" ]; then
-    LogMsg "Successfully changed RX and TX values on ${SYNTH_NET_INTERFACES[*]}."
-    SetTestStateCompleted
-    exit 0
-else
-    LogMsg "The values provided aren't matching the real values of RX and TX. Check the logs."
-    SetTestStateFailed
-    exit 0
-fi
+    # Compare provided values with values after changes
+    if [ "$rx_modified" == "$rx" ] && [ "$tx_modified" == "$tx" ]; then
+        LogMsg "Successfully changed RX and TX values on ${ifc}."
+    else
+        LogMsg "The values provided aren't matching the real values of RX and TX. Check the logs."
+        SetTestStateFailed
+        exit 0
+    fi
+done
+SetTestStateCompleted
+exit 0
