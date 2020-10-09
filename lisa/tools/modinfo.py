30d7c58cdd9dc6d474cb08f2c0e0658eef3aef85
Chi Song <27178119+squirrelsc@users.noreply.github.com>
2020-10-09 19:00:01 +0800

    add modinfo tool for parse mod version

---
+++ lisa/tools/modinfo.py
@@ -0,0 +1,38 @@
+import re
+from typing import Any, Dict
+
+from lisa.executable import Tool
+from lisa.util import find_patterns_in_lines
+from lisa.util.process import ExecutableResult
+
+
+class Modinfo(Tool):
+    __version_pattern = re.compile(r"^vermagic:[ \t]*([^ \n]*)")
+
+    @property
+    def command(self) -> str:
+        return self._command
+
+    def _check_exists(self) -> bool:
+        return True
+
+    def _initialize(self, *args: Any, **kwargs: Any) -> None:
+        self._command = "modinfo"
+        self._cached_result: Dict[str, ExecutableResult] = {}
+
+    def get_info(self, mod_name: str, force_run: bool = False) -> str:
+        cached_result = self._cached_result.get(mod_name)
+        if cached_result is None or force_run:
+            cached_result = self.run(mod_name)
+            if cached_result.exit_code != 0:
+                # CentOS may not include the path when started,
+                # specify path and try again.
+                self._command = "/usr/sbin/modinfo"
+                cached_result = self.run(mod_name)
+            self._cached_result[mod_name] = cached_result
+        return cached_result.stdout
+
+    def get_version(self, mod_name: str, force_run: bool = False) -> str:
+        output = self.get_info(mod_name=mod_name, force_run=force_run)
+        found_version = find_patterns_in_lines(output, [self.__version_pattern])
+        return found_version[0][0] if found_version[0] else ""
