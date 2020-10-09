6533dbb9cef8295be8fcb264df95634b2d5fe2b1
Chi Song <27178119+squirrelsc@users.noreply.github.com>
2020-10-09 19:00:01 +0800

    add modinfo tool for parse mod version

--- lisa/tools/__init__.py
+++ lisa/tools/__init__.py
@@ -5,7 +5,19 @@ from .gcc import Gcc
 from .git import Git
 from .lscpu import Lscpu
 from .make import Make
+from .modinfo import Modinfo
 from .ntttcp import Ntttcp
 from .uname import Uname
 
-__all__ = ["Cat", "Dmesg", "Echo", "Gcc", "Git", "Lscpu", "Make", "Ntttcp", "Uname"]
+__all__ = [
+    "Cat",
+    "Dmesg",
+    "Echo",
+    "Gcc",
+    "Git",
+    "Lscpu",
+    "Make",
+    "Modinfo",
+    "Ntttcp",
+    "Uname",
+]
