b94555b59688ba387046c9a0b051eb3b799caa23
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2020-10-09 23:28:13 -0700

    Make Node fixture more reusable
    Extend Fabric’s Connection class with a new (and simple) command “cat”
    which return the value of a remote file as a string.
    
    Setup a Config for the Connection when creating it that echoes every
    command, disables the stdin forwarding (since we’re running under
    Pytest), and fixes the PATH since the remote commands don’t run under a
    login shell.

--- testsuites/test_lis.py
+++ testsuites/test_lis.py
@@ -1,4 +1,6 @@
+from io import BytesIO
 from pathlib import Path
+from typing import Iterator
 
 from fabric import Config, Connection  # type: ignore
 
@@ -7,24 +9,41 @@ import pytest
 LINUX_SCRIPTS = Path("../Testscripts/Linux")
 
 
+class Node(Connection):
+    """Extends 'fabric.Connection' with our own utilities."""
+
+    def cat(self, path: str) -> str:
+        """Gets the value of a remote file without a temporary file."""
+        with BytesIO() as buf:
+            self.get(path, buf)
+            return buf.getvalue().decode("utf-8").strip()
+
+
 # TODO: Make the hostname a parameter.
 @pytest.fixture
-def node() -> Connection:
-    config = Config(overrides={"run": {"in_stream": False}})
-    with Connection("centos", config=config) as connection:
-        yield connection
+def node() -> Iterator[Node]:
+    """Yields a safe remote Node on which to run commands."""
+    config = Config(
+        overrides={
+            "run": {
+                # Show each command as its run.
+                "echo": True,
+                # Disable stdin forwarding.
+                "in_stream": False,
+                # Set PATH since it’s not a login shell.
+                "env": {"PATH": "$PATH:/usr/local/sbin:/usr/sbin"},
+            }
+        }
+    )
+    with Node("centos", config=config, inline_ssh_env=True) as n:
+        yield n
 
 
-def test_lis_version(node: Connection) -> None:
+def test_lis_driver_version(node: Node) -> None:
     # TODO: Include “utils.sh” automatically? Or something...
     for f in ["utils.sh", "LIS-VERSION-CHECK.sh"]:
         node.put(LINUX_SCRIPTS / f)
         node.run(f"chmod +x {f}")
     node.sudo("yum install -y bc")
-    # TODO: Fix this PATH issue.
-    node.run(
-        "PATH=$PATH:/usr/local/sbin:/usr/sbin ./LIS-VERSION-CHECK.sh",
-    )
-    node.get("state.txt")
-    with open("state.txt") as f:
-        assert f.readline().strip() == "TestCompleted"
+    node.run("./LIS-VERSION-CHECK.sh")
+    assert node.cat("state.txt") == "TestCompleted"
