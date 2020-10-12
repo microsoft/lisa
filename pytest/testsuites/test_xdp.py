91a54d8dfd6ed746e4dc71438478786f38108aae
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2020-10-12 16:10:02 -0700

    Create and delete VM resource

--- pytest/testsuites/test_xdp.py
+++ pytest/testsuites/test_xdp.py
@@ -13,15 +13,25 @@ from node_plugin import Node
     tags=["xdp", "network", "hv_netvsc", "sriov"],
     priority=0,
 )
-@pytest.mark.deploy(setup="OneVM2NIC", networking="SRIOV", vm_size="Standard_DS4_v2")
-@pytest.mark.skip(reason="Not Implemented")
+@pytest.mark.deploy(
+    setup="OneVM2NIC",
+    networking="SRIOV",
+    vm_image="Canonical:0001-com-ubuntu-server-focal:20_04-lts:latest",
+    vm_size="Standard_DS4_v2",
+)
+@pytest.mark.skip(reason="Not Finished")
 def test_verify_xdp_compliance(node: Node) -> None:
     for f in [
-        "xdpdumpsetup.sh",
-        "xdputils.sh",
         "utils.sh",
+        "XDPDumpSetup.sh",
+        "XDPUtils.sh",
         "enable_passwordless_root.sh",
         "enable_root.sh",
     ]:
         node.put(conftest.LINUX_SCRIPTS / f)
         node.run(f"chmod +x {f}")
+    node.run("./enable_root.sh")
+    node.run("./enable_passwordless_root.sh")
+    synth_interface = node.run("source XDPUtils.sh ; get_extra_synth_nic").stdout
+    node.run(f"./XDPDumpSetup.sh {node.internal_address} {synth_interface}")
+    assert node.cat("state.txt") == "TestCompleted"
