9f31ce1f69f16956535be95243b561208a8fe901
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2020-10-12 11:56:42 -0700

    Allow untyped decorators
    As supplying types for these would be supremely annoying.

--- testsuites/test_lis.py
+++ testsuites/test_lis.py
@@ -7,7 +7,7 @@ from node_plugin import Node
 LINUX_SCRIPTS = Path("../Testscripts/Linux")
 
 
-@pytest.mark.host("centos")  # type: ignore
+@pytest.mark.host("centos")
 def test_lis_driver_version(node: Node) -> None:
     # TODO: Include “utils.sh” automatically? Or something...
     for f in ["utils.sh", "LIS-VERSION-CHECK.sh"]:
