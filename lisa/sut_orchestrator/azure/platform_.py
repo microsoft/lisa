4f98bbd6e795a4aa62ae02243f7bc1dde83c16b0
Chi Song <27178119+squirrelsc@users.noreply.github.com>
2020-10-16 19:20:19 +0800

    remove 1 core vm sizes from default
    1 core is slow, and there is no special coverage need it. So by default,
    use 2 cores VM for test cases.
    
    Update UTs also.

--- lisa/sut_orchestrator/azure/platform_.py
+++ lisa/sut_orchestrator/azure/platform_.py
@@ -50,8 +50,10 @@ AZURE_DEPLOYMENT_NAME = "lisa_default_deployment_script"
 AZURE_RG_NAME_KEY = "resource_group_name"
 
 VM_SIZE_FALLBACK_LEVELS = [
-    re.compile(r"Standard_DS(\d)+_v2"),
-    re.compile(r"Standard_A(\d)+"),
+    # exclude Standard_DS1_v2, because one core is too slow,
+    # and doesn't work in some distro
+    re.compile(r"Standard_DS((?!1)[\d]{1}|[\d]{2,})_v2"),
+    re.compile(r"Standard_A((?!1)[\d]{1}|[\d]{2,})"),
 ]
 LOCATIONS = ["westus2", "eastus2"]
 RESOURCE_GROUP_LOCATION = "westus2"
