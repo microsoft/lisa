462059f8c6cce48883232d9cffc16ce8be60d4a6
Chi Song <27178119+squirrelsc@users.noreply.github.com>
2020-10-19 14:00:12 +0800

    add reboot, uptime tools to reboot node

---
+++ lisa/tools/uptime.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from lisa.executable import Tool
+from lisa.util import LisaException
+
+
+class Uptime(Tool):
+    @property
+    def command(self) -> str:
+        return "uptime"
+
+    def _check_exists(self) -> bool:
+        return True
+
+    def since_time(self, no_error_log: bool = True) -> datetime:
+        command_result = self.run("-s", no_error_log=no_error_log)
+        if command_result.exit_code != 0:
+            raise LisaException(
+                f"get unexpected non-zero exit code {command_result.exit_code}"
+            )
+        return datetime.fromisoformat(command_result.stdout)
