5b2cc9552a6f913022d0d73bca4ac6afd77676c7
Chi Song <27178119+squirrelsc@users.noreply.github.com>
2020-10-26 18:28:34 +0800

    hide modinfo stderr by default

--- lisa/tools/modinfo.py
+++ lisa/tools/modinfo.py
@@ -20,10 +20,18 @@ class Modinfo(Tool):
         self._command = "modinfo"
         self._cached_result: Dict[str, ExecutableResult] = {}
 
-    def get_info(self, mod_name: str, force_run: bool = False) -> str:
+    def get_info(
+        self,
+        mod_name: str,
+        force_run: bool = False,
+        no_info_log: bool = True,
+        no_error_log: bool = True,
+    ) -> str:
         cached_result = self._cached_result.get(mod_name)
         if cached_result is None or force_run:
-            cached_result = self.run(mod_name)
+            cached_result = self.run(
+                mod_name, no_info_log=no_info_log, no_error_log=no_error_log
+            )
             if cached_result.exit_code != 0:
                 # CentOS may not include the path when started,
                 # specify path and try again.
@@ -32,7 +40,18 @@ class Modinfo(Tool):
             self._cached_result[mod_name] = cached_result
         return cached_result.stdout
 
-    def get_version(self, mod_name: str, force_run: bool = False) -> str:
-        output = self.get_info(mod_name=mod_name, force_run=force_run)
+    def get_version(
+        self,
+        mod_name: str,
+        force_run: bool = False,
+        no_info_log: bool = True,
+        no_error_log: bool = True,
+    ) -> str:
+        output = self.get_info(
+            mod_name=mod_name,
+            force_run=force_run,
+            no_info_log=no_info_log,
+            no_error_log=no_error_log,
+        )
         found_version = find_patterns_in_lines(output, [self.__version_pattern])
         return found_version[0][0] if found_version[0] else ""
