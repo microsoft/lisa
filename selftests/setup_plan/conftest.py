8be09cbb3fecd5e1e37bcff6c5c98c6c04caaa83
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2020-11-03 17:45:30 -0800

    Demonstrate parallelism with shared feature fixture
    Running with ‘-n 1’ takes 10 seconds, or 3 seconds for each feature in
    sequence plus overhead. Running with ‘-n 8’ takes 4 seconds, 3 seconds
    for each in parallel plus overhead.

--- selftests/setup_plan/conftest.py
+++ selftests/setup_plan/conftest.py
@@ -1,7 +1,10 @@
 """Proof-of-concept to schedule a comprehensive test plan."""
 from __future__ import annotations
 
+import time
 import typing
+
+from filelock import FileLock  # type: ignore
 
 import pytest
 
@@ -9,6 +12,8 @@ if typing.TYPE_CHECKING:
     from typing import List
 
     from _pytest.config import Config
+    from _pytest.fixtures import SubRequest
+    from _pytest.tmpdir import TempPathFactory
 
     from pytest import Item, Session
 
@@ -21,6 +26,7 @@ def pytest_collection_modifyitems(
     for item in items:
         marker = item.get_closest_marker("feature")
         if marker is None:
+            keep.append(item)
             continue
         feature = marker.args[0]
         if item.name.endswith(f"[{feature}]"):
@@ -29,5 +35,24 @@ def pytest_collection_modifyitems(
 
 
 @pytest.fixture(scope="session", autouse=True, params=["xdp", "gpu", "rdma"])
-def feature(request) -> str:
-    yield request.param
+def feature(
+    request: SubRequest, tmp_path_factory: TempPathFactory, worker_id: str
+) -> str:
+    """Pretend that this sets up the environment."""
+    assert request.param
+    if worker_id == "master":
+        return str(request.param)
+    # Get the shared temp directory.
+    tmp_dir = tmp_path_factory.getbasetemp().parent
+    fn = tmp_dir / request.param
+    data: str = ""
+    with FileLock(str(fn) + ".lock"):
+        print(f"Worker {worker_id} using feature {request.param}")
+        if fn.is_file():
+            data = fn.read_text()
+        else:
+            # Pretend to do some expensive setup and cache it.
+            time.sleep(3)
+            data = request.param
+            fn.write_text(data)
+    return data
