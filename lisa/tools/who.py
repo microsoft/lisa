3112c4e8a54f888f7fd839096391de94109aebb1
Chi Song <27178119+squirrelsc@users.noreply.github.com>
2020-11-12 09:19:14 +0800

    fix who doesn't exist on some distros
    If it doesn't exist, raise an exception.

--- lisa/tools/who.py
+++ lisa/tools/who.py
@@ -12,8 +12,9 @@ class Who(Tool):
     def command(self) -> str:
         return "who"
 
-    def _check_exists(self) -> bool:
-        return True
+    @property
+    def can_install(self) -> bool:
+        return False
 
     def last_boot(self, no_error_log: bool = True) -> datetime:
         command_result = self.run("-b", no_error_log=no_error_log)
