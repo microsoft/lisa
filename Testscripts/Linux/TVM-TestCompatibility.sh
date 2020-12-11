b3c4d14bfadc9041673465fb03fb0d8817bccb8f
Vinay Kotak <vikotak@microsoft.com>
2020-12-10 16:30:21 -0800

    Addressed PR comments and added code to download the tool from packages.microsoft.com

--- Testscripts/Linux/TVM-TestCompatibility.sh
+++ Testscripts/Linux/TVM-TestCompatibility.sh
@@ -4,24 +4,58 @@
 
 . utils.sh || {
     echo "Error: unable to source utils.sh!"
+    echo "TestAborted" > state.txt
     exit 0
 }
 
 UtilsInit
-homeDir=$(dirname "$0")
 
-if [ ! -e "$homeDir"/tvminfo ]; then
-    LogErr "tvminfo tool is not on the system"
+GetDistro
+case $DISTRO in
+    redhat*|centos*|oracle*)
+        echo "[packages-microsoft-com-azurecore]" | tee -a /etc/yum.repos.d/azurecore.repo
+        echo "name=packages-microsoft-com-azurecore" | tee -a /etc/yum.repos.d/azurecore.repo
+        echo "baseurl=https://packages.microsoft.com/yumrepos/azurecore/" | tee -a /etc/yum.repos.d/azurecore.repo
+        echo "enabled=1" | tee -a /etc/yum.repos.d/azurecore.repo
+        echo "gpgcheck=0" | tee -a /etc/yum.repos.d/azurecore.repo
+    ;;
+    ubuntu*|debian*)
+        echo "deb [arch=amd64] http://packages.microsoft.com/repos/azurecore/ trusty main" | sudo tee -a /etc/apt/sources.list.d/azure.list
+        echo "deb [arch=amd64] http://packages.microsoft.com/repos/azurecore/ xenial main" | sudo tee -a /etc/apt/sources.list.d/azure.list
+        echo "deb [arch=amd64] http://packages.microsoft.com/repos/azurecore/ bionic main" | sudo tee -a /etc/apt/sources.list.d/azure.list
+
+        wget https://packages.microsoft.com/keys/microsoft.asc
+        wget https://packages.microsoft.com/keys/msopentech.asc
+
+        apt-key add microsoft.asc
+        apt-key add msopentech.asc
+    ;;
+    suse*|opensuse*|sles*|sle_hpc*)
+        zypper ar -t rpm-md -n "packages-microsoft-com-azurecore" --no-gpgcheck https://packages.microsoft.com/yumrepos/azurecore/ azurecore
+    ;;
+    *)
+        LogErr "Distro not supported. Aborting..."
+        UpdateSummary "Distro not supported. Aborting..."
+        SetTestStateAborted
+        exit 0
+    ;;
+esac
+
+LogMsg "Trying to install azure-compatscanner package..."
+update_repos
+install_package "azure-compatscanner"
+
+if [ ! -e /usr/bin/mbinfo ]; then
+    LogErr "mbinfo tool is not on the system"
     SetTestStateAborted
     exit 0
 fi
 
-chmod +x "$homeDir"/tvminfo
 
-sudo "$homeDir"/tvminfo
+sudo mbinfo
 rc=$?
 
-if [ rc == 0]; then
+if [ rc == 0 ]; then
     UpdateSummary "This OS image is compatible with TVM."
     SetTestStateCompleted
 else
