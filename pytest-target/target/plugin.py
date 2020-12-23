f876c7cfb49d7e6dea38aeb25b9580ba48a60285
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2020-12-22 16:00:09 -0800

    Refactor to use locking context manager instead of session fixture
    This uses a context manager with a file lock to safely access a shared
    pool cached to disk (via Pytest’s built-in cache). Instead of having a
    session-scoped `pool` fixture we actively create a `Target` instance
    each time a `target` fixture is used (so this is now 1-1). This means
    instances will be in a clean-state, and don’t need to persist across a
    session, but also means that reusable targets must be properly cached to
    disk (see the reference implementations). We handle deletion of targets
    in the `configure` and `unconfigure` hooks, which are safe to call
    regardless of the use of pytest-xdist, and don’t require a session.

--- pytest-target/target/plugin.py
+++ pytest-target/target/plugin.py
@@ -9,6 +9,8 @@ from __future__ import annotations
 
 import logging
 import typing
+import warnings
+from contextlib import contextmanager
 from pathlib import Path
 from uuid import uuid4
 
@@ -18,10 +20,10 @@ from filelock import FileLock  # type: ignore
 
 # See https://pypi.org/project/schema/
 from schema import Optional, Or, Schema  # type: ignore
-from target.target import SSH, Target
+from target.target import SSH, Target, TargetData
 
 if typing.TYPE_CHECKING:
-    from typing import Any, Dict, Iterator, List
+    from typing import Any, Dict, Generator, Iterator, List
 
     from _pytest.config import Config
     from _pytest.config.argparsing import Parser
@@ -29,21 +31,6 @@ if typing.TYPE_CHECKING:
     from _pytest.mark.structures import Mark
     from _pytest.python import Metafunc
     from pytest import Session
-
-
-def get_target_cache(config: Config) -> List[Target]:
-    assert config.cache is not None
-    lock = Path(config.cache.makedir("target")) / "pool.lock"
-    with FileLock(str(lock)):
-        cache = config.cache.get("target/pool", [])
-    return [Target.from_json(**t) for t in cache]
-
-
-def set_target_cache(config: Config, pool: List[Target]) -> None:
-    assert config.cache is not None
-    lock = Path(config.cache.makedir("target")) / "pool.lock"
-    with FileLock(str(lock)):
-        config.cache.set("target/pool", [t.to_json() for t in pool])
 
 
 def pytest_addoption(parser: Parser) -> None:
@@ -55,28 +42,6 @@ def pytest_addoption(parser: Parser) -> None:
     group.addoption(
         "--delete-targets", action="store_true", help="Deletes all cached targets."
     )
-
-
-def pytest_configure(config: Config) -> None:
-    """Pytest hook to perform initial configuration.
-
-    We're registering our custom marker so that it passes
-    `--strict-markers`.
-
-    """
-    config.addinivalue_line(
-        "markers",
-        "target(platform, features, reuse, count): Specify target requirements.",
-    )
-
-    if config.getoption("delete_targets"):
-        logging.info("Deleting all cached targets!")
-        pool = get_target_cache(config)
-        for t in pool:
-            t.delete()
-        pool.clear()
-        set_target_cache(config, pool)
-        pytest.exit("Deleted all cached targets!", pytest.ExitCode.OK)
 
 
 def pytest_playbook_schema(schema: Dict[Any, Any]) -> None:
@@ -126,22 +91,85 @@ def pytest_playbook_schema(schema: Dict[Any, Any]) -> None:
     )
 
 
-def get_target(pool: List[Target], request: SubRequest) -> Target:
+@contextmanager
+def target_pool(config: Config) -> Generator[Dict[str, Any], None, None]:
+    """Exclusive access to the cached targets pool.
+
+    This handles access to the Pytest cache of serialized targets. The
+    cache is a dict of ``{target.name: target.to_json()}``. We use a
+    file lock to provide exclusive access even if Pytest is being run
+    in parallel with xdist. Entries have a `locked` property and must
+    only be modified during a session when locked by that session.
+    Locking means setting `locked` to `True` and updating the entry
+    before exiting this context manager.
+
+    """
+    # TODO: Handle edge case where cache plugin is disabled.
+    assert config.cache is not None
+    lock = Path(config.cache.makedir("target")) / "pool.lock"
+    with FileLock(str(lock)):
+        pool = config.cache.get("target/pool", {})
+        yield pool
+        config.cache.set("target/pool", pool)
+
+
+def delete_targets(config: Config) -> None:
+    """Deletes all cached targets."""
+    with target_pool(config) as pool:
+        for name, json in pool.items():
+            try:
+                Target.from_json(json).delete()
+            except Exception as e:
+                warnings.warn(f"Failed to delete '{name}': {e}")
+        pool.clear()
+
+
+def pytest_configure(config: Config) -> None:
+    """Pytest hook to perform initial configuration.
+
+    https://docs.pytest.org/en/stable/reference.html#pytest.hookspec.pytest_configure
+
+    We're registering our custom marker so that it passes
+    `--strict-markers`.
+
+    """
+    config.addinivalue_line(
+        "markers",
+        "target(platform, features, reuse, count): Specify target requirements.",
+    )
+
+    if config.getoption("delete_targets"):
+        logging.info("Deleting all cached targets!")
+        delete_targets(config)
+        pytest.exit("Deleted all cached targets!", pytest.ExitCode.OK)
+
+
+def pytest_unconfigure(config: Config) -> None:
+    """Pytest hook to perform teardown.
+
+    https://docs.pytest.org/en/stable/reference.html#pytest.hookspec.pytest_unconfigure
+
+    """
+    if not config.getoption("keep_targets"):
+        logging.info("Deleting targets! Pass `--keep-targets` to prevent this.")
+        delete_targets(config)
+
+
+def get_target(request: SubRequest) -> Target:
     """Common case of getting one target."""
     marker = request.node.get_closest_marker("target")
     count = marker.kwargs.get("count", 1)
     assert count == 1, "Use `targets` fixture with `count` instead!"
-    return get_targets(pool, request).pop()
+    return get_targets(request).pop()
 
 
-def get_targets(pool: List[Target], request: SubRequest) -> List[Target]:
+def get_targets(request: SubRequest) -> List[Target]:
     """This function gets or creates an appropriate number of `Target`s.
 
     1. Unpack request into params, required features, and count
     2. Setup fitness criteria for target(s)
     3. Find or create necessary targets
-    4. Update cache with modified `pool`
-    5. Return targets
+    4. Return targets
 
     """
     params: Dict[Any, Any] = request.param
@@ -150,111 +178,117 @@ def get_targets(pool: List[Target], request: SubRequest) -> List[Target]:
     features = mark.kwargs.get("features", [])
     count = mark.kwargs.get("count", 1)
 
-    def fits(t: Target) -> bool:
-        # TODO: Implement full feature comparison, etc. and not just
-        # proof-of-concept string set comparison.
-        logging.debug(f"Checking fit of {t.to_json()}...")
-        return (
-            not t.locked
-            and params == t.params
-            and set(features) <= t.features
-            and count <= sum(t.group == x.group for x in pool)
-        )
+    targets: List[Target] = []
+    with target_pool(request.config) as pool:
 
-    # TODO: If `t` is not already in use, deallocate the previous
-    # target, and ensure the tests have been sorted (and so grouped)
-    # by their requirements.
-    ts: List[Target] = []
-    logging.debug(f"Looking for {count} target(s) which fit: {params}...")
-    for i in range(count):
-        for t in pool:
-            if fits(t):
-                logging.debug(f"Found fit target '{i}'!")
-                t.locked = True
-                ts.append(t)
-                break
-        if count == len(ts):
-            break
-    else:
-        group = f"pytest-{uuid4()}"
+        def fits(t: TargetData) -> bool:
+            """Checks if a given Target fits the current search criteria.
+
+            Converting the cached JSON to a `TargetData` instance is
+            cheap and lets us use typed fields here.
+
+            TODO: Implement full feature comparison, etc. and not just
+            proof-of-concept string set comparison.
+
+            """
+            logging.debug(f"Checking fit of {t}...")
+            return (
+                not t.locked
+                and params == t.params
+                and set(features) <= set(t.features)
+                and count <= sum(t.group == x["group"] for x in pool.values())
+            )
+
+        # TODO: If `t` is not already in use, deallocate the previous
+        # target, and ensure the tests have been sorted (and so grouped)
+        # by their requirements.
+        logging.debug(f"Looking for {count} target(s) which fit: {params}...")
         for i in range(count):
-            logging.info(f"Instantiating target '{group}/{i}': {params}...")
-            t = Target.from_json(group, params, features, {}, i, False)
-            pool.append(t)
-            ts.append(t)
-    set_target_cache(request.config, pool)
-    return ts
+            for name, json in pool.items():
+                if fits(TargetData(**json)):
+                    logging.debug(f"Found fit target '{i}'!")
+                    # TODO: De-duplicate this logic and its
+                    # counterpart below.
+                    t = Target.from_json(json)
+                    assert name == t.name  # Sanity check.
+                    t.locked = True
+                    pool[t.name] = t.to_json()
+                    targets.append(t)
+                    break
+            if count == len(targets):
+                # TODO: This is a kludgy way to either use the found
+                # targets or give up and instantiate new ones instead.
+                # Theoretically the length here should either be zero
+                # or `count` because of the check in `fits`, but
+                # perhaps we should handle the erroneous case where
+                # it’s in-between.
+                break
+        else:
+            group = f"pytest-{uuid4()}"
+            for i in range(count):
+                logging.info(f"Instantiating target '{group}-{i}': {params}...")
+                cls = Target.get_platform(params["platform"])
+                t = cls(group, params, features, {}, i)
+                pool[t.name] = t.to_json()
+                targets.append(t)
+    return targets
 
 
-def cleanup_target(t: Target, pool: List[Target], request: SubRequest) -> None:
+def cleanup_target(t: Target, request: SubRequest) -> None:
     """This is called by fixtures after they're done with a target."""
     t.conn.close()
     mark: Optional[Mark] = request.node.get_closest_marker("target")
     assert mark is not None
-    if mark.kwargs.get("reuse", True):
-        t.locked = False
-    else:
-        logging.info(f"Deleting target '{t.group}/{t.number}'...")
-        t.delete()
-        pool.remove(t)
-    set_target_cache(request.config, pool)
-
-
-@pytest.fixture(scope="session")
-def target_pool(request: SubRequest) -> Iterator[List[Target]]:
-    """This fixture tracks all deployed target resources."""
-    pool = get_target_cache(request.config)
-    yield pool
-    # TODO: Catch interrupts and always delete targets:
-    # `UnexpectedExit`, `KeyboardInterrupt`, `SystemExit`.
-    if not request.config.getoption("keep_targets"):
-        logging.info("Deleting targets! Pass `--keep-targets` to prevent this.")
-        for t in pool:
+    with target_pool(request.config) as pool:
+        if mark.kwargs.get("reuse", True):
+            t.locked = False
+            pool[t.name] = t.to_json()
+        else:
+            logging.info(f"Deleting target '{t.group}/{t.number}'...")
             t.delete()
-        pool.clear()
-    set_target_cache(request.config, pool)
+            del pool[t.name]
 
 
 @pytest.fixture
-def target(target_pool: List[Target], request: SubRequest) -> Iterator[Target]:
+def target(request: SubRequest) -> Iterator[Target]:
     """This fixture provides a connected target for each test.
 
     It is parametrized indirectly in `pytest_generate_tests`.
 
     """
-    t = get_target(target_pool, request)
+    t = get_target(request)
     yield t
-    cleanup_target(t, target_pool, request)
+    cleanup_target(t, request)
 
 
 @pytest.fixture
-def targets(target_pool: List[Target], request: SubRequest) -> Iterator[List[Target]]:
+def targets(request: SubRequest) -> Iterator[List[Target]]:
     """This fixture is the same as `target` but gets a list of targets.
 
     For example, use `pytest.mark.target(count=2)` to get a list of
     two targets with the same parameters, in the same group.
 
     """
-    ts = get_targets(target_pool, request)
+    ts = get_targets(request)
     yield ts
     for t in ts:
-        cleanup_target(t, target_pool, request)
+        cleanup_target(t, request)
 
 
 @pytest.fixture(scope="class")
-def c_target(target_pool: List[Target], request: SubRequest) -> Iterator[Target]:
+def c_target(request: SubRequest) -> Iterator[Target]:
     """This fixture is the same as `target` but shared across a class."""
-    t = get_target(target_pool, request)
+    t = get_target(request)
     yield t
-    cleanup_target(t, target_pool, request)
+    cleanup_target(t, request)
 
 
 @pytest.fixture(scope="module")
-def m_target(target_pool: List[Target], request: SubRequest) -> Iterator[Target]:
+def m_target(request: SubRequest) -> Iterator[Target]:
     """This fixture is the same as `target` but shared across a module."""
-    t = get_target(target_pool, request)
+    t = get_target(request)
     yield t
-    cleanup_target(t, target_pool, request)
+    cleanup_target(t, request)
 
 
 target_params: Dict[str, Dict[str, Any]] = {}
