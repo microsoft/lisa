6df423d941ca1f581eae5c7a119f2c71e9fb75b3
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2020-12-22 16:18:05 -0800

    Emit full flake8/mypy errors in CI

--- conftest.py
+++ conftest.py
@@ -7,6 +7,7 @@ stable should be sent upstream.
 """
 from __future__ import annotations
 
+import logging
 import typing
 
 if typing.TYPE_CHECKING:
@@ -16,3 +17,9 @@ if typing.TYPE_CHECKING:
 def pytest_html_report_title(report: HTMLReport) -> None:
     """pytest-html hook to set the HTML report title."""
     report.title = "LISAv3 Results"
+
+
+def pytest_configure() -> None:
+    """Pytest hook for plugin configuration."""
+    # Flake8 is noisy so we make it quieter here.
+    logging.getLogger("flake8").setLevel(logging.WARNING)
