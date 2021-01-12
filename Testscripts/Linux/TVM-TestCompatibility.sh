c90f76cd65478681f1b4c70d550e0ecaff3cfec5
Vinay Kotak <vinnykotak@gmail.com>
2021-01-11 17:23:28 -0800

    Added functional tests to test TVM compatibility for Linux images (#1124)
    * Added functional tests to test distro compatibility for TVMs.
    
    * Reverted changes to TVM-TestSecureBoot test case.
    
    * Addressed PR comments and added code to download the tool from packages.microsoft.com
    
    * Fixed test script error.
    
    * Addressed PR comments.
    
    Changes the log message to indicate that the test will be skipped if
    the distro image is not supported by the test case.
    
    * Addressed PR comments.
    
    * Addressed PR comments.
    
    * Addressed PR comments.
    
    * Addressed PR comments.
    
    * Addressed PR comments.
    
    Co-authored-by: Vinay Kotak <vikotak@microsoft.com>

---
+++ Testscripts/Linux/TVM-TestCompatibility.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache License.
+
+. utils.sh || {
+    echo "Error: unable to source utils.sh!"
+    echo "TestAborted" > state.txt
+    exit 0
+}
+
+UtilsInit
+
+GetDistro
+case $DISTRO in
+    redhat*|centos*|oracle*)
+        echo "[packages-microsoft-com-azurecore]" | tee -a /etc/yum.repos.d/azurecore.repo
+        echo "name=packages-microsoft-com-azurecore" | tee -a /etc/yum.repos.d/azurecore.repo
+        echo "baseurl=https://packages.microsoft.com/yumrepos/azurecore/" | tee -a /etc/yum.repos.d/azurecore.repo
+        echo "enabled=1" | tee -a /etc/yum.repos.d/azurecore.repo
+        echo "gpgcheck=0" | tee -a /etc/yum.repos.d/azurecore.repo
+    ;;
+    ubuntu*|debian*)
+        release=$(lsb_release -c -s)
+        echo "deb [arch=amd64] http://packages.microsoft.com/repos/azurecore/ $release main" | sudo tee -a /etc/apt/sources.list.d/azure.list
+
+        wget -qO - https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+        wget -qO - https://packages.microsoft.com/keys/msopentech.asc | apt-key add -
+    ;;
+    suse*|opensuse*|sles*|sle_hpc*)
+        zypper ar -t rpm-md -n "packages-microsoft-com-azurecore" --no-gpgcheck https://packages.microsoft.com/yumrepos/azurecore/ azurecore
+    ;;
+    *)
+        LogErr "Distro not supported. Skipping test case..."
+        UpdateSummary "Distro not supported. Skipping test case..."
+        SetTestStateSkipped
+        exit 0
+    ;;
+esac
+
+LogMsg "Trying to install azure-compatscanner package..."
+update_repos
+install_package "azure-compatscanner"
+
+if [ ! -e /usr/bin/mbinfo ]; then
+    LogErr "mbinfo tool is not on the system"
+    SetTestStateAborted
+    exit 0
+fi
+
+
+output=$(sudo mbinfo)
+ret=$?
+LogMsg "$output"
+
+if [ $ret == 0 ]; then
+    UpdateSummary "This OS image is compatible with TVM."
+    SetTestStateCompleted
+else
+    UpdateSummary "This OS image is not compatible with TVM."
+    SetTestStateFailed
+fi
+
+exit 0
