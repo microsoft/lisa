f0ba957059d7c70cece192741ed409def9c43389
Chi Song <27178119+squirrelsc@users.noreply.github.com>
2021-02-04 17:47:41 +0800

    git implement checkout

--- lisa/tools/git.py
+++ lisa/tools/git.py
@@ -1,11 +1,14 @@
 import pathlib
+import re
 
 from lisa.executable import Tool
 from lisa.operating_system import Linux
-from lisa.util import LisaException
+from lisa.util import LisaException, get_matched_str
 
 
 class Git(Tool):
+    CODE_FOLDER_PATTERN = re.compile(r"Cloning into '(.+)'")
+
     @property
     def command(self) -> str:
         return "git"
@@ -24,6 +27,17 @@ class Git(Tool):
             )
         return self._check_exists()
 
-    def clone(self, url: str, cwd: pathlib.PurePath) -> None:
+    def clone(
+        self, url: str, cwd: pathlib.PurePath, branch: str = "", dir_name: str = ""
+    ) -> None:
+        cmd = f"clone {url} {dir_name}"
         # git print to stderr for normal info, so set no_error_log to True.
-        self.run(f"clone {url}", cwd=cwd, no_error_log=True)
+        result = self.run(cmd, cwd=cwd, no_error_log=True)
+
+        code_dir = get_matched_str(result.stderr, self.CODE_FOLDER_PATTERN)
+        full_path = cwd / code_dir
+        if branch:
+            self.checkout(branch, cwd=full_path)
+
+    def checkout(self, branch: str, cwd: pathlib.PurePath) -> None:
+        self.run(f"checkout {branch}", cwd=cwd, no_info_log=True, no_error_log=True)
