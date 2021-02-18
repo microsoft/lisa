a337d82851a4b128079934cd5068d11fdb21df76
sharsonia <77515807+sharsonia@users.noreply.github.com>
2021-02-17 16:03:29 -0800

    get_public_key_data() is not working expectedly. (#1176)
    This method doesn't strip the extension from the private key path which
    is why resulting in wrong public key path

--- lisa/util/__init__.py
+++ lisa/util/__init__.py
@@ -83,7 +83,8 @@ def get_public_key_data(private_key_file_path: str) -> str:
     if not private_key_path.exists():
         raise LisaException(f"private key file not exist {private_key_file_path}")
 
-    public_key_path = private_key_path.parent / f"{private_key_path.name}.pub"
+    public_key_file = Path(private_key_path).stem
+    public_key_path = private_key_path.parent / f"{public_key_file}.pub"
     try:
         with open(public_key_path, "r") as fp:
             public_key_data = fp.read()
