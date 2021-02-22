2562c06e46f5d101905ac2fcbdfb3ee68e58de98
Chi Song <27178119+squirrelsc@users.noreply.github.com>
2021-02-22 20:29:50 +0800

    lscpu: fix end line is not matched correct
    There is an extra `\r` at end of line, so regular expressions failed.
    It may need to investigate deeper to understand this change.
    For now, just support `\r` in pattern.
    
    Minor improvement, switch to assertpy.

--- lisa/tools/lscpu.py
+++ lisa/tools/lscpu.py
@@ -1,12 +1,16 @@
 import re
 from typing import Any, Optional, Type
 
+from assertpy import assert_that  # type: ignore
+
 from lisa.executable import Tool
 
 
 class Lscpu(Tool):
-    __pattern_cores_per_socket = re.compile(r"^Core\(s\) per socket:[ ]+([\d]+)$", re.M)
-    __pattern_sockets = re.compile(r"^Socket\(s\):[ ]+([\d]+)$", re.M)
+    __pattern_cores_per_socket = re.compile(
+        r"^Core\(s\) per socket:[ ]+([\d]+)\r?$", re.M
+    )
+    __pattern_sockets = re.compile(r"^Socket\(s\):[ ]+([\d]+)\r?$", re.M)
 
     def _initialize(self, *args: Any, **kwargs: Any) -> None:
         self._core_count: Optional[int] = None
@@ -26,10 +30,16 @@ class Lscpu(Tool):
         if self._core_count is None or force:
             result = self.run()
             matched = self.__pattern_cores_per_socket.findall(result.stdout)
-            assert len(matched) == 1, "cores per socket should have exact one line"
+            assert_that(
+                len(matched),
+                f"cores per socket should have exact one line, but got {matched}",
+            ).is_equal_to(1)
             core_per_socket = int(matched[0])
             matched = self.__pattern_sockets.findall(result.stdout)
-            assert len(matched) == 1, "sockets should have exact one line"
+            assert_that(
+                len(matched),
+                f"sockets should have exact one line, but got {matched}",
+            ).is_equal_to(1)
             sockets = int(matched[0])
             self._core_count = sockets * core_per_socket
         return self._core_count
