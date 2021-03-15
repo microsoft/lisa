ffb93a8e99d3f4f6d4c58583537572f192d2373d
Lili Deng <lildeng@microsoft.com>
2021-03-15 16:43:39 +0800

    CPU count is our needed value.

--- lisa/tools/lscpu.py
+++ lisa/tools/lscpu.py
@@ -10,10 +10,7 @@ from lisa.executable import Tool
 
 
 class Lscpu(Tool):
-    __pattern_cores_per_socket = re.compile(
-        r"^Core\(s\) per socket:[ ]+([\d]+)\r?$", re.M
-    )
-    __pattern_sockets = re.compile(r"^Socket\(s\):[ ]+([\d]+)\r?$", re.M)
+    __vcpu_sockets = re.compile(r"^CPU\(s\):[ ]+([\d]+)\r?$", re.M)
 
     def _initialize(self, *args: Any, **kwargs: Any) -> None:
         self._core_count: Optional[int] = None
@@ -32,19 +29,12 @@ class Lscpu(Tool):
     def get_core_count(self, force: bool = False) -> int:
         if self._core_count is None or force:
             result = self.run()
-            matched = self.__pattern_cores_per_socket.findall(result.stdout)
+            matched = self.__vcpu_sockets.findall(result.stdout)
             assert_that(
                 len(matched),
-                f"cores per socket should have exact one line, but got {matched}",
+                f"cpu count should have exact one line, but got {matched}",
             ).is_equal_to(1)
-            core_per_socket = int(matched[0])
-            matched = self.__pattern_sockets.findall(result.stdout)
-            assert_that(
-                len(matched),
-                f"sockets should have exact one line, but got {matched}",
-            ).is_equal_to(1)
-            sockets = int(matched[0])
-            self._core_count = sockets * core_per_socket
+            self._core_count = int(matched[0]) * 1
         return self._core_count
 
 
