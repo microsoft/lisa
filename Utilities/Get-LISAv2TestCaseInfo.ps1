09e23bd69f735303dc596c7b69b29270a1f31e81
Liz Zhang <lizzha@microsoft.com>
2021-03-16 02:50:32 -0700

    Fix issue for case with no priority

--- Utilities/Get-LISAv2TestCaseInfo.ps1
+++ Utilities/Get-LISAv2TestCaseInfo.ps1
@@ -11,45 +11,45 @@ param (
 )
 
 function Delete-DeprecatedRecord($CaseNameList) {
-	$server = $XmlSecrets.secrets.DatabaseServer
-	$dbuser = $XmlSecrets.secrets.DatabaseUser
-	$dbpassword = $XmlSecrets.secrets.DatabasePassword
-	$database = $XmlSecrets.secrets.DatabaseName
+    $server = $XmlSecrets.secrets.DatabaseServer
+    $dbuser = $XmlSecrets.secrets.DatabaseUser
+    $dbpassword = $XmlSecrets.secrets.DatabasePassword
+    $database = $XmlSecrets.secrets.DatabaseName
 
-	$connectionString = "Server=$server;uid=$dbuser; pwd=$dbpassword;Database=$database;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
-	$connection = New-Object System.Data.SqlClient.SqlConnection
-	$connection.ConnectionString = $connectionString
-	$connection.Open()
+    $connectionString = "Server=$server;uid=$dbuser; pwd=$dbpassword;Database=$database;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+    $connection = New-Object System.Data.SqlClient.SqlConnection
+    $connection.ConnectionString = $connectionString
+    $connection.Open()
     
     try {
-		$sqlQuery = "SELECT ID,TestCaseName from $TableName"
-		$command = $connection.CreateCommand()
-		$command.CommandText = $SQLQuery
-		$reader = $command.ExecuteReader()
-		$deprecatedIDList = @()
-		while ($reader.Read()) {
-			$id = $reader.GetValue($reader.GetOrdinal("ID"))
-			$caseName = $reader.GetValue($reader.GetOrdinal("TestCaseName"))
-			if (-not $CaseNameList.contains($caseName)) {
-				$deprecatedIDList += $id
-			}
-		}
-		$reader.Close()
-		if ($deprecatedIDList.Count -gt 0) {
-			$sqlQuery = "delete from $TableName where"
-			$isFirst = $true
-			foreach ($id in $deprecatedIDList) {
-				if (-not $isFirst) {
-					$sqlQuery += " or"
-				}
-				$sqlQuery += " ID=$id"
+        $sqlQuery = "SELECT ID,TestCaseName from $TableName"
+        $command = $connection.CreateCommand()
+        $command.CommandText = $SQLQuery
+        $reader = $command.ExecuteReader()
+        $deprecatedIDList = @()
+        while ($reader.Read()) {
+            $id = $reader.GetValue($reader.GetOrdinal("ID"))
+            $caseName = $reader.GetValue($reader.GetOrdinal("TestCaseName"))
+            if (-not $CaseNameList.contains($caseName)) {
+                $deprecatedIDList += $id
+            }
+        }
+        $reader.Close()
+        if ($deprecatedIDList.Count -gt 0) {
+            $sqlQuery = "delete from $TableName where"
+            $isFirst = $true
+            foreach ($id in $deprecatedIDList) {
+                if (-not $isFirst) {
+                    $sqlQuery += " or"
+                }
+                $sqlQuery += " ID=$id"
                 $isFirst = $false
-			}
-			
-			Write-LogInfo "Executing command: $sqlQuery"
-			$command.CommandText = $SQLQuery
-			$null = $command.executenonquery()
-		}
+            }
+            
+            Write-LogInfo "Executing command: $sqlQuery"
+            $command.CommandText = $SQLQuery
+            $null = $command.executenonquery()
+        }
     } catch {
         $line = $_.InvocationInfo.ScriptLineNumber
         $script_name = ($_.InvocationInfo.ScriptName).Replace($PWD,".")
@@ -64,15 +64,15 @@ function Delete-DeprecatedRecord($CaseNameList) {
 }
 
 function Update-DatabaseRecord($TestCaseList) {
-	$server = $XmlSecrets.secrets.DatabaseServer
-	$dbuser = $XmlSecrets.secrets.DatabaseUser
-	$dbpassword = $XmlSecrets.secrets.DatabasePassword
-	$database = $XmlSecrets.secrets.DatabaseName
+    $server = $XmlSecrets.secrets.DatabaseServer
+    $dbuser = $XmlSecrets.secrets.DatabaseUser
+    $dbpassword = $XmlSecrets.secrets.DatabasePassword
+    $database = $XmlSecrets.secrets.DatabaseName
 
-	$connectionString = "Server=$server;uid=$dbuser; pwd=$dbpassword;Database=$database;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
-	$connection = New-Object System.Data.SqlClient.SqlConnection
-	$connection.ConnectionString = $connectionString
-	$connection.Open()
+    $connectionString = "Server=$server;uid=$dbuser; pwd=$dbpassword;Database=$database;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+    $connection = New-Object System.Data.SqlClient.SqlConnection
+    $connection.ConnectionString = $connectionString
+    $connection.Open()
     
 
     try {
@@ -91,9 +91,14 @@ function Update-DatabaseRecord($TestCaseList) {
                 $testTags = $reader.GetValue($reader.GetOrdinal("TestTags"))
                 $testPlatform = $reader.GetValue($reader.GetOrdinal("TestPlatform"))
 
-                if ($testCategory -ne $TestCaseData.Category -or $testArea -ne $TestCaseData.Area -or $testPriority -ne $TestCaseData.Priority -or
+                $priority = $TestCaseData.Priority
+                if (-not $priority) {
+                    $priority = -1
+                }
+
+                if ($testCategory -ne $TestCaseData.Category -or $testArea -ne $TestCaseData.Area -or $testPriority -ne $priority -or
                     $testTags -ne $TestCaseData.Tags -or $testPlatform -ne $TestCaseData.Platform) {
-                    $sqlQuery = "Update $tableName Set TestCategory='$($TestCaseData.Category)', TestArea='$($TestCaseData.Area)', TestPriority='$($TestCaseData.Priority)',
+                    $sqlQuery = "Update $tableName Set TestCategory='$($TestCaseData.Category)', TestArea='$($TestCaseData.Area)', TestPriority=$priority,
                         TestTags='$($TestCaseData.Tags)',TestPlatform='$($TestCaseData.Platform)' where TestCaseName='$($TestCaseData.testName)'"
                 }
             # If the record doesn't exist, insert a new record
@@ -123,30 +128,30 @@ function Update-DatabaseRecord($TestCaseList) {
 
 #Load libraries
 if (!$global:LogFileName) {
-	Set-Variable -Name LogFileName -Value $LogFileName -Scope Global -Force
+    Set-Variable -Name LogFileName -Value $LogFileName -Scope Global -Force
 }
 Get-ChildItem .\Libraries -Recurse | Where-Object { $_.FullName.EndsWith(".psm1") } | ForEach-Object { Import-Module $_.FullName -Force -Global -DisableNameChecking }
 
 #Read secrets file and terminate if not present.
 if ($AzureSecretsFile) {
-	$secretsFile = $AzureSecretsFile
+    $secretsFile = $AzureSecretsFile
 }
 elseif ($env:Azure_Secrets_File) {
-	$secretsFile = $env:Azure_Secrets_File
+    $secretsFile = $env:Azure_Secrets_File
 }
 else {
-	 Write-Host "-AzureSecretsFile and env:Azure_Secrets_File are empty. Exiting."
-	 exit 1
+     Write-Host "-AzureSecretsFile and env:Azure_Secrets_File are empty. Exiting."
+     exit 1
 }
 
 if (Test-Path $secretsFile) {
-	Write-Host "Secrets file found."
-	$secrets = [xml](Get-Content -Path $secretsFile)
-	Set-Variable -Name XmlSecrets -Value $secrets -Scope Global -Force
+    Write-Host "Secrets file found."
+    $secrets = [xml](Get-Content -Path $secretsFile)
+    Set-Variable -Name XmlSecrets -Value $secrets -Scope Global -Force
 }
  else {
-	 Write-Host "Secrets file not found. Exiting."
-	 exit 1
+     Write-Host "Secrets file not found. Exiting."
+     exit 1
 }
 
 # Load test case xmls
@@ -157,7 +162,7 @@ foreach ($fname in $files) {
     $xml = [xml](Get-Content $fname)
     foreach ($item in $xml.TestCases.test) {
         $testCaseList += $item
-		$caseNameList += $item.testName
+        $caseNameList += $item.testName
     }
     Update-DatabaseRecord -TestCaseList $testCaseList
 }
