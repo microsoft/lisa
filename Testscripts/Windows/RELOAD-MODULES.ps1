733187b866fbde7f938348dffa8494d448fae526
Lili Deng <lildeng@microsoft.com>
2021-03-31 11:33:05 +0800

    Fix test issue in reload-modules

--- Testscripts/Windows/RELOAD-MODULES.ps1
+++ Testscripts/Windows/RELOAD-MODULES.ps1
@@ -30,40 +30,33 @@ function Check-Result {
         Start-Sleep -Seconds 20
         Write-LogInfo "Test is running. Attempt number ${attempts} to reach VM"
         $attempts++
-        $isVmAlive = Is-VmAlive -AllVMDataObject $AllVMData -MaxRetryCount 5
-        if ($isVmAlive -eq "True") {
-            $TestConnection = Run-LinuxCmd -ip $VmIp -port $VMPort -username $User -password $Password -command "echo Connected"
-            if ($TestConnection -eq "Connected") {
-                $state = Run-LinuxCmd -ip $VmIp -port $VMPort -username $User -password $Password -command "cat state.txt" -ignoreLinuxExitCode
-                if (-not $state) {
-                    if ($TestPlatform -eq "HyperV" -and (Get-VMIntegrationService $VMName -ComputerName $HvServer | `
-                        Where-Object {$_.name -eq "Heartbeat"}).PrimaryStatusDescription `
-                        -match "No Contact|Lost Communication") {
+        try {
+            $state = Run-LinuxCmd -ip $VmIp -port $VMPort -username $User -password $Password -command "cat state.txt" -ignoreLinuxExitCode
+            if (-not $state) {
+                if ($TestPlatform -eq "HyperV" -and (Get-VMIntegrationService $VMName -ComputerName $HvServer | `
+                    Where-Object {$_.name -eq "Heartbeat"}).PrimaryStatusDescription `
+                    -match "No Contact|Lost Communication") {
 
-                        Stop-VM -Name $VMName -ComputerName $HvServer -Force -TurnOff
-                        Write-LogErr "Lost Communication or No Contact to VM!"
-                        break
-                    } else {
-                        Write-LogInfo "Current VM is inaccessible, please wait for a while."
-                    }
+                    Stop-VM -Name $VMName -ComputerName $HvServer -Force -TurnOff
+                    Write-LogErr "Lost Communication or No Contact to VM!"
+                    break
                 } else {
-                    if ($state -eq $testRunning){
-                        Write-LogInfo "Test is still running!"
-                    } elseif (($state -eq $testCompleted) -or ($state -eq $testSkipped)) {
-                        Write-LogInfo "state file contains ${state}"
-                        $retVal = $True
-                        break
-                    } elseif (($state -eq $testAborted) -or ($state -eq $testFailed)) {
-                        Write-LogErr "state file contains ${state}"
-                        break
-                    }
+                    Write-LogInfo "Current VM is inaccessible, please wait for a while."
                 }
             } else {
-                Write-LogInfo "Current VM is inaccessible, please wait for a while."
-                continue
+                if ($state -eq $testRunning){
+                    Write-LogInfo "Test is still running!"
+                } elseif (($state -eq $testCompleted) -or ($state -eq $testSkipped)) {
+                    Write-LogInfo "state file contains ${state}"
+                    $retVal = $True
+                    break
+                } elseif (($state -eq $testAborted) -or ($state -eq $testFailed)) {
+                    Write-LogErr "state file contains ${state}"
+                    break
+                }
             }
-        } else {
-            Write-LogInfo "Current VM is inaccessible, please wait for a while."
+        } catch {
+            Write-LogDbg "Current VM is inaccessible, please wait for a while."
         }
     }
     if ($sw.elapsed -ge $timeout) {
