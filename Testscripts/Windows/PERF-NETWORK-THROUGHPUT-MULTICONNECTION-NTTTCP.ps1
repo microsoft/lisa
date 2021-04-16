05a5dcf4b6f412ce4f92b86ad74231f3b81dcf17
Johnson George <johgeorg@microsoft.com>
2021-04-16 09:18:10 -0700

    Response time increases when running with higher connection for synthetic ifc

--- Testscripts/Windows/PERF-NETWORK-THROUGHPUT-MULTICONNECTION-NTTTCP.ps1
+++ Testscripts/Windows/PERF-NETWORK-THROUGHPUT-MULTICONNECTION-NTTTCP.ps1
@@ -142,7 +142,7 @@ collect_VM_properties
 
         #region MONITOR TEST
         while ((Get-Job -Id $testJob).State -eq "Running") {
-            $currentStatus = Run-LinuxCmd -ip $clientVMData.PublicIP -port $clientVMData.SSHPort -username "root" -password $password -command "tail -2 ntttcpConsoleLogs.txt | head -1" -ignoreLinuxExitCode:$true -maxRetryCount 60
+            $currentStatus = Run-LinuxCmd -ip $clientVMData.PublicIP -port $clientVMData.SSHPort -username "root" -password $password -command "tail -2 ntttcpConsoleLogs.txt | head -1" -ignoreLinuxExitCode:$true -maxRetryCount 60 -runMaxAllowedTime 1200
             Write-LogInfo "Current Test Status : $currentStatus."
             Write-LogInfo "Sleep for 1 min."
             Wait-Time -seconds 60
