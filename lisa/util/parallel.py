3ad6cef4132d95c83c812963082f6f782bc0bcb5
Chi Song <chisong@example.com>
2021-05-30 13:44:00 -0700

    parallel: remove unused code.

--- lisa/util/parallel.py
+++ lisa/util/parallel.py
@@ -5,7 +5,6 @@ from concurrent.futures import FIRST_COMPLETED, Future, ThreadPoolExecutor, wait
 from typing import Any, Callable, Generic, List, Optional, TypeVar
 
 from . import LisaException
-from .logger import Logger
 
 T_RESULT = TypeVar("T_RESULT")
 
@@ -53,79 +52,6 @@ class TaskManager(Generic[T_RESULT]):
             # exception will throw at this point
             self._callback(result)
         return len(self._futures) > 0
-
-
-def _cancel_threads(
-    futures: List[Any],
-    completed_callback: Optional[Callable[[Any], None]] = None,
-) -> List[Any]:
-    success_futures: List[Any] = []
-    for future in futures:
-        if future.done() and future.exception():
-            # throw exception, if it's not here.
-            future.result()
-        elif not future.done():
-            # cancel running threads. It may need cancellation callback
-            result = future.cancel()
-            if not result and completed_callback:
-                # make sure it's status changed to canceled
-                completed_callback(False)
-            # join exception back to main thread
-            future.result()
-        else:
-            success_futures.append(future)
-    # return empty list to prevent cancel again.
-    return success_futures
-
-
-def run_in_threads(
-    methods: List[Any],
-    max_workers: int = 0,
-    completed_callback: Optional[Callable[[Any], None]] = None,
-    log: Optional[Logger] = None,
-) -> List[Any]:
-    """
-    start methods in a thread pool
-    """
-
-    results: List[Any] = []
-    if max_workers <= 0:
-        max_workers = len(methods)
-    with ThreadPoolExecutor(max_workers) as pool:
-        futures = [pool.submit(method) for method in methods]
-        if completed_callback:
-            for future in futures:
-                future.add_done_callback(completed_callback)
-        try:
-            while any(not x.done() for x in futures):
-                # if there is any change, skip sleep to get faster
-                changed = False
-                for future in futures:
-                    # join exceptions of subthreads to main thread
-                    if future.done():
-                        changed = True
-                        # removed finished threads
-                        futures.remove(future)
-                        # exception will throw at this point
-                        results.append(future.result())
-                        break
-                if not changed:
-                    time.sleep(0.1)
-
-        except KeyboardInterrupt:
-            if log:
-                log.info("received CTRL+C, stopping threads...")
-            # support to interrupt runs on local debugging.
-            futures = _cancel_threads(futures, completed_callback=completed_callback)
-            pool.shutdown(True)
-        finally:
-            if log:
-                log.debug("finalizing threads...")
-            futures = _cancel_threads(futures, completed_callback=completed_callback)
-        for future in futures:
-            # exception will throw at this point
-            results.append(future.result())
-    return results
 
 
 _default_task_manager: Optional[TaskManager[Any]] = None
