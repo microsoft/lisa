a821a9f96de122e00ab3f1013c171ccbb41913d7
Matthew McGovern (HE/HIM) <mamcgove@microsoft.com>
2021-06-04 10:44:31 -0700

    add extra error checking for 0 length return on ip addr show

--- Testscripts/Linux/dpdkPmdsBasicCheck.sh
+++ Testscripts/Linux/dpdkPmdsBasicCheck.sh
@@ -149,6 +149,10 @@ IFS=',' read -r -a pmd_array <<< "$pmds"
 exitcode=0
 eth1_vfinfo=$(ip addr show master eth1)
 checkCmdExitStatus "ip addr show master eth1"
+if [ $(echo "$eth1_vfinfo" | wc -c ) -ne 0 ];then
+	LogErr "Could not find vfinfo for eth1"
+	SetTestStateAborted
+fi
 LogMsg "eth1 vfinfo found: $eth1_vfinfo"
 eth1_vfname=$(echo "$eth1_vfinfo" | awk -F ': ' '{print $2}')
 bus_info=$(ethtool -i $eth1_vfname|grep bus-info|awk '{print $2}')
