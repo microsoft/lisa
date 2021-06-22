4c7b0fbd93d0dafafdeee39013121939d5d06e51
Sonia Sharma <sosha@microsoft.com>
2021-06-22 15:00:13 -0700

    Add tool for find command

---
+++ lisa/tools/find.py
@@ -0,0 +1,50 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+from pathlib import PurePath
+from typing import List
+
+from lisa.executable import Tool
+from lisa.util import LisaException
+
+
+class Find(Tool):
+    @property
+    def command(self) -> str:
+        return "find"
+
+    @property
+    def can_install(self) -> bool:
+        return False
+
+    def find_files(
+        self,
+        start_path: PurePath,
+        name_pattern: str = "",
+        path_pattern: str = "",
+        ignore_case: bool = False,
+    ) -> List[str]:
+        if not self.node.shell.exists(start_path):
+            raise LisaException(f"Path {start_path} does not exist.")
+
+        cmd = str(start_path)
+        if name_pattern:
+            if ignore_case:
+                cmd += f" -name '{name_pattern}'"
+            else:
+                cmd += f" -iname '{name_pattern}'"
+        if path_pattern:
+            if ignore_case:
+                cmd += f" -path '{path_pattern}'"
+            else:
+                cmd += f" -ipath '{path_pattern}'"
+
+        # for possibility of newline character in the file/folder name.
+        cmd += "-print0"
+
+        result = self.run(cmd)
+        if result.exit_code != 0:
+            raise LisaException(
+                f"{cmd} command got non-zero exit code: {result.exit_code}"
+            )
+        return result.stdout.split("\x00")
