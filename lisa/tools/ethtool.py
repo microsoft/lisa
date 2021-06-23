c5b9fa3b9e8a6584c207ecd9f6e5406212568ab8
Sonia Sharma <sosha@microsoft.com>
2021-06-23 15:35:57 -0700

    get device ring buffer settings info

--- lisa/tools/ethtool.py
+++ lisa/tools/ethtool.py
@@ -8,6 +8,29 @@ from lisa.util import LisaException
 
 from .find import Find
 
+# Few ethtool device settings follow similar pattern like -
+#   ethtool device channel info from "ethtool -l eth0"
+#   ethtool ring buffer setting info from "ethtool -g eth0"
+# ~$ ethtool -g eth0
+#   Ring parameters for eth0:
+#   Pre-set maximums:
+#   RX:             18811
+#   RX Mini:        0
+#   RX Jumbo:       0
+#   TX:             2560
+#   Current hardware settings:
+#   RX:             9709
+#   RX Mini:        0
+#   RX Jumbo:       0
+#   TX:             170
+_max_settings_pattern = re.compile(
+    r"Pre-set maximums:(\s+)(?P<settings>.*?)Current hardware settings:",
+    re.DOTALL,
+)
+_current_settings_pattern = re.compile(
+    r"Current hardware settings:(\s+)(?P<settings>.*?)$", re.DOTALL
+)
+
 
 class DeviceChannel:
     # ethtool device channel info is in format -
@@ -23,33 +46,26 @@ class DeviceChannel:
     #   TX:             0
     #   Other:          0
     #   Combined:       1
-    __max_settings_pattern = re.compile(
-        r"Pre-set maximums:(\s+)(?P<settings>.*?)Current hardware settings:",
-        re.DOTALL,
-    )
-    __current_settings_pattern = re.compile(
-        r"Current hardware settings:(\s+)(?P<settings>.*?)$", re.DOTALL
-    )
-    __channel_count_param_pattern = re.compile(
-        r"(?P<param>Combined):( +)(?P<value>.*)$", re.MULTILINE
+    _channel_count_param_pattern = re.compile(
+        r"(?P<param>Combined):[ \t]*(?P<value>.*)$", re.MULTILINE
     )
 
     def __init__(self, interface: str, device_channel_raw: str) -> None:
         self._parse_channel_info(interface, device_channel_raw)
 
     def _parse_channel_info(self, interface: str, raw_str: str) -> None:
-        current_settings = self.__current_settings_pattern.match(raw_str)
-        max_settings = self.__max_settings_pattern.match(raw_str)
+        current_settings = _current_settings_pattern.match(raw_str)
+        max_settings = _max_settings_pattern.match(raw_str)
         if (not current_settings) or (not max_settings):
             raise LisaException(
                 f"Cannot get {interface} device channel current and/or"
                 " max settings information"
             )
 
-        current_param = self.__channel_count_param_pattern.match(
+        current_param = self._channel_count_param_pattern.match(
             current_settings.group("settings")
         )
-        max_param = self.__channel_count_param_pattern.match(
+        max_param = self._channel_count_param_pattern.match(
             max_settings.group("settings")
         )
         if (not current_param) or (not max_param):
@@ -74,10 +90,10 @@ class DeviceFeatures:
     #           tx-checksum-fcoe-crc: off [fixed]
     #           tx-checksum-sctp: off [fixed]
     #         scatter-gather: on
-    __feature_info_pattern = re.compile(
+    _feature_info_pattern = re.compile(
         r"Features for (?P<interface>[\w]*):[\s]*(?P<value>.*?)$", re.DOTALL
     )
-    __feature_settings_pattern = re.compile(
+    _feature_settings_pattern = re.compile(
         r"^[\s]*(?P<name>.*):(?P<value>.*?)?$", re.MULTILINE
     )
 
@@ -85,14 +101,14 @@ class DeviceFeatures:
         self._parse_feature_info(interface, device_feature_raw)
 
     def _parse_feature_info(self, interface: str, raw_str: str) -> None:
-        matched_features_info = self.__feature_info_pattern.match(raw_str)
+        matched_features_info = self._feature_info_pattern.match(raw_str)
         if not matched_features_info:
             raise LisaException(f"Cannot get {interface} features settings info")
 
         self.device_name = interface
         self.enabled_features = []
         for row in matched_features_info.group("value").splitlines():
-            feature_info = self.__feature_settings_pattern.match(row)
+            feature_info = self._feature_settings_pattern.match(row)
             if not feature_info:
                 raise LisaException(
                     f"Could not get feature setting for device {interface}"
@@ -147,6 +163,64 @@ class DeviceLinkSettings:
             )
 
 
+class DeviceRingBufferSettings:
+    # ~$ ethtool -g eth0
+    #   Ring parameters for eth0:
+    #   Pre-set maximums:
+    #   RX:             18811
+    #   RX Mini:        0
+    #   RX Jumbo:       0
+    #   TX:             2560
+    _ring_buffer_settings_pattern = re.compile(
+        r"(?P<param>.*):[ \t]*(?P<value>.*)$", re.MULTILINE
+    )
+
+    def __init__(self, interface: str, device_ring_buffer_settings_raw: str) -> None:
+        self._parse_ring_buffer_settings_info(
+            interface, device_ring_buffer_settings_raw
+        )
+
+    def _parse_ring_buffer_settings_info(self, interface: str, raw_str: str) -> None:
+        current_settings_info = _current_settings_pattern.match(raw_str)
+        max_settings_info = _max_settings_pattern.match(raw_str)
+        if (not current_settings_info) or (not max_settings_info):
+            raise LisaException(
+                f"Cannot get {interface} device ring buffer current and/or"
+                " max settings information"
+            )
+
+        self.device_name = interface
+        self.current_ring_buffer_settings: Dict[str, str] = {}
+        self.max_ring_buffer_settings: Dict[str, str] = {}
+        for row in current_settings_info.group("settings").splitlines():
+            current_setting = self._ring_buffer_settings_pattern.match(row)
+            if not current_setting:
+                continue
+            self.current_ring_buffer_settings[
+                current_setting.group("name")
+            ] = current_setting.group("value")
+
+        if not self.current_ring_buffer_settings:
+            raise LisaException(
+                f"Could not get current ring buffer settings for device {interface}"
+                " in the defined pattern"
+            )
+
+        for row in max_settings_info.group("settings").splitlines():
+            max_setting = self._ring_buffer_settings_pattern.match(row)
+            if not max_setting:
+                continue
+            self.max_ring_buffer_settings[
+                max_setting.group("name")
+            ] = max_setting.group("value")
+
+        if not self.max_ring_buffer_settings:
+            raise LisaException(
+                f"Could not get max ring buffer settings for device {interface}"
+                " in the defined pattern"
+            )
+
+
 class Ethtool(Tool):
     @property
     def command(self) -> str:
@@ -162,6 +236,7 @@ class Ethtool(Tool):
         self._device_channel_map: Dict[str, DeviceChannel] = {}
         self._device_features_map: Dict[str, DeviceFeatures] = {}
         self._device_link_settings_map: Dict[str, DeviceLinkSettings] = {}
+        self._device_ring_buffer_settings_map: Dict[str, DeviceRingBufferSettings] = {}
 
     def _install(self) -> bool:
         posix_os: Posix = cast(Posix, self.node.os)
@@ -245,6 +320,25 @@ class Ethtool(Tool):
 
         return device_link_settings
 
+    def get_device_ring_buffer_settings(
+        self, interface: str
+    ) -> DeviceRingBufferSettings:
+        if interface in self._device_ring_buffer_settings_map.keys():
+            return self._device_ring_buffer_settings_map[interface]
+
+        result = self.run(f"-g {interface}")
+        if result.exit_code != 0:
+            raise LisaException(
+                f"ethtool -g {interface} command got non-zero"
+                f" exit_code: {result.exit_code}"
+                f" stderr: {result.stderr}"
+            )
+
+        device_ring_buffer_settings = DeviceRingBufferSettings(interface, result.stdout)
+        self._device_ring_buffer_settings_map[interface] = device_ring_buffer_settings
+
+        return device_ring_buffer_settings
+
     def get_all_device_channels_info(self) -> List[DeviceChannel]:
         devices_channel_list = []
         self._get_device_list()
@@ -268,3 +362,13 @@ class Ethtool(Tool):
             devices_link_settings_list.append(self.get_device_link_settings(device))
 
         return devices_link_settings_list
+
+    def get_all_device_ring_buffer_settings(self) -> List[DeviceRingBufferSettings]:
+        devices_ring_buffer_settings_list = []
+        self._get_device_list()
+        for device in self._device_list:
+            devices_ring_buffer_settings_list.append(
+                self.get_device_ring_buffer_settings(device)
+            )
+
+        return devices_ring_buffer_settings_list
