33d599a28e7d7d4750347ee18738c776a86c7afd
WenyiQ <v-wenyiqian@microsoft.com>
2021-07-09 15:32:04 +0800

    ignore webs generated by sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -12,59 +12,60 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+
+sys.path.insert(0, os.path.abspath("../"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Linux Integration Services Automation (LISA)'
-copyright = '2021, Microsoft'
-author = 'Microsoft'
+project = "Linux Integration Services Automation (LISA)"
+copyright = "2021, Microsoft"
+author = "Microsoft"
 
-release = ''
+release = ""
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Add any Sphinx extension module names here, as strings. They can be extensions
+# coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinxemoji.sphinxemoji',
-    'sphinx.ext.autosectionlabel',
+    "sphinx.ext.autodoc",
+    "sphinxemoji.sphinxemoji",
+    "sphinx.ext.autosectionlabel",
+    "sphinx_copybutton",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
-source_suffix = ['.rst']
+source_suffix = [".rst"]
 
 # List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+# directories to ignore when looking for source files. This pattern also affects
+# html_static_path and html_extra_path.
+# exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
+# The theme to use for HTML and HTML Help pages.  See the documentation for a
+# list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# relative to this directory. They are copied after the builtin static files, so
+# a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
 
 html_theme_options = {
-    'collapse_navigation': False,
-    'navigation_depth': 2,
+    "collapse_navigation": False,
+    "navigation_depth": 2,
 }
 
 html_logo = "img/LSG_Logo.png"
 
 html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
+    "logo_only": True,
+    "display_version": False,
 }
