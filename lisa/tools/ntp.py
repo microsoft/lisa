790da2a0f0fc342ed7ee5b0a7c190f8aa58b53e6
Lili Deng <lildeng@microsoft.com>
2021-07-25 13:38:19 +0800

    Add tools used by time sync cases.

---
+++ lisa/tools/ntp.py
@@ -0,0 +1,72 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+import re
+from typing import cast
+
+from retry import retry
+
+from lisa.executable import Tool
+from lisa.operating_system import Debian, Posix, Redhat, Suse
+from lisa.tools import Echo
+from lisa.util import LisaException
+
+from .service import Service
+
+
+class Ntp(Tool):
+    # associd=0 status=0614 leap_none, sync_ntp, 1 event, freq_mode,
+    __leap_pattern = re.compile(r"([\w\W]*?)leap_none.*$", re.MULTILINE)
+    __no_server_set = "No association ID's returned"
+
+    @property
+    def command(self) -> str:
+        return "ntpq"
+
+    @property
+    def can_install(self) -> bool:
+        return True
+
+    def install(self) -> bool:
+        posix_os: Posix = cast(Posix, self.node.os)
+        posix_os.install_packages("ntp")
+        return self._check_exists()
+
+    @retry(tries=300, delay=1)  # type: ignore
+    def check_leap_code(self) -> None:
+        cmd_result = self.run("-c rv", shell=True, sudo=True, force_run=True)
+        if not self.__leap_pattern.match(cmd_result.stdout):
+            # leap_none - normal synchronized state
+            # leap_alarm - NTP was never synchronized
+            raise LisaException(
+                "The leap code is not lean_none."
+                " Please check ntp server setting and service status."
+            )
+
+    def set_server_setting(self) -> None:
+        cmd_result = self.run("-np", shell=True, sudo=True, force_run=True)
+        if self.__no_server_set in cmd_result.stdout:
+            echo = self.node.tools[Echo]
+            echo.run("server 0.pool.ntp.org >> /etc/ntp.conf", shell=True, sudo=True)
+            echo.run("server 1.pool.ntp.org >> /etc/ntp.conf", shell=True, sudo=True)
+            echo.run("server 2.pool.ntp.org >> /etc/ntp.conf", shell=True, sudo=True)
+            echo.run("server 3.pool.ntp.org >> /etc/ntp.conf", shell=True, sudo=True)
+
+    def restart(self) -> None:
+        if isinstance(self.node.os, Debian) or (
+            isinstance(self.node.os, Suse)
+            and self.node.os.information.version <= "11.0.0"
+        ):
+            service_name = "ntp"
+        elif isinstance(self.node.os, Redhat) or (
+            isinstance(self.node.os, Suse)
+            and self.node.os.information.version >= "12.0.0"
+        ):
+            service_name = "ntpd"
+        else:
+            posix_os: Posix = cast(Posix, self.node.os)
+            raise LisaException(
+                "Please double check ntp service name in distro "
+                f"{posix_os.name} {posix_os.information.version}"
+            )
+        service = self.node.tools[Service]
+        service.restart_service(service_name)
---
+++ lisa/tools/ntpstat.py
@@ -0,0 +1,51 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+from typing import Any, cast
+
+from retry import retry
+
+from lisa.executable import Tool
+from lisa.operating_system import Posix
+from lisa.util import LisaException
+
+from .gcc import Gcc
+from .git import Git
+
+
+class Ntpstat(Tool):
+    repo = "https://github.com/darkhelmet/ntpstat"
+    __not_sync = "unsynchronised"
+
+    @property
+    def command(self) -> str:
+        return self._command
+
+    @property
+    def can_install(self) -> bool:
+        return True
+
+    def _initialize(self, *args: Any, **kwargs: Any) -> None:
+        self._command = "ntpstat"
+
+    def _install_from_src(self) -> None:
+        tool_path = self.get_tool_path()
+        self.node.shell.mkdir(tool_path, exist_ok=True)
+        git = self.node.tools[Git]
+        git.clone(self.repo, tool_path)
+        gcc = self.node.tools[Gcc]
+        code_path = tool_path.joinpath("ntpstat")
+        gcc.compile(f"{code_path}/ntpstat.c", "ntpstat")
+        self._command = "./ntpstat"
+
+    def install(self) -> bool:
+        posix_os: Posix = cast(Posix, self.node.os)
+        posix_os.install_packages("ntpstat")
+        if not self._check_exists():
+            self._install_from_src()
+        return self._check_exists()
+
+    @retry(exceptions=LisaException, tries=40, delay=0.5)  # type: ignore
+    def check_time_sync(self) -> None:
+        cmd_result = self.run(shell=True, sudo=True, force_run=True)
+        if self.__not_sync in cmd_result.stdout:
+            raise LisaException("Local time is unsynchronised with time server.")
