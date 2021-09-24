0c96a1f0dd19355a2051d808628a2bbef79194e2
Divyansh <divyan@microsoft.com>
2021-09-24 12:49:05 -0700

    Update runner.py

--- lisa/runner.py
+++ lisa/runner.py
@@ -310,7 +310,7 @@ class RootRunner(Action):
 
                 with CodeTimer("has idle worker loop", self._log):
                     while task_manager.has_idle_worker():
-                        for runner in remaining_runners:
+                        for runner in list(remaining_runners):
                             self._log.debug(f"Checking runner {runner.id} for tasks...")
                             while not runner.is_done and task_manager.has_idle_worker():
                                 # fetch a task and submit
