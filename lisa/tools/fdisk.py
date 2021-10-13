0c526e59ecfe558d3391d623da88713a21f6a1f2
Lili Deng <lildeng@microsoft.com>
2021-10-13 13:11:15 +0800

    Implement _get_partitions and refine delete_partitions to delete more than 1 partitions.

--- lisa/tools/fdisk.py
+++ lisa/tools/fdisk.py
@@ -1,18 +1,13 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
-from enum import Enum
-from typing import cast
+import re
+from typing import List, cast
 
 from lisa.executable import Tool
 from lisa.operating_system import Posix
-from lisa.util import LisaException
+from lisa.util import find_patterns_in_lines
 
-from .mkfs import Mkfsext, Mkfsxfs
-
-FileSystem = Enum(
-    "mkfs",
-    ["xfs", "ext2", "ext3", "ext4"],
-)
+from .mkfs import FileSystem, Mkfs
 
 
 class Fdisk(Tool):
@@ -36,33 +31,42 @@ class Fdisk(Tool):
         # "" => Use default 2048 for 'First sector'
         # "" => Use default 2147483647 as 'Last sector'
         # w => write table to disk and exit
+        mkfs = self.node.tools[Mkfs]
         self.node.execute(
             f"(echo n; echo p; echo 1; echo ; echo; echo ; echo w) | "
             f"{self.command} {disk_name}",
             shell=True,
             sudo=True,
         )
-        if file_system == FileSystem.xfs:
-            mkfs_xfs = self.node.tools[Mkfsxfs]
-            mkfs_xfs.mkfs(f"{disk_name}p1", str(file_system))
-        elif file_system in [FileSystem.ext2, FileSystem.ext3, FileSystem.ext4]:
-            mkfs_ext = self.node.tools[Mkfsext]
-            mkfs_ext.mkfs(f"{disk_name}p1", str(file_system))
-        else:
-            raise LisaException(f"Unrecognized file system {file_system}.")
+        # get the partition, e.g. /dev/sdc1 or /dev/nvme0n1p1
+        partition_disk = self._get_partitions(disk_name)
+        mkfs.format_disk(partition_disk[0], file_system)
 
-    def delete_partition(self, disk_name: str) -> None:
+    def delete_partitions(self, disk_name: str) -> None:
         """
-        disk: delete one partition against the disk.
-        Delete the only partition of this disk.
+        disk: delete all partitions against the disk.
+        Get the partitions of this disk.
+        Delete all partitions of this disk.
         """
-        # d => delete a partition
-        # w => write table to disk and exit
-        self.node.execute(
-            f"(echo d; echo w) | {self.command} {disk_name}", shell=True, sudo=True
-        )
+        partitions = self._get_partitions(disk_name)
+        for _ in range(1, len(partitions) + 1):
+            # d => delete a partition
+            # w => write table to disk and exit
+            self.node.execute(
+                f"(echo d; echo ; echo w) | {self.command} {disk_name}",
+                shell=True,
+                sudo=True,
+            )
 
     def _install(self) -> bool:
         posix_os: Posix = cast(Posix, self.node.os)
         posix_os.install_packages("util-linux")
         return self._check_exists()
+
+    def _get_partitions(self, disk_name: str) -> List[str]:
+        partition_pattern = re.compile(rf"({disk_name}p[0-9]|{disk_name}[0-9])+")
+        cmd_result = self.node.execute(
+            "ls -lt /dev/sd* && ls -lt /dev/nvme*", shell=True, sudo=True
+        )
+        matched = find_patterns_in_lines(cmd_result.stdout, [partition_pattern])
+        return matched[0]
