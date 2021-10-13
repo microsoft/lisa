e6538e9105fd7a9e66a25f387a751a48c96de969
Sonia Sharma <sosha@microsoft.com>
2021-10-12 19:57:36 -0700

    tools: modinfo: add method to get filename from modinfo output

--- lisa/tools/modinfo.py
+++ lisa/tools/modinfo.py
@@ -5,11 +5,17 @@ import re
 from typing import Any
 
 from lisa.executable import Tool
-from lisa.util import find_patterns_in_lines
+from lisa.util import find_patterns_in_lines, get_matched_str
 
 
 class Modinfo(Tool):
+    # (note - version_pattern is found only when LIS drivers are installed)
+    #  modinfo hv_vmbus
+    #   filename:       /lib/modules/2.6.32-754.29.1.el6.x86_64/kernel/drivers/hv/
+    #                   hv_vmbus.ko
+    #   version:        3.1
     __version_pattern = re.compile(r"^version:[ \t]*([^ \n]*)")
+    __filename_pattern = re.compile(r"filename:[ \t]*([^ \n]*)")
 
     @property
     def command(self) -> str:
@@ -54,3 +60,19 @@ class Modinfo(Tool):
         )
         found_version = find_patterns_in_lines(output, [self.__version_pattern])
         return found_version[0][0] if found_version[0] else ""
+
+    def get_filename(
+        self,
+        mod_name: str,
+        force_run: bool = False,
+        no_info_log: bool = True,
+        no_error_log: bool = True,
+    ) -> str:
+        output = self.get_info(
+            mod_name=mod_name,
+            force_run=force_run,
+            no_info_log=no_info_log,
+            no_error_log=no_error_log,
+        )
+        found_filename = get_matched_str(output, self.__filename_pattern)
+        return found_filename if found_filename else ""
