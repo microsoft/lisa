2990c80f4d933c9f55216a16df724c9f7c21f376
SrikanthMyakam <m.srikanth.m@gmail.com>
2021-10-29 22:49:35 +0530

    Update kernel_source_installer.py

--- lisa/transformers/kernel_source_installer.py
+++ lisa/transformers/kernel_source_installer.py
@@ -307,7 +307,7 @@ class RepoLocation(BaseLocation):
             self._log.info(f"checkout code from: '{runbook.ref}'")
             git.checkout(ref=runbook.ref, cwd=code_path)
         
-        result = self._node.execute("git log --pretty=format:%h | head -1", cwd=code_path, sudo=True)
+        result = self._node.execute("git log --pretty=format:%h | head -1", cwd=code_path, shell=True))
         self._log.info(f"HEAD is now at : '{result}'")
 
         return code_path
