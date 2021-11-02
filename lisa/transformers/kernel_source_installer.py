3020be52ca3d2eda535b9f7a36d5c9d0a5a68264
SrikanthMyakam <m.srikanth.m@gmail.com>
2021-11-03 01:04:01 +0530

    Update kernel_source_installer.py

--- lisa/transformers/kernel_source_installer.py
+++ lisa/transformers/kernel_source_installer.py
@@ -316,8 +316,9 @@ class RepoLocation(BaseLocation):
         
         result = self._node.execute("git log --pretty=format:%h | head -1", cwd=code_path, shell=True)
         self._log.info(f"HEAD is now at : {result}")
-        kernel_version = self._node.execute("make kernelrelease 2>/dev/null",
+        result = self._node.execute("make kernelrelease 2>/dev/null",
                               cwd=code_path, shell=True)
+        kernel_version = result.stdout                             
         self._log.info(f"Kernel source version : {kernel_version}")
         
         return code_path
