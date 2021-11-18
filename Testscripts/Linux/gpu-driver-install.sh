9dbbb1e154080722a056ff4041c84ee1a5bdbf9b
Lili Deng <lildeng@microsoft.com>
2021-11-18 20:11:11 +0800

    Set CUDA Driver version for ubuntu.

--- Testscripts/Linux/gpu-driver-install.sh
+++ Testscripts/Linux/gpu-driver-install.sh
@@ -59,7 +59,7 @@ function skip_test() {
 			fi
 			if [[ $DISTRO == "ubuntu_x" ]]; then
 				# skip other ubuntu version than 16.04, 18.04, 20.04, 21.04
-				if [[ $VERSION_ID != "16.04" && $VERSION_ID != "18.04" && $VERSION_ID != "20.04" && $VERSION_ID != "21.04" ]]; then
+				if [[ $VERSION_ID != "16.04" && $VERSION_ID != "18.04" && $VERSION_ID != "20.04" && $VERSION_ID != "21.04" && $VERSION_ID != "21.10" ]]; then
 					unsupport_flag=1
 				fi
 			fi
@@ -125,6 +125,7 @@ function InstallCUDADrivers() {
 
 	ubuntu*)
 		GetOSVersion
+		CUDADriverVersion="10.1.168-1"
 		# Temporary fix till driver for ubuntu19 and ubuntu20 series list under http://developer.download.nvidia.com/compute/cuda/repos/
 		if [[ $os_RELEASE =~ 19.* ]] || [[ $os_RELEASE =~ 20.* ]] || [[ $os_RELEASE =~ 21.* ]]; then
 			LogMsg "There is no cuda driver for $os_RELEASE, used the one for 18.10"
