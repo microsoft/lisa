33c036156fc3739d9160f68e71c0f31fc350b74b
Divyansh <divyan@microsoft.com>
2021-11-19 15:11:40 -0800

    Add sudo to wget

--- lisa/base_tools/wget.py
+++ lisa/base_tools/wget.py
@@ -34,11 +34,12 @@ class Wget(Tool):
         filename: str = "",
         overwrite: bool = True,
         executable: bool = False,
+        sudo: bool = False,
     ) -> str:
         is_valid_url(url)
 
         # create folder when it doesn't exist
-        self.node.execute(f"mkdir -p {file_path}", shell=True)
+        self.node.execute(f"mkdir -p {file_path}", shell=True, sudo=sudo)
         # combine download file path
         # TODO: support current lisa folder in pathlib.
         # So that here can use the corresponding path format.
@@ -46,10 +47,10 @@ class Wget(Tool):
         if overwrite:
             extra_param = " -nc "
         if filename:
-            run_command = f" {url} {extra_param} -O {download_path}"
+            run_command = f" '{url}' {extra_param} -O {download_path}"
         else:
-            run_command = f" {url} {extra_param} -P {download_path}"
-        command_result = self.run(run_command, no_error_log=True, shell=True)
+            run_command = f" '{url}' {extra_param} -P {download_path}"
+        command_result = self.run(run_command, no_error_log=True, shell=True, sudo=sudo)
         matched_result = self.__pattern_path.match(command_result.stdout)
         if matched_result:
             download_file_path = matched_result.group("path")
@@ -58,10 +59,12 @@ class Wget(Tool):
                 f"cannot find file path in stdout of '{run_command}', it may cause by "
                 f"download failed or pattern mismatch. stdout: {command_result.stdout}"
             )
-        actual_file_path = self.node.execute(f"ls {download_file_path}", shell=True)
+        actual_file_path = self.node.execute(
+            f"ls {download_file_path}", shell=True, sudo=sudo
+        )
         if actual_file_path.exit_code != 0:
             raise LisaException(f"File {actual_file_path} doesn't exist.")
         if executable:
-            self.node.execute(f"chmod +x {actual_file_path}")
+            self.node.execute(f"chmod +x {actual_file_path}", sudo=sudo)
 
         return actual_file_path.stdout
