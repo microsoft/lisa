f7be713d736f78bae2d261e1e7d8fd9ac6ab3ce3
Lili Deng <lildeng@microsoft.com>
2021-12-24 11:13:03 +0800

    Add nvme perf test case.

---
+++ microsoft/testsuites/performance/common.py
@@ -0,0 +1,73 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+from typing import Dict, List, Optional
+
+from lisa import Node, notifier
+from lisa.environment import Environment
+from lisa.notifier import DiskPerformanceMessage, DiskSetupType, DiskType
+from lisa.tools import FIOMODES, Fio, FIOResult
+from lisa.util import dict_to_fields
+
+
+def run_perf_test(
+    node: Node,
+    start_iodepth: int,
+    max_iodepth: int,
+    filename: str,
+    num_jobs: Optional[List[int]] = None,
+    block_size: int = 4,
+    time: int = 120,
+    size_gb: int = 0,
+    numjob: int = 0,
+    overwrite: bool = False,
+) -> List[DiskPerformanceMessage]:
+    fio_result_list: List[FIOResult] = []
+    fio = node.tools[Fio]
+    numjobiterator = 0
+    for mode in FIOMODES:
+        iodepth = start_iodepth
+        numjobindex = 0
+        if num_jobs:
+            numjob = num_jobs[numjobindex]
+        while iodepth <= max_iodepth:
+            fio_result = fio.launch(
+                name=f"iteration{numjobiterator}",
+                filename=filename,
+                mode=mode.name,
+                time=time,
+                size_gb=size_gb,
+                block_size=f"{block_size}K",
+                iodepth=iodepth,
+                overwrite=overwrite,
+                numjob=numjob,
+            )
+            fio_result_list.append(fio_result)
+            iodepth = iodepth * 2
+            numjobindex += 1
+            numjobiterator += 1
+    fio_messages: List[DiskPerformanceMessage] = fio.create_performance_messages(
+        fio_result_list
+    )
+    return fio_messages
+
+
+def handle_and_send_back_results(
+    core_count: int,
+    disk_count: int,
+    environment: Environment,
+    disk_setup_type: DiskSetupType,
+    disk_type: DiskType,
+    test_case_name: str,
+    fio_messages: List[DiskPerformanceMessage],
+    block_size: int = 4,
+) -> None:
+    information: Dict[str, str] = environment.get_information()
+    for fio_message in fio_messages:
+        fio_message = dict_to_fields(information, fio_message)
+        fio_message.core_count = core_count
+        fio_message.disk_count = disk_count
+        fio_message.test_case_name = test_case_name
+        fio_message.block_size = block_size
+        fio_message.disk_setup_type = disk_setup_type
+        fio_message.disk_type = disk_type
+        notifier.notify(fio_message)
---
+++ microsoft/testsuites/performance/nvmeperf.py
@@ -0,0 +1,79 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+from typing import List
+
+from assertpy import assert_that
+
+from lisa import (
+    Node,
+    TestCaseMetadata,
+    TestSuite,
+    TestSuiteMetadata,
+    simple_requirement,
+)
+from lisa.environment import Environment
+from lisa.features import Nvme, NvmeSettings
+from lisa.notifier import DiskPerformanceMessage, DiskSetupType, DiskType
+from lisa.tools import Echo, Lscpu
+from microsoft.testsuites.performance.common import (
+    handle_and_send_back_results,
+    run_perf_test,
+)
+
+
+@TestSuiteMetadata(
+    area="nvme",
+    category="performance",
+    description="""
+    This test suite is to validate NVMe disk performance of Linux VM using fio tool.
+    """,
+)
+class NvmePerformace(TestSuite):  # noqa
+    TIME_OUT = 5000
+
+    @TestCaseMetadata(
+        description="""
+        This test case uses fio to test NVMe disk performance.
+        """,
+        priority=3,
+        timeout=TIME_OUT,
+        requirement=simple_requirement(
+            supported_features=[NvmeSettings(disk_count=8)],
+        ),
+    )
+    def perf_nvme(self, node: Node, environment: Environment) -> None:
+        nvme = node.features[Nvme]
+        nvme_namespaces = nvme.get_namespaces()
+        disk_count = len(nvme_namespaces)
+        assert_that(disk_count).described_as(
+            "At least 1 NVMe disk for fio testing."
+        ).is_greater_than(0)
+        filename = ":".join(nvme_namespaces)
+        echo = node.tools[Echo]
+        # This will have kernel avoid sending IPI to finish I/O on the issuing CPUs
+        # if they are not on the same NUMA node of completion CPU.
+        # This setting will give a better and more stable IOPS.
+        for nvme_namespace in nvme_namespaces:
+            # /dev/nvme0n1 => nvme0n1
+            disk_name = nvme_namespace.split("/")[-1]
+            echo.write_to_file(
+                "0",
+                node.get_pure_path(f"/sys/block/{disk_name}/queue/rq_affinity"),
+                sudo=True,
+            )
+        cpu = node.tools[Lscpu]
+        core_count = cpu.get_core_count()
+        start_iodepth = core_count
+        max_iodepth = start_iodepth * 256
+        fio_messages: List[DiskPerformanceMessage] = run_perf_test(
+            node, start_iodepth, max_iodepth, filename, numjob=core_count
+        )
+        handle_and_send_back_results(
+            core_count,
+            disk_count,
+            environment,
+            DiskSetupType.raw,
+            DiskType.nvme,
+            "perf_nvme",
+            fio_messages,
+        )
