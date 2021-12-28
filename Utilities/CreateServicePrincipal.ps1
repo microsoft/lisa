57a0f11d02acf94c28feadf3065bb302133da8a8
Lili Deng <lildeng@microsoft.com>
2021-12-28 12:00:16 +0800

    Update create SP based on latest Azure PS.

--- Utilities/CreateServicePrincipal.ps1
+++ Utilities/CreateServicePrincipal.ps1
@@ -5,150 +5,12 @@
 # This script will create Azure Active Directory application, and Service Principal that can access Azure resources
 # It will return clientID, tenantID, client secret that can be used for LISAv2 test on Azure
 ##############################################################################################
-
-# https://gallery.technet.microsoft.com/Generate-a-random-and-5c879ed5
-function New-SWRandomPassword {
-    <#
-    .Synopsis
-        Generates one or more complex passwords designed to fulfill the requirements for Active Directory
-    .DESCRIPTION
-        Generates one or more complex passwords designed to fulfill the requirements for Active Directory
-        Strongly recommend the subscription owner executes the script. Non-owner may see warning message
-        such as "don't have enough permission to assign a role" in New-AzRoleAssignment
-    .EXAMPLE
-        New-SWRandomPassword
-        C&3SX6Kn
-
-        Will generate one password with a length between 8 and 12 chars.
-    .EXAMPLE
-        New-SWRandomPassword -MinPasswordLength 8 -MaxPasswordLength 12 -Count 4
-        7d&5cnaB
-        !Bh776T"Fw
-        9"C"RxKcY
-        %mtM7#9LQ9h
-
-       Will generate four passwords, each with a length of between 8 and 12 chars.
-    .EXAMPLE
-        New-SWRandomPassword -InputStrings abc, ABC, 123 -PasswordLength 4
-        3ABa
-
-        Generates a password with a length of 4 containing at least one char from each InputString
-    .EXAMPLE
-        New-SWRandomPassword -InputStrings abc, ABC, 123 -PasswordLength 4 -FirstChar abcdefghijkmnpqrstuvwxyzABCEFGHJKLMNPQRSTUVWXYZ
-        3ABa
-
-        Generates a password with a length of 4 containing at least one char from each InputString that will start with a letter from
-        the string specified with the parameter FirstChar
-    .OUTPUTS
-        [String]
-    .NOTES
-        Written by Simon Wahlin, blog.simonw.se
-        I take no responsibility for any issues caused by this script.
-    .FUNCTIONALITY
-        Generates random passwords
-    #>
-    [CmdletBinding(DefaultParameterSetName='FixedLength',ConfirmImpact='None')]
-    [OutputType([String])]
-    Param
-    (
-        # Specifies minimum password length
-        [Parameter(Mandatory=$false,
-                   ParameterSetName='RandomLength')]
-        [ValidateScript({$_ -gt 0})]
-        [Alias('Min')]
-        [int]$MinPasswordLength = 8,
-
-        # Specifies maximum password length
-        [Parameter(Mandatory=$false,
-                   ParameterSetName='RandomLength')]
-        [ValidateScript({
-                if($_ -ge $MinPasswordLength){$true}
-                else{Throw 'Max value cannot be lesser than min value.'}})]
-        [Alias('Max')]
-        [int]$MaxPasswordLength = 12,
-
-        # Specifies a fixed password length
-        [Parameter(Mandatory=$false, ParameterSetName='FixedLength')]
-        [ValidateRange(1,2147483647)]
-        [int]$PasswordLength = 8,
-
-        # Specifies an array of strings containing character groups from which the password will be generated.
-        # At least one char from each group (string) will be used.
-        [String[]]$InputStrings = @('abcdefghijkmnpqrstuvwxyz', 'ABCEFGHJKLMNPQRSTUVWXYZ', '23456789', '!#%=_'),
-
-        # Specifies a string containing a character group from which the first character in the password will be generated.
-        # Useful for systems which requires first char in password to be alphabetic.
-        [String] $FirstChar,
-
-        # Specifies number of passwords to generate.
-        [ValidateRange(1,2147483647)]
-        [int]$Count = 1
-    )
-    Begin {
-        Function Get-Seed{
-            # Generate a seed for randomization
-            $RandomBytes = New-Object -TypeName 'System.Byte[]' 4
-            $Random = New-Object -TypeName 'System.Security.Cryptography.RNGCryptoServiceProvider'
-            $Random.GetBytes($RandomBytes)
-            [BitConverter]::ToUInt32($RandomBytes, 0)
-        }
-    }
-    Process {
-        For($iteration = 1;$iteration -le $Count; $iteration++){
-            $Password = @{}
-            # Create char arrays containing groups of possible chars
-            [char[][]]$CharGroups = $InputStrings
-
-            # Create char array containing all chars
-            $AllChars = $CharGroups | ForEach-Object {[Char[]]$_}
-
-            # Set password length
-            if($PSCmdlet.ParameterSetName -eq 'RandomLength')
-            {
-                if($MinPasswordLength -eq $MaxPasswordLength) {
-                    # If password length is set, use set length
-                    $PasswordLength = $MinPasswordLength
-                }
-                else {
-                    # Otherwise randomize password length
-                    $PasswordLength = ((Get-Seed) % ($MaxPasswordLength + 1 - $MinPasswordLength)) + $MinPasswordLength
-                }
-            }
-
-            # If FirstChar is defined, randomize first char in password from that string.
-            if($PSBoundParameters.ContainsKey('FirstChar')){
-                $Password.Add(0,$FirstChar[((Get-Seed) % $FirstChar.Length)])
-            }
-            # Randomize one char from each group
-            Foreach($Group in $CharGroups) {
-                if($Password.Count -lt $PasswordLength) {
-                    $Index = Get-Seed
-                    While ($Password.ContainsKey($Index)){
-                        $Index = Get-Seed
-                    }
-                    $Password.Add($Index,$Group[((Get-Seed) % $Group.Count)])
-                }
-            }
-
-            # Fill out with chars from $AllChars
-            for($i=$Password.Count;$i -lt $PasswordLength;$i++) {
-                $Index = Get-Seed
-                While ($Password.ContainsKey($Index)){
-                    $Index = Get-Seed
-                }
-                $Password.Add($Index,$AllChars[((Get-Seed) % $AllChars.Count)])
-            }
-            Write-Output -InputObject $(-join ($Password.GetEnumerator() | Sort-Object -Property Name | Select-Object -ExpandProperty Value))
-        }
-    }
-}
-
 function Write-Prompt($Message) {
     Write-Host $Message -ForegroundColor Yellow
 }
 function New-ServicePrincipal() {
     $ErrorActionPreference = "Stop"
-    Login-AzAccount
+    Connect-AzAccount
     $subscription = Get-AzSubscription
 
     $subCount = 1
@@ -184,14 +46,11 @@ function New-ServicePrincipal() {
     $homePage = "http://" + $Identifier
 
     $tenantId = $subscription.TenantId
-    $clientSecret = New-SWRandomPassword -PasswordLength 30
-    $securestring = ConvertTo-SecureString $clientSecret -AsPlainText -Force
 
     Write-Host "Create Active Directory application..."
-    $application = New-AzADApplication -DisplayName $identifier -HomePage $homePage -IdentifierUris $idUris -Password $securestring
-
-    $ClientId = $application.ApplicationId
-
+    $application = New-AzADApplication -DisplayName $identifier
+    $objectId = $application.Id
+    $ClientId = $application.AppId
     $ErrorActionPreference = "Continue"
 
     while ($true) {
@@ -203,6 +62,11 @@ function New-ServicePrincipal() {
     }
 
     $ErrorActionPreference = "Stop"
+    $context = Get-AzContext
+    Connect-AzureAD -TenantId $context.Tenant.TenantId -AccountId $context.Account.Id
+    $secretStartDate = Get-Date
+    $secretEndDate = $secretStartDate.AddYears(10)
+    $clientSecret = New-AzureADApplicationPasswordCredential -ObjectId $objectId -CustomKeyIdentifier "GraphClientSecret" -StartDate $secretStartDate -EndDate $secretEndDate
 
     Write-Host "Create Service Principal..."
     New-AzADServicePrincipal -ApplicationId $ClientId
@@ -228,11 +92,17 @@ function New-ServicePrincipal() {
     Write-Host "Assign roles to Service Principal..."
     try {
         if ($Privilege -eq 2) {
-            New-AzRoleAssignment -RoleDefinitionName "Owner" -ApplicationId $ClientId
+            $role = "Owner"
+        } else {
+            $role = "Contributor"
         }
-        else {
-            New-AzRoleAssignment -RoleDefinitionName "Contributor" -ApplicationId $ClientId
+        $subscriptionId = $context.Subscription.Id
+        $spRoleAssignment = @{
+                      ObjectId = $spCheck.id;
+                      RoleDefinitionName = $role;
+                      Scope = "/subscriptions/$subscriptionId"
         }
+        New-AzRoleAssignment @spRoleAssignment
         Write-Host "Successfully created Service Principal and assigned role...`n"
     } catch {
         $line = $_.InvocationInfo.ScriptLineNumber
@@ -261,8 +131,7 @@ function New-ServicePrincipal() {
     Write-Host "SUBSCRIPTION_ID:" $subscription.Id
     Write-Host "CLIENT_ID:      " $clientId
     Write-Host "TENANT_ID:      " $tenantId
-    Write-Host "CLIENT_SECRET:  " $clientSecret
+    Write-Host "CLIENT_SECRET:  " $clientSecret.Value
 }
 
 New-ServicePrincipal
-
