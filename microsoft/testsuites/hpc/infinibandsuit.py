95520e6e5211b771227d60742367a57d300dbdce
Kameron Carr <kameroncarr@microsoft.com>
2022-01-19 16:19:40 -0800

    Write ping-pong test
    Uses two VMs to run a simple test of sending data back and forth to ensure RDMA / InfiniBand is functioning.

--- microsoft/testsuites/hpc/infinibandsuit.py
+++ microsoft/testsuites/hpc/infinibandsuit.py
@@ -4,6 +4,7 @@
 from assertpy import assert_that
 
 from lisa import (
+    Environment,
     Logger,
     Node,
     TestCaseMetadata,
@@ -18,7 +19,7 @@ from lisa.util import SkippedException
 
 
 @TestSuiteMetadata(
-    area="core",
+    area="hpc",
     category="functional",
     description="""
     Tests the functionality of infiniband.
@@ -62,7 +63,6 @@ class InfinibandSuit(TestSuite):
             "rdma_cm",
             "iw_cm",
             "ib_cm",
-            "ib_core",
         ]
 
         for module in expected_modules:
@@ -100,3 +100,51 @@ class InfinibandSuit(TestSuite):
             assert_that(modprobe.is_module_loaded(module)).described_as(
                 f"Module {module} is not loaded."
             ).is_true()
+
+    @TestCaseMetadata(
+        description="""
+        This test case will
+        1. Identify the infiniband devices and their cooresponding network interfaces
+        2. Run several ping-pong tests to check RDMA / Infiniband functionality
+        """,
+        priority=1,
+        requirement=simple_requirement(
+            supported_features=[Infiniband],
+            min_count=2,
+        ),
+    )
+    def verify_ping_pong(self, environment: Environment, log: Logger) -> None:
+        # Constants
+        ping_pong_tests = ["ibv_rc_pingpong", "ibv_uc_pingpong", "ibv_ud_pingpong"]
+
+        server_node = environment.nodes[0]
+        client_node = environment.nodes[1]
+
+        infiniband = server_node.features[Infiniband]
+        ib_interfaces = infiniband.get_ib_interfaces()
+
+        for interface in ib_interfaces:
+            ib_device_name = interface[0]
+            ip_addr = interface[2]
+
+            for test in ping_pong_tests:
+                server_process = server_node.execute_async(
+                    f"{test} -g 0 -d {ib_device_name}"
+                )
+                client_process = client_node.execute_async(
+                    f"{test} -g 0 -d {ib_device_name} {ip_addr}"
+                )
+
+                client_result = client_process.wait_result()
+                client_result.assert_exit_code(
+                    0,
+                    f"Client ping-pong test {test} failed with exit code "
+                    f"{client_result.exit_code} and output {client_result.stdout}",
+                )
+
+                server_result = server_process.wait_result()
+                server_result.assert_exit_code(
+                    0,
+                    f"Server ping-pong test {test} failed with exit code "
+                    f"{server_result.exit_code} and output {server_result.stdout}",
+                )
