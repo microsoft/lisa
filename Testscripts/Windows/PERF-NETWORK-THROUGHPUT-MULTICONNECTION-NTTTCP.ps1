bca58d98cf0711c1c20ac915675037e944d6d075
SrikanthMyakam <m.srikanth.m@gmail.com>
2022-01-25 21:57:26 +0530

    Update PERF-NETWORK-THROUGHPUT-MULTICONNECTION-NTTTCP.ps1

--- Testscripts/Windows/PERF-NETWORK-THROUGHPUT-MULTICONNECTION-NTTTCP.ps1
+++ Testscripts/Windows/PERF-NETWORK-THROUGHPUT-MULTICONNECTION-NTTTCP.ps1
@@ -78,9 +78,9 @@ function Main {
 
         Write-LogInfo "Update kernel to latest..."
         Run-LinuxCmd -ip $clientVMData.PublicIP -port 1112 -username $user `
-                    -password $password -command "sudo rpm -ivh https://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-ml-5.16.2-1.el7.elrepo.x86_64.rpm && sed -i 's/GRUB_DEFAULT=.*/GRUB_DEFAULT=1/' /etc/default/grub && sudo grub2-mkconfig -o /boot/efi/EFI/centos/grub.cfg" -runAsSudo | Out-Null
+                    -password $password -command "sudo rpm -ivh https://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-ml-5.16.2-1.el7.elrepo.x86_64.rpm ; sed -i 's/GRUB_DEFAULT=.*/GRUB_DEFAULT=0/' /etc/default/grub && sudo grub2-mkconfig -o /boot/efi/EFI/centos/grub.cfg" -runAsSudo | Out-Null
         Run-LinuxCmd -ip $clientVMData.PublicIP -port 1111 -username $user `
-                    -password $password -command "sudo rpm -ivh https://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-ml-5.16.2-1.el7.elrepo.x86_64.rpm && sed -i 's/GRUB_DEFAULT=.*/GRUB_DEFAULT=1/' /etc/default/grub && sudo grub2-mkconfig -o /boot/efi/EFI/centos/grub.cfg" -runAsSudo | Out-Null
+                    -password $password -command "sudo rpm -ivh https://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-ml-5.16.2-1.el7.elrepo.x86_64.rpm ; sed -i 's/GRUB_DEFAULT=.*/GRUB_DEFAULT=0/' /etc/default/grub && sudo grub2-mkconfig -o /boot/efi/EFI/centos/grub.cfg" -runAsSudo | Out-Null
 
                     
         # Restart VM to apply systemd setting
