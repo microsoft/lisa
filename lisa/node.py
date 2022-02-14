900a744190da229e0a821c27fa966484c1703815
Matthew G McGovern <mamcgove@microsoft.com>
2022-02-14 14:10:08 -0800

    Fix: move freespace function to Node instead of RemoteNode

--- lisa/node.py
+++ lisa/node.py
@@ -262,6 +262,23 @@ class Node(subclasses.BaseClassWithRunbookMixin, ContextMixin, InitializableMixi
         self.log.debug(f"capturing system information to {saved_path}.")
         self.os.capture_system_information(saved_path)
 
+    def find_partition_with_freespace(self, size_in_gb: int) -> str:
+        if self.os.is_windows:
+            raise NotImplementedError()
+
+        df = self.tools[Df]
+        home_partition = df.get_partition_by_mountpoint("/home")
+        if home_partition and df.check_partition_size(home_partition, size_in_gb):
+            return home_partition.mountpoint
+
+        mnt_partition = df.get_partition_by_mountpoint("/mnt")
+        if mnt_partition and df.check_partition_size(mnt_partition, size_in_gb):
+            return mnt_partition.mountpoint
+
+        raise LisaException(
+            f"No partition with Required disk space of {size_in_gb}GB found"
+        )
+
     def _initialize(self, *args: Any, **kwargs: Any) -> None:
         self.log.info(f"initializing node '{self.name}' {self}")
         self.shell.initialize()
@@ -425,23 +442,6 @@ class RemoteNode(Node):
         result = echo.run(working_path, shell=True)
 
         return self.get_pure_path(result.stdout)
-
-    def find_partition_with_freespace(self, size_in_gb: int) -> str:
-        if self.os.is_windows:
-            raise NotImplementedError()
-
-        df = self.tools[Df]
-        home_partition = df.get_partition_by_mountpoint("/home")
-        if home_partition and df.check_partition_size(home_partition, size_in_gb):
-            return home_partition.mountpoint
-
-        mnt_partition = df.get_partition_by_mountpoint("/mnt")
-        if mnt_partition and df.check_partition_size(mnt_partition, size_in_gb):
-            return mnt_partition.mountpoint
-
-        raise LisaException(
-            f"No partition with Required disk space of {size_in_gb}GB found"
-        )
 
 
 class LocalNode(Node):
