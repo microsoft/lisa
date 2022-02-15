2bcbee9ad7f2c8e642fc3a212afe448e10c2f357
Lili Deng <lildeng@microsoft.com>
2022-02-15 11:09:00 +0800

    fix

--- Testscripts/Windows/PERF-STORAGE-MULTIDISK-RAID0-FIO.ps1
+++ Testscripts/Windows/PERF-STORAGE-MULTIDISK-RAID0-FIO.ps1
@@ -216,6 +216,8 @@ function Main {
 
     try {
         Provision-VMsForLisa -allVMData $allVMData -installPackagesOnRoleNames "none"
+        $TestProvider.RestartAllDeployments($AllVMData)
+        Start-Sleep 120
         New-ConstantsFile -LogDir $LogDir -CurrentTestData $CurrentTestData
 
         Write-LogInfo "Starting test..."
