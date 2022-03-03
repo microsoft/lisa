64e4db5fa2b904b608cd5c6660a87446adf05bfb
Divyansh <divyan@microsoft.com>
2022-03-03 12:08:43 -0800

    Add CPU offline-online storage and network workload testcases

--- microsoft/testsuites/cpu/cpusuite.py
+++ microsoft/testsuites/cpu/cpusuite.py
@@ -2,6 +2,10 @@
 # Licensed under the MIT license.
 from __future__ import annotations
 
+from typing import cast
+
+from assertpy import assert_that
+
 from lisa import (
     Logger,
     Node,
@@ -10,6 +14,9 @@ from lisa import (
     TestSuiteMetadata,
     simple_requirement,
 )
+from lisa.environment import Environment
+from lisa.node import RemoteNode
+from lisa.tools import Fio, Iperf3, Kill
 from microsoft.testsuites.cpu.common import verify_cpu_hot_plug
 
 
@@ -51,3 +58,75 @@ class CPUSuite(TestSuite):
     )
     def verify_cpu_hot_plug(self, log: Logger, node: Node) -> None:
         verify_cpu_hot_plug(log, node)
+
+    @TestCaseMetadata(
+        description="""
+            This test will check cpu hotplug with storage workload.
+            The cpu hotplug steps are same as `verify_cpu_hot_plug` test case.
+            """,
+        priority=3,
+    )
+    def verify_cpu_offline_storage_workload(self, log: Logger, node: Node) -> None:
+        # run fio process asynchronously on the node
+        try:
+            fio_process = node.tools[Fio].launch_async(
+                name="workload",
+                filename="fiodata",
+                mode="readwrite",
+                iodepth=128,
+                numjob=10,
+                time=300,
+                block_size="1M",
+                size_gb=1,
+                group_reporting=False,
+                overwrite=True,
+                time_based=True,
+            )
+
+            # verify cpu hotplug functionality
+            verify_cpu_hot_plug(log, node)
+
+            # verify that the fio was running when hotplug was triggered
+            assert_that(
+                fio_process.is_running(),
+                "Storage workload was not running during CPUhotplug",
+            ).is_true()
+        finally:
+            # kill fio process
+            node.tools[Kill].by_name("fio")
+
+    @TestCaseMetadata(
+        description="""
+            This test will check cpu hotplug with network workload.
+            The cpu hotplug steps are same as `verify_cpu_hot_plug` test case.
+            """,
+        priority=3,
+        requirement=simple_requirement(
+            min_count=2,
+        ),
+    )
+    def verify_cpu_offline_network_workload(
+        self, log: Logger, environment: Environment
+    ) -> None:
+        server = cast(RemoteNode, environment.nodes[0])
+        client = cast(RemoteNode, environment.nodes[1])
+
+        try:
+            # run iperf3 process asynchronously on the server and client
+            server.tools[Iperf3].run_as_server_async()
+            client_iperf_process = client.tools[Iperf3].run_as_client_async(
+                server.internal_address
+            )
+
+            # verify cpu hotplug functionality
+            verify_cpu_hot_plug(log, client)
+
+            # verify that the iperf3 was running when hotplug was triggered
+            assert_that(
+                client_iperf_process.is_running(),
+                "Network workload was not running during CPUhotplug",
+            ).is_true()
+        finally:
+            # kill fio process
+            server.tools[Kill].by_name("iperf3")
+            client.tools[Kill].by_name("iperf3")
