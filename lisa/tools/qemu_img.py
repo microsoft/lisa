bc4bea093f99015c8cf68ad80f6bfb43ee87f9fb
Chris Gunn <chrisgun@microsoft.com>
2022-03-04 13:32:57 -0800

    Move qemu-img command into QemuImg tool

--- lisa/tools/qemu_img.py
+++ lisa/tools/qemu_img.py
@@ -9,6 +9,14 @@ class QemuImg(Tool):
     def command(self) -> str:
         return "qemu-img"
 
+    def create_new_qcow2(self, output_img_path: str, size_mib: int) -> None:
+        self.run(
+            f'create -f qcow2 "{output_img_path}" {size_mib}M',
+            force_run=True,
+            expected_exit_code=0,
+            expected_exit_code_failure_message="Failed to create disk image.",
+        )
+
     def create_diff_qcow2(self, output_img_path: str, backing_img_path: str) -> None:
         params = f'create -F qcow2 -f qcow2 -b "{backing_img_path}" "{output_img_path}"'
         self.run(
