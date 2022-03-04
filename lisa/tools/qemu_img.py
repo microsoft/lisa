b153c2c77d6228312446bed14671a7a1e6048020
Chris Gunn <chrisgun@microsoft.com>
2022-03-03 16:57:38 -0800

    Bug fix for QemuImg

--- lisa/tools/qemu_img.py
+++ lisa/tools/qemu_img.py
@@ -10,7 +10,7 @@ class QemuImg(Tool):
         return "qemu-img"
 
     def create_diff_qcow2(self, output_img_path: str, backing_img_path: str) -> None:
-        params = f"create -F qcow2 -f qcow2 -b {backing_img_path} {output_img_path}"
+        params = f'create -F qcow2 -f qcow2 -b "{backing_img_path}" "{output_img_path}"'
         self.run(
             params,
             force_run=True,
