b63781292ad9b55994d8c0bb4caa8c3dc4c2ebc8
Lili Deng <lildeng@microsoft.com>
2022-03-23 22:45:00 +0800

    Add set_datetime and get for tool hwclock.

--- lisa/tools/hwclock.py
+++ lisa/tools/hwclock.py
@@ -1,10 +1,14 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
-
+from datetime import datetime
 from typing import cast
+
+from dateutil.parser import parser
+from retry import retry
 
 from lisa.executable import Tool
 from lisa.operating_system import Posix
+from lisa.util import LisaException
 
 
 class Hwclock(Tool):
@@ -25,3 +29,25 @@ class Hwclock(Tool):
     def set_rtc_clock_to_system_time(self) -> None:
         cmd_result = self.run("--systohc", shell=True, sudo=True)
         cmd_result.assert_exit_code()
+
+    def set_datetime(self, time: datetime) -> None:
+        self.run(
+            f"--set --date='{time}'",
+            shell=True,
+            sudo=True,
+            force_run=True,
+            expected_exit_code=0,
+            expected_exit_code_failure_message="fail to set date",
+        )
+
+    @retry(exceptions=LisaException, tries=20, delay=0.5)  # type: ignore
+    def get(self, no_error_log: bool = True) -> datetime:
+        command_result = self.run(
+            no_error_log=no_error_log,
+            force_run=True,
+            sudo=True,
+            timeout=10,
+            expected_exit_code=0,
+            expected_exit_code_failure_message="fail to get date",
+        )
+        return parser().parse(command_result.stdout)
