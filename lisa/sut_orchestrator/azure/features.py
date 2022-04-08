cc7253ae2f2dab499cd530858e60f9fc97e40486
Lili Deng <lildeng@microsoft.com>
2022-04-08 12:01:43 +0800

    Skip hibernation case when enable Availability Set.

--- lisa/sut_orchestrator/azure/features.py
+++ lisa/sut_orchestrator/azure/features.py
@@ -32,6 +32,7 @@ from lisa.tools import Dmesg, Lspci, Modprobe
 from lisa.util import (
     LisaException,
     NotMeetRequirementException,
+    SkippedException,
     constants,
     find_patterns_in_lines,
 )
@@ -984,6 +985,12 @@ class Hibernation(AzureFeatureMixin, features.Hibernation):
 
     @classmethod
     def _enable_hibernation(cls, *args: Any, **kwargs: Any) -> None:
+        parameters: Any = kwargs.get("arm_parameters")
+        if parameters.use_availability_sets:
+            raise SkippedException(
+                "Hibernation cannot be enabled on Virtual Machines created in an"
+                " Availability Set."
+            )
         template: Any = kwargs.get("template")
         log = cast(Logger, kwargs.get("log"))
         log.debug("updating arm template to support vm hibernation.")
