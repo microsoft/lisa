9aa97f967553d2e4dd1e68dc3cdfc836055e26ff
Divyansh <divyan@microsoft.com>
2022-05-08 18:58:24 -0700

    Add can_install to dnsmasq tool

--- lisa/tools/dnsmasq.py
+++ lisa/tools/dnsmasq.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT license.
 
 from lisa.executable import Tool
+from lisa.operating_system import Posix
 from lisa.tools.kill import Kill
 
 
@@ -9,6 +10,15 @@ class Dnsmasq(Tool):
     @property
     def command(self) -> str:
         return "dnsmasq"
+
+    @property
+    def can_install(self) -> bool:
+        return True
+
+    def install(self) -> bool:
+        posix_os: Posix = self.node.os  # type: ignore
+        posix_os.install_packages("dnsmasq")
+        return self._check_exists()
 
     def start(
         self,
