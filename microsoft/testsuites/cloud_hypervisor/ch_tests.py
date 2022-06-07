f5547ee2b48422a2be21be7a106920b82c2dbe63
Anirudh Rayabharam <anrayabh@microsoft.com>
2022-06-07 08:03:22 +0000

    Various style fixes and other cleanups

--- microsoft/testsuites/cloud_hypervisor/ch_tests.py
+++ microsoft/testsuites/cloud_hypervisor/ch_tests.py
@@ -4,8 +4,17 @@ from pathlib import Path
 
 from assertpy.assertpy import assert_that
 
-from lisa import Logger, Node, TestCaseMetadata, TestSuite, TestSuiteMetadata
-from microsoft.testsuites.cloud_hypervisor.ch_tests_tool import CloudHypervisorTests 
+from lisa import (
+    Logger,
+    Node,
+    TestCaseMetadata,
+    TestSuite,
+    TestSuiteMetadata,
+    node_requirement,
+    schema,
+    search_space,
+)
+from microsoft.testsuites.cloud_hypervisor.ch_tests_tool import CloudHypervisorTests
 
 
 @TestSuiteMetadata(
@@ -22,18 +31,17 @@ class CloudHypervisorTestSuite(TestSuite):
             Runs cloud-hypervisor integration tests.
         """,
         priority=3,
+        requirement=node_requirement(
+            node=schema.NodeSpace(
+                core_count=16, memory_mb=search_space.IntRange(min=16 * 1024)
+            ),
+        ),
     )
     def verify_cloud_hypervisor_integration_tests(
-        self,
-        log: Logger,
-        node: Node,
-        log_path: Path
+        self, log: Logger, node: Node, log_path: Path
     ) -> None:
-        excluded_tests = [ "test_vfio" ]
-        failures = node.tools[CloudHypervisorTests].run_tests(
-            "integration",
-            excluded_tests
-        )
+        skip_tests = ["test_vfio"]
+        failures = node.tools[CloudHypervisorTests].run_tests("integration", skip_tests)
         assert_that(failures).is_empty()
 
     @TestCaseMetadata(
@@ -41,12 +49,14 @@ class CloudHypervisorTestSuite(TestSuite):
             Runs cloud-hypervisor live migration tests.
         """,
         priority=3,
+        requirement=node_requirement(
+            node=schema.NodeSpace(
+                core_count=16, memory_mb=search_space.IntRange(min=16 * 1024)
+            ),
+        ),
     )
     def verify_cloud_hypervisor_live_migration_tests(
-        self,
-        log: Logger,
-        node: Node,
-        log_path: Path
+        self, log: Logger, node: Node, log_path: Path
     ) -> None:
         failures = node.tools[CloudHypervisorTests].run_tests(
             "integration-live-migration"
--- microsoft/testsuites/cloud_hypervisor/ch_tests_tool.py
+++ microsoft/testsuites/cloud_hypervisor/ch_tests_tool.py
@@ -1,17 +1,17 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 import re
-import string
-from pathlib import Path, PurePath
-from typing import Any, List, Type, cast
+from pathlib import PurePath
+from typing import Any, List, Type
 
 from lisa.executable import Tool
-from lisa.operating_system import CBLMariner, Posix
-from lisa.tools import Git, Docker, Modprobe
+from lisa.operating_system import CBLMariner
+from lisa.tools import Docker, Git, Modprobe
+
 
 class CloudHypervisorTests(Tool):
-    TIME_OUT = 3600
-    
+    TIME_OUT = 1800
+
     repo = "https://github.com/cloud-hypervisor/cloud-hypervisor.git"
 
     cmd_path: PurePath
@@ -29,43 +29,48 @@ class CloudHypervisorTests(Tool):
     def dependencies(self) -> List[Type[Tool]]:
         return [Git, Docker]
 
-    def run_tests(self, test_type: str, exclude: List[str] = []) -> None:
+    def run_tests(self, test_type: str, skip: List[str] = None) -> List[str]:
         self.node.tools[Modprobe].load("openvswitch")
-        self.node.tools[Docker].start()
 
-        skip_args = ' '.join(map(lambda t: f"--skip {t}", excluded_tests))
+        if skip is not None:
+            skip_args = " ".join(map(lambda t: f"--skip {t}", skip))
+        else:
+            skip_args = ""
+
         result = self.run(
             f"tests --{test_type} -- -- {skip_args}",
             timeout=self.TIME_OUT,
             force_run=True,
             cwd=self.repo_root,
             no_info_log=False,  # print out result of each test
-            # expected_exit_code=0,
+            expected_exit_code=0,
         )
 
-        return self._extract_failed_tests(result.stdout);
-        
+        return self._extract_failed_tests(result.stdout)
+
     def _initialize(self, *args: Any, **kwargs: Any) -> None:
         tool_path = self.get_tool_path(use_global=True)
         self.repo_root = tool_path / "cloud-hypervisor"
         self.cmd_path = self.repo_root / "scripts" / "dev_cli.sh"
         print(str(self.cmd_path))
- 
+
     def _install(self) -> bool:
         git = self.node.tools[Git]
         git.clone(self.repo, self.get_tool_path(use_global=True))
         if isinstance(self.node.os, CBLMariner):
             daemon_json_file = "/etc/docker/daemon.json"
-            daemon_json = '{"default-ulimits":{"nofile":{"Hard":65535,"Name":"nofile","Soft":65535}}}'
+            daemon_json = '{"default-ulimits":{"nofile":{"Hard":65535,"Name":"nofile","Soft":65535}}}'  # noqa: E501
             self.node.execute(
                 f"echo '{daemon_json}' | sudo tee {daemon_json_file}",
-                shell=True,sudo=True
+                shell=True,
+                sudo=True,
             )
+        self.node.tools[Docker].start()
 
         return self._check_exists()
 
     def _extract_failed_tests(self, output: str) -> List[str]:
-        lines = output.split('\n')
+        lines = output.split("\n")
         failures = []
         found_failures = False
         failure_re = re.compile("\\s+(\\S+)")
@@ -80,6 +85,4 @@ class CloudHypervisorTests(Tool):
                     found_failures = False
                     continue
                 failures.append(matches.group(1))
-        print(failures)
         return failures
-
