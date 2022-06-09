bcd4e0fd117bd70b342fc624b5927f019c96164e
Matthew McGovern <mamcgove@microsoft.com>
2022-06-08 18:22:21 -0700

    Kill: add option to not assert exit code for kill

--- lisa/tools/kill.py
+++ lisa/tools/kill.py
@@ -20,18 +20,30 @@ class Kill(Tool):
     def can_install(self) -> bool:
         return False
 
-    def by_name(self, process_name: str, signum: int = SIGKILL) -> None:
+    def by_name(
+        self, process_name: str, signum: int = SIGKILL, assert_exit_code: bool = True
+    ) -> None:
         running_processes = self.node.tools[Pgrep].get_processes(process_name)
         for process in running_processes:
-            self.by_pid(process.id, signum)
+            self.by_pid(process.id, signum, assert_exit_code)
 
-    def by_pid(self, pid: str, signum: int = SIGKILL) -> None:
-        self.run(
-            f"-{signum} {pid}",
-            shell=True,
-            sudo=True,
-            force_run=True,
-            expected_exit_code=0,
-            expected_exit_code_failure_message="fail to run "
-            f"{self.command} -{signum} {pid}",
-        )
+    def by_pid(
+        self, pid: str, signum: int = SIGKILL, assert_exit_code: bool = True
+    ) -> None:
+        if assert_exit_code:
+            self.run(
+                f"-{signum} {pid}",
+                shell=True,
+                sudo=True,
+                force_run=True,
+                expected_exit_code=0,
+                expected_exit_code_failure_message="fail to run "
+                f"{self.command} -{signum} {pid}",
+            )
+        else:
+            self.run(
+                f"-{signum} {pid}",
+                shell=True,
+                sudo=True,
+                force_run=True,
+            )
