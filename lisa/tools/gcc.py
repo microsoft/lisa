1c965dd79e54e8b4baa06f87190195cfa1274231
Lili Deng <lildeng@microsoft.com>
2022-06-21 22:48:19 +0800

    add version method for tool gcc

--- lisa/tools/gcc.py
+++ lisa/tools/gcc.py
@@ -1,13 +1,23 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+import re
 from typing import cast
+
+from semver import VersionInfo
 
 from lisa.executable import Tool
 from lisa.operating_system import Posix
+from lisa.util import LisaException
 
 
 class Gcc(Tool):
+    # gcc (Ubuntu 11.2.0-19ubuntu1) 11.2.0
+    # gcc (GCC) 8.5.0 20210514 (Red Hat 8.5.0-10)
+    _version_pattern = re.compile(
+        r"gcc \(.*\) (?P<major>\d+).(?P<minor>(\d+)).(?P<patch>(\d+))", re.M
+    )
+
     @property
     def command(self) -> str:
         return "gcc"
@@ -21,6 +31,20 @@ class Gcc(Tool):
             self.run(f"{filename} -o {output_name}")
         else:
             self.run(filename)
+
+    def get_version(self) -> VersionInfo:
+        output = self.run(
+            "--version",
+            expected_exit_code=0,
+            expected_exit_code_failure_message="fail to get gcc dumpversion",
+        ).stdout
+        matched_version = self._version_pattern.match(output)
+        if matched_version:
+            major = matched_version.group("major")
+            minor = matched_version.group("minor")
+            patch = matched_version.group("patch")
+            return VersionInfo(int(major), int(minor), int(patch))
+        raise LisaException("fail to get gcc version")
 
     def _install(self) -> bool:
         posix_os: Posix = cast(Posix, self.node.os)
