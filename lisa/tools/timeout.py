07f069a940d2515b8ef05a0cd36ca62c4750047d
Matthew McGovern <mamcgove@microsoft.com>
2022-06-30 09:06:46 -0700

    add coreutil timeout tool

---
+++ lisa/tools/timeout.py
@@ -0,0 +1,30 @@
+from lisa.executable import ExecutableResult, Tool
+
+
+class Timeout(Tool):
+    @property
+    def command(self) -> str:
+        return "timeout"
+
+    @property
+    def can_install(self) -> bool:
+        return False
+
+    def run_with_timeout(
+        self, command: str, signal: str, timeout: int, kill_timeout: int = 0
+    ) -> ExecutableResult:
+        # timeout [OPTION] DURATION COMMAND [ARG]...
+        params = f"-s {signal} --preserve-status {timeout} {command}"
+        if kill_timeout:
+            params = f"--kill-after {kill_timeout} " + params
+        command_timeout = timeout
+        if kill_timeout:
+            command_timeout = kill_timeout + 10
+
+        return self.run(
+            parameters=params,
+            force_run=True,
+            shell=True,
+            sudo=True,
+            timeout=command_timeout,
+        )
