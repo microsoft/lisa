52791fe02aaff156ec600b66a8952aa67a174f12
Divyansh <somm14divi@gmail.com>
2022-07-29 11:46:12 -0700

    Add serial console basic test case

---
+++ microsoft/testsuites/core/serial_console.py
@@ -0,0 +1,42 @@
+from assertpy.assertpy import assert_that
+
+from lisa import (
+    Logger,
+    Node,
+    TestCaseMetadata,
+    TestSuite,
+    TestSuiteMetadata,
+    simple_requirement,
+)
+from lisa.features import SerialConsole
+
+
+@TestSuiteMetadata(
+    area="serial_console",
+    category="functional",
+    description="""
+    This tests functionality of connecting to serial console.
+    """,
+)
+class SerialConsoleSuite(TestSuite):
+    @TestCaseMetadata(
+        description="""
+        The test runs `echo back` command on serial console and verifies
+        that the command has been successfully put to the
+        serial console.
+        """,
+        priority=3,
+        requirement=simple_requirement(
+            supported_features=[SerialConsole],
+        ),
+    )
+    def verify_serial_console(self, log: Logger, node: Node) -> None:
+        command = "echo back"
+        serial_console = node.features[SerialConsole]
+        _ = serial_console.read()
+        serial_console.write(command)
+        output = serial_console.read()
+
+        assert_that(
+            output, "output from serial console should be equal to command"
+        ).is_equal_to(command)
