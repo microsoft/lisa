898d4d6b60c9195c425dde480b737cb05161f1b7
Lili Deng <lildeng@microsoft.com>
2022-08-09 16:29:53 +0800

    Add sudo for ping tool to avoid Operation not permitted error.

--- lisa/tools/ping.py
+++ lisa/tools/ping.py
@@ -1,5 +1,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
+import re
 from typing import Optional
 
 from lisa.executable import Tool
@@ -9,6 +10,12 @@ INTERNET_PING_ADDRESS = "8.8.8.8"
 
 
 class Ping(Tool):
+    # ping: SO_BINDTODEVICE: Operation not permitted
+    _no_permission_pattern = re.compile(
+        r"ping: SO_BINDTODEVICE: Operation not permitted",
+        re.M,
+    )
+
     @property
     def command(self) -> str:
         return "ping"
@@ -20,6 +27,7 @@ class Ping(Tool):
         count: int = 5,
         interval: float = 0.2,
         package_size: Optional[int] = None,
+        sudo: bool = False,
     ) -> Process:
         if not target:
             target = INTERNET_PING_ADDRESS
@@ -29,7 +37,7 @@ class Ping(Tool):
         if package_size:
             args += f" -s {package_size}"
 
-        return self.run_async(args, force_run=True)
+        return self.run_async(args, force_run=True, sudo=sudo)
 
     def ping(
         self,
@@ -39,6 +47,7 @@ class Ping(Tool):
         interval: float = 0.2,
         package_size: Optional[int] = None,
         ignore_error: bool = False,
+        sudo: bool = False,
     ) -> bool:
         if not target:
             target = INTERNET_PING_ADDRESS
@@ -48,7 +57,20 @@ class Ping(Tool):
             count=count,
             interval=interval,
             package_size=package_size,
+            sudo=sudo,
         ).wait_result()
+        # for some distro like RHEL, ping with -I nic_name needs sudo
+        # otherwise, ping fails with below output:
+        # 'ping: SO_BINDTODEVICE: Operation not permitted'
+        if not sudo and self._no_permission_pattern.findall(result.stdout):
+            result = self.ping_async(
+                target=target,
+                nic_name=nic_name,
+                count=count,
+                interval=interval,
+                package_size=package_size,
+                sudo=True,
+            ).wait_result()
         if not ignore_error:
             result.assert_exit_code(
                 message="failed on ping. The server may not be reached.",
