baef2ed8cc31f15990f6799f70c7b5348583903c
Matthew G McGovern <mamcgove@microsoft.com>
2022-08-16 13:09:53 -0700

    DPDK: add kernel check

--- microsoft/testsuites/dpdk/common.py
+++ microsoft/testsuites/dpdk/common.py
@@ -2,10 +2,19 @@
 # Licensed under the MIT license.
 
 from lisa import Node
-from lisa.operating_system import Debian, Oracle, Redhat, Suse, Ubuntu
-from lisa.util import UnsupportedDistroException
+from lisa.operating_system import Debian, Oracle, Posix, Redhat, Suse, Ubuntu
+from lisa.util import UnsupportedDistroException, UnsupportedKernelException
 
 DPDK_STABLE_GIT_REPO = "https://dpdk.org/git/dpdk-stable"
+
+
+def _check_kernel_version(node: Node, kernel_version: str, supported_os: bool) -> None:
+    if isinstance(node.os, Posix):
+        kernel = node.os.get_kernel_information()
+        if supported_os and kernel.version < kernel_version:
+            raise UnsupportedKernelException(
+                node.os, f"DPDK requires kernel version {kernel_version} on this OS"
+            )
 
 
 def check_dpdk_support(node: Node) -> None:
@@ -15,12 +24,16 @@ def check_dpdk_support(node: Node) -> None:
     if isinstance(node.os, Debian):
         if isinstance(node.os, Ubuntu):
             supported = node.os.information.version >= "18.4.0"
+            _check_kernel_version(node, "4.15.0", supported)
         else:
             supported = node.os.information.version >= "10.0.0"
+            _check_kernel_version(node, "4.19.0", supported)
     elif isinstance(node.os, Redhat) and not isinstance(node.os, Oracle):
         supported = node.os.information.version >= "7.5.0"
+        _check_kernel_version(node, "3.10.0", supported)
     elif isinstance(node.os, Suse):
         supported = node.os.information.version >= "15.0.0"
+        _check_kernel_version(node, "4.12.14", supported)
     else:
         # this OS is not supported
         raise UnsupportedDistroException(
