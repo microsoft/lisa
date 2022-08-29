d12f394ae3f1cc0603e21702c170192711c82ef7
Anirudh Rayabharam <anrayabh@microsoft.com>
2022-08-29 10:31:07 +0000

    Support for CBLMariner in libvirt TCK testsuite
    - Separate set of expected failures
    - Modify /etc/libvirt/qemu.conf to configure libvirt to run qemu
      as root. Otherwise multiple tests fail with permission errors.
    - Use hidepid=0 for /proc filesystem while mounting.
    - Start libvirtd explicitly since it is always in failed state after
      boot in Mariner.
    - Use cpanminus instead of cpan to install libvirt-tck dependencies.
    
    Signed-off-by: Anirudh Rayabharam <anrayabh@microsoft.com>

--- microsoft/testsuites/libvirt/libvirt_tck_tool.py
+++ microsoft/testsuites/libvirt/libvirt_tck_tool.py
@@ -1,5 +1,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
+import time
 import xml.etree.ElementTree as ETree
 from dataclasses import dataclass
 from pathlib import Path, PurePath
@@ -10,9 +11,9 @@ from assertpy.assertpy import assert_that
 from lisa import Environment, notifier
 from lisa.executable import Tool
 from lisa.messages import SubTestMessage, TestStatus, create_test_result_message
-from lisa.operating_system import Posix
+from lisa.operating_system import CBLMariner, Posix, Ubuntu
 from lisa.testsuite import TestResult
-from lisa.tools import Git
+from lisa.tools import Chmod, Echo, Git, Sed, Service, Whoami
 
 
 @dataclass
@@ -26,23 +27,36 @@ class LibvirtTck(Tool):
 
     # The failures in these tests need to be investigated and fixed. Until then, treat
     # them as expected failures.
-    EXPECTED_FAILURES = [
-        "100-apply-verify-host_t",
-        "220-no-ip-spoofing_t",
-        "230-no-mac-broadcast_t",
-    ]
+    EXPECTED_FAILURES = {
+        CBLMariner.__name__: [
+            "nwfilter_050-apply-verify-host_t",
+        ],
+        Ubuntu.__name__: [
+            "100-apply-verify-host_t",
+            "220-no-ip-spoofing_t",
+            "230-no-mac-broadcast_t",
+        ],
+    }
 
     repo = "https://gitlab.com/libvirt/libvirt-tck.git"
     deps = [
+        "cpanminus",
         "expat-devel",
         "gcc",
+        "glibc-devel",
+        "kernel-headers",
         "libguestfs-tools",
         "libsys-virt-perl",
         "libtest-xml-perl",
+        "libvirt",
+        "libvirt-client",
         "libvirt-daemon-system",
         "libvirt-dev",
         "make",
         "perl",
+        "perl-App-cpanminus",
+        "perl-Sys-Virt",
+        "perl-XML-SAX",
         "qemu-kvm",
         "qemu-system-x86",
     ]
@@ -90,6 +104,8 @@ class LibvirtTck(Tool):
                 r.status,
             )
 
+        archive_path = self.repo_root / "results.tar.gz"
+        self.node.tools[Chmod].chmod(str(archive_path), "a+r", sudo=True)
         self.node.shell.copy_back(
             self.repo_root / "results.tar.gz",
             log_path / "libvirt_tck_results.tar.gz",
@@ -113,22 +129,71 @@ class LibvirtTck(Tool):
     def _install(self) -> bool:
         self._install_dep()
 
+        if isinstance(self.node.os, CBLMariner):
+            # tell libvirt to run qemu as root
+            libvirt_qemu_conf = PurePath("/etc/libvirt/qemu.conf")
+            self.node.tools[Echo].write_to_file(
+                'user = "root"',
+                libvirt_qemu_conf,
+                sudo=True,
+                append=True,
+            )
+            self.node.tools[Echo].write_to_file(
+                'group = "root"',
+                libvirt_qemu_conf,
+                sudo=True,
+                append=True,
+            )
+
+            username = self.node.tools[Whoami].get_username()
+            self.node.execute(f"usermod -aG libvirt {username}", shell=True, sudo=True)
+
+            # Workaround for error:
+            #
+            # error from service: GDBus.Error:org.gtk.GDBus.UnmappedGError.Quark
+            # ._g_2dfile_2derror_2dquark.Code4: Failed to open file
+            # “/proc/2192/status”: No such file or directory
+            self.node.tools[Sed].substitute(
+                "hidepid=2",
+                "hidepid=0",
+                "/etc/fstab",
+                sudo=True,
+            )
+
+            self.node.reboot()
+
+            # After reboot, libvirtd service is in failed state and needs to
+            # be restarted manually. Doing it immediately after restarts
+            # fails. So wait for a while before restarting libvirtd.
+            # This is an issue in Mariner and below lines can be removed once
+            # it has been addressed.
+            tries = 0
+            while tries <= 10:
+                try:
+                    self.node.tools[Service].restart_service("libvirtd")
+                    break
+                except Exception:
+                    time.sleep(1)
+                    tries += 1
+
         modules_to_install = " ".join(
             [
+                "inc::latest",
                 "Module::Build",
                 "IO::Interface::Simple",
                 "Net::OpenSSH",
             ]
         )
         self.node.execute(
-            f"PERL_MM_USE_DEFAULT=1 cpan install {modules_to_install}",
+            f"cpanm install {modules_to_install}",
             sudo=True,
-            shell=True,
             timeout=self.TIME_OUT,
         )
-        self.node.execute("perl Build.PL", cwd=self.repo_root, expected_exit_code=0)
         self.node.execute(
-            "./Build installdeps",
+            "perl Build.PL", cwd=self.repo_root, expected_exit_code=0, sudo=True
+        )
+        self.node.execute(
+            "cpanm --installdeps .",
             cwd=self.repo_root,
             sudo=True,
             expected_exit_code=0,
@@ -141,10 +206,11 @@ class LibvirtTck(Tool):
         return self._check_exists()
 
     def _is_expected_failure(self, name: str) -> bool:
+        distro = type(self.node.os).__name__
         # The test name in the output appears with its full path. Whereas the
         # self.EXPECTED_FAILURES contain just the test names. That's why
         # endswith() is used here.
-        return len([f for f in self.EXPECTED_FAILURES if name.endswith(f)]) > 0
+        return len([f for f in self.EXPECTED_FAILURES[distro] if name.endswith(f)]) > 0
 
     def _extract_test_results(self, output: str) -> List[LibvirtTckTestResult]:
         results: List[LibvirtTckTestResult] = []
