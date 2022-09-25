2db73fc75da2bd8db2a4bcb079ab4f79015dce39
Lili Deng <lildeng@microsoft.com>
2022-09-25 22:40:43 +0800

    extend timeout for unattended-upgrade

--- microsoft/testsuites/core/dns.py
+++ microsoft/testsuites/core/dns.py
@@ -47,6 +47,7 @@ class Dns(TestSuite):
                 shell=True,
                 expected_exit_code=0,
                 expected_exit_code_failure_message="fail to run unattended-upgrade",
+                timeout=1200,
             )
         elif isinstance(node.os, Posix):
             node.os.update_packages("")
