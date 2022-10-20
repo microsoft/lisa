0121015af44ddbe9e5a94602b7d79b9ff4fefa34
smit-gardhariya <gardhariya.smit@gmail.com>
2022-10-20 12:40:58 +0530

    PR review changes, use perf_disk for FIO test

--- microsoft/testsuites/performance/storageperf.py
+++ microsoft/testsuites/performance/storageperf.py
@@ -2,12 +2,9 @@
 # Licensed under the MIT license.
 
 import inspect
-import json
-import os
-from pathlib import PurePosixPath
-from pathlib import Path
+import uuid
+from pathlib import Path, PurePosixPath
 from typing import Any, Dict, cast
-
 
 from assertpy import assert_that
 
@@ -29,8 +26,6 @@ from lisa.node import RemoteNode
 from lisa.operating_system import SLES, Debian, Redhat
 from lisa.testsuite import TestResult
 from lisa.tools import FileSystem, Lscpu, Mkfs, Mount, NFSClient, NFSServer, Sysctl
-from lisa.tools import Fio,Rm
-
 from lisa.util import SkippedException
 from microsoft.testsuites.performance.common import (
     perf_disk,
@@ -196,12 +191,13 @@ class StoragePerformance(TestSuite):
 
     @TestCaseMetadata(
         description="""
-        This test case uses fio to test data disk performance with 4K block size.
+        This test case uses fio to test data disk performance.
         """,
         priority=3,
         timeout=TIME_OUT,
     )
-    def perf_storage_generic_fio_test(self,
+    def perf_storage_generic_fio_test(
+        self,
         log: Logger,
         node: Node,
         environment: Environment,
@@ -210,92 +206,68 @@ class StoragePerformance(TestSuite):
         variables: Dict[str, Any],
     ) -> None:
         testcases = variables.get("fio_testcase_list", None)
-        if (not testcases):
-            testcases_file_path = os.path.join(
-                os.path.abspath(__file__).replace(os.path.basename(__file__), ""),
-                "default_fio_testcases.json",
-            )
-            with open(testcases_file_path, "r") as f:
-                testcases = json.loads(f.read())["testcases"]
+        if not testcases or len(testcases) == 0:
+            # Run default testcases
+            for size in [512, 4096]:
+                id = str(uuid.uuid4())
+                start_iodepth = 1
+                max_iodepth = 4
+                num_jobs = []
 
-        log.debug(f"Given testcases with params : {testcases}")
-        fio = node.tools[Fio]
-        rm = node.tools[Rm]
-        fio_result = {}
+                core_count = node.tools[Lscpu].get_core_count()
+                iodepth_iter = start_iodepth
+                while iodepth_iter <= max_iodepth:
+                    num_jobs.append(min(iodepth_iter, core_count))
+                    iodepth_iter = iodepth_iter * 2
 
-        for testcase in testcases:
-            test_name = testcase["testcase_name"]
-            fio_result[test_name] = {}
-            fio_result[test_name]["runtime_param"] = testcase
-
-            try:
-                path = node.get_working_path()
-
-                log.info(f"Executing the FIO testcase : {test_name}")
-
-                name = testcase.get("name", None)
-                filename = testcase.get("filename", None)
-                mode = testcase.get("mode", None)
-                iodepth = testcase.get("iodepth", None)
-                numjob = testcase.get("numjob", None)
-                time = testcase.get("time", 120)
-                ssh_timeout = testcase.get("ssh_timeout", 6400)
-                block_size = testcase.get("block_size", "4k")
-                size_mb = testcase.get("size_mb", 0)
-                direct = testcase.get("direct", True)
-                gtod_reduce = testcase.get("gtod_reduce", False)
-                ioengine = testcase.get("ioengine", "libaio")
-                group_reporting = testcase.get("group_reporting", True)
-                overwrite = testcase.get("overwrite", False)
-                time_based = testcase.get("time_based", False)
-
-                result = fio.launch(
-                    name=name,
-                    filename=filename,
-                    mode=mode,
-                    iodepth=iodepth,
-                    numjob=numjob,
-                    time=time,
-                    ssh_timeout=ssh_timeout,
-                    block_size=block_size,
-                    size_gb=size_mb,
-                    direct=direct,
-                    gtod_reduce=gtod_reduce,
-                    ioengine=ioengine,
-                    group_reporting=group_reporting,
-                    overwrite=overwrite,
-                    time_based=time_based,
-                    cwd=path
+                perf_disk(
+                    node=node,
+                    start_iodepth=start_iodepth,
+                    max_iodepth=max_iodepth,
+                    filename=f"{size}_MB_FIO_{id}",
+                    test_result=result,
+                    test_name=f"{size}_MB_4K",
+                    num_jobs=num_jobs,
+                    block_size=4,
+                    time=240,
+                    size_mb=size,
                 )
+        else:
+            for testcase in testcases:
+                id = str(uuid.uuid4())
+                try:
+                    start_iodepth = testcase.get("start_iodepth", 1)
+                    max_iodepth = testcase.get("max_iodepth", 1)
+                    num_jobs = []
+                    iodepth_iter = start_iodepth
+                    core_count = node.tools[Lscpu].get_core_count()
+                    while iodepth_iter <= max_iodepth:
+                        num_jobs.append(min(iodepth_iter, core_count))
+                        iodepth_iter = iodepth_iter * 2
 
-                fio_result[test_name]["status"] = "Succeeded"
-                fio_result[test_name]["error"] = ""
-                fio_result[test_name]["metrics"] = {
-                    "qdepth": result.qdepth,
-                    "mode": result.mode,
-                    "iops": str(result.iops),
-                    "latency": str(result.latency),
-                    "iodepth": result.iodepth,
-                }
+                    time = testcase.get("time", 240)
+                    block_size = testcase.get("block_size", 4)
+                    size_mb = testcase.get("size_mb", 512)
+                    overwrite = testcase.get("overwrite", False)
 
-            except Exception as e:
-                fio_result[test_name]["status"] = "Failed"
-                fio_result[test_name]["error"] = str(e)
-                fio_result[test_name]["metrics"] = {}
-                log.error(e)
+                    test_name = f"{size_mb}_MB_{block_size}K"
+                    log.debug(f"Executing the FIO testcase : {test_name}")
 
-            rm.remove_directory(str(path.joinpath(f"{filename}*")), sudo=True)
-
-        log.debug(fio_result)
-
-        # Saving the result
-        report_path = node.get_working_path().joinpath("fio_report")
-        report_file = report_path.joinpath("fio_test_result.json")
-        if not os.path.exists(report_path):
-            os.mkdir(report_path)
-
-        with open(report_file, "w") as f:
-            f.write(json.dumps(fio_result))
+                    perf_disk(
+                        node=node,
+                        start_iodepth=start_iodepth,
+                        max_iodepth=max_iodepth,
+                        filename=f"{size_mb}_MB_FIO_{id}",
+                        test_result=result,
+                        test_name=test_name,
+                        num_jobs=num_jobs,
+                        block_size=block_size,
+                        time=time,
+                        size_mb=size_mb,
+                        overwrite=overwrite,
+                    )
+                except Exception as e:
+                    log.error(e)
 
     def _perf_nfs(
         self,
