4d6213c8c380d89212523e45c55947f578e2f496
Johnson George <johgeorg@microsoft.com>
2022-11-01 22:18:21 -0700

    Update plink.exe version

--- Libraries/TestHelpers.psm1
+++ Libraries/TestHelpers.psm1
@@ -732,7 +732,7 @@ function New-ZipFile( $zipFileName, $sourceDir ) {
 Function Get-LISAv2Tools($XMLSecretFile) {
 	# Copy required binary files to working folder
 	$CurrentDirectory = Get-Location
-	$CmdArray = @('7za.exe', 'dos2unix.exe', 'gawk', 'jq', 'plink_0.71.exe', 'pscp.exe', `
+	$CmdArray = @('7za.exe', 'dos2unix.exe', 'gawk', 'jq', 'plink_0.77.exe', 'pscp.exe', `
 			'kvp_client32', 'kvp_client64', 'nc.exe', 'lz4.exe')
 
 	if ($XMLSecretFile) {
@@ -759,8 +759,8 @@ Function Get-LISAv2Tools($XMLSecretFile) {
 				}
 				else {
 					Write-LogInfo "File $_ successfully downloaded in Tools folder: $CurrentDirectory\Tools."
-					if ($_ -eq "plink_0.71.exe") {
-						Rename-Item -Path "$CurrentDirectory\Tools\plink_0.71.exe" -NewName "plink.exe" -Force | Out-Null
+					if ($_ -eq "plink_0.77.exe") {
+						Rename-Item -Path "$CurrentDirectory\Tools\plink_0.77.exe" -NewName "plink.exe" -Force | Out-Null
 						Write-LogInfo "Successfully renamed File $_ to plink.exe"
 					}
 				}
