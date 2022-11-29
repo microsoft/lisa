2d80ac538f2c51a61525895476420c055db1c162
smit-gardhariya <gardhariya.smit@gmail.com>
2022-11-29 11:19:16 +0530

    kernel installer changes for CBLMariner-Dom0

--- lisa/tools/cp.py
+++ lisa/tools/cp.py
@@ -14,5 +14,5 @@ class Cp(Tool):
     def can_install(self) -> bool:
         return False
 
-    def copy(self, src: PurePath, dest: PurePath) -> None:
-        self.run(f"{src} {dest}", force_run=True, expected_exit_code=0)
+    def copy(self, src: PurePath, dest: PurePath, sudo: bool = False) -> None:
+        self.run(f"{src} {dest}", force_run=True, expected_exit_code=0, sudo=sudo)
