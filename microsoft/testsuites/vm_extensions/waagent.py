627094fa5e7ae4a5ed227e6d5f5d205dd8b2019a
narrieta <narrieta>
2022-12-02 11:56:25 -0800

    flake issue

--- microsoft/testsuites/vm_extensions/waagent.py
+++ microsoft/testsuites/vm_extensions/waagent.py
@@ -32,6 +32,7 @@ class WaAgentBvt(TestSuite):
         # Any extension will do, use CustomScript for convenience.
         # Use the extension to create a unique file on the test machine.
         test_file = "/tmp/{0}".format(uuid4())
+        settings = { "commandToExecute": "touch {0} && echo Created {0}".format(test_file) }
         extension = node.features[AzureExtension]
         result = extension.create_or_update(
             name="CustomScript",
@@ -39,17 +40,15 @@ class WaAgentBvt(TestSuite):
             type="CustomScript",
             type_handler_version="2.0",
             auto_upgrade_minor_version=True,
-            settings={
-                "commandToExecute": "touch {0} && echo Created {0}".format(test_file)
-            },
+            settings=settings,
             force_update_tag=test_file
         )
         assert_that(result['provisioning_state']).is_equal_to("Succeeded")
 
         # Double-check that the file was actually created.
+        failure_message = "File {0} was not created on the test machine".format(test_file)
         node.execute(
             "ls '{0}'".format(test_file),
             shell=True,
             expected_exit_code=0,
-            expected_exit_code_failure_message=
-                "File {0} was not created on the test machine".format(test_file))
+            expected_exit_code_failure_message=failure_message)
