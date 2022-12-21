c9baf504749ca5b98fa50d4d8cadcd83babcdd49
smit-gardhariya <gardhariya.smit@gmail.com>
2022-12-21 17:50:11 +0530

    Install cargo dependency separately
    Install cargo dependency separately
    
    Install cargo dependency separately
    
    flake 8 changes
    
    version import order corretion for semver in curl.py

--- lisa/tools/cargo.py
+++ lisa/tools/cargo.py
@@ -4,11 +4,12 @@ from pathlib import PurePath
 from typing import List, Optional, Type, cast
 
 from lisa.executable import Tool
-from lisa.operating_system import Posix
+from lisa.operating_system import CBLMariner, Posix, Ubuntu
 from lisa.tools.curl import Curl
 from lisa.tools.echo import Echo
 from lisa.tools.gcc import Gcc
 from lisa.tools.ln import Ln
+from lisa.util import LisaException
 from lisa.util.process import ExecutableResult
 
 
@@ -26,44 +27,58 @@ class Cargo(Tool):
         return [Gcc]
 
     def _install(self) -> bool:
-        posix_os: Posix = cast(Posix, self.node.os)
+        os = self.node.os
         cargo_source_url = "https://sh.rustup.rs"
         try:
-            # install prerequisites
-            posix_os.install_packages(["build-essential", "cmake"])
-            gcc = self.node.tools[Gcc]
-            gcc_version_info = gcc.get_version().to_dict()
-            self.node.log.info(f"Gcc Version: {gcc_version_info}")
-            # install cargo/rust
-            curl = self.node.tools[Curl]
-            curl_version_info = curl.get_version().stdout
-            self.node.log.info(f"Curl Version: {curl_version_info}")
-            result = curl.fetch(
-                arg="-sSf",
-                url=cargo_source_url,
-                execute_arg="-s -- -y",
-                shell=True,
-            )
-            result.assert_exit_code()
+            if isinstance(os, CBLMariner) or isinstance(os, Ubuntu):
+                self.__install_dependencies()
+
+                # install cargo/rust
+                curl = self.node.tools[Curl]
+                result = curl.fetch(
+                    arg="-sSf",
+                    url=cargo_source_url,
+                    execute_arg="-s -- -y",
+                    shell=True,
+                )
+                result.assert_exit_code()
+
+                echo = self.node.tools[Echo]
+                home_dir = echo.run(
+                    "$HOME",
+                    shell=True,
+                    expected_exit_code=0,
+                    expected_exit_code_failure_message="failure to grab $PATH via echo",
+                ).stdout
+
+                ln = self.node.tools[Ln]
+                ln.create_link(
+                    is_symbolic=True,
+                    target=f"{home_dir}/.cargo/bin/cargo",
+                    link="/usr/local/bin/cargo",
+                )
+            else:
+                raise LisaException(
+                    f"os '{os.name}' doesn't support cargo installation."
+                )
         except Exception as e:
             self._log.debug(f"failed to install cargo: {e}")
 
-        echo = self.node.tools[Echo]
-        home_dir = echo.run(
-            "$HOME",
-            shell=True,
-            expected_exit_code=0,
-            expected_exit_code_failure_message="failure to grab $PATH via echo",
-        ).stdout
-
-        ln = self.node.tools[Ln]
-        ln.create_link(
-            is_symbolic=True,
-            target=f"{home_dir}/.cargo/bin/cargo",
-            link="/usr/local/bin/cargo",
-        )
-
         return self._check_exists()
+
+    def __install_dependencies(self) -> None:
+        os: Posix = cast(Posix, self.node.os)
+
+        # install prerequisites
+        os.install_packages(["build-essential", "cmake"])
+
+        gcc = self.node.tools[Gcc]
+        gcc_version_info = gcc.get_version()
+        self.node.log.info(f"Gcc Version: {gcc_version_info}")
+
+        curl = self.node.tools[Curl]
+        curl_version_info = curl.get_version()
+        self.node.log.info(f"Curl Version: {curl_version_info}")
 
     def build(
         self,
