935b222bb0641d4843f03c94a48841590e51b2fa
smit-gardhariya <gardhariya.smit@gmail.com>
2022-12-21 10:38:26 +0530

    Implement Curl tool and use it to install cargo
    Implement Curl tool and use it to install cargo
    
    Implement Curl tool and use it to install cargo
    
    Implement Curl tool and use it to install cargo
    
    Implement Curl tool and use it to install cargo
    
    flake 8 validation changes
    
    flake 8 validation changes
    
    flake 8 validation changes for cargo tool
    
    Pass updated_env for _check_exists() to provide env variables

---
+++ lisa/tools/curl.py
@@ -0,0 +1,66 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+from pathlib import PurePath
+from typing import Optional, cast
+
+from lisa.executable import Tool
+from lisa.operating_system import Posix
+from lisa.util.process import ExecutableResult
+
+
+class Curl(Tool):
+    @property
+    def command(self) -> str:
+        return "curl"
+
+    @property
+    def can_install(self) -> bool:
+        return True
+
+    def _install(self) -> bool:
+        posix_os: Posix = cast(Posix, self.node.os)
+        try:
+            posix_os.install_packages("curl")
+        except Exception as e:
+            self._log.debug(f"failed to install curl: {e}")
+        return self._check_exists()
+
+    def fetch(
+        self,
+        url: str,
+        arg: str,
+        execute_arg: str,
+        sudo: bool = False,
+        shell: bool = False,
+        cwd: Optional[PurePath] = None,
+    ) -> ExecutableResult:
+        err_msg = "curl fetch failed"
+        cmd_arg = f" {arg} {url}"
+        if execute_arg:
+            cmd_arg = f"{cmd_arg} | sh {execute_arg}"
+        result = self.run(
+            cmd_arg,
+            expected_exit_code=0,
+            expected_exit_code_failure_message=err_msg,
+            sudo=sudo,
+            cwd=cwd,
+            shell=shell,
+        )
+        return result
+
+    def get_version(
+        self,
+        sudo: bool = False,
+        shell: bool = False,
+        cwd: Optional[PurePath] = None,
+    ) -> ExecutableResult:
+        err_msg = "curl get_version failed"
+        result = self.run(
+            " --version",
+            expected_exit_code=0,
+            expected_exit_code_failure_message=err_msg,
+            sudo=sudo,
+            cwd=cwd,
+            shell=shell,
+        )
+        return result
--- microsoft/testsuites/rust_vmm_mshv/rust_vmm_mshv_test.py
+++ microsoft/testsuites/rust_vmm_mshv/rust_vmm_mshv_test.py
@@ -43,25 +43,18 @@ class RustVmmTestSuite(TestSuite):
         log_path: Path,
         result: TestResult,
     ) -> None:
-        # node.tools[RustVmmTests].run_rust_vmm_mshv_tests(
-        #     result, environment, "integration", hypervisor, log_path
-        # )
         repo = "https://github.com/rust-vmm/mshv.git"
         git = node.tools[Git]
         repo_root = git.clone(repo, node.get_working_path())
         testcase_log = log_path.joinpath("rust_vmm_mshv.log")
-
         cargo = node.tools[Cargo]
         test_result: ExecutableResult = cargo.test(cwd=repo_root, sudo=True)
-
         with open(testcase_log, "w") as f:
             f.write(f"{test_result.stdout} {test_result.stderr}")
-
         self.__process_result(
             test_result.stdout,
             result,
             environment,
-            node,
         )
 
     def __process_result(
@@ -69,38 +62,33 @@ class RustVmmTestSuite(TestSuite):
         data: str,
         result: TestResult,
         environment: Environment,
-        node: Node,
     ) -> None:
-
         pattern = r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])"
         ansi_escape = re.compile(pattern)
-        data = ansi_escape.sub('', data)
-
+        data = ansi_escape.sub("", data)
         match = re.findall(
             r"test (.*?) ... (ok|ignored|failed)(.*?)\n",
             data,
             re.IGNORECASE,
         )
-
         failed_testcases = []
         for testcase in match:
             status = TestStatus.QUEUED
             testcase_name = testcase[0]
-            if (testcase[-2] and testcase[-2].strip().lower() == "ok"):
+            log_status = testcase[-2]
+            if log_status and log_status.strip().lower() == "ok":
                 status = TestStatus.PASSED
-            elif (testcase[-2] and testcase[-2].strip().lower() == "failed"):
+            elif log_status and log_status.strip().lower() == "failed":
                 failed_testcases.append(testcase_name)
                 status = TestStatus.FAILED
-            elif (testcase[-2] and testcase[-2].strip().lower() == "ignored"):
+            elif log_status and log_status.strip().lower() == "ignored":
                 status = TestStatus.SKIPPED
-
             self.__send_subtest_msg(
                 result,
                 environment,
                 testcase_name,
                 status,
             )
-
         assert_that(
             failed_testcases, f"Failed Testcases: {failed_testcases}"
         ).is_empty()
