2e689945e8fa592355c06959a0861a51d64a9eb0
Lili Deng <lildeng@microsoft.com>
2023-02-17 23:58:35 +0800

    add tool lsof

---
+++ lisa/tools/lsof.py
@@ -0,0 +1,24 @@
+from typing import cast
+
+from lisa.executable import Tool
+from lisa.operating_system import Posix
+
+
+class Lsof(Tool):
+    @property
+    def command(self) -> str:
+        return "lsof"
+
+    @property
+    def can_install(self) -> bool:
+        return True
+
+    def is_port_opened(self, port: int) -> bool:
+        cmd = f"-i :{port}"
+        result = self.run(cmd, force_run=True, shell=True, sudo=True)
+        return result.exit_code == 0
+
+    def install(self) -> bool:
+        posix_os: Posix = cast(Posix, self.node.os)
+        posix_os.install_packages("lsof")
+        return self._check_exists()
