8fcfdaa6dd621393d3fb9e77e6f801f15aa07a12
adityanagesh <adityagesh@gmail.com>
2023-02-22 10:55:56 +0530

    GPU CUDA Driver installation for RHEL 9

--- lisa/features/gpu.py
+++ lisa/features/gpu.py
@@ -221,18 +221,23 @@ class Gpu(Feature):
 
         if isinstance(self._node.os, Redhat):
             release = os_information.release.split(".")[0]
-            cuda_repo_pkg = f"cuda-repo-rhel{release}-{version}.x86_64.rpm"
-            cuda_repo = (
-                "http://developer.download.nvidia.com/"
-                f"compute/cuda/repos/rhel{release}/x86_64/{cuda_repo_pkg}"
-            )
-            try:
-                # download and install the cuda driver package from the repo
-                self._node.os._install_package_from_url(
-                    f"{cuda_repo}", package_name="cuda-drivers.rpm", signed=False
+            if release == "9":
+                self._node.os.add_repository(
+                    "http://developer.download.nvidia.com/compute/cuda/repos/rhel9/x86_64/cuda-rhel9.repo"
                 )
-            except Exception as e:
-                raise LisaException(f"Failed to install driver from source, {str(e)}")
+            else:
+                cuda_repo_pkg = f"cuda-repo-rhel{release}-{version}.x86_64.rpm"
+                cuda_repo = (
+                    "http://developer.download.nvidia.com/"
+                    f"compute/cuda/repos/rhel{release}/x86_64/{cuda_repo_pkg}"
+                )
+                try:
+                    # download and install the cuda driver package from the repo
+                    self._node.os._install_package_from_url(cuda_repo)
+                except Exception as e:
+                    raise LisaException(
+                        f"Failed to install driver from source, {str(e)}"
+                    )
             self._node.os.install_packages("cuda-drivers", signed=False)
 
         elif isinstance(self._node.os, Ubuntu):
@@ -294,7 +299,7 @@ class Gpu(Feature):
                 self._node.os.install_packages("cuda-drivers-515")
         else:
             raise LisaException(
-                f"Distro {self._node.os.name}" "not supported to install CUDA driver."
+                f"Distro {self._node.os.name} not supported to install CUDA driver."
             )
 
     def _install_gpu_dep(self) -> None:
