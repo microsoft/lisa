de25016002bebbcd41c45494479558f4d4199fea
Matthew G McGovern <mamcgove@microsoft.com>
2023-03-22 13:29:08 -0700

    add default size for property of os disks (not always present)

--- lisa/sut_orchestrator/azure/platform_.py
+++ lisa/sut_orchestrator/azure/platform_.py
@@ -1256,7 +1256,7 @@ class AzurePlatform(Platform):
             )
             arm_parameters.osdisk_size_in_gb = max(
                 arm_parameters.osdisk_size_in_gb,
-                image_info.os_disk_image.additional_properties["sizeInGb"],
+                image_info.os_disk_image.additional_properties.get("sizeInGb", 0),
             )
             if not arm_parameters.purchase_plan and image_info.plan:
                 # expand values for lru cache
