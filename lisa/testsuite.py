1136264641249c8d050bc68a9ff0799af015eeca
Aditya Nagesh <adityanagesh@microsoft.com>
2023-05-11 09:35:28 +0530

    Save serial console after test failure (#2756)
    * Save serial console after test failure
    
    It maybe not be always possible to reproduce a failure.
    Save serial console after test failure to triage the failure
    from logs

--- lisa/testsuite.py
+++ lisa/testsuite.py
@@ -19,6 +19,7 @@ from retry.api import retry_call
 from lisa import notifier, schema, search_space
 from lisa.environment import Environment, EnvironmentSpace, EnvironmentStatus
 from lisa.feature import Feature
+from lisa.features import SerialConsole
 from lisa.messages import TestResultMessage, TestStatus, _is_completed_status
 from lisa.operating_system import OperatingSystem, Windows
 from lisa.util import (
@@ -640,6 +641,9 @@ class TestSuite:
                 log=case_log,
             )
 
+            if case_result.status == TestStatus.FAILED:
+                self.__save_serial_log(environment, case_log_path)
+
             case_log.info(
                 f"result: {case_result.status.name}, " f"elapsed: {total_timer}"
             )
@@ -662,6 +666,15 @@ class TestSuite:
 
     def stop(self) -> None:
         self._should_stop = True
+
+    def __save_serial_log(self, environment: Environment, log_path: Path) -> None:
+        nodes = environment.nodes
+        for node in nodes.list():
+            if hasattr(node, "features") and node.features.is_supported(SerialConsole):
+                serial_console = node.features[SerialConsole]
+                log_dir = log_path / Path(f"serial_console_{node.name}")
+                log_dir.mkdir(parents=True)
+                serial_console.get_console_log(log_dir, force_run=True)
 
     def __create_case_log_path(self, case_name: str) -> Path:
         while True:
