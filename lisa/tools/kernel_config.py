855f0115647dda0f6cfe93e6a32e9a0e501b5e8f
Divyansh Srivastava <somm14divi@gmail.com>
2023-05-23 22:49:32 +0000

    Add FreeBSD support for hv module testcases

--- lisa/tools/kernel_config.py
+++ lisa/tools/kernel_config.py
@@ -1,10 +1,10 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
-from typing import Any
+from typing import Any, Optional, Type
 
 from lisa.executable import Tool
 from lisa.operating_system import CoreOs
-from lisa.tools import Uname
+from lisa.tools import Cat, Uname
 
 
 class KernelConfig(Tool):
@@ -16,6 +16,10 @@ class KernelConfig(Tool):
     @property
     def command(self) -> str:
         return ""
+
+    @classmethod
+    def _freebsd_tool(cls) -> Optional[Type[Tool]]:
+        return KernelConfigFreeBSD
 
     @property
     def can_install(self) -> bool:
@@ -53,3 +57,28 @@ class KernelConfig(Tool):
             self.config_path = f"/usr/boot/config-{kernel_ver}"
         else:
             self.config_path = f"/boot/config-{kernel_ver}"
+
+
+class KernelConfigFreeBSD(KernelConfig):
+    def is_built_in(self, config_name: str) -> bool:
+        return self.node.tools[KLDStat].module_statically_linked(config_name)
+
+    def is_built_as_module(self, config_name: str) -> bool:
+        output = self.node.tools[Cat].read(self.config_path, sudo=True)
+        return f'{config_name}="YES"' in output
+
+    def _initialize(self, *args: Any, **kwargs: Any) -> None:
+        self.config_path = "/boot/loader.conf"
+
+
+class KLDStat(Tool):
+    @property
+    def command(self) -> str:
+        return "kldstat"
+
+    def module_statically_linked(
+        self,
+        mod_name: str,
+    ) -> bool:
+        output = self.run("-v -i 1", sudo=True).stdout
+        return mod_name in output
