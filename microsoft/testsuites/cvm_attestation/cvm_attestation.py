d1b6fc4f55f45b67f827955b923d114fdc98cb0b
Smit Gardhariya <113590758+smit-gardhariya@users.noreply.github.com>
2023-07-11 10:36:27 +0530

    Add nested CVM attestation testcase (#2842)

---
+++ microsoft/testsuites/cvm_attestation/cvm_attestation.py
@@ -0,0 +1,63 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+from pathlib import Path
+from typing import Any, Dict
+
+from lisa import (
+    Environment,
+    Logger,
+    Node,
+    TestCaseMetadata,
+    TestSuite,
+    TestSuiteMetadata,
+)
+from lisa.testsuite import TestResult
+from lisa.tools import Ls
+from lisa.util import SkippedException
+from microsoft.testsuites.cvm_attestation.cvm_attestation_tool import (
+    CVMAttestationTests,
+)
+
+
+@TestSuiteMetadata(
+    area="cvm",
+    category="attestation",
+    description="""
+    This test suite is for generating and verifying CVM attestation report.
+    """,
+)
+class CVMAttestationTestSuite(TestSuite):
+    def before_case(self, log: Logger, **kwargs: Any) -> None:
+        node = kwargs["node"]
+        sev_guest_exists = node.tools[Ls].path_exists(
+            path="/dev/sev-guest",
+            sudo=True,
+        )
+        if not sev_guest_exists:
+            raise SkippedException("/dev/sev-guest: Device Not Found")
+
+    @TestCaseMetadata(
+        description="""
+            Runs get-snp-report tool to generate
+            and verify attestation report for nested cvm.
+        """,
+        priority=3,
+    )
+    def verify_cvm_attestation_report(
+        self,
+        log: Logger,
+        node: Node,
+        environment: Environment,
+        log_path: Path,
+        result: TestResult,
+        variables: Dict[str, Any],
+    ) -> None:
+        host_data = variables.get("host_data", "")
+        if not host_data:
+            raise SkippedException("host_data is empty")
+        node.tools[CVMAttestationTests].run_cvm_attestation(
+            result,
+            environment,
+            log_path,
+            host_data,
+        )
