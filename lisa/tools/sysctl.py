5fe07231c95c50148a076d7181d7c201dc546904
Matthew McGovern <mamcgove@microsoft.com>
2023-07-28 13:26:20 -0700

    handle BSD busy polling weirdness

--- lisa/tools/sysctl.py
+++ lisa/tools/sysctl.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT license.
 
 from lisa.executable import Tool
+from lisa.operating_system import BSD
 
 
 class Sysctl(Tool):
@@ -33,3 +34,18 @@ class Sysctl(Tool):
             expected_exit_code_failure_message=f"fail to get {variable}'s value",
         )
         return result.stdout
+
+    def enable_busy_polling(self, value: str) -> None:
+        # NOTE: handle BSD idiosyncracy
+        # see: https://man.freebsd.org/cgi/man.cgi?query=polling
+        # The historic kern.polling.enable, which enabled polling for all inter-
+        #  faces, can be replaced with the following code:
+        #  for i in `ifconfig	-l` ;
+        #    do ifconfig $i polling; # use -polling to disable
+        #  done
+        bsd_poll_enable = "for i in `ifconfig -l` ; do ifconfig $i polling; done"
+        if isinstance(self.node.os, BSD):
+            self.node.execute(bsd_poll_enable, sudo=True, shell=True)
+        else:
+            self.write("net.core.busy_poll", value)
+            self.write("net.core.busy_read", value)
