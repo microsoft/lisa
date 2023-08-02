78b60d236aa96419fce501e4d102226c28f28b96
Srikanth Myakam <smyakam@microsoft.com>
2023-08-03 00:14:54 +0530

    add ' max_data_disk_count = 64'

--- microsoft/testsuites/core/storage.py
+++ microsoft/testsuites/core/storage.py
@@ -447,9 +447,10 @@ class Storage(TestSuite):
         lsblk = node.tools[Lsblk]
 
         # get max data disk count for the node
-        assert node.capability.disk
-        assert isinstance(node.capability.disk.max_data_disk_count, int)
-        max_data_disk_count = node.capability.disk.max_data_disk_count
+        # assert node.capability.disk
+        # assert isinstance(node.capability.disk.max_data_disk_count, int)
+        # max_data_disk_count = node.capability.disk.max_data_disk_count
+        max_data_disk_count = 64
         log.debug(f"max_data_disk_count: {max_data_disk_count}")
 
         # get the number of data disks already added to the vm
@@ -506,9 +507,10 @@ class Storage(TestSuite):
         lsblk = node.tools[Lsblk]
 
         # get max data disk count for the node
-        assert node.capability.disk
-        assert isinstance(node.capability.disk.max_data_disk_count, int)
-        max_data_disk_count = node.capability.disk.max_data_disk_count
+        # assert node.capability.disk
+        # assert isinstance(node.capability.disk.max_data_disk_count, int)
+        # max_data_disk_count = node.capability.disk.max_data_disk_count
+        max_data_disk_count = 64
         log.debug(f"max_data_disk_count: {max_data_disk_count}")
 
         # get the number of data disks already added to the vm
