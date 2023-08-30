f6ed49a2b257f3264beea57423cf5fd10b381284
Bam Muir <braydenmuir@microsoft.com>
2023-08-30 14:14:52 -0700

    refactor helpers, manual OS major version check

--- microsoft/testsuites/vm_extensions/azure_disk_encryption.py
+++ microsoft/testsuites/vm_extensions/azure_disk_encryption.py
@@ -23,85 +23,7 @@ from lisa.sut_orchestrator.azure.common import (
 from lisa.sut_orchestrator.azure.features import AzureExtension
 from lisa.sut_orchestrator.azure.platform_ import AzurePlatform, AzurePlatformSchema
 from lisa.testsuite import TestResult, simple_requirement
-from lisa.util import generate_random_chars
-
-
-def _enable_ade_extension(node: Node, log: Logger, result: TestResult) -> Any:
-    environment = result.environment
-    assert environment, "fail to get environment from testresult"
-    platform = environment.platform
-    assert isinstance(platform, AzurePlatform)
-    runbook = platform.runbook.get_extended_runbook(AzurePlatformSchema)
-    tenant_id = get_tenant_id(platform.credential)
-    if tenant_id is None:
-        raise ValueError("Environment variable 'tenant_id' is not set.")
-    application_id = runbook.service_principal_client_id
-    object_id = get_identity_id(platform=platform, application_id=application_id)
-    if object_id is None:
-        raise ValueError("Environment variable 'object_id' is not set.")
-
-    node_capability = node.capability.get_extended_runbook(AzureNodeSchema, AZURE)
-    location = node_capability.location
-    shared_resource_group = runbook.shared_resource_group_name
-
-    # Create key vault if there is not available adelisa key vault for that region
-    existing_vault = get_matching_key_vault_name(
-        platform, location, shared_resource_group, r"adelisa-\w{5}"
-    )
-    if existing_vault:
-        vault_name = existing_vault
-    else:
-        random_str = generate_random_chars(string.ascii_lowercase + string.digits, 5)
-        vault_name = f"adelisa-{random_str}"
-
-    vault_properties = VaultProperties(
-        tenant_id=tenant_id,
-        sku=KeyVaultSku(name="standard"),
-        enabled_for_disk_encryption=True,
-        access_policies=[
-            AccessPolicyEntry(
-                tenant_id=tenant_id,
-                object_id=object_id,
-                permissions=Permissions(
-                    keys=["all"], secrets=["all"], certificates=["all"]
-                ),
-            ),
-        ],
-    )
-    # If the KV exists, this will just ensure the properties are correct for ADE
-    keyvault_result = create_keyvault(
-        platform=platform,
-        location=location,
-        vault_name=vault_name,
-        resource_group_name=shared_resource_group,
-        vault_properties=vault_properties,
-    )
-
-    # Check if KeyVault is successfully created or updated before proceeding
-    assert (
-        keyvault_result
-    ), f"Failed to create or update KeyVault with name: {vault_name}"
-
-    # Run ADE Extension
-    extension_name = "AzureDiskEncryptionForLinux"
-    extension_publisher = "Microsoft.Azure.Security"
-    extension_version = "1.4"
-    settings = {
-        "EncryptionOperation": "EnableEncryption",
-        "KeyVaultURL": keyvault_result.properties.vault_uri,
-        "KeyVaultResourceId": keyvault_result.id,
-        "KeyEncryptionAlgorithm": "RSA-OAEP",
-        "VolumeType": "Os",
-    }
-    extension = node.features[AzureExtension]
-    extension_result = extension.create_or_update(
-        name=extension_name,
-        publisher=extension_publisher,
-        type_=extension_name,
-        type_handler_version=extension_version,
-        settings=settings,
-    )
-    return extension_result
+from lisa.util import SkippedException, generate_random_chars
 
 
 @TestSuiteMetadata(
@@ -112,10 +34,16 @@ def _enable_ade_extension(node: Node, log: Logger, result: TestResult) -> Any:
         min_memory_mb=8 * 1024,
         supported_features=[AzureExtension],
         supported_platform_type=[AZURE],
-        supported_os=[Ubuntu, CBLMariner, CentOs, Oracle, Redhat],
     ),
 )
 class AzureDiskEncryption(TestSuite):
+    def before_case(self, log: Logger, **kwargs: Any) -> None:
+        node = kwargs["node"]
+        if not self._is_supported_linux_distro(node):
+            raise SkippedException(
+                f"{str(node.os.information.full_version)} is not supported."
+            )
+
     @TestCaseMetadata(
         description="""
         Runs the ADE extension and verifies it
@@ -127,7 +55,7 @@ class AzureDiskEncryption(TestSuite):
     def verify_azure_disk_encryption_enabled(
         self, log: Logger, node: Node, result: TestResult
     ) -> None:
-        extension_result = _enable_ade_extension(node, log, result)
+        extension_result = self._enable_ade_extension(node, log, result)
         assert_that(extension_result["provisioning_state"]).described_as(
             "Expected the extension to succeed"
         ).is_equal_to("Succeeded")
@@ -186,8 +114,108 @@ class AzureDiskEncryption(TestSuite):
     def verify_azure_disk_encryption_provisioned(
         self, log: Logger, node: Node, result: TestResult
     ) -> None:
-        extension_result = _enable_ade_extension(node, log, result)
+        extension_result = self._enable_ade_extension(node, log, result)
 
         assert_that(extension_result["provisioning_state"]).described_as(
             "Expected the extension to succeed"
         ).is_equal_to("Succeeded")
+
+    def _enable_ade_extension(self, node: Node, log: Logger, result: TestResult) -> Any:
+        environment = result.environment
+        assert environment, "fail to get environment from testresult"
+        platform = environment.platform
+        assert isinstance(platform, AzurePlatform)
+        runbook = platform.runbook.get_extended_runbook(AzurePlatformSchema)
+        tenant_id = get_tenant_id(platform.credential)
+        if tenant_id is None:
+            raise ValueError("Environment variable 'tenant_id' is not set.")
+        application_id = runbook.service_principal_client_id
+        object_id = get_identity_id(platform=platform, application_id=application_id)
+        if object_id is None:
+            raise ValueError("Environment variable 'object_id' is not set.")
+
+        node_capability = node.capability.get_extended_runbook(AzureNodeSchema, AZURE)
+        location = node_capability.location
+        shared_resource_group = runbook.shared_resource_group_name
+
+        # Create key vault if there is not available adelisa key vault for that region
+        existing_vault = get_matching_key_vault_name(
+            platform, location, shared_resource_group, r"adelisa-\w{5}"
+        )
+        if existing_vault:
+            vault_name = existing_vault
+        else:
+            random_str = generate_random_chars(
+                string.ascii_lowercase + string.digits, 5
+            )
+            vault_name = f"adelisa-{random_str}"
+
+        vault_properties = VaultProperties(
+            tenant_id=tenant_id,
+            sku=KeyVaultSku(name="standard"),
+            enabled_for_disk_encryption=True,
+            access_policies=[
+                AccessPolicyEntry(
+                    tenant_id=tenant_id,
+                    object_id=object_id,
+                    permissions=Permissions(
+                        keys=["all"], secrets=["all"], certificates=["all"]
+                    ),
+                ),
+            ],
+        )
+        # If the KV exists, this will just ensure the properties are correct for ADE
+        keyvault_result = create_keyvault(
+            platform=platform,
+            location=location,
+            vault_name=vault_name,
+            resource_group_name=shared_resource_group,
+            vault_properties=vault_properties,
+        )
+
+        # Check if KeyVault is successfully created or updated before proceeding
+        assert (
+            keyvault_result
+        ), f"Failed to create or update KeyVault with name: {vault_name}"
+
+        # Run ADE Extension
+        extension_name = "AzureDiskEncryptionForLinux"
+        extension_publisher = "Microsoft.Azure.Security"
+        extension_version = "1.4"
+        settings = {
+            "EncryptionOperation": "EnableEncryption",
+            "KeyVaultURL": keyvault_result.properties.vault_uri,
+            "KeyVaultResourceId": keyvault_result.id,
+            "KeyEncryptionAlgorithm": "RSA-OAEP",
+            "VolumeType": "Os",
+        }
+        extension = node.features[AzureExtension]
+        extension_result = extension.create_or_update(
+            name=extension_name,
+            publisher=extension_publisher,
+            type_=extension_name,
+            type_handler_version=extension_version,
+            settings=settings,
+        )
+        return extension_result
+
+    def _is_supported_linux_distro(self, node: Node) -> bool:
+        supported_major_versions = {
+            Redhat: [7, 8, 9],
+            CentOs: [7, 8],
+            Oracle: [8],
+            Ubuntu: [18, 20, 22],
+            CBLMariner: [2],
+        }
+
+        for distro in supported_major_versions:
+            if type(node.os) == distro:
+                version_list = supported_major_versions.get(distro)
+                if (
+                    version_list is not None
+                    and node.os.information.version.major in version_list
+                ):
+                    return True
+                else:
+                    return False
+        return False
