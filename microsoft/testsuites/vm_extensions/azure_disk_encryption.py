225845762b89bbcc8e7475479720428e1ca3bc5b
Bam Muir <braydenmuir@microsoft.com>
2023-09-01 13:44:53 -0700

    allow RHEL 7.4

--- microsoft/testsuites/vm_extensions/azure_disk_encryption.py
+++ microsoft/testsuites/vm_extensions/azure_disk_encryption.py
@@ -231,7 +231,7 @@ class AzureDiskEncryption(TestSuite):
         unsupported_versions: Dict[type, UnsupportedVersionInfo] = {
             Oracle: {"major": 8, "minor": 5},
             CentOs: [{"major": 8, "minor": 1}, {"major": 7, "minor": 4}],
-            Redhat: [{"major": 8, "minor": 1}, {"major": 7, "minor": 4}],
+            Redhat: [{"major": 8, "minor": 1}, {"major": 7, "minor": 3}],
         }
 
         version_info = node.os.information.version
