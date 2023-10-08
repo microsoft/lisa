56d02d109b7ea4852eb8946c04dcd2080270be35
Anirudh Rayabharam <anrayabh@linux.microsoft.com>
2023-10-08 08:44:52 +0530

    Improve Systemctl and Journalctl tools
    - Add mask method to Systemctl tool - same as `systemctl mask xyz`.
    - Allow capturing all boot logs instead of 1st n lines and redirect them
      to a file.
    
    Signed-off-by: Anirudh Rayabharam <anrayabh@linux.microsoft.com>

--- lisa/tools/journalctl.py
+++ lisa/tools/journalctl.py
@@ -1,6 +1,9 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+from pathlib import PurePosixPath
+from typing import Optional
+
 from lisa.executable import Tool
 
 
@@ -24,13 +27,29 @@ class Journalctl(Tool):
         return result.stdout
 
     def first_n_logs_from_boot(
-        self, boot_id: str = "", no_of_lines: int = 1000, sudo: bool = True
+        self,
+        boot_id: str = "",
+        no_of_lines: int = 1000,
+        out_file: Optional[PurePosixPath] = None,
+        sudo: bool = True,
     ) -> str:
+        cmd = f"-b {boot_id} --no-pager"
+
+        if no_of_lines > 0:
+            cmd = cmd + f" | head -n {no_of_lines}"
+
+        if out_file is not None:
+            cmd = cmd + f" > {out_file}"
+
+        # if an output file is given, don't flood lisa logs
+        no_debug_log = True if out_file is not None else False
+
         result = self.run(
-            f"-b {boot_id} | head -n {no_of_lines} ",
+            cmd,
             force_run=True,
             shell=True,
             sudo=sudo,
+            no_debug_log=no_debug_log,
             expected_exit_code=0,
         )
         return result.stdout
