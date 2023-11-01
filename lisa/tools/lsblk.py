9a652f2e70d7dbd15f733596628124da6e54c5f9
Lili Deng <lildeng@microsoft.com>
2023-11-01 09:49:20 +0800

    lsblk tool: switch back to use raw output if lsblk doesn't support json format

--- lisa/tools/lsblk.py
+++ lisa/tools/lsblk.py
@@ -8,7 +8,7 @@ from typing import Dict, List, Optional, Type
 
 from lisa.executable import Tool
 from lisa.operating_system import BSD, Posix
-from lisa.util import LisaException, find_groups_in_lines
+from lisa.util import LisaException, find_groups_in_lines, find_patterns_groups_in_lines
 
 
 def _get_size_in_bytes(size: float, size_unit: str) -> int:
@@ -126,6 +126,17 @@ class DiskInfo(object):
 
 
 class Lsblk(Tool):
+    _INVAILD_JSON_OPTION_PATTERN = re.compile(r"lsblk: invalid option -- 'J'", re.M)
+    # NAME="loop2" SIZE="34017280" TYPE="loop" MOUNTPOINT="/snap/snapd/13640"
+    _LSBLK_ENTRY_REGEX = re.compile(
+        r'NAME="(?P<name>\S+)"\s+SIZE="(?P<size>\d+)"\s+'
+        r'TYPE="(?P<type>\S+)"\s+MOUNTPOINT="(?P<mountpoint>\S*)"'
+        r'\s+FSTYPE="(?P<fstype>\S*)"'
+    )
+
+    # sda
+    _DISK_NAME_REGEX = re.compile(r"\s*(?P<name>\D+)\s*")
+
     @property
     def command(self) -> str:
         return "lsblk"
@@ -147,18 +158,9 @@ class Lsblk(Tool):
             )
         return self._check_exists()
 
-    def get_disks(self, force_run: bool = False) -> List[DiskInfo]:
+    def parse_lsblk_json_output(self, output: str) -> List[DiskInfo]:
         disks: List[DiskInfo] = []
-
-        # parse output of lsblk
-        # -b print SIZE in bytes rather than in human readable format
-        # -J output in JSON format
-        # -o list of columns to output
-        output = self.run(
-            "-b -J -o NAME,SIZE,TYPE,MOUNTPOINT,FSTYPE", sudo=True, force_run=force_run
-        ).stdout
         lsblk_entries = json.loads(output)["blockdevices"]
-
         for lsblk_entry in lsblk_entries:
             disk_mountpoint = lsblk_entry["mountpoint"]
             if disk_mountpoint == "/mnt/wslg/distro":
@@ -187,6 +189,82 @@ class Lsblk(Tool):
                 )
             # add disk to list of disks
             disks.append(disk_info)
+        return disks
+
+    def parse_lsblk_raw_output(self, output: str) -> List[DiskInfo]:
+        disks: List[DiskInfo] = []
+        lsblk_entries = find_patterns_groups_in_lines(
+            output, [self._LSBLK_ENTRY_REGEX]
+        )[0]
+
+        # create partition map
+        disk_partition_map: Dict[str, List[PartitionInfo]] = {}
+        for lsblk_entry in lsblk_entries:
+            # we only need to add partitions to the map
+            if not lsblk_entry["type"] == "part":
+                continue
+
+            # extract drive name from partition name
+            matched = find_patterns_groups_in_lines(
+                lsblk_entry["name"], [self._DISK_NAME_REGEX]
+            )[0]
+            assert len(matched) == 1, "Could not extract drive name from partition name"
+
+            # add partition to disk partition map
+            drive_name = matched[0]["name"]
+            if drive_name not in disk_partition_map:
+                disk_partition_map[drive_name] = []
+
+            disk_partition_map[drive_name].append(
+                PartitionInfo(
+                    name=lsblk_entry["name"],
+                    size=int(lsblk_entry["size"]),
+                    dev_type=lsblk_entry["type"],
+                    mountpoint=lsblk_entry["mountpoint"],
+                    fstype=lsblk_entry["fstype"],
+                )
+            )
+
+        # create disk info
+        for lsblk_entry in lsblk_entries:
+            # we only add physical disks to the list
+            if not lsblk_entry["type"] == "disk":
+                continue
+
+            # add disk to list of disks
+            disks.append(
+                DiskInfo(
+                    name=lsblk_entry["name"],
+                    mountpoint=lsblk_entry["mountpoint"],
+                    size=int(lsblk_entry["size"]),
+                    dev_type=lsblk_entry["type"],
+                    partitions=disk_partition_map.get(lsblk_entry["name"], []),
+                )
+            )
+        return disks
+
+    def get_disks(self, force_run: bool = False) -> List[DiskInfo]:
+        disks: List[DiskInfo] = []
+
+        # parse output of lsblk
+        # -b print SIZE in bytes rather than in human readable format
+        # -J output in JSON format
+        # -o list of columns to output
+        cmd_result = self.run(
+            "-b -J -o NAME,SIZE,TYPE,MOUNTPOINT,FSTYPE", sudo=True, force_run=force_run
+        )
+        output = cmd_result.stdout
+        if cmd_result.exit_code != 0 and re.match(
+            self._INVAILD_JSON_OPTION_PATTERN, output
+        ):
+            output = self.run(
+                "-b -P -o NAME,SIZE,TYPE,MOUNTPOINT,FSTYPE",
+                sudo=True,
+                force_run=force_run,
+            ).stdout
+            disks = self.parse_lsblk_raw_output(output)
+        else:
+            disks = self.parse_lsblk_json_output(output)
 
         # sort disk with OS disk first
         disks.sort(key=lambda disk: disk.is_os_disk, reverse=True)
