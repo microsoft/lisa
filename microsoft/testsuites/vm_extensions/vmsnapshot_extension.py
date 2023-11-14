d170d10c2ec452e6bda505cf637baee4c2f5457a
arisettisanjana <varshasanjana29@gmail.com>
2023-11-14 14:40:06 +0530

    added a test to validate exclude disk scenario

--- microsoft/testsuites/vm_extensions/vmsnapshot_extension.py
+++ microsoft/testsuites/vm_extensions/vmsnapshot_extension.py
@@ -1,9 +1,13 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+import base64
+import glob
+import os
 import time
 import uuid
 from datetime import datetime
+from pathlib import PurePosixPath
 
 from assertpy.assertpy import assert_that
 
@@ -25,6 +29,7 @@ from lisa.sut_orchestrator.azure.common import (
 )
 from lisa.sut_orchestrator.azure.features import AzureExtension
 from lisa.sut_orchestrator.azure.platform_ import AzurePlatform
+from lisa.tools.find import Find
 
 
 @TestSuiteMetadata(
@@ -42,7 +47,7 @@ class VmSnapsotLinuxBVTExtension(TestSuite):
         Validate response of the restore point for validity.
         Attempt it a few items to rule out cases when VM is under changes.
         """,
-        priority=1,
+        priority=0,
         requirement=simple_requirement(supported_features=[AzureExtension]),
     )
     def verify_vmsnapshot_extension(
@@ -133,6 +138,7 @@ class VmSnapsotLinuxBVTExtension(TestSuite):
                 restore_point_name=restore_point,
                 expand=None,
             )
+            log.info(response)
             if response.provisioning_state == "Succeeded":
                 log.info(f"restore point {restore_point} created")
                 consistency_mode = response.consistency_mode
@@ -155,3 +161,161 @@ class VmSnapsotLinuxBVTExtension(TestSuite):
             "Restore point provisioning status not Succeeded "
             "after multiple attempts."
         )
+
+    # Function to find the 'main' directory
+    def find_main_directory(self, base_path):
+        for main_directory in glob.glob(os.path.join(base_path, "main")):
+            return main_directory
+
+    def copy_to_node(self, node: Node, filename: str, lib_file: str) -> str:
+        parent_directory = ""
+
+        f_name = node.working_path / filename
+        temp_file = os.path.join(parent_directory, filename)
+        temp_file = PurePosixPath(temp_file)
+        file_path = os.path.join((os.path.dirname(__file__)), "scripts", temp_file)
+        print(temp_file)
+        print("file_path", file_path)
+        lib_name = node.working_path / lib_file
+        temp_lib = os.path.join(parent_directory, lib_file)
+        temp_lib = PurePosixPath(temp_lib)
+        print(temp_lib)
+        lib_path = os.path.join((os.path.dirname(__file__)), "scripts", temp_lib)
+        print("lib path", lib_path)
+        node.shell.copy(file_path, f_name)
+        node.shell.copy(lib_path, lib_name)
+        print("working path", node.working_path)
+        startpath = "/var/lib/waagent/"
+        namepattern = "Microsoft.Azure.RecoveryServices.VMSnapshotLinux-1.0.*"
+        sudo = True
+        find_tool = node.tools[Find]
+        file_list = find_tool.find_files(
+            start_path=startpath, name_pattern=namepattern, sudo=sudo
+        )
+        # print(file_list)
+        extension_directory = ""
+        if len(file_list) == 1:
+            extension_directory = file_list[0]
+        return extension_directory
+
+    @TestCaseMetadata(
+        description="""
+        Runs the custom script extension and verifies it execution on the
+        remote machine.
+        """,
+        priority=0,
+        requirement=simple_requirement(supported_features=[AzureExtension]),
+    )
+    def verify_exclude_disk(self, log: Logger, node: Node) -> None:
+        # Any extension will do, use CustomScript for convenience.
+        # Copy the local files into the VM.
+        # Move the files to the extension directory
+        # Give the execution permissions to the files
+        # Run the files
+        # Validate the results
+        extension_dir = self.copy_to_node(node, "handle.txt", "install_libraries.txt")
+        if extension_dir != "":
+            # moving the handle.py file
+            script = (
+                "#!/bin/sh\n"
+                f"mv {node.working_path}/handle.txt {extension_dir}/handle.py"
+            )
+            script_base64 = base64.b64encode(bytes(script, "utf-8")).decode("utf-8")
+            settings = {"script": script_base64}
+            extension = node.features[AzureExtension]
+            result = extension.create_or_update(
+                name="CustomScript",
+                publisher="Microsoft.Azure.Extensions",
+                type_="CustomScript",
+                type_handler_version="2.1",
+                auto_upgrade_minor_version=True,
+                settings=settings,
+            )
+
+            # moving the libraries file
+            script = (
+                "#!/bin/sh\n"
+                f"mv {node.working_path}/install_libraries.txt"
+                f" {extension_dir}/install_libraries.py"
+            )
+
+            script_base64 = base64.b64encode(bytes(script, "utf-8")).decode("utf-8")
+            settings = {"script": script_base64}
+            extension = node.features[AzureExtension]
+            result = extension.create_or_update(
+                name="CustomScript",
+                publisher="Microsoft.Azure.Extensions",
+                type_="CustomScript",
+                type_handler_version="2.1",
+                auto_upgrade_minor_version=True,
+                settings=settings,
+            )
+
+            # give the execution permissions to the file
+            script = f"#!/bin/sh\nchmod 777 {extension_dir}/handle.py"
+            script_base64 = base64.b64encode(bytes(script, "utf-8")).decode("utf-8")
+            settings = {"script": script_base64}
+            extension = node.features[AzureExtension]
+            result = extension.create_or_update(
+                name="CustomScript",
+                publisher="Microsoft.Azure.Extensions",
+                type_="CustomScript",
+                type_handler_version="2.1",
+                auto_upgrade_minor_version=True,
+                settings=settings,
+            )
+
+            # give the execution permissions to the file
+            script = f"#!/bin/sh\nchmod 777 {extension_dir}/install_libraries.py"
+            script_base64 = base64.b64encode(bytes(script, "utf-8")).decode("utf-8")
+            settings = {"script": script_base64}
+            extension = node.features[AzureExtension]
+            result = extension.create_or_update(
+                name="CustomScript",
+                publisher="Microsoft.Azure.Extensions",
+                type_="CustomScript",
+                type_handler_version="2.1",
+                auto_upgrade_minor_version=True,
+                settings=settings,
+            )
+
+            # execute the files
+            script = f"#!/bin/sh\npython3 {extension_dir}/install_libraries.py"
+            script_base64 = base64.b64encode(bytes(script, "utf-8")).decode("utf-8")
+            settings = {"script": script_base64}
+            extension = node.features[AzureExtension]
+            result = extension.create_or_update(
+                name="CustomScript",
+                publisher="Microsoft.Azure.Extensions",
+                type_="CustomScript",
+                type_handler_version="2.1",
+                auto_upgrade_minor_version=True,
+                settings=settings,
+            )
+
+            script = f"#!/bin/sh\npython2.7 {extension_dir}/handle.py"
+            script_base64 = base64.b64encode(bytes(script, "utf-8")).decode("utf-8")
+            settings = {"script": script_base64}
+            extension = node.features[AzureExtension]
+            result = extension.create_or_update(
+                name="CustomScript",
+                publisher="Microsoft.Azure.Extensions",
+                type_="CustomScript",
+                type_handler_version="2.1",
+                auto_upgrade_minor_version=True,
+                settings=settings,
+            )
+            assert_that(result["provisioning_state"]).described_as(
+                "Expected the extension to succeed"
+            ).is_equal_to("Succeeded")
+
+            # get the results
+            extension_name = "CustomScript"
+            extension_instance_view = extension.get(
+                name=extension_name,
+            ).instance_view
+
+            for substatus in extension_instance_view.substatuses:
+                print(f"Substatus : '{substatus}'")
+
+            print(result)
