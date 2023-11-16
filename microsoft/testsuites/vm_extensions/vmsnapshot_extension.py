3ae7902660002681702e7e4257fc31da3c388afa
arisettisanjana <varshasanjana29@gmail.com>
2023-11-16 14:02:32 +0530

    modified logic

--- microsoft/testsuites/vm_extensions/vmsnapshot_extension.py
+++ microsoft/testsuites/vm_extensions/vmsnapshot_extension.py
@@ -2,12 +2,12 @@
 # Licensed under the MIT license.
 
 import base64
-import glob
 import os
 import time
 import uuid
 from datetime import datetime
 from pathlib import PurePath, PurePosixPath
+from typing import cast
 
 from assertpy.assertpy import assert_that
 
@@ -20,6 +20,7 @@ from lisa import (
     TestSuiteMetadata,
     simple_requirement,
 )
+from lisa.operating_system import Posix
 from lisa.sut_orchestrator import AZURE
 from lisa.sut_orchestrator.azure.common import (
     AzureNodeSchema,
@@ -29,6 +30,7 @@ from lisa.sut_orchestrator.azure.common import (
 )
 from lisa.sut_orchestrator.azure.features import AzureExtension
 from lisa.sut_orchestrator.azure.platform_ import AzurePlatform
+from lisa.tools.curl import Curl
 from lisa.tools.find import Find
 
 
@@ -51,6 +53,153 @@ class VmSnapsotLinuxBVTExtension(TestSuite):
         requirement=simple_requirement(supported_features=[AzureExtension]),
     )
     def verify_vmsnapshot_extension(
+        self, log: Logger, node: Node, environment: Environment
+    ) -> None:
+        self._verify_vmsnapshot_extension(log, node, environment)
+
+    def get_restore_point(
+        self,
+        log: Logger,
+        environment: Environment,
+        resource_group_name: str,
+        restore_point_collection: str,
+        restore_point: str,
+    ) -> None:
+        assert environment.platform
+        platform: AzurePlatform = environment.platform  # type: ignore
+        assert isinstance(platform, AzurePlatform)
+        client = get_compute_client(platform)
+        attempts = 0
+        max_attempts = 450
+        while attempts < max_attempts:
+            response = client.restore_points.get(
+                resource_group_name=resource_group_name,
+                restore_point_collection_name=restore_point_collection,
+                restore_point_name=restore_point,
+                expand=None,
+            )
+            if response.provisioning_state == "Succeeded":
+                log.info(f"restore point {restore_point} created")
+                consistency_mode = response.consistency_mode
+                log.info(f"consistency mode is {consistency_mode}")
+                if (
+                    "FileSystemConsistent" in consistency_mode
+                    or "ApplicationConsistent" in consistency_mode
+                ):
+                    return
+                else:
+                    raise ValueError(
+                        "Restore point consistency mode is not "
+                        "FileSystemConsistent or ApplicationConsistent"
+                    )
+            else:
+                log.info(f"rp status is {response.provisioning_state}")
+                attempts += 1
+                time.sleep(2)
+        raise ValueError(
+            "Restore point provisioning status not Succeeded "
+            "after multiple attempts."
+        )
+
+    def find_extension_dir(self, node: Node) -> str:
+        startpath = PurePath("/var/lib/waagent/")
+        namepattern = "Microsoft.Azure.RecoveryServices.VMSnapshotLinux-1.0.*"
+        sudo = True
+        find_tool = node.tools[Find]
+        file_list = find_tool.find_files(
+            start_path=startpath, name_pattern=namepattern, sudo=sudo
+        )
+        extension_directory = ""
+        if len(file_list) == 1:
+            extension_directory = file_list[0]
+        return extension_directory
+
+    def copy_to_node(self, node: Node, filename: str) -> None:
+        parent_directory = ""
+
+        f_name = node.working_path / filename
+        temp_file = PurePosixPath(os.path.join(parent_directory, filename))
+        file_path = PurePath(
+            os.path.join((os.path.dirname(__file__)), "scripts", temp_file)
+        )
+        node.shell.copy(file_path, f_name)
+
+    @TestCaseMetadata(
+        description="""
+        Runs the custom script extension and verifies it execution on the
+        remote machine.
+        """,
+        priority=0,
+        requirement=simple_requirement(
+            supported_features=[AzureExtension],
+        ),
+    )
+    def verify_exclude_disk(
+        self, log: Logger, node: Node, environment: Environment
+    ) -> None:
+        # Any extension will do, use CustomScript for convenience.
+        # Copy the local files into the VM.
+        # Move the files to the extension directory
+        # Give the execution permissions to the files
+        # Run the files
+        # Validate the results
+        extension_dir = self.find_extension_dir(node)
+        if extension_dir == "":
+            self._verify_vmsnapshot_extension(log, node, environment)
+            extension_dir = self.find_extension_dir(node)
+        else:
+            # installing all the required packages
+            posix_os: Posix = cast(Posix, node.os)
+            posix_os.install_packages("python2.7")
+            posix_os.install_packages("pip")
+            url = "https://bootstrap.pypa.io/pip/2.7/get-pip.py"
+            args = "-o get-pip.py"
+            curl = Curl(node)
+            curl.fetch(url=url, arg=args, shell=True, execute_arg="", sudo=True)
+            node.execute(cmd="python2.7 get-pip.py", shell=True, sudo=True)
+            node.execute(cmd="python2.7 -m pip --version")
+            node.execute(cmd="python2.7 -m pip install mock", sudo=True)
+            # copy the file into the vm
+            self.copy_to_node(node, "handle.txt")
+
+            if extension_dir != "":
+                # moving the handle.py file to extension directory
+                script = (
+                    "#!/bin/sh\n"
+                    f"mv {node.working_path}/handle.txt {extension_dir}/handle.py"
+                )
+                script_base64 = base64.b64encode(bytes(script, "utf-8")).decode("utf-8")
+                settings = {"script": script_base64}
+                extension = node.features[AzureExtension]
+                result = extension.create_or_update(
+                    name="CustomScript",
+                    publisher="Microsoft.Azure.Extensions",
+                    type_="CustomScript",
+                    type_handler_version="2.1",
+                    auto_upgrade_minor_version=True,
+                    settings=settings,
+                )
+                log.info(extension_dir)
+                # give the execution permissions to the file
+                node.execute(
+                    cmd=f"chmod 777 {extension_dir}/handle.py", shell=True, sudo=True
+                )
+                # execute the file
+                result = node.execute(
+                    cmd=f"python2.7 {extension_dir}/handle.py", shell=True, sudo=True
+                )
+                print(type(result.stdout))
+                print(result.stdout)
+                if "True" in result.stdout:
+                    # test should partially pass
+                    log.info("unsupported distro")
+                else:
+                    # pass the test
+                    log.info("supported distro")
+            else:
+                log.info("failed to find the extension directory")
+
+    def _verify_vmsnapshot_extension(
         self, log: Logger, node: Node, environment: Environment
     ) -> None:
         unique_name = str(uuid.uuid4())
@@ -116,207 +265,3 @@ class VmSnapsotLinuxBVTExtension(TestSuite):
             time.sleep(1)
             count = count + 1
         assert_that(count, "Restore point creation failed.").is_less_than(10)
-
-    def get_restore_point(
-        self,
-        log: Logger,
-        environment: Environment,
-        resource_group_name: str,
-        restore_point_collection: str,
-        restore_point: str,
-    ) -> None:
-        assert environment.platform
-        platform: AzurePlatform = environment.platform  # type: ignore
-        assert isinstance(platform, AzurePlatform)
-        client = get_compute_client(platform)
-        attempts = 0
-        max_attempts = 450
-        while attempts < max_attempts:
-            response = client.restore_points.get(
-                resource_group_name=resource_group_name,
-                restore_point_collection_name=restore_point_collection,
-                restore_point_name=restore_point,
-                expand=None,
-            )
-            if response.provisioning_state == "Succeeded":
-                log.info(f"restore point {restore_point} created")
-                consistency_mode = response.consistency_mode
-                log.info(f"consistency mode is {consistency_mode}")
-                if (
-                    "FileSystemConsistent" in consistency_mode
-                    or "ApplicationConsistent" in consistency_mode
-                ):
-                    return
-                else:
-                    raise ValueError(
-                        "Restore point consistency mode is not "
-                        "FileSystemConsistent or ApplicationConsistent"
-                    )
-            else:
-                log.info(f"rp status is {response.provisioning_state}")
-                attempts += 1
-                time.sleep(2)
-        raise ValueError(
-            "Restore point provisioning status not Succeeded "
-            "after multiple attempts."
-        )
-
-    # Function to find the 'main' directory
-    def find_main_directory(self, base_path):
-        for main_directory in glob.glob(os.path.join(base_path, "main")):
-            return main_directory
-
-    def copy_to_node(self, node: Node, filename: str, lib_file: str) -> str:
-        parent_directory = ""
-
-        f_name = node.working_path / filename
-        temp_file = PurePosixPath(os.path.join(parent_directory, filename))
-        file_path = PurePath(
-            os.path.join((os.path.dirname(__file__)), "scripts", temp_file)
-        )
-        print(temp_file)
-        print("file_path", file_path)
-        lib_name = node.working_path / lib_file
-        temp_lib = PurePosixPath(os.path.join(parent_directory, lib_file))
-        print(temp_lib)
-        lib_path = PurePath(
-            os.path.join((os.path.dirname(__file__)), "scripts", temp_lib)
-        )
-        print("lib path", lib_path)
-        node.shell.copy(file_path, f_name)
-        node.shell.copy(lib_path, lib_name)
-        print("working path", node.working_path)
-        startpath = PurePath("/var/lib/waagent/")
-        namepattern = "Microsoft.Azure.RecoveryServices.VMSnapshotLinux-1.0.*"
-        sudo = True
-        find_tool = node.tools[Find]
-        file_list = find_tool.find_files(
-            start_path=startpath, name_pattern=namepattern, sudo=sudo
-        )
-        # print(file_list)
-        extension_directory = ""
-        if len(file_list) == 1:
-            extension_directory = file_list[0]
-        return extension_directory
-
-    @TestCaseMetadata(
-        description="""
-        Runs the custom script extension and verifies it execution on the
-        remote machine.
-        """,
-        priority=0,
-        requirement=simple_requirement(supported_features=[AzureExtension]),
-    )
-    def verify_exclude_disk(self, log: Logger, node: Node) -> None:
-        # Any extension will do, use CustomScript for convenience.
-        # Copy the local files into the VM.
-        # Move the files to the extension directory
-        # Give the execution permissions to the files
-        # Run the files
-        # Validate the results
-        extension_dir = self.copy_to_node(node, "handle.txt", "install_libraries.txt")
-        if extension_dir != "":
-            # moving the handle.py file
-            script = (
-                "#!/bin/sh\n"
-                f"mv {node.working_path}/handle.txt {extension_dir}/handle.py"
-            )
-            script_base64 = base64.b64encode(bytes(script, "utf-8")).decode("utf-8")
-            settings = {"script": script_base64}
-            extension = node.features[AzureExtension]
-            result = extension.create_or_update(
-                name="CustomScript",
-                publisher="Microsoft.Azure.Extensions",
-                type_="CustomScript",
-                type_handler_version="2.1",
-                auto_upgrade_minor_version=True,
-                settings=settings,
-            )
-
-            # moving the libraries file
-            script = (
-                "#!/bin/sh\n"
-                f"mv {node.working_path}/install_libraries.txt"
-                f" {extension_dir}/install_libraries.py"
-            )
-
-            script_base64 = base64.b64encode(bytes(script, "utf-8")).decode("utf-8")
-            settings = {"script": script_base64}
-            extension = node.features[AzureExtension]
-            result = extension.create_or_update(
-                name="CustomScript",
-                publisher="Microsoft.Azure.Extensions",
-                type_="CustomScript",
-                type_handler_version="2.1",
-                auto_upgrade_minor_version=True,
-                settings=settings,
-            )
-
-            # give the execution permissions to the file
-            script = f"#!/bin/sh\nchmod 777 {extension_dir}/handle.py"
-            script_base64 = base64.b64encode(bytes(script, "utf-8")).decode("utf-8")
-            settings = {"script": script_base64}
-            extension = node.features[AzureExtension]
-            result = extension.create_or_update(
-                name="CustomScript",
-                publisher="Microsoft.Azure.Extensions",
-                type_="CustomScript",
-                type_handler_version="2.1",
-                auto_upgrade_minor_version=True,
-                settings=settings,
-            )
-
-            # give the execution permissions to the file
-            script = f"#!/bin/sh\nchmod 777 {extension_dir}/install_libraries.py"
-            script_base64 = base64.b64encode(bytes(script, "utf-8")).decode("utf-8")
-            settings = {"script": script_base64}
-            extension = node.features[AzureExtension]
-            result = extension.create_or_update(
-                name="CustomScript",
-                publisher="Microsoft.Azure.Extensions",
-                type_="CustomScript",
-                type_handler_version="2.1",
-                auto_upgrade_minor_version=True,
-                settings=settings,
-            )
-
-            # execute the files
-            script = f"#!/bin/sh\npython3 {extension_dir}/install_libraries.py"
-            script_base64 = base64.b64encode(bytes(script, "utf-8")).decode("utf-8")
-            settings = {"script": script_base64}
-            extension = node.features[AzureExtension]
-            result = extension.create_or_update(
-                name="CustomScript",
-                publisher="Microsoft.Azure.Extensions",
-                type_="CustomScript",
-                type_handler_version="2.1",
-                auto_upgrade_minor_version=True,
-                settings=settings,
-            )
-
-            script = f"#!/bin/sh\npython2.7 {extension_dir}/handle.py"
-            script_base64 = base64.b64encode(bytes(script, "utf-8")).decode("utf-8")
-            settings = {"script": script_base64}
-            extension = node.features[AzureExtension]
-            result = extension.create_or_update(
-                name="CustomScript",
-                publisher="Microsoft.Azure.Extensions",
-                type_="CustomScript",
-                type_handler_version="2.1",
-                auto_upgrade_minor_version=True,
-                settings=settings,
-            )
-            assert_that(result["provisioning_state"]).described_as(
-                "Expected the extension to succeed"
-            ).is_equal_to("Succeeded")
-
-            # get the results
-            extension_name = "CustomScript"
-            extension_instance_view = extension.get(
-                name=extension_name,
-            ).instance_view
-
-            for substatus in extension_instance_view.substatuses:
-                print(f"Substatus : '{substatus}'")
-
-            print(result)
