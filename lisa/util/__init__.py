c5881a3ae37d8b60c5f170ed496193e0aa6931c4
Matthew McGovern <mamcgove@microsoft.com>
2024-01-10 10:05:42 -0800

    refactor and rename check_package_source_url

--- lisa/util/__init__.py
+++ lisa/util/__init__.py
@@ -48,7 +48,7 @@ global_ssh_key_access_lock = Lock()
 # source -
 # https://github.com/django/django/blob/stable/1.3.x/django/core/validators.py#L45
 __url_pattern = re.compile(
-    r"^(?:http|s?ftp)s?://"  # http:// or https://
+    r"^(?:http|https|sftp|ftp)://"  # http:// or https://
     r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)"
     r"+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # ...domain
     r"localhost|"  # localhost...
@@ -584,9 +584,7 @@ def find_group_in_lines(
     return result
 
 
-def deep_update_dict(
-    src: Dict[str, Any], dest: Optional[Dict[str, Any]]
-) -> Dict[str, Any]:
+def deep_update_dict(src: Dict[str, Any], dest: Dict[str, Any]) -> Dict[str, Any]:
     if (
         dest is None
         or isinstance(dest, int)
@@ -600,7 +598,7 @@ def deep_update_dict(
 
     if isinstance(result, dict):
         for key, value in src.items():
-            if dest and isinstance(value, dict) and key in dest:
+            if isinstance(value, dict) and key in dest:
                 value = deep_update_dict(value, dest[key])
             result[key] = value
     elif isinstance(src, dict):
@@ -621,31 +619,57 @@ def is_valid_url(url: str, raise_error: bool = True) -> bool:
     return is_url
 
 
-def is_valid_source_code_package(
+def _raise_or_log_failure(log: "Logger", raise_error: bool, failure_msg: str) -> bool:
+    if raise_error:
+        raise LisaException(failure_msg)
+    else:
+        log.debug(failure_msg)
+        return False
+
+
+# big function to check the parts of a url
+# allow raising exceptions or log and return a bool
+# allows checks for:
+#   expected domains
+#   protocols (require https, sftp, etc)
+#   filenames (pattern matching)
+def check_url(
+    log: "Logger",
     source_url: str,
-    expected_package_name_pattern: Pattern[str],
     allowed_protocols: Optional[List[str]] = None,
-    expected_domains: Optional[List[str]] = None,
+    expected_domains_pattern: Optional[Pattern[str]] = None,
+    expected_filename_pattern: Optional[Pattern[str]] = None,
+    raise_error: bool = False,
 ) -> bool:
     # avoid using a mutable default parameter
     if not allowed_protocols:
         allowed_protocols = [
             "https",
-            "sftp",
         ]
+    # pylinter doesn't like returning too many times in a function
+    # instead we'll assign to a boolean and check it repeatedly.
+    # thanks, linter.
+    result = True
     # first, check if it's a url.
-    if not is_valid_url(url=source_url, raise_error=False):
+    failure_msg = f"{source_url} is not a valid URL, check your arguments."
+    if not (
+        is_valid_url(url=source_url, raise_error=False)
+        or _raise_or_log_failure(log, raise_error, failure_msg)
+    ):
+        # fast return false, other checks depend on this one
         return False
 
     # NOTE: urllib might not work as you'd expect.
     # It doesn't throw on lots of things you wouldn't expect to be urls.
     # You must verify the parts on your own, some of them may be empty, some null.
     # check: https://docs.python.org/3/library/urllib.parse.html#url-parsing
-
+    failure_msg = f"urlparse failed to parse url {source_url}, check your arguments."
     try:
         parts = urlparse(source_url)
     except ValueError:
-        return False
+        if not _raise_or_log_failure(log, raise_error, failure_msg):
+            # another fast return, other checks depend on this one
+            return False
 
     # ex: from https://www.com/path/to/file.tar
     # scheme : https
@@ -654,23 +678,51 @@ def is_valid_source_code_package(
 
     # get the filename from the path portion of the url
     file_path = parts.path.split("/")[-1]
-    full_match = expected_package_name_pattern.match(file_path)
-    if not full_match:
-        return False
+    full_match = None
+    # check we can match against the filename
+    if expected_filename_pattern:
+        full_match = expected_filename_pattern.match(file_path)
+        failure_msg = (
+            f"File at {source_url} did not match pattern "
+            f"{expected_filename_pattern.pattern}."
+        )
+        if not full_match:
+            result &= _raise_or_log_failure(log, raise_error, failure_msg)
 
     # check the expected domain is correct if present
-    valid_netloc = not expected_domains or any(
-        [domain.endswith(parts.netloc) for domain in expected_domains]
-    )
+    if (
+        result
+        and expected_domains_pattern
+        and not expected_domains_pattern.match(parts.netloc)
+    ):
+        # logging domains requires check that expected_domains != None
+        failure_msg = (
+            f"net location of url {source_url} did not match "
+            f"expected domains { expected_domains_pattern.pattern } "
+        )
+        result &= _raise_or_log_failure(log, raise_error, failure_msg)
 
-    # optional but default is check access is via sftp/https
-    valid_scheme = any([parts.scheme == x for x in allowed_protocols])
-    return (
-        valid_scheme
-        and parts.netloc != ""
-        and valid_netloc
-        and (full_match.group(0) == file_path)
+    # Check the protocol (aka scheme) in the url
+    # default is check access is via https
+    failure_msg = (
+        f"URL {source_url} uses an invalid protocol "
+        "or net location! Check url argument."
     )
+    valid_scheme = any([parts.scheme == x for x in allowed_protocols])
+    if result and not valid_scheme:
+        result &= _raise_or_log_failure(log, raise_error, failure_msg)
+    # finally verify the full match we found matches the actual filename
+    # avoids an accidental partial match
+    if result and expected_filename_pattern and full_match:
+        path_matches = full_match.group(0) == file_path
+        failure_msg = (
+            f"File at url {source_url} failed to match"
+            f" pattern {expected_filename_pattern.pattern}."
+        )
+        if not path_matches:
+            result &= _raise_or_log_failure(log, raise_error, failure_msg)
+
+    return result
 
 
 def filter_ansi_escape(content: str) -> str:
