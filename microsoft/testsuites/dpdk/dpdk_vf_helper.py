fcb88b368e536f6228c409acaea3552d250cbceb
Matthew McGovern <mamcgove@microsoft.com>
2024-01-18 12:23:56 -0800

    Fix receive limit

--- microsoft/testsuites/dpdk/dpdk_vf_helper.py
+++ microsoft/testsuites/dpdk/dpdk_vf_helper.py
@@ -48,7 +48,7 @@ class DpdkVfHelper:
         },
         MSFT_MANA: {
             SINGLE_QUEUE: {SEND: 8_000_000, RECV: 6_000_000},
-            MULTI_QUEUE: {SEND: 48_000_000, RECV: 45_000_000},
+            MULTI_QUEUE: {SEND: 48_000_000, RECV: 38_000_000},
         },
     }
 
