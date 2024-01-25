e18e1b600e825efd90fcebf11deb2ef9a64b7366
Anirudh Rayabharam <anrayabh@microsoft.com>
2024-01-25 08:28:06 +0530

    Add Windows variants of cp and unzip tools (#3151)
    * tools/cp: add Windows variant
    
    Add a Windows variant of the cp tool. Use the Copy-Item cmdlet under the
    hood to achieve the required functionality.
    
    Signed-off-by: Anirudh Rayabharam <anrayabh@microsoft.com>
    
    * tools/unzip: add Windows variant
    
    Add WindowsUnzip tool. Use the Expand-Archive cmdlet to achieve the
    unzip functionality.
    
    Signed-off-by: Anirudh Rayabharam <anrayabh@microsoft.com>
    
    * tools/cp: use Optional[PurePath] instead of PurePath | None
    
    Fixes PR gate failure.
    
    Signed-off-by: Anirudh Rayabharam <anrayabh@microsoft.com>
    
    * tools/unzip: use mkdir tool instead of raw command
    
    Signed-off-by: Anirudh Rayabharam <anrayabh@microsoft.com>
    
    ---------
    
    Signed-off-by: Anirudh Rayabharam <anrayabh@microsoft.com>

--- lisa/tools/unzip.py
+++ lisa/tools/unzip.py
@@ -2,8 +2,11 @@
 # Licensed under the MIT license.
 
 
+from typing import Optional, Type
+
 from lisa.executable import Tool
 from lisa.operating_system import Posix
+from lisa.tools import Mkdir, PowerShell
 
 
 class Unzip(Tool):
@@ -35,4 +38,29 @@ class Unzip(Tool):
         )
         result.assert_exit_code(
             0, f"Failed to extract file to {dest_dir}, {result.stderr}"
+        )
+
+    @classmethod
+    def _windows_tool(cls) -> Optional[Type[Tool]]:
+        return WindowsUnzip
+
+
+class WindowsUnzip(Unzip):
+    @property
+    def command(self) -> str:
+        return ""
+
+    @property
+    def can_install(self) -> bool:
+        return False
+
+    def _check_exists(self) -> bool:
+        return True
+
+    def extract(self, file: str, dest_dir: str, sudo: bool = False) -> None:
+        self.node.tools[Mkdir].create_directory(dest_dir, sudo=sudo)
+        self.node.tools[PowerShell].run_cmdlet(
+            f"Expand-Archive -Path {file} -DestinationPath {dest_dir} -Force",
+            sudo=sudo,
+            fail_on_error=True,
         )
