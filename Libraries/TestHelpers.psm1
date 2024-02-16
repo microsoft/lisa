8f186278ff672ba5dba87a75ec136cac102007ac
Srikanth Myakam <m.srikanth.m@gmail.com>
2024-02-16 14:42:28 +0530

    Update TestHelpers.psm1

--- Libraries/TestHelpers.psm1
+++ Libraries/TestHelpers.psm1
@@ -811,11 +811,13 @@ Function Get-SSHKey ($XMLSecretFile) {
 				#	$privateSSHKeyName = $privateSSHKey.Split('?')[0].Split('/')[-1]
 				#}
 				#$WebClient.DownloadFile("$privateSSHKey", "$temp_Folder/$privateSSHKeyName")
+				echo "Downloading privateSSHKey to $temp_Folder/$privateSSHKeyName"
 				Invoke-WebRequest $privateSSHKey -OutFile "$temp_Folder/$privateSSHKeyName"
-				echo "Downloaded $privateSSHKey to $temp_Folder/$privateSSHKeyName"
 				ls "$temp_Folder"
 			}
 			catch {
+				echo "Downloaded $privateSSHKey to $temp_Folder/$privateSSHKeyName"
+				ls "$temp_Folder"
 				Throw "Failed to download from '$privateSSHKey', please double check the path."
 			}
 			$sshKeyPath = "$temp_Folder/$privateSSHKeyName"
