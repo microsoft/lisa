eb7fc7b415e0dd7e79554d68dcee84c15ab209c9
Anirudh Rayabharam <anrayabh@microsoft.com>
2024-02-19 11:25:11 +0530

    sut_orchestrator: hyperv: use RemoteCopy tool to copy source files
    Signed-off-by: Anirudh Rayabharam <anrayabh@microsoft.com>

--- lisa/sut_orchestrator/hyperv/source.py
+++ lisa/sut_orchestrator/hyperv/source.py
@@ -1,12 +1,12 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
-from pathlib import Path, PureWindowsPath
-from typing import List, Optional, Type
+from pathlib import PurePath, PureWindowsPath
+from typing import List, Type
 
 from lisa import schema
 from lisa.node import RemoteNode
 from lisa.tools.ls import Ls
-from lisa.tools.mkdir import Mkdir
+from lisa.tools.remote_copy import RemoteCopy
 from lisa.tools.unzip import Unzip
 from lisa.util import InitializableMixin, subclasses
 from lisa.util.logger import get_logger
@@ -54,61 +54,30 @@ class LocalSource(Source):
     def _download_file(
         self, file: SourceFileSchema, server: RemoteNode
     ) -> List[PureWindowsPath]:
-        local_path = Path(file.source)
+        local_path = PurePath(file.source)
 
-        local_files = []
-        if local_path.is_dir():
-            local_files = [f for f in local_path.iterdir() if f.is_file()]
-        else:
-            local_files = [local_path]
+        downloaded_paths: List[PurePath] = []
 
-        downloaded_paths: List[PureWindowsPath] = []
-
-        for f in local_files:
-            downloaded_paths += self._download_single_file(
-                f, file.destination, file.unzip, server
-            )
-
-        return downloaded_paths
-
-    def _download_single_file(
-        self,
-        local_path: Path,
-        destination: Optional[str],
-        unzip: bool,
-        server: RemoteNode,
-    ) -> List[PureWindowsPath]:
-        defined_destination = None
-        if destination:
-            if PureWindowsPath(destination).is_absolute():
-                defined_destination = PureWindowsPath(destination)
+        if file.destination:
+            if PureWindowsPath(file.destination).is_absolute():
+                dest = PureWindowsPath(file.destination)
             else:
-                defined_destination = PureWindowsPath(server.working_path / destination)
+                dest = PureWindowsPath(server.working_path) / file.destination
+        else:
+            dest = PureWindowsPath(server.working_path) / "sources"
 
-        dest_path = PureWindowsPath(
-            defined_destination / local_path.name
-            if defined_destination
-            else server.working_path / "sources" / local_path.name
+        downloaded_paths = server.tools[RemoteCopy].copy_to_remote(
+            local_path, dest, recurse=True
         )
 
-        server.tools[Mkdir].create_directory(str(dest_path.parent))
+        if file.unzip:
+            assert len(downloaded_paths) == 1
+            zip_path = PureWindowsPath(downloaded_paths[0])
+            server.tools[Unzip].extract(str(zip_path), str(zip_path.parent))
+            server.shell.remove(zip_path)
 
-        self._log.debug(f"Copying {local_path} to server")
-        server.shell.copy(local_path, dest_path)
-        self._log.debug("Finished copying.")
+            extracted_files = server.tools[Ls].list(str(zip_path.parent))
 
-        if unzip:
-            final_destination = (
-                defined_destination
-                if defined_destination
-                else PureWindowsPath(server.working_path) / "sources"
-            )
+            return [zip_path.parent.joinpath(f) for f in extracted_files]
 
-            server.tools[Unzip].extract(str(dest_path), str(dest_path.parent))
-            server.shell.remove(dest_path)
-
-            extracted_files = server.tools[Ls].list(str(dest_path.parent))
-
-            return [final_destination.joinpath(f) for f in extracted_files]
-
-        return [dest_path]
+        return [PureWindowsPath(p) for p in downloaded_paths]
