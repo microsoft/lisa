8872eb0442ab034eb28e668d43839ed4401d49a2
kanchansenlaskar <ksenlaskar@gmail.com>
2024-03-07 17:58:05 +0530

    Use sudo with cat /etc/waagent.conf to avoid permission issue

--- lisa/sut_orchestrator/azure/tools.py
+++ lisa/sut_orchestrator/azure/tools.py
@@ -115,7 +115,9 @@ class Waagent(Tool):
         waagent_conf_file = self._get_waagent_conf_path()
 
         config = {}
-        cfg = self.node.tools[Cat].run(waagent_conf_file, force_run=force_run, sudo=True).stdout
+        cfg = self.node.tools[Cat].run(
+            waagent_conf_file, force_run=force_run, sudo=True
+        ).stdout
         for line in cfg.splitlines():
             matched = self._key_value_regex.fullmatch(line)
             if matched:
