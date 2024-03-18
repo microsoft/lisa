474b4abfc653607ceddcd6734663184da219ee73
John Feng <johnfeng@microsoft.com>
2024-03-18 09:40:00 -0700

    change assess test priorty to 1

--- microsoft/testsuites/vm_extensions/linux_patch_extension.py
+++ microsoft/testsuites/vm_extensions/linux_patch_extension.py
@@ -50,7 +50,7 @@ class LinuxPatchExtensionBVT(TestSuite):
         Perform assess patches to trigger Microsoft.CPlat.Core.LinuxPatchExtension creation in a virtual machine.
         Verify status file response for validity.
         """,
-        priority=0,
+        priority=1,
     )
     def verify_vm_assess_patches(self, node:Node, environment:Environment, log:Logger)->None:
         assert environment.platform
@@ -77,40 +77,5 @@ class LinuxPatchExtensionBVT(TestSuite):
         assert_that(assess_result["error"]['code']).described_as(
             "Expected no error in assess patches operation"
         ).is_equal_to("0")
-
-    @TestCaseMetadata(
-        description="""
-        Verify walinuxagent or waagent is running on virtual machine.
-        Perform install patches to trigger Microsoft.CPlat.Core.LinuxPatchExtension creation in a virtual machine.
-        Verify status file response for validity.
-        """,
-        priority=1,
-    )
-    def verify_vm_install_patches(self, node:Node, environment:Environment, log:Logger)->None:
-        assert environment.platform
-        platform: AzurePlatform = environment.platform  # type: ignore
-        assert isinstance(platform, AzurePlatform)
-        compute_client = get_compute_client(platform)
-        node_context = get_node_context(node)
-        resource_group_name = node_context.resource_group_name
-        vm_name = node_context.vm_name
-
-        # verify vm agent is running
-        _verify_vm_agent_running(node, log)
-
-        operation = compute_client.virtual_machines.begin_install_patches(resource_group_name=resource_group_name,vm_name=vm_name)
-
-        # pause fetching operation by 1min, so status file can be generated
-        result =wait_operation(operation, 60000)
-        log.debug(f"result: {result}")
-
-        assert_that(result["status"]).described_as(
-            "Expected the install patches to succeed"
-        ).is_equal_to("Succeeded")
-
-        assert_that(result["error"]['code']).described_as(
-            "Expected no error in install patches operation"
-        ).is_equal_to("0")
-
     
     
