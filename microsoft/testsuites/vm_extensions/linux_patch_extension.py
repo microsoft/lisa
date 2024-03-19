670a0ff2fdc1c30000625e2a371f48654091e78a
John Feng <johnfeng@microsoft.com>
2024-03-19 13:45:07 -0700

    add logic for install patches operation

--- microsoft/testsuites/vm_extensions/linux_patch_extension.py
+++ microsoft/testsuites/vm_extensions/linux_patch_extension.py
@@ -2,7 +2,6 @@
 # Licensed under the MIT license.
 
 
-import time
 from assertpy.assertpy import assert_that
 from lisa.base_tools.service import Service
 from lisa.sut_orchestrator.azure.common import (
@@ -22,7 +21,6 @@ from lisa import (
 )
 
 from lisa.sut_orchestrator.azure.platform_ import AzurePlatform
-from lisa.util import LisaException
 
 def _verify_vm_agent_running(node: Node, log: Logger) -> None:
     service = node.tools[Service]
@@ -77,5 +75,43 @@ class LinuxPatchExtensionBVT(TestSuite):
         assert_that(assess_result["error"]['code']).described_as(
             "Expected no error in assess patches operation"
         ).is_equal_to("0")
+
+
+    @TestCaseMetadata(
+        description="""
+        Verify walinuxagent or waagent service is running on virtual machine.
+        Perform install patches to trigger Microsoft.CPlat.Core.LinuxPatchExtension creation in a virtual machine.
+        Verify status file response for validity.
+        """,
+        priority=1,
+    )
+    def verify_vm_install_patches(self, node:Node, environment:Environment, log:Logger)->None:
+        assert environment.platform, "platform shouldn't be None."
+        platform: AzurePlatform = environment.platform  # type: ignore
+        assert isinstance(platform, AzurePlatform), "platform should be AzurePlatform instance"
+        compute_client = get_compute_client(platform)
+        node_context = get_node_context(node)
+        resource_group_name = node_context.resource_group_name
+        vm_name = node_context.vm_name
+
+        # verify vm agent is running
+        _verify_vm_agent_running(node, log)
+
+        try: 
+            operation = compute_client.virtual_machines.begin_install_patches(resource_group_name=resource_group_name,vm_name=vm_name)
+            # set wait operation timeout 10 min, status file should be generated before timeout
+            install_result = wait_operation(operation, 600)
+        except Exception as e:
+                raise e
+
+        assert install_result, "assess_result shouldn't be None"
+        assert_that(install_result["status"]).described_as(
+            "Expected the assess patches to succeed"
+        ).is_equal_to("Succeeded")
+
+        assert_that(install_result["error"]['code']).described_as(
+            "Expected no error in assess patches operation"
+        ).is_equal_to("0")
+    
     
     
