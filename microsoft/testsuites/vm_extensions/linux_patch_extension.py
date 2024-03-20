aee35da3dd9db8d85186d637051b0aeeef1b91ac
John Feng <johnfeng@microsoft.com>
2024-03-20 11:33:14 -0700

    reformat py file

--- microsoft/testsuites/vm_extensions/linux_patch_extension.py
+++ microsoft/testsuites/vm_extensions/linux_patch_extension.py
@@ -1,5 +1,4 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
+# Copyright (c) Microsoft Corporation. Licensed under the MIT license.
 
 from assertpy.assertpy import assert_that
 from lisa.base_tools.service import Service
@@ -18,18 +17,23 @@ from lisa import (
     TestSuiteMetadata,
     simple_requirement,
 )
-
 from lisa.sut_orchestrator.azure.platform_ import AzurePlatform
+
 
 def _verify_vm_agent_running(node: Node, log: Logger) -> None:
     service = node.tools[Service]
-    is_vm_agent_running = service.is_service_running("walinuxagent.service") or service.is_service_running("waagent.service")
-    
-    log.debug(f"verify walinuxagent or waagent service is running: {is_vm_agent_running}")
-    
+    is_vm_agent_running = service.is_service_running(
+        "walinuxagent.service"
+    ) or service.is_service_running("waagent.service")
+
+    log.debug(
+        f"verify walinuxagent or waagent service running:{is_vm_agent_running}"
+    )
+
     assert_that(is_vm_agent_running).described_as(
         "Expected walinuxagent or waagent service is running"
     ).is_true()
+
 
 @TestSuiteMetadata(
     area="vm_extension",
@@ -40,26 +44,34 @@ def _verify_vm_agent_running(node: Node, log: Logger) -> None:
 class LinuxPatchExtensionBVT(TestSuite):
     @TestCaseMetadata(
         description="""
-        Verify walinuxagent or waagent service is running on virtual machine.
-        Perform assess patches to trigger Microsoft.CPlat.Core.LinuxPatchExtension creation in a virtual machine.
-        Verify status file response for validity.
+        Verify walinuxagent or waagent service is running on vm. Perform assess
+        patches to trigger Microsoft.CPlat.Core.LinuxPatchExtension creation in
+        vm. Verify status file response for validity.
         """,
         priority=1,
     )
-    def verify_vm_assess_patches(self, node:Node, environment:Environment, log:Logger)->None:
+    def verify_vm_assess_patches(
+        self, node: Node, environment: Environment, log: Logger
+    ) -> None:
         assert environment.platform, "platform shouldn't be None."
         platform: AzurePlatform = environment.platform  # type: ignore
-        assert isinstance(platform, AzurePlatform), "platform should be AzurePlatform instance"
+        assert isinstance(
+            platform, AzurePlatform
+        ), "platform should be AzurePlatform instance"
         compute_client = get_compute_client(platform)
         node_context = get_node_context(node)
         resource_group_name = node_context.resource_group_name
         vm_name = node_context.vm_name
 
-        # verify vm agent service is running, lpe is a dependent of vm agent service
+        # verify vm agent service is running, lpe is a dependent of vm agent
+        # service
         _verify_vm_agent_running(node, log)
 
-        operation = compute_client.virtual_machines.begin_assess_patches(resource_group_name=resource_group_name,vm_name=vm_name)
-        # set wait operation timeout 10 min, status file should be generated before timeout
+        operation = compute_client.virtual_machines.begin_assess_patches(
+            resource_group_name=resource_group_name, vm_name=vm_name
+        )
+        # set wait operation timeout 10 min, status file should be generated
+        # before timeout
         assess_result = wait_operation(operation, 600)
 
         assert assess_result, "assess_result shouldn't be None"
@@ -67,7 +79,6 @@ class LinuxPatchExtensionBVT(TestSuite):
             "Expected the assess patches to succeed"
         ).is_equal_to("Succeeded")
 
-        assert_that(assess_result["error"]['code']).described_as(
+        assert_that(assess_result["error"]["code"]).described_as(
             "Expected no error in assess patches operation"
         ).is_equal_to("0")
-    
