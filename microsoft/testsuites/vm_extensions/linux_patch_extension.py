d9e39b838155edf527809ae6de9fd3c09306c2bb
feng-j678 <127874208+feng-j678@users.noreply.github.com>
2024-03-26 07:18:21 -0700

    Add/install patches test (#3229)
    * add assess_patch test
    
    * add install patches operation test
    
    * change assess test priorty to 1
    
    * change assess test priority to 1
    
    * remove if condition in verify agent running and change timout to 600
    
    * add logic for install patches operation
    
    * add install patches input parm
    
    * remove try exception
    
    * remove try exception and blank space
    
    * reformat py file
    
    * resort the imports
    
    * fix flake8 pylint issue
    
    * fix flake8 pylint, import sort issues
    
    * remove .service from verify vm agent running method, add log debug()
    
    * add timeout and log debug()
    
    * use self.timeout as parm for wait operation()

--- microsoft/testsuites/vm_extensions/linux_patch_extension.py
+++ microsoft/testsuites/vm_extensions/linux_patch_extension.py
@@ -63,6 +63,8 @@ def _verify_vm_agent_running(node: Node, log: Logger) -> None:
     ),
 )
 class LinuxPatchExtensionBVT(TestSuite):
+    TIMEOUT = 12600  # 3H30M
+
     @TestCaseMetadata(
         description="""
         Verify walinuxagent or waagent service is running on vm. Perform assess
@@ -75,7 +77,6 @@ class LinuxPatchExtensionBVT(TestSuite):
         self, node: Node, environment: Environment, log: Logger
     ) -> None:
         compute_client, resource_group_name, vm_name = _set_up_vm(node, environment)
-
         # verify vm agent service is running, lpe is a dependent of vm agent
         # service
         _verify_vm_agent_running(node, log)
@@ -88,10 +89,56 @@ class LinuxPatchExtensionBVT(TestSuite):
         assess_result = wait_operation(operation, 600)
 
         assert assess_result, "assess_result shouldn't be None"
+        log.debug(f"assess_result:{assess_result}")
         assert_that(assess_result["status"]).described_as(
             "Expected the assess patches to succeed"
         ).is_equal_to("Succeeded")
 
         assert_that(assess_result["error"]["code"]).described_as(
             "Expected no error in assess patches operation"
+        ).is_equal_to("0")
+
+    @TestCaseMetadata(
+        description="""
+        Verify walinuxagent or waagent service is running on vm. Perform install
+        patches to trigger Microsoft.CPlat.Core.LinuxPatchExtension creation in vm.
+        Verify status file response for validity.
+        """,
+        priority=3,
+        timeout=TIMEOUT,
+    )
+    def verify_vm_install_patches(
+        self, node: Node, environment: Environment, log: Logger
+    ) -> None:
+        compute_client, resource_group_name, vm_name = _set_up_vm(node, environment)
+        install_patches_input = {
+            "maximumDuration": "PT3H30M",
+            "rebootSetting": "IfRequired",
+            "linuxParameters": {
+                "classificationsToInclude": ["Security", "Critical"],
+                "packageNameMasksToInclude": ["ca-certificates*", "php7-openssl*"],
+            },
+        }
+
+        # verify vm agent service is running, lpe is a dependent of vm agent
+        # service
+        _verify_vm_agent_running(node, log)
+
+        operation = compute_client.virtual_machines.begin_install_patches(
+            resource_group_name=resource_group_name,
+            vm_name=vm_name,
+            install_patches_input=install_patches_input,
+        )
+        # set wait operation max duration 3H30M timeout, status file should be
+        # generated before timeout
+        install_result = wait_operation(operation, self.TIMEOUT)
+
+        assert install_result, "install_result shouldn't be None"
+        log.debug(f"install_result:{install_result}")
+        assert_that(install_result["status"]).described_as(
+            "Expected the install patches to succeed"
+        ).is_equal_to("Succeeded")
+
+        assert_that(install_result["error"]["code"]).described_as(
+            "Expected no error in install patches operation"
         ).is_equal_to("0")
