ee6c1d7d19d46e848f31ba493b13ca0705e3c193
Matthew McGovern <mamcgove@microsoft.com>
2024-04-11 10:17:45 -0700

    mdatp: swap from bitmask to byte value with precedence
    -Move to using an int as return code to avoid conflicting
    with existing shell exit codes.
    
    This is done to help differentiate between os/shell errors and
    a successful validation (either fail or pass)

--- microsoft/testsuites/mdatp/check-mdatp.sh
+++ microsoft/testsuites/mdatp/check-mdatp.sh
@@ -7,22 +7,24 @@
 # checks Azure Extension directories using globs, since they're not 
 # well-known.
 
-# Returns nonzero value on detection of any defender info
-# Return value is a bitmask showing the types of info detected.
-# Bits are ordered by seriousness of the issue:
-# Least significant bit is bad
-# Most significant bit is worst.
+# Returns nonzero value on detection of any defender info.
+# Return value are ordered by seriousness of the issue:
+# Lower value is bad
+# Highest value is worst.
+
+# Publishers: Re-run and remediate until you get a 0 return code.
+
 EXIT_CODE=0
 # if any mdatp install in /etc/opt is found
-EXIT_MDATP_AGENT_INSTALLED=1
+EXIT_MDATP_AGENT_INSTALLED=251
 # if mdatp az extension is installed
-EXIT_MDE_INSTALLED=2
+EXIT_MDE_INSTALLED=252
 # if any log dirs are found
-EXIT_MDATP_LOGS_FOUND=4
+EXIT_MDATP_LOGS_FOUND=253
 # if an installation log is found
-EXIT_MDATP_INSTALL_LOGS_FOUND=8
+EXIT_MDATP_INSTALL_LOGS_FOUND=254
 # if an onboarding blob is found
-EXIT_ONBOARD_INFO_FOUND=16
+EXIT_ONBOARD_INFO_FOUND=255
 MDATP_OPT_DIR='/etc/opt/microsoft/mdatp'
 MDATP_LOG_DIR='/var/log/microsoft/mdatp'
 ERROR_MSG_HEADER="----------------------------------------------------------------------"
@@ -43,7 +45,7 @@ check_unexpected_file () {
 for mde_dir in /var/lib/waagent/Microsoft.Azure.AzureDefenderForServers.MDE.Linux* ; do
     # for all the versioned folders we find...
     if [ -e "$mde_dir" ]; then
-        EXIT_CODE=$((EXIT_CODE|EXIT_MDE_INSTALLED))
+        EXIT_CODE=$EXIT_MDE_INSTALLED
         if [ -d "$mde_dir" ]; then
             # find regular files, skip printing them if they are binary
             find_printable_files "$mde_dir"
@@ -58,7 +60,7 @@ done
 for log_dir in /var/log/azure/Microsoft.Azure.AzureDefenderForServers.MDE.Linux* ; do
     # for all the versioned folders we find...
     if [ -e "$log_dir" ]; then
-        EXIT_CODE=$((EXIT_CODE|EXIT_MDATP_LOGS_FOUND))
+        EXIT_CODE=$EXIT_MDATP_LOGS_FOUND
         echo "checking $log_dir..."
         if [ -d "$log_dir" ]; then
             find_printable_files "$log_dir"
@@ -73,7 +75,7 @@ done
 for log_dir in /var/lib/GuestConfig/extension_logs/Microsoft.Azure.AzureDefenderForServers.MDE.Linux* ; do
     # for all the versioned folders we find...
     if [ -e "$log_dir" ]; then
-        EXIT_CODE=$((EXIT_CODE|EXIT_MDATP_LOGS_FOUND))
+        EXIT_CODE=$EXIT_MDATP_LOGS_FOUND
         echo "checking $log_dir..."
         if [ -d "$log_dir" ]; then
             find_printable_files "$log_dir"
@@ -85,13 +87,13 @@ done
 
 # check for mde agent install in /etc/opt
 if [  -e "$MDATP_OPT_DIR" ]; then
-    EXIT_CODE=$((EXIT_CODE|EXIT_MDATP_AGENT_INSTALLED))
+    EXIT_CODE=$EXIT_MDATP_AGENT_INSTALLED
     find_printable_files "$MDATP_OPT_DIR"
 fi
 
 # check for install or runtime logs in /var/log
 if [  -e "$MDATP_LOG_DIR" ]; then
-    EXIT_CODE=$((EXIT_CODE|EXIT_MDATP_LOGS_FOUND))
+    EXIT_CODE=$EXIT_MDATP_LOGS_FOUND
     find_printable_files "$MDATP_LOG_DIR"
 fi
 
@@ -100,7 +102,7 @@ if [ -f "$MDATP_LOG_DIR/install.log" ]; then
     echo "$ERROR_MSG_HEADER" >&2
     echo "ERROR: mdatp install logs are present in this image!" >&2
     echo "Publishers should remove this data before publishing public images." >&2
-    EXIT_CODE=$((EXIT_CODE|EXIT_MDATP_INSTALL_LOGS_FOUND))
+    EXIT_CODE=$EXIT_MDATP_INSTALL_LOGS_FOUND
 fi
 
 # special log line for mdatp_onboard.json
@@ -108,7 +110,7 @@ if [ -f "$MDATP_OPT_DIR/mdatp_onboard.json" ]; then
     echo "$ERROR_MSG_HEADER" >&2
     echo "ERROR: mdatp onboarding info is present in this image!" >&2
     echo "Publishers should remove this data before publishing public images." >&2
-    EXIT_CODE=$((EXIT_CODE|EXIT_ONBOARD_INFO_FOUND))
+    EXIT_CODE=$EXIT_ONBOARD_INFO_FOUND
 fi
 
 # returns nonzero value if defender info is found
