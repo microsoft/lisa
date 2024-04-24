d36c0da377c45bfa0dd0ddc405b8a72bb75202c3
John Feng <johnfeng@microsoft.com>
2024-04-23 22:43:47 -0700

    add logic to skip test for unspported images

--- microsoft/testsuites/vm_extensions/linux_patch_extension.py
+++ microsoft/testsuites/vm_extensions/linux_patch_extension.py
@@ -1,6 +1,6 @@
 # Copyright (c) Microsoft Corporation. Licensed under the MIT license.
 
-from typing import Any
+from typing import Any, Dict, List
 
 from assertpy.assertpy import assert_that
 
@@ -14,7 +14,7 @@ from lisa import (
     simple_requirement,
 )
 from lisa.base_tools.service import Service
-from lisa.operating_system import BSD
+from lisa.operating_system import BSD, SLES, CBLMariner, Debian, Redhat
 from lisa.sut_orchestrator import AZURE
 from lisa.sut_orchestrator.azure.common import (
     get_compute_client,
@@ -22,9 +22,49 @@ from lisa.sut_orchestrator.azure.common import (
     wait_operation,
 )
 from lisa.sut_orchestrator.azure.platform_ import AzurePlatform
+from lisa.util import SkippedException, UnsupportedDistroException
+
+
+def _is_os_unsupported(self, node: Node) -> None:
+    """
+    Below specific Linux distro versions that are
+    not supported by linux patch extension.
+    debian debian-11 11 0.20230124.1270
+    redhat rhel-arm64 8_6-arm64 8.6.2022060901
+    debian debian-11 11-backports 0.20221219.1234
+    microsoftcblmariner cbl-mariner cbl-mariner-2-arm64 2.20230126.01
+    suse sles-15-sp5 gen2 2024.02.07
+    """
+    unsupported_versions_x86_64: Dict[type, List[str]] = {
+        Debian: [10, 11],
+        SLES: [15],
+        CBLMariner: [2],
+    }
+    unsupported_versions_arm64: Dict[type, List[str]] = {
+        Redhat: [8_6],
+    }
+
+    arch = node.os.get_kernel_information().hardware_platform  # type: ignore
+    if arch == "aarch64":
+        unsupported_versions = unsupported_versions_arm64
+    else:
+        unsupported_versions = unsupported_versions_x86_64
+
+    # Extract Debian version from the complete version string
+    debian_version = node.os.information.version.major.split()[1]
+
+    for distro, version_list in unsupported_versions.items():
+        if isinstance(node.os, distro) and debian_version in version_list:
+            # Both node.os and distro are in the version_list, so raise an exception
+            raise SkippedException(
+                UnsupportedDistroException(
+                    node.os, "This Distro version is unsupported"
+                )
+            )
 
 
 def _set_up_vm(node: Node, environment: Environment) -> Any:
+    _is_os_unsupported(node)
     assert environment.platform, "platform shouldn't be None."
     platform: AzurePlatform = environment.platform  # type: ignore
     assert isinstance(
