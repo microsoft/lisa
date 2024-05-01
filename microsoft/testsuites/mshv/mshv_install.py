6254e41fae06792bd733a194bd1a3ae1b1e535c5
Cameron Baird <cameronbaird@microsoft.com>
2024-05-01 18:53:17 +0000

    drop test_data dir, parse and validate mshv_binpath instead
    lint flake8
    
    lint 2
    
    lint 3
    
    lint 4

--- microsoft/testsuites/mshv/mshv_install.py
+++ microsoft/testsuites/mshv/mshv_install.py
@@ -1,68 +1,58 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
-import os
-import re
 from pathlib import Path
 from typing import Any, Dict
 
-import time
-
-from assertpy import assert_that
-
-from lisa import (
-    Environment,
-    Logger,
-    Node,
-    TestCaseMetadata,
-    TestSuite,
-    TestSuiteMetadata
-)
-# from lisa.features import SerialConsole
+from lisa import Logger, Node, TestCaseMetadata, TestSuite, TestSuiteMetadata
 from lisa.testsuite import TestResult
-from lisa.tools import Cp, Dmesg, Ls, Reboot
+from lisa.tools import Cp, Ls, Reboot
+from lisa.util import SkippedException
+
 
 @TestSuiteMetadata(
     area="mshv",
     category="functional",
     description="""
-    This test suite is to test VM working well after updating MSHV on VM and rebooting.
+    This test suite is to test VM working well after updating MSHV on VM
+    and rebooting.
     """,
 )
 class MshvHostInstallSuite(TestSuite):
     CONFIG_BINPATH = "mshv_binpath"
 
     _test_path_init_hvix = (
-        Path("/home/cloud") / f"hvix64.exe"
+        Path("/home/cloud") / "hvix64.exe"
     )
 
     _init_path_init_kdstub = (
-        Path("/home/cloud") / f"kdstub.dll"
+        Path("/home/cloud") / "kdstub.dll"
     )
 
     _init_path_init_lxhvloader = (
-        Path("/home/cloud") / f"lxhvloader.dll"
+        Path("/home/cloud") / "lxhvloader.dll"
     )
 
     _test_path_dst_hvix = (
-        Path("/boot/efi/Windows/System32") / f"hvix64.exe"
+        Path("/boot/efi/Windows/System32") / "hvix64.exe"
     )
 
     _test_path_dst_kdstub = (
-        Path("/boot/efi/Windows/System32") / f"kdstub.dll"
+        Path("/boot/efi/Windows/System32") / "kdstub.dll"
     )
 
     _test_path_dst_lxhvloader = (
-        Path("/boot/efi") / f"lxhvloader.dll"
+        Path("/boot/efi") / "lxhvloader.dll"
     )
 
     @TestCaseMetadata(
         description="""
         This test case will
-        1. Update to new MSHV components over old ones in a pre-configured MSHV image
+        1. Update to new MSHV components over old ones in a
+            pre-configured MSHV image
         2. Reboot VM, check that mshv comes up
 
-        The test expects the MSHV binaries to be installed to be placed under lisa/microsoft/testsuites/mshv/test_data
-        before lisa is executed.
+        The test expects the directory containing MSHV binaries to be passed in
+        the mshv_binpath variable.
         """,
         timeout=120,
     )
@@ -75,25 +65,33 @@ class MshvHostInstallSuite(TestSuite):
         result: TestResult,
     ) -> None:
         binpath = variables.get(self.CONFIG_BINPATH, "")
-        test_hvix_file_path = Path(binpath) / f"hvix64.exe"
-        test_kdstub_file_path = Path(binpath) / f"kdstub.dll"
-        test_lxhvloader_file_path = Path(binpath) / f"lxhvloader.dll"
+        if not binpath:
+            raise SkippedException(
+                "Requires a path to MSHV binaries to be passed via mshv_binpath"
+            )
+
+        test_hvix_file_path = Path(binpath) / "hvix64.exe"
+        test_kdstub_file_path = Path(binpath) / "kdstub.dll"
+        test_lxhvloader_file_path = Path(binpath) / "lxhvloader.dll"
 
         log.info(f"binpath: {binpath}")
 
         # Copy Hvix64.exe, kdstub.dll, lxhvloader.dll into test machine
         node.shell.copy(test_hvix_file_path, self._test_path_init_hvix)
-        node.tools[Cp].copy(self._test_path_init_hvix.as_posix(), self._test_path_dst_hvix.as_posix(), sudo=True)
+        node.tools[Cp].copy(self._test_path_init_hvix,
+                            self._test_path_dst_hvix, sudo=True)
 
         node.shell.copy(test_kdstub_file_path, self._init_path_init_kdstub)
-        node.tools[Cp].copy(self._init_path_init_kdstub.as_posix(), self._test_path_dst_kdstub.as_posix(), sudo=True)
-        
+        node.tools[Cp].copy(self._init_path_init_kdstub,
+                            self._test_path_dst_kdstub, sudo=True)
+
         node.shell.copy(test_lxhvloader_file_path, self._init_path_init_lxhvloader)
-        node.tools[Cp].copy(self._init_path_init_lxhvloader.as_posix(), self._test_path_dst_lxhvloader.as_posix(), sudo=True)
+        node.tools[Cp].copy(self._init_path_init_lxhvloader,
+                            self._test_path_dst_lxhvloader, sudo=True)
 
         reboot_tool = node.tools[Reboot]
         reboot_tool.reboot_and_check_panic(log_path)
 
         # 2. check that mshv comes up
-        mshvUp = node.tools[Ls].path_exists("/dev/mshv", sudo=True)
-        assert mshvUp
+        mshv = node.tools[Ls].path_exists("/dev/mshv", sudo=True)
+        assert mshv
