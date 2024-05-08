3eb501511b64398398d925fcb1402093afee29bf
Mariner <mingheren@microsoft.com>
2024-05-08 23:42:12 +0000

    fix lint

--- microsoft/testsuites/core/azure_image_standard.py
+++ microsoft/testsuites/core/azure_image_standard.py
@@ -945,8 +945,11 @@ class AzureImageStandard(TestSuite):
                 raise LisaException("cloud-init.log not exists")
         else:
             raise SkippedException(
-                UnsupportedDistroException(node.os, "unsupported distro to run verify_cloud_init_error_status test.")
+                UnsupportedDistroException(
+                    node.os, 
+                    "unsupported distro to run verify_cloud_init_error_status test."
                 )
+            )
 
     @TestCaseMetadata(
         description="""
