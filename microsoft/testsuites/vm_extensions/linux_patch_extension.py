8f0b7bd100d4da2f0197bd0e2a3a7685f21ad876
John Feng <johnfeng@microsoft.com>
2024-05-23 09:28:16 -0700

    add logic to check for unspported imges and unspported agent version

--- microsoft/testsuites/vm_extensions/linux_patch_extension.py
+++ microsoft/testsuites/vm_extensions/linux_patch_extension.py
@@ -1,8 +1,9 @@
 # Copyright (c) Microsoft Corporation. Licensed under the MIT license.
 
-from typing import Any, Dict, List
+from typing import Any
 
 from assertpy.assertpy import assert_that
+from azure.core.exceptions import HttpResponseError
 
 from lisa import (
     Environment,
@@ -14,7 +15,7 @@ from lisa import (
     simple_requirement,
 )
 from lisa.base_tools.service import Service
-from lisa.operating_system import BSD, SLES, CBLMariner, Debian, Redhat
+from lisa.operating_system import BSD
 from lisa.sut_orchestrator import AZURE
 from lisa.sut_orchestrator.azure.common import (
     get_compute_client,
@@ -25,46 +26,18 @@ from lisa.sut_orchestrator.azure.platform_ import AzurePlatform
 from lisa.util import SkippedException, UnsupportedDistroException
 
 
-def _is_os_unsupported(self, node: Node) -> None:
-    """
-    Below specific Linux distro versions that are
-    not supported by linux patch extension.
-    debian debian-11 11 0.20230124.1270
-    redhat rhel-arm64 8_6-arm64 8.6.2022060901
-    debian debian-11 11-backports 0.20221219.1234
-    microsoftcblmariner cbl-mariner cbl-mariner-2-arm64 2.20230126.01
-    suse sles-15-sp5 gen2 2024.02.07
-    """
-    unsupported_versions_x86_64: Dict[type, List[str]] = {
-        Debian: [10, 11],
-        SLES: [15],
-        CBLMariner: [2],
-    }
-    unsupported_versions_arm64: Dict[type, List[str]] = {
-        Redhat: [8_6],
-    }
-
-    arch = node.os.get_kernel_information().hardware_platform  # type: ignore
-    if arch == "aarch64":
-        unsupported_versions = unsupported_versions_arm64
-    else:
-        unsupported_versions = unsupported_versions_x86_64
-
-    # Extract Debian version from the complete version string
-    debian_version = node.os.information.version.major.split()[1]
-
-    for distro, version_list in unsupported_versions.items():
-        if isinstance(node.os, distro) and debian_version in version_list:
-            # Both node.os and distro are in the version_list, so raise an exception
-            raise SkippedException(
-                UnsupportedDistroException(
-                    node.os, "This Distro version is unsupported"
-                )
-            )
+def _verify_unsupported_vm_agent(
+    node: Node, status_result: Any, error_code: str
+) -> None:
+    if (
+        error_code == "1"
+        and "Unsupported older Azure Linux Agent version"
+        in status_result["error"]["details"][0]["message"]
+    ):
+        raise SkippedException(UnsupportedDistroException(node.os))
 
 
 def _set_up_vm(node: Node, environment: Environment) -> Any:
-    _is_os_unsupported(node)
     assert environment.platform, "platform shouldn't be None."
     platform: AzurePlatform = environment.platform  # type: ignore
     assert isinstance(
@@ -121,20 +94,33 @@ class LinuxPatchExtensionBVT(TestSuite):
         # service
         _verify_vm_agent_running(node, log)
 
-        operation = compute_client.virtual_machines.begin_assess_patches(
-            resource_group_name=resource_group_name, vm_name=vm_name
-        )
-        # set wait operation timeout 10 min, status file should be generated
-        # before timeout
-        assess_result = wait_operation(operation, 600)
+        try:
+            operation = compute_client.virtual_machines.begin_assess_patches(
+                resource_group_name=resource_group_name, vm_name=vm_name
+            )
+            # set wait operation timeout 10 min, status file should be generated
+            # before timeout
+            assess_result = wait_operation(operation, 600)
+
+        except HttpResponseError as identifier:
+            if any(
+                s in str(identifier) for s in ["The selected VM image is not supported"]
+            ):
+                raise SkippedException(UnsupportedDistroException(node.os))
+            else:
+                raise identifier
 
         assert assess_result, "assess_result shouldn't be None"
         log.debug(f"assess_result:{assess_result}")
+        error_code = assess_result["error"]["code"]
+
+        _verify_unsupported_vm_agent(node, assess_result, error_code)
+
         assert_that(assess_result["status"]).described_as(
             "Expected the assess patches to succeed"
         ).is_equal_to("Succeeded")
 
-        assert_that(assess_result["error"]["code"]).described_as(
+        assert_that(error_code).described_as(
             "Expected no error in assess patches operation"
         ).is_equal_to("0")
 
@@ -164,17 +150,30 @@ class LinuxPatchExtensionBVT(TestSuite):
         # service
         _verify_vm_agent_running(node, log)
 
-        operation = compute_client.virtual_machines.begin_install_patches(
-            resource_group_name=resource_group_name,
-            vm_name=vm_name,
-            install_patches_input=install_patches_input,
-        )
-        # set wait operation max duration 3H30M timeout, status file should be
-        # generated before timeout
-        install_result = wait_operation(operation, self.TIMEOUT)
+        try:
+            operation = compute_client.virtual_machines.begin_install_patches(
+                resource_group_name=resource_group_name,
+                vm_name=vm_name,
+                install_patches_input=install_patches_input,
+            )
+            # set wait operation max duration 3H30M timeout, status file should be
+            # generated before timeout
+            install_result = wait_operation(operation, self.TIMEOUT)
+
+        except HttpResponseError as identifier:
+            if any(
+                s in str(identifier) for s in ["The selected VM image is not supported"]
+            ):
+                raise SkippedException(UnsupportedDistroException(node.os))
+            else:
+                raise identifier
 
         assert install_result, "install_result shouldn't be None"
         log.debug(f"install_result:{install_result}")
+        error_code = install_result["error"]["code"]
+
+        _verify_unsupported_vm_agent(node, install_result, error_code)
+
         assert_that(install_result["status"]).described_as(
             "Expected the install patches to succeed"
         ).is_equal_to("Succeeded")
