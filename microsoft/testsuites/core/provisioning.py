536098c58aebb86f461a3b8935f4e837db7828a9
kanchansenlaskar <ksenlaskar@gmail.com>
2024-06-04 19:29:19 +0530

    change timeout for boot

--- microsoft/testsuites/core/provisioning.py
+++ microsoft/testsuites/core/provisioning.py
@@ -49,8 +49,8 @@ from lisa.util.shell import wait_tcp_port_ready
     """,
 )
 class Provisioning(TestSuite):
-    TIME_OUT = 300
-    PLATFORM_TIME_OUT = 600
+    TIME_OUT = 7200
+    PLATFORM_TIME_OUT = 7200
 
     @TestCaseMetadata(
         description="""
