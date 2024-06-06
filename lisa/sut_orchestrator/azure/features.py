aa2c490b7faa8d9316cf6c3d3d7c2653ed133c40
Srikanth Myakam <m.srikanth.m@gmail.com>
2024-06-06 17:52:43 +0530

    Update features.py

--- lisa/sut_orchestrator/azure/features.py
+++ lisa/sut_orchestrator/azure/features.py
@@ -1625,44 +1625,43 @@ class Disk(AzureFeatureMixin, features.Disk):
         files = ls_tools.list("/dev/disk/azure/scsi1", sudo=True)
 
         azure_scsi_disks = []
-        # assert self._node.capability.disk
-        # assert isinstance(self._node.capability.disk.max_data_disk_count, int)
-        # if len(files) == 0 and self._node.capability.disk.data_disk_count != 0:
-        if len(files) == 0:
-            os = self._node.os
-            # https://docs.microsoft.com/en-us/troubleshoot/azure/virtual-machines/troubleshoot-device-names-problems#get-the-latest-azure-storage-rules  # noqa: E501
-            # there are known issues on ubuntu 16.04 and rhel 9.0
-            # try to workaround it
-            if (isinstance(os, Ubuntu) and os.information.release <= "16.04") or (
-                isinstance(os, Redhat) and os.information.release >= "9.0"
-            ):
-                self._log.debug(
-                    "download udev rules to construct a set of "
-                    "symbolic links under the /dev/disk/azure path"
-                )
-                if ls_tools.is_file(
-                    self._node.get_pure_path("/dev/disk/azure"), sudo=True
-                ):
-                    self._node.tools[Rm].remove_file("/dev/disk/azure", sudo=True)
-                self._node.tools[Curl].fetch(
-                    arg="-o /etc/udev/rules.d/66-azure-storage.rules",
-                    execute_arg="",
-                    url="https://raw.githubusercontent.com/Azure/WALinuxAgent/master/config/66-azure-storage.rules",  # noqa: E501
-                    sudo=True,
-                    cwd=self._node.get_pure_path("/etc/udev/rules.d/"),
-                )
-                cmd_result = self._node.execute(
-                    "udevadm trigger --settle --subsystem-match=block", sudo=True
-                )
-                if get_matched_str(cmd_result.stdout, self.UN_SUPPORT_SETTLE):
-                    self._node.execute(
-                        "udevadm trigger --subsystem-match=block", sudo=True
-                    )
-                check_till_timeout(
-                    lambda: len(ls_tools.list("/dev/disk/azure/scsi1", sudo=True)) > 0,
-                    timeout_message="wait for dev rule take effect",
-                )
-                files = ls_tools.list("/dev/disk/azure/scsi1", sudo=True)
+        #assert self._node.capability.disk
+        #assert isinstance(self._node.capability.disk.max_data_disk_count, int)
+        #if len(files) == 0 and self._node.capability.disk.data_disk_count != 0:
+        #    os = self._node.os
+        #    # https://docs.microsoft.com/en-us/troubleshoot/azure/virtual-machines/troubleshoot-device-names-problems#get-the-latest-azure-storage-rules  # noqa: E501
+        #    # there are known issues on ubuntu 16.04 and rhel 9.0
+        #    # try to workaround it
+        #    if (isinstance(os, Ubuntu) and os.information.release <= "16.04") or (
+        #        isinstance(os, Redhat) and os.information.release >= "9.0"
+        #    ):
+        #        self._log.debug(
+        #            "download udev rules to construct a set of "
+        #            "symbolic links under the /dev/disk/azure path"
+        #        )
+        #        if ls_tools.is_file(
+        #            self._node.get_pure_path("/dev/disk/azure"), sudo=True
+        #        ):
+        #            self._node.tools[Rm].remove_file("/dev/disk/azure", sudo=True)
+        #        self._node.tools[Curl].fetch(
+        #            arg="-o /etc/udev/rules.d/66-azure-storage.rules",
+        #            execute_arg="",
+        #            url="https://raw.githubusercontent.com/Azure/WALinuxAgent/master/config/66-azure-storage.rules",  # noqa: E501
+        #            sudo=True,
+        #            cwd=self._node.get_pure_path("/etc/udev/rules.d/"),
+        #        )
+        #        cmd_result = self._node.execute(
+        #            "udevadm trigger --settle --subsystem-match=block", sudo=True
+        #        )
+        #        if get_matched_str(cmd_result.stdout, self.UN_SUPPORT_SETTLE):
+        #            self._node.execute(
+        #                "udevadm trigger --subsystem-match=block", sudo=True
+        #            )
+        #        check_till_timeout(
+        #            lambda: len(ls_tools.list("/dev/disk/azure/scsi1", sudo=True)) > 0,
+        #            timeout_message="wait for dev rule take effect",
+        #        )
+        #        files = ls_tools.list("/dev/disk/azure/scsi1", sudo=True)
         azure_scsi_disks = [
             x for x in files if get_matched_str(x, self.SCSI_PATTERN) != ""
         ]
