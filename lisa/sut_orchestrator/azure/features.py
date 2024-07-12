6d7879c90ee7072e844afccf8004d88fa1868ece
Srikanth Myakam <m.srikanth.m@gmail.com>
2024-07-12 12:07:37 +0530

    Update features.py

--- lisa/sut_orchestrator/azure/features.py
+++ lisa/sut_orchestrator/azure/features.py
@@ -1645,8 +1645,8 @@ class Disk(AzureFeatureMixin, features.Disk):
 
         azure_scsi_disks = []
         assert self._node.capability.disk
-        assert isinstance(self._node.capability.disk.max_data_disk_count, int)
-        if len(files) == 0 and self._node.capability.disk.data_disk_count != 0:
+        # assert isinstance(self._node.capability.disk.max_data_disk_count, int)
+        if len(files) == 0: #and self._node.capability.disk.data_disk_count != 0:
             os = self._node.os
             # https://docs.microsoft.com/en-us/troubleshoot/azure/virtual-machines/troubleshoot-device-names-problems#get-the-latest-azure-storage-rules  # noqa: E501
             # there are known issues on ubuntu 16.04 and rhel 9.0
