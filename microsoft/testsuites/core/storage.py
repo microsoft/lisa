16808e4bc4baf8c49cd42dd148c9d0e706029b62
Kanchan Sen Laskar <kasenlaskar@microsoft.com>
2024-08-09 14:43:08 +0530

    Comment scsi dtc specific checks

--- microsoft/testsuites/core/storage.py
+++ microsoft/testsuites/core/storage.py
@@ -714,18 +714,18 @@ class Storage(TestSuite):
                 f" size should be equal to {size} GB",
             ).is_equal_to(size)
 
-            # verify the lun number from linux VM
-            linux_device_luns_after = disk.get_luns()
-            linux_device_lun_diff = [
-                linux_device_luns_after[k]
-                for k in set(linux_device_luns_after) - set(linux_device_luns)
-            ][0]
-            log.debug(f"linux_device_luns: {linux_device_luns}")
-            log.debug(f"linux_device_luns_after: {linux_device_luns_after}")
-            log.debug(f"linux_device_lun_diff: {linux_device_lun_diff}")
-            assert_that(
-                linux_device_lun_diff, "No new device lun found on VM"
-            ).is_equal_to(lun)
+            # # verify the lun number from linux VM
+            # linux_device_luns_after = disk.get_luns()
+            # linux_device_lun_diff = [
+            #     linux_device_luns_after[k]
+            #     for k in set(linux_device_luns_after) - set(linux_device_luns)
+            # ][0]
+            # log.debug(f"linux_device_luns: {linux_device_luns}")
+            # log.debug(f"linux_device_luns_after: {linux_device_luns_after}")
+            # log.debug(f"linux_device_lun_diff: {linux_device_lun_diff}")
+            # assert_that(
+            #     linux_device_lun_diff, "No new device lun found on VM"
+            # ).is_equal_to(lun)
 
         # Remove all attached data disks
         for disk_added in disks_added:
