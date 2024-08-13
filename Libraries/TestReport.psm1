eabba6827031922ee775fe80d1139cee629a171f
Abhishek Tiwari <abhitiwari@microsoft.com>
2024-08-13 23:22:37 +0530

    adding testcategory to junit

--- Libraries/TestReport.psm1
+++ Libraries/TestReport.psm1
@@ -160,9 +160,7 @@ Class JUnitReportGenerator
 		$newElement.SetAttribute("TestCategory", $this.TestCategory)
 		if ( $global:BaseOSVHD ) {
 			$newElement.SetAttribute("ImageUnderTest",  $global:BaseOSVHD )
-			if ($global:deviceSoCFWVer.count -gt 0 -and $global:deviceSoCFWVer[0].length -gt 0) {
-				$newElement.SetAttribute("DeviceUnderTest",  $global:deviceInfo )	
-			}
+			$newElement.SetAttribute("DeviceUnderTest",  $global:deviceInfo )	
 		}
 		$testsuiteNode = $this.ReportRootNode.AppendChild($newElement)
 
