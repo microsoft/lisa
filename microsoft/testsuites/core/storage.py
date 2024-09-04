c75a76656f1c04389a64a6db0c5e757eceaf0039
SrikanthMyakam <m.srikanth.m@gmail.com>
2024-09-04 18:01:52 +0530

    Update storage.py

--- microsoft/testsuites/core/storage.py
+++ microsoft/testsuites/core/storage.py
@@ -642,10 +642,11 @@ class Storage(TestSuite):
 
         # get max data disk count for the node
         assert node.capability.disk
-        assert isinstance(
-            node.capability.disk.max_data_disk_count, int
-        ), f"actual type: {node.capability.disk.max_data_disk_count}"
-        max_data_disk_count = node.capability.disk.max_data_disk_count
+        #assert isinstance(
+        #    node.capability.disk.max_data_disk_count, int
+        #), f"actual type: {node.capability.disk.max_data_disk_count}"
+        #max_data_disk_count = node.capability.disk.max_data_disk_count
+        max_data_disk_count = 32
         log.debug(f"max_data_disk_count: {max_data_disk_count}")
 
         # get the number of data disks already added to the vm
@@ -737,10 +738,11 @@ class Storage(TestSuite):
 
         # get max data disk count for the node
         assert node.capability.disk
-        assert isinstance(
-            node.capability.disk.max_data_disk_count, int
-        ), f"actual type: {node.capability.disk.max_data_disk_count}"
-        max_data_disk_count = node.capability.disk.max_data_disk_count
+        #assert isinstance(
+        #    node.capability.disk.max_data_disk_count, int
+        #), f"actual type: {node.capability.disk.max_data_disk_count}"
+        # max_data_disk_count = node.capability.disk.max_data_disk_count
+        max_data_disk_count = 32
         log.debug(f"max_data_disk_count: {max_data_disk_count}")
 
         # get the number of data disks already added to the vm
