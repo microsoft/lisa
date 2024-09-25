945b9676e1a39b724cf1256a79296fcf208e2fab
Matthew G McGovern <mamcgove@microsoft.com>
2024-09-25 16:00:28 -0700

    RdmaCore: make rdma-core source installer
    Implement rdma-core source and package manager build
    using Installer class. This will be handy when
    implementing the 32bit dpdk/rdma-core test.
    
    Debian kernel version compare
    
    prefer kernel version over codename

--- microsoft/testsuites/dpdk/common.py
+++ microsoft/testsuites/dpdk/common.py
@@ -1,7 +1,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from datetime import datetime
 from pathlib import PurePath
 from typing import Any, Callable, Dict, List, Optional, Sequence, Type, Union
 
@@ -60,7 +59,7 @@ class DependencyInstaller:
         )
         # find the match for an OS, install the packages.
         # stop on list end or if exclusive_match parameter is true.
-        packages = []
+        packages: List[Union[str, Tool, Type[Tool]]] = []
         for requirement in self.requirements:
             if requirement.matcher(os) and requirement.packages:
                 packages += requirement.packages
@@ -275,43 +274,25 @@ def force_dpdk_default_source(variables: Dict[str, Any]) -> None:
         variables["dpdk_source"] = DPDK_STABLE_GIT_REPO
 
 
-# rough check for ubuntu supported versions.
-# assumes:
-# - canonical convention of YEAR.MONTH for major versions
-# - canoical release cycle of EVEN_YEAR.04 for lts versions.
-# - 4 year support cycle. 6 year for ESM
-# get the age of the distro, if negative or 0, release is new.
-# if > 6, distro is out of support
+# Ubuntu LTS releases
+# TODO: set calendar reminder to update every two years.
+UBUNTU_LTS_VERSIONS = ["24.4.0", "22.4.0", "20.4.0"]
+
+
 def is_ubuntu_lts_version(distro: Ubuntu) -> bool:
-    # asserts if not ubuntu OS object
-    version_info = distro.information.version
-    distro_age = _get_ubuntu_distro_age(distro)
-    is_even_year = (version_info.major % 2) == 0
-    is_april_release = version_info.minor == 4
-    is_within_support_window = distro_age <= 6
-    return is_even_year and is_april_release and is_within_support_window
+    version = distro.information.version
+    # check major version matches
+    major_match = [
+        version.major == x.split(".", maxsplit=1)[0] for x in UBUNTU_LTS_VERSIONS
+    ]
+    # check minor version matches
+    minor_match = [version.minor == x.split(".")[1] for x in UBUNTU_LTS_VERSIONS]
+    # check if a major and minor version matched for any items in the list
+    return any([(x == y) for x, y in zip(major_match, minor_match)])
 
 
 def is_ubuntu_latest_or_prerelease(distro: Ubuntu) -> bool:
-    distro_age = _get_ubuntu_distro_age(distro)
-    return distro_age <= 2
-
-
-def _get_ubuntu_distro_age(distro: Ubuntu) -> int:
-    version_info = distro.information.version
-    # check release is within esm window
-    year_string = str(datetime.today().year)
-    assert_that(len(year_string)).described_as(
-        "Package bug: The year received from datetime module is an "
-        "unexpected size. This indicates a broken package or incorrect "
-        "date in this computer."
-    ).is_greater_than_or_equal_to(4)
-    # TODO: handle the century rollover edge case in 2099
-    current_year = int(year_string[-2:])
-    release_year = int(version_info.major)
-    # 23-18 == 5
-    # long term support and extended security updates for ~6 years
-    return current_year - release_year
+    return bool(distro.information.version >= max(UBUNTU_LTS_VERSIONS))
 
 
 def check_dpdk_support(node: Node) -> None:
@@ -385,3 +366,52 @@ def unsupported_os_thrower(os: Posix) -> bool:
         os,
         message=("Installer did not define dependencies for this os."),
     )
+
+
+def get_debian_backport_repo_args(os: Debian) -> List[str]:
+    # ex: 'bionic-backports' or 'buster-backports'
+    # these backport repos are available for the older OS's
+    # and include backported fixes which need to be opted into.
+    # So filter for recent OS's and
+    # add the backports repo if it's available.
+    if not isinstance(os, Debian):
+        return []
+    # workaround for recent Ubuntu distros which may have a
+    # backport repo defined but will raise an error if you
+    # try to use it explicitly for our packages.
+    if isinstance(os, Ubuntu) and (
+        is_ubuntu_latest_or_prerelease(os) or not is_ubuntu_lts_version(os)
+    ):
+        return []
+    repos = os.get_repositories()
+    backport_repo = f"{os.information.codename}-backports"
+    if any([backport_repo in repo.name for repo in repos]):
+        return [f"-t {backport_repo}"]
+    return []
+
+
+# NOTE: mana_ib was added in 6.2 and backported to 5.15
+# this ends up lining up with kernels that need to be updated before
+# starting our DPDK tests. This function is not meant for general use
+# outside of the DPDK suite.
+def update_kernel_from_repo(node: Node) -> None:
+    assert isinstance(
+        node.os, (Debian, Fedora, Suse)
+    ), f"DPDK test does not support OS type: {type(node.os)}"
+    if (
+        isinstance(node.os, Debian)
+        and node.os.get_kernel_information().version < "6.5.0"
+    ):
+        package = "linux-azure"
+    elif (
+        isinstance(node.os, (Fedora, Suse))
+        and node.os.get_kernel_information().version < "5.15.0"
+    ):
+        package = "kernel"
+    else:
+        return
+    if node.os.is_package_in_repo(package):
+        node.os.install_packages(package)
+        node.reboot()
+    else:
+        node.log.debug(f"Kernel update package '{package}' was not found.")
--- microsoft/testsuites/dpdk/dpdktestpmd.py
+++ microsoft/testsuites/dpdk/dpdktestpmd.py
@@ -18,7 +18,6 @@ from lisa.tools import (
     Kill,
     Lscpu,
     Lspci,
-    Make,
     Meson,
     Modprobe,
     Ninja,
@@ -26,7 +25,6 @@ from lisa.tools import (
     Pip,
     Pkgconfig,
     Python,
-    Tar,
     Timeout,
     Wget,
 )
@@ -40,8 +38,7 @@ from microsoft.testsuites.dpdk.common import (
     OsPackageDependencies,
     PackageManagerInstall,
     TarDownloader,
-    is_ubuntu_latest_or_prerelease,
-    is_ubuntu_lts_version,
+    get_debian_backport_repo_args,
     is_url_for_git_repo,
     is_url_for_tarball,
     unsupported_os_thrower,
@@ -53,6 +50,15 @@ PACKAGE_MANAGER_SOURCE = "package_manager"
 # declare package dependencies for package manager DPDK installation
 DPDK_PACKAGE_MANAGER_PACKAGES = DependencyInstaller(
     requirements=[
+        # install linux-modules-extra-azure if it's available for mana_ib
+        # older debian kernels won't have mana_ib packaged,
+        # so skip the check on those kernels.
+        OsPackageDependencies(
+            matcher=lambda x: isinstance(x, Debian)
+            and bool(x.get_kernel_information().version >= "5.15.0")
+            and x.is_package_in_repo("linux-modules-extra-azure"),
+            packages=["linux-modules-extra-azure"],
+        ),
         OsPackageDependencies(
             matcher=lambda x: isinstance(x, Debian),
             packages=["dpdk", "dpdk-dev"],
@@ -101,6 +107,15 @@ DPDK_SOURCE_INSTALL_PACKAGES = DependencyInstaller(
             ],
             stop_on_match=True,
         ),
+        # install linux-modules-extra-azure if it's available for mana_ib
+        # older debian kernels won't have mana_ib packaged,
+        # so skip the check on those kernels.
+        OsPackageDependencies(
+            matcher=lambda x: isinstance(x, Debian)
+            and bool(x.get_kernel_information().version >= "5.15.0")
+            and x.is_package_in_repo("linux-modules-extra-azure"),
+            packages=["linux-modules-extra-azure"],
+        ),
         OsPackageDependencies(
             matcher=lambda x: isinstance(x, Debian),
             packages=[
@@ -110,7 +125,6 @@ DPDK_SOURCE_INSTALL_PACKAGES = DependencyInstaller(
                 "python3-pyelftools",
                 "libelf-dev",
                 "pkg-config",
-                "linux-modules-extra-azure",
             ],
             stop_on_match=True,
         ),
@@ -144,27 +158,11 @@ DPDK_SOURCE_INSTALL_PACKAGES = DependencyInstaller(
 )
 
 
-def get_debian_backport_repo_args(os: Debian) -> List[str]:
-    if not isinstance(os, Debian):
-        return []
-    if isinstance(os, Ubuntu) and (
-        is_ubuntu_latest_or_prerelease(os) or not is_ubuntu_lts_version(os)
-    ):
-        return []
-    repos = os.get_repositories()
-    backport_repo = f"{os.information.codename}-backports"
-    if any([backport_repo in repo.name for repo in repos]):
-        return [f"-t {backport_repo}"]
-    return []
-
-
 class DpdkPackageManagerInstall(PackageManagerInstall):
     def _setup_node(self) -> None:
         if isinstance(self._os, Debian):
             self._package_manager_extra_args = get_debian_backport_repo_args(self._os)
-            if self._os.information.version < "22.4.0":
-                self._os.update_packages("linux-azure")
-                self._node.reboot()
+
         elif isinstance(self._os, Fedora):
             self._os.install_epel()
 
@@ -201,7 +199,7 @@ class DpdkSourceInstall(Installer):
     def _setup_node(self) -> None:
         if isinstance(self._os, Debian):
             self._package_manager_extra_args = get_debian_backport_repo_args(self._os)
-            if self._os.information.version < "22.4.0":
+            if isinstance(self._os, Ubuntu) and self._os.information.version < "22.4.0":
                 self._os.update_packages("linux-azure")
                 self._node.reboot()
         # install( Tool ) doesn't seem to install the tool until it's used :\
@@ -396,22 +394,6 @@ class DpdkTestpmd(Tool):
         _rx_pps_key: r"Rx-pps:\s+([0-9]+)",
     }
     _source_build_dest_dir = "/usr/local/bin"
-
-    def get_rdma_core_package_name(self) -> str:
-        distro = self.node.os
-        package = ""
-        # check if rdma-core is installed already...
-        if self.node.tools[Pkgconfig].package_info_exists("libibuverbs"):
-            return package
-        if isinstance(distro, Debian):
-            package = "rdma-core ibverbs-providers libibverbs-dev"
-        elif isinstance(distro, Suse):
-            package = "rdma-core-devel librdmacm1"
-        elif isinstance(distro, Fedora):
-            package = "librdmacm-devel"
-        else:
-            fail("Invalid OS for rdma-core source installation.")
-        return package
 
     @property
     def can_install(self) -> bool:
@@ -833,54 +815,6 @@ class DpdkTestpmd(Tool):
             f"empty or all zeroes for dpdktestpmd.{rx_or_tx.lower()}_pps_data."
         ).is_true()
 
-    def _install_upstream_rdma_core_for_mana(self) -> None:
-        node = self.node
-        wget = node.tools[Wget]
-        make = node.tools[Make]
-        tar = node.tools[Tar]
-        distro = node.os
-
-        if isinstance(distro, Debian):
-            distro.install_packages(
-                "cmake libudev-dev "
-                "libnl-3-dev libnl-route-3-dev ninja-build pkg-config "
-                "valgrind python3-dev cython3 python3-docutils pandoc "
-                "libssl-dev libelf-dev python3-pip libnuma-dev"
-            )
-        elif isinstance(distro, Fedora):
-            distro.group_install_packages("Development Tools")
-            distro.install_packages(
-                "cmake gcc libudev-devel "
-                "libnl3-devel pkg-config "
-                "valgrind python3-devel python3-docutils  "
-                "openssl-devel unzip "
-                "elfutils-devel python3-pip libpcap-devel  "
-                "tar wget dos2unix psmisc kernel-devel-$(uname -r)  "
-                "librdmacm-devel libmnl-devel kernel-modules-extra numactl-devel  "
-                "kernel-headers elfutils-libelf-devel meson ninja-build libbpf-devel "
-            )
-        else:
-            # check occcurs before this function
-            return
-
-        tar_path = wget.get(
-            url=(
-                "https://github.com/linux-rdma/rdma-core/"
-                "releases/download/v50.1/rdma-core-50.1.tar.gz"
-            ),
-            file_path=str(node.working_path),
-        )
-
-        tar.extract(tar_path, dest_dir=str(node.working_path), gzip=True, sudo=True)
-        source_path = node.working_path.joinpath("rdma-core-50.1")
-        node.execute(
-            "cmake -DIN_PLACE=0 -DNO_MAN_PAGES=1 -DCMAKE_INSTALL_PREFIX=/usr",
-            shell=True,
-            cwd=source_path,
-            sudo=True,
-        )
-        make.make_install(source_path)
-
     def _install(self) -> bool:
         self._testpmd_output_after_reenable = ""
         self._testpmd_output_before_rescind = ""
@@ -892,14 +826,11 @@ class DpdkTestpmd(Tool):
         if isinstance(node.os, Ubuntu) and node.os.information.codename == "bionic":
             # bionic needs to update to latest first
             node.os.update_packages("")
-        if self.is_mana:
-            if not (isinstance(node.os, Ubuntu) or isinstance(node.os, Fedora)):
-                raise SkippedException("MANA DPDK test is not supported on this OS")
-            # ensure no older dependency is installed
-            node.os.uninstall_packages("rdma-core")
-            self._install_upstream_rdma_core_for_mana()
-        else:
-            node.os.install_packages(self.get_rdma_core_package_name())
+        if self.is_mana and not (
+            isinstance(node.os, Ubuntu) or isinstance(node.os, Fedora)
+        ):
+            raise SkippedException("MANA DPDK test is not supported on this OS")
+
         self.installer.do_installation()
         self._dpdk_version_info = self.installer.get_installed_version()
         self._load_drivers_for_dpdk()
