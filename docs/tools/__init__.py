f3c3ea0fab046032db350c17d201a44444b59ec2
Chi Song (from Dev Box) <chisong@microsoft.com>
2024-09-27 13:34:37 -0700

    doc: add lisa case training data.
    It generate a jsonl file for llm training purpose. The input is test case descriptions of a test file, output is the test case file content.

--- docs/tools/__init__.py
+++ docs/tools/__init__.py
@@ -1,7 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+from .finetune_gen import update_finetune_data
 from .test_spec_gen import update_file
 from .test_summary_gen import update_summary
 
-__all__ = ["update_summary", "update_file"]
+__all__ = ["update_summary", "update_file", "update_finetune_data"]
