9992daa8be1ee538009ecdc4d22e56e2fcda5a30
Shekhar Sorot ( MSFT ) <shsorot@microsoft.com>
2024-10-03 19:50:05 +0530

    Update features.py
    added space around equal based on flake8 recomendations

--- lisa/sut_orchestrator/azure/features.py
+++ lisa/sut_orchestrator/azure/features.py
@@ -3237,9 +3237,9 @@ class AzureFileShare(AzureFeatureMixin, Feature):
         file_share_names: List[str],
         environment: Environment,
         allow_shared_key_access: bool = False,
-        sku: str="Standard_LRS",
-        kind: str="StorageV2",
-        enable_https_traffic_only: bool=True,
+        sku: str = "Standard_LRS",
+        kind: str = "StorageV2",
+        enable_https_traffic_only: bool = True,
     ) -> Dict[str, str]:
         platform: AzurePlatform = self._platform  # type: ignore
         information = environment.get_information()
