c815c926207d56da8363d1fa3481d415d80b9913
Shekhar Sorot ( MSFT ) <shekharssorot2002@gmail.com>
2024-10-08 11:37:17 +0530

    Update features.py

--- lisa/sut_orchestrator/azure/features.py
+++ lisa/sut_orchestrator/azure/features.py
@@ -172,9 +172,9 @@ class StartStop(AzureFeatureMixin, features.StartStop):
         node_info = self._node.connection_info
         node_info[constants.ENVIRONMENTS_NODES_REMOTE_PUBLIC_ADDRESS] = public_ip
         node_info[constants.ENVIRONMENTS_NODES_REMOTE_ADDRESS] = private_ip
-        node_info[constants.ENVIRONMENTS_NODES_REMOTE_USE_PUBLIC_ADDRESS] = (
-            platform._azure_runbook.use_public_address
-        )
+        node_info[
+            constants.ENVIRONMENTS_NODES_REMOTE_USE_PUBLIC_ADDRESS
+        ] = platform._azure_runbook.use_public_address
         self._node.set_connection_info(**node_info)
         self._node._is_initialized = False
         self._node.initialize()
@@ -2347,14 +2347,14 @@ class SecurityProfile(AzureFeatureMixin, features.SecurityProfile):
                 settings = security_profile[0]
                 assert isinstance(settings, SecurityProfileSettings)
                 assert isinstance(settings.security_profile, SecurityProfileType)
-                node_parameters.security_profile["security_type"] = (
-                    cls._security_profile_mapping[settings.security_profile]
-                )
+                node_parameters.security_profile[
+                    "security_type"
+                ] = cls._security_profile_mapping[settings.security_profile]
                 if settings.security_profile == SecurityProfileType.Stateless:
                     node_parameters.security_profile["secure_boot"] = False
-                    node_parameters.security_profile["encryption_type"] = (
-                        "NonPersistedTPM"
-                    )
+                    node_parameters.security_profile[
+                        "encryption_type"
+                    ] = "NonPersistedTPM"
                 else:
                     node_parameters.security_profile["secure_boot"] = True
                     node_parameters.security_profile["encryption_type"] = (
@@ -2362,9 +2362,9 @@ class SecurityProfile(AzureFeatureMixin, features.SecurityProfile):
                         if settings.encrypt_disk
                         else "VMGuestStateOnly"
                     )
-                node_parameters.security_profile["disk_encryption_set_id"] = (
-                    settings.disk_encryption_set_id
-                )
+                node_parameters.security_profile[
+                    "disk_encryption_set_id"
+                ] = settings.disk_encryption_set_id
 
                 if node_parameters.security_profile["security_type"] == "":
                     node_parameters.security_profile.clear()
