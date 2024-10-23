7646a02cc2ea1f2eb366215ee37d60957b31a8e0
SrikanthMyakam <m.srikanth.m@gmail.com>
2024-10-23 08:44:27 +0530

    NVMe and resource disc testcase fixes
    NVMe and resource disc testcase fixes
    
    1. Added new testcase "Nvme.verify_nvme_function_unpartitioned".
    2. NVMe test case fixes for disc controller type NVMe.
    3. "Nvme.verify_nvme_fstrim" - use fstrim testfile size based on available space.
    4. Resource disc testcases fixes for VMs with Local NVMEs.
    5. Added below new methods:
        a. check_resource_disk_mounted
        b. get_resource_disks
        c. get_resource_disk_type
        d. get_disk_type
        e. _remove_nvme_os_disk
        f. _get_os_disk_nvme_device
        g. _verify_nvme_function

--- lisa/sut_orchestrator/azure/features.py
+++ lisa/sut_orchestrator/azure/features.py
@@ -1925,6 +1925,42 @@ class Disk(AzureFeatureMixin, features.Disk):
         self._node.capability.disk.data_disk_count -= len(names)
         self._node.close()
 
+    # verify that resource disk is mounted
+    # function returns successfully if disk matching mount point is present.
+    # raises exception if the resource disk is not mounted
+    # in Azure only SCSI disks are mounted but not NVMe disks
+    def check_resource_disk_mounted(self) -> bool:
+        resource_disk_mount_point = self.get_resource_disk_mount_point()
+        resourcedisk = self.get_partition_with_mount_point(resource_disk_mount_point)
+        if not resourcedisk:
+            raise LisaException(
+                f"Resource disk is not mounted at {resource_disk_mount_point}"
+            )
+        return True
+
+    # get resource disk type
+    # function returns the type of resource disk/disks available on the VM
+    # raises exception if no resource disk is available
+    def get_resource_disk_type(self) -> schema.ResourceDiskType:
+        resource_disks = self.get_resource_disks()
+        if not resource_disks:
+            raise LisaException("No Resource disks are available on VM")
+        return schema.ResourceDiskType(
+            self._node.features[Disk].get_disk_type(disk=resource_disks[0])
+        )
+
+    def get_resource_disks(self) -> List[str]:
+        resource_disk_list = []
+        resource_disk_mount_point = self.get_resource_disk_mount_point()
+        resourcedisk = self._node.features[Disk].get_partition_with_mount_point(
+            resource_disk_mount_point
+        )
+        if resourcedisk:
+            resource_disk_list = [resourcedisk.name]
+        else:
+            resource_disk_list = self._node.features[Nvme].get_raw_nvme_disks()
+        return resource_disk_list
+
     def get_resource_disk_mount_point(self) -> str:
         # get customize mount point from cloud-init configuration file from /etc/cloud/
         # if not found, use default mount point /mnt for cloud-init
