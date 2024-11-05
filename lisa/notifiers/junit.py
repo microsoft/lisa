4b5ac5ed4e50d804fc20c2f2de5baf8f6aaa1fe8
Shekhar Sorot ( MSFT ) <shsorot@microsoft.com>
2024-11-05 14:15:44 +0530

    Update junit.py
    Added 3 new parameters
        include_passed_messages:  ( Bool ) Include message and stacktrace in Junit XML output
        include_failed_messages: ( Bool ) Include message and stacktrace in Junit XML output
        include_skipped_messages: ( Bool ) Include message and stacktrace in Junit XML output
    
    Enhancements : Stacktrace now exists as an XML subproperty, making XML much cleaner.
    
    If "message" and "stacktrace" properties are empty  in the TestResultMessage object, its relevant subXML objects will not be added, makes XML parsing cleaner.
    
    how to call ? Example code from runbook
    notifier:
      - type: html
      - type: env_stats
      - type: junit
        name: junit.lisa.xml
        include_subtest: true
        include_passed_messages: false
        include_failed_messages: true
        include_skipped_messages: true

--- lisa/notifiers/junit.py
+++ lisa/notifiers/junit.py
@@ -28,6 +28,12 @@ class JUnitSchema(schema.Notifier):
     path: str = "lisa.junit.xml"
     # respect the original behavior, include subtest by default
     include_subtest: bool = True
+    # show passed case 'message' and 'stacktraces'
+    include_passed_messages: bool = False
+    # show 'message' for failed cases and 'stacktrace'
+    include_failed_messages: bool = True
+    # show 'message' for skipped cases
+    include_skipped_messages: bool = True
 
 
 class _TestSuiteInfo:
@@ -291,6 +297,8 @@ class JUnit(Notifier):
         class_name: str,
         elapsed: float,
     ) -> None:
+        # Creating a JunitSchema object to read the passed parameters to Junit class
+        runbook: JUnitSchema = cast(JUnitSchema, self.runbook)
         testsuite_info = self._testsuites_info.get(suite_full_name)
         if not testsuite_info:
             raise LisaException("Test suite not started.")
@@ -302,18 +310,27 @@ class JUnit(Notifier):
 
         if message.status == TestStatus.FAILED:
             failure = ET.SubElement(testcase, "failure")
-            failure.attrib["message"] = message.message
-            failure.text = message.stacktrace
-
+            if runbook.include_failed_messages:
+                failure.attrib["message"] = message.message
+                # failure.text = message.stacktrace
+                if str(message.stacktrace) != "":
+                    failure.attrib["stacktrace"] = str(message.stacktrace)
             testsuite_info.failed_count += 1
 
-        elif (
-            message.status == TestStatus.SKIPPED
-            or message.status == TestStatus.ATTEMPTED
-        ):
+        elif message.status in [TestStatus.SKIPPED, TestStatus.ATTEMPTED]:
             skipped = ET.SubElement(testcase, "skipped")
-            skipped.attrib["message"] = message.message
-
+            if runbook.include_skipped_messages:
+                skipped.attrib["message"] = message.message
+                if str(message.stacktrace) != "":
+                    skipped.attrib["stacktrace"] = str(message.stacktrace)
+        # Only add XML sub element if include_passed_messages is True in runbook
+        # By default, its assumed subtest is a pass
+        elif message.status == TestStatus.PASSED and runbook.include_passed_messages:
+            passed = ET.SubElement(testcase, "passed")
+            passed.attrib["message"] = message.message
+            # passed.text = message.stacktrace
+            if str(message.stacktrace) != "":
+                passed.attrib["stacktrace"] = str(message.stacktrace)
         testsuite_info.test_count += 1
 
         # Write out current results to file.
