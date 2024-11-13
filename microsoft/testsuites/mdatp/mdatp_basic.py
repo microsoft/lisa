99c7e074f111b0641fc9158f74a2fe578b88e2ac
Atul Bagga <atbagga@microsoft.com>
2024-11-14 01:10:04 +0530

    MDE tool no longer in the Tools directory

--- microsoft/testsuites/mdatp/mdatp_basic.py
+++ microsoft/testsuites/mdatp/mdatp_basic.py
@@ -15,7 +15,7 @@ from lisa import (
 )
 from lisa.operating_system import BSD
 from lisa.testsuite import TestResult
-from lisa.tools import MDE, Curl
+from lisa.tools import Curl
 from lisa.util import LisaException, SkippedException
 
 from microsoft.testsuites.mdatp.mdatp_tools import Mdatp
