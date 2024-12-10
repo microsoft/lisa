07ba11e9e46410d51e7292ba5178872d4c839d88
paull <paull@microsfot.com>
2024-12-09 16:47:03 -0800

    Added ip_power control for BareMetal platform

--- lisa/sut_orchestrator/baremetal/schema.py
+++ lisa/sut_orchestrator/baremetal/schema.py
@@ -75,12 +75,6 @@ class KeyLoaderSchema(schema.TypedSchema, schema.ExtendableSchemaMixin):
 
 @dataclass_json()
 @dataclass
-class BootConfigSchema(schema.TypedSchema, schema.ExtendableSchemaMixin):
-    type: str = field(default="boot_config", metadata=field_metadata(required=True))
-
-
-@dataclass_json()
-@dataclass
 class IPPowerSchema(schema.TypedSchema, schema.ExtendableSchemaMixin):
     type: str = field(default="Ip9285", metadata=field_metadata(required=True))
     hostname: str = ""
@@ -90,7 +84,9 @@ class IPPowerSchema(schema.TypedSchema, schema.ExtendableSchemaMixin):
     def __post_init__(self, *args: Any, **kwargs: Any) -> None:
         add_secret(self.password)
 
-        
+
+@dataclass_json()
+@dataclass      
 class BootConfigSchema(schema.TypedSchema, schema.ExtendableSchemaMixin):
     type: str = field(default="boot_config", metadata=field_metadata(required=True))
 
