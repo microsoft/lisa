8a26bc463f9129e93ad255c4bcd2ebc2d98c1a29
SrikanthMyakam <m.srikanth.m@gmail.com>
2024-12-17 05:28:11 +0530

    Update disk.py

--- lisa/sut_orchestrator/hyperv/disk.py
+++ lisa/sut_orchestrator/hyperv/disk.py
@@ -8,14 +8,12 @@ from lisa import features
 from .context import get_node_context
 
 
-class Disk(features.Disk, node: Node):
-    def __init__(self, node: RemoteNode) -> None:
-        node_context = get_node_context(node)
-        self._vm_name = node_context.vm_name
-
-        node_context = get_node_context(node)
-        node_context.vm_name = vm_name
-        node_context.host = self._server
+class Disk(features.Disk):
+    def _initialize(self, *args: Any, **kwargs: Any) -> None:
+        super()._initialize(*args, **kwargs)
+        node_context = get_node_context(self._node)
+        _vm_name = node_context.vm_name
+        _server = node_context.host
 
     def get_all_disks(
         self,
