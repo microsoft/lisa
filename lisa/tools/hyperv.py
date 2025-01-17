e5a607824c1c85d10e37ac5689933ac84411ecd0
SrikanthMyakam <m.srikanth.m@gmail.com>
2025-01-17 21:21:01 +0530

    Update hyperv.py

--- lisa/tools/hyperv.py
+++ lisa/tools/hyperv.py
@@ -451,7 +451,7 @@ class HyperV(Tool):
         self.node.reboot()
 
         # wait for Hyper-V services to start
-        service.wait_for_service_start("vmms")
+        self.node.tools[Service].wait_for_service_start("vmms")
         self.node.tools[Service].wait_for_service_start("vmcompute")
 
         return self._check_exists()
