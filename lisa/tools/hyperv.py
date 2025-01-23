f5c781f5d18df70525dd019d2b39640b98b7564d
SrikanthMyakam <m.srikanth.m@gmail.com>
2025-01-23 23:19:55 +0530

    Update hyperv.py

--- lisa/tools/hyperv.py
+++ lisa/tools/hyperv.py
@@ -8,7 +8,7 @@ from enum import Enum
 from typing import Dict, Optional
 
 from assertpy import assert_that
-from dataclasses_json import config, dataclass_json
+from dataclasses_json import config, dataclass_json, from_dict
 
 from lisa.base_tools import Service
 from lisa.executable import Tool
@@ -27,8 +27,8 @@ class HypervSwitchType(Enum):
 @dataclass_json
 @dataclass
 class VMSwitch:
-    name: str = field(metadata=config(field_name="Name"))
-    type: Optional[HypervSwitchType] = None
+    name: str = ""
+    type: HypervSwitchType = HypervSwitchType.EXTERNAL
 
 
 class HyperV(Tool):
@@ -212,21 +212,23 @@ class HyperV(Tool):
         )
 
     # get default switch from hyperv
-    def get_default_switch(self) -> Optional[VMSwitch]:
+    def get_default_switch(self) -> VMSwitch:
         if self._default_switch is None:
             # try to get external switch first
             for switch_type in (HypervSwitchType.EXTERNAL, HypervSwitchType.INTERNAL):
                 switch_json = self.node.tools[PowerShell].run_cmdlet(
                     f'Get-VMSwitch | Where-Object {{$_.SwitchType -eq "{switch_type.value}"}}'  # noqa: E501
-                    " | Select -First 1 | select Name | ConvertTo-Json",
+                    " | Select -First 1",
                     force_run=True,
+                    output_json=True,
                 )
                 if switch_json:
-                    self._default_switch = VMSwitch.from_json(switch_json)  # type: ignore
+                    self._default_switch = VMSwitch()
+                    self._default_switch.name = switch_json["Name"]
                     self._default_switch.type = switch_type
                     break
 
-            if not self._default_switch:
+            if self._default_switch is None:
                 raise LisaException("Could not find any default switch")
         return self._default_switch
 
