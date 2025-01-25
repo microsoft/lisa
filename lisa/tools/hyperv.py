a409eba8160f8f8ac00c7de2a853caea16fc3239
SrikanthMyakam <m.srikanth.m@gmail.com>
2025-01-25 13:33:45 +0530

    Improvements to "HyperVPreparationTransformer" (#3613)
    * Improvements to "HyperVPreparationTransformer"
    
    Changed the transformer to use recently added methods to HyperV tool instead of running commands.
    
    * Update hyperv_preparation.py
    
    * Update hyperv.py

--- lisa/tools/hyperv.py
+++ lisa/tools/hyperv.py
@@ -232,9 +232,12 @@ class HyperV(Tool):
                 raise LisaException("Could not find any Internal or External switch")
         return self._default_switch
 
-    def exists_switch(self, name: str) -> bool:
+    def exists_switch(self, name: str, switch_type: str = "") -> bool:
+        cmd = f"Get-VMSwitch -Name {name}"
+        if switch_type != "":
+            cmd += f"  -SwitchType '{switch_type}'"
         output = self.node.tools[PowerShell].run_cmdlet(
-            f"Get-VMSwitch -Name {name}",
+            cmdlet=cmd,
             fail_on_error=False,
             force_run=True,
         )
@@ -248,8 +251,8 @@ class HyperV(Tool):
             )
 
     def create_switch(self, name: str, switch_type: str = "Internal") -> None:
-        # remove switch if it exists
-        self.delete_switch(name)
+        if self.exists_switch(name, switch_type):
+            return
 
         # create a new switch
         self.node.tools[PowerShell].run_cmdlet(
