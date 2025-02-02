046525f7074a27a78198e42d7ecc8d6ab67c5e4f
SrikanthMyakam <m.srikanth.m@gmail.com>
2025-02-03 00:52:25 +0530

    [Hyper-v platform] Enabling more testcases to run on Hyper-V platform

--- microsoft/testsuites/core/dhcp.py
+++ microsoft/testsuites/core/dhcp.py
@@ -13,7 +13,7 @@ from lisa import (
     UnsupportedDistroException,
     simple_requirement,
 )
-from lisa.sut_orchestrator import AZURE, READY
+from lisa.sut_orchestrator import AZURE, READY, HYPERV
 from lisa.tools import Dhclient
 
 
@@ -33,7 +33,7 @@ class Dhcp(TestSuite):
 
         """,
         priority=1,
-        requirement=simple_requirement(supported_platform_type=[AZURE, READY]),
+        requirement=simple_requirement(supported_platform_type=[AZURE, READY, HYPERV]),
     )
     def verify_dhcp_client_timeout(self, node: Node) -> None:
         dhclient = node.tools[Dhclient]
