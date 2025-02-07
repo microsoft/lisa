microsoft/lisa

commit 603bd277196f8ac647c0482cd2ff2015fdebac99

Enabled wait_output() to search from latest log buffer

lisa/util/process.py
@@ -214,6 +214,7 @@ class Process:
 
         # add a string stream handler to the logger
         self.log_buffer = io.StringIO()
+        self.log_buffer_offset = 0
 
     @_retry_spawn
     def start(
@@ -492,6 +493,7 @@ class Process:
         timeout: int = 300,
         error_on_missing: bool = True,
         interval: float = 1,
+        delta_only: bool = False,
     ) -> bool:
         # check if stdout buffers contain the string "keyword" to determine if
         # it is running
@@ -503,10 +505,14 @@ class Process:
             self._stderr_writer.flush()
 
             # check if buffer contains the keyword
-            if keyword in self.log_buffer.getvalue():
+            find_pos = self.log_buffer_offset if delta_only else 0
+            if self.log_buffer.getvalue().find(keyword, find_pos) >= 0:
+                self.log_buffer_offset = len(self.log_buffer.getvalue())
                 return True
 
             time.sleep(interval)
+
+        self.log_buffer_offset = len(self.log_buffer.getvalue())
 
         if error_on_missing:
             raise LisaException(

commit b5f3fbc40bea6d9afc7493ca221fe3b73371ebbf

Process: fix found position bug with wait_output delta_only
Fixes: 603bd277196f8

If a buffer is length N and found message 0 is not at
buffer[N-len(message0):N], the current code can result in missed
messages on future calls to wait_output.

wait_output currently stores the total length of the buffer as
the search buffer offset, even when a keyword is found.

Consider a buffer of 100 characters, keyword of len 10 found at 50.
The old code will record 100 as the last length. So if your next
keyword is length 10 and found at position 70, it will be lost
if you call wait_output with delta_only again.

This commit fixes that issue by saving only the end position
of slice buffer[start:end] which contains the first keyword.

Otherwise, it saves the length of the buffer if no keyword was
found.

Signed-off-by: Matthew G McGovern <mamcgove@microsoft.com>

lisa/util/process.py
@@ -498,6 +498,7 @@ class Process:
         # check if stdout buffers contain the string "keyword" to determine if
         # it is running
         start_time = time.time()
+        searched_len = 0
         while time.time() - start_time < timeout:
             # LogWriter only flushes if "\n" is written, so we need to flush
             # manually.
@@ -505,14 +506,21 @@ class Process:
             self._stderr_writer.flush()
 
             # check if buffer contains the keyword
+            search_buffer = self.log_buffer.getvalue()
             find_pos = self.log_buffer_offset if delta_only else 0
-            if self.log_buffer.getvalue().find(keyword, find_pos) >= 0:
-                self.log_buffer_offset = len(self.log_buffer.getvalue())
+            found_pos = search_buffer.find(keyword, find_pos)
+            if found_pos >= 0:
+                # note: find returns start for slice of search_buffer[start:end]
+                # So calculate end; start+len, as the new offset
+                self.log_buffer_offset = found_pos + len(keyword)
                 return True
-
+            # else, remember amount we've searched so far
+            searched_len = len(search_buffer)
             time.sleep(interval)
 
-        self.log_buffer_offset = len(self.log_buffer.getvalue())
+        # if the keyword was not found, still retain the
+        # buffer length for a second future delta_only caller.
+        self.log_buffer_offset = searched_len
 
         if error_on_missing:
             raise LisaException(