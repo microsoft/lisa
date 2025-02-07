34b4d6441e713dfd49cd499db77bca65d7ff0324
SrikanthMyakam <m.srikanth.m@gmail.com>
2025-02-07 15:09:51 +0530

    Update hyperv.py

--- lisa/tools/hyperv.py
+++ lisa/tools/hyperv.py
@@ -583,15 +583,20 @@ class HyperV(Tool):
         powershell = self.node.tools[PowerShell]
 
         # Get the first disk that is not initialized
-        disk_number = powershell.run_cmdlet(
-            "(Get-Disk | Where-Object PartitionStyle -Eq 'RAW').Number[0]",
+        raw_disks = powershell.run_cmdlet(
+            "Get-Disk | Where-Object PartitionStyle -Eq 'RAW'",
             force_run=True,
+            output_json=True,
         )
         # If no un-initialized disk is found, log a warning and return
-        if not disk_number:
+        if not raw_disks:
             self._log.warning("No un-initialized disk found.")
             self._log.warning("Skipping lisa working dir setup on additional disk.")
             return
+        if isinstance(raw_disks, list):
+            disk_number = raw_disks[0]["Number"]
+        else:
+            disk_number = raw_disks["Number"]
         # Initialize the disk
         powershell.run_cmdlet(
             f"Initialize-Disk -Number {disk_number}",
