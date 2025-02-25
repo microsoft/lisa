cca6ab82cb792f1c86af15cd188a2c1c5ae63249
Umang Francis <umfranci@microsoft.com>
2025-02-25 16:05:19 +0530

    increasing the ltp timeout

--- microsoft/testsuites/ltp/ltpsuite.py
+++ microsoft/testsuites/ltp/ltpsuite.py
@@ -28,7 +28,7 @@ from microsoft.testsuites.ltp.ltp import Ltp
     """,
 )
 class LtpTestsuite(TestSuite):
-    _TIME_OUT = 18000
+    _TIME_OUT = 36000
     # LTP_LITE_TESTS = ["math", "ipc", "mm"]
     LTP_LITE_TESTS = ['syscalls', 'fs', 'fs_perms_simple', 'dio', 'mm', 'ipc', 'irq', 'sched', 'math', 'nptl', 'pty', 'containers', 'fs_bind', 'controllers', 'fcntl-locktests', 'power_management_tests', 'hugetlb', 'commands', 'hyperthreading', 'can', 'cpuhotplug', 'net.ipv6_lib', 'input', 'cve', 'crypto', 'kernel_misc', 'uevent', 'watchqueue']
     LTP_REQUIRED_DISK_SIZE_IN_GB = 2
