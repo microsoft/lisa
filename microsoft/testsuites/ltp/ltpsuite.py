4bb87977d7b8dc5cef7675f92c2876a11b6e65b5
Umang Francis <umfranci@microsoft.com>
2025-03-04 18:41:17 +0530

    changes as per Black and Flake8

--- microsoft/testsuites/ltp/ltpsuite.py
+++ microsoft/testsuites/ltp/ltpsuite.py
@@ -29,7 +29,18 @@ from microsoft.testsuites.ltp.ltp import Ltp
 )
 class LtpTestsuite(TestSuite):
     _TIME_OUT = 21000
-    LTP_LITE_TESTS = ["math", "ipc", "mm", "sched", "pty", "fs", "uevent", "syscalls", "hyperthreading", "irq"]
+    LTP_LITE_TESTS = [
+        "math",
+        "ipc",
+        "mm",
+        "sched",
+        "pty",
+        "fs",
+        "uevent",
+        "syscalls",
+        "hyperthreading",
+        "irq",
+    ]
     LTP_REQUIRED_DISK_SIZE_IN_GB = 2
 
     @TestCaseMetadata(
