cb73de1b55725e65755ba3ebac6c6c1bdcc09d9f
Umang Francis <umfranci@microsoft.com>
2025-03-07 11:31:33 +0530

    Reverting LTP Test Suite _TIME_OUT to 18000

--- microsoft/testsuites/ltp/ltpsuite.py
+++ microsoft/testsuites/ltp/ltpsuite.py
@@ -28,7 +28,7 @@ from microsoft.testsuites.ltp.ltp import Ltp
     """,
 )
 class LtpTestsuite(TestSuite):
-    _TIME_OUT = 21000
+    _TIME_OUT = 18000
     LTP_LITE_TESTS = ["math", "ipc", "mm", "sched", "pty", "fs"]
     LTP_REQUIRED_DISK_SIZE_IN_GB = 2
 
