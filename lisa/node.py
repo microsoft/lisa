0a3748627444cee9a094070348f189f3142d3a65
rabdulfaizy <rabdulfaizy@microsoft.com>
2025-03-19 12:12:02 -0700

    16 nics + 1200 sec reboot time

--- lisa/node.py
+++ lisa/node.py
@@ -239,7 +239,7 @@ class Node(subclasses.BaseClassWithRunbookMixin, ContextMixin, InitializableMixi
         )
         return node
 
-    def reboot(self, time_out: int = 300) -> None:
+    def reboot(self, time_out: int = 1200) -> None:
         self.tools[Reboot].reboot(time_out)
 
     def execute(
