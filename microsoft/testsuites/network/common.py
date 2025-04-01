410a118bb3bf56ab247e730a3f5edebbd8486627
Umang Francis <umfranci@microsoft.com>
2025-04-01 12:42:29 +0530

    logging the values in variables and adding checks

--- microsoft/testsuites/network/common.py
+++ microsoft/testsuites/network/common.py
@@ -107,7 +107,19 @@ def sriov_vf_connection_test(
     max_retry_times = 10
     for _, source_nic_info in vm_nics[source_node.name].items():
         matched_dest_nic_name = ""
-
+        source_node.log.debug(
+            f"copy file from {source_nic_info.ip_addr} on {source_node.name} to "
+            f"{dest_node.name}"
+        )
+        if source_nic_info.name is None:
+            source_node.log.debug(
+                f"no nic name found {_}:{str(source_nic_info)} on {source_node.name}"
+            )
+        if source_nic_info.ip_addr is None:
+            source_node.log.debug(
+                f"no ip address for nic {source_nic_info.name} on {source_node.name}"
+            )
+            continue
         # find the same subnet nic on dest node
         for dest_nic_name, dest_nic_info in vm_nics[dest_node.name].items():
             # only when IPs are in the same subnet, IP1 of machine A can connect to
