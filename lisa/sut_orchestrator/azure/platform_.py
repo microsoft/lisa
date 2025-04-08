2e93913fa5d932e1a628cea3e6a2417a5454dbaa
Umang Francis <umfranci@microsoft.com>
2025-04-09 03:28:59 +0530

    fixing RG deletion

--- lisa/sut_orchestrator/azure/platform_.py
+++ lisa/sut_orchestrator/azure/platform_.py
@@ -635,7 +635,10 @@ class AzurePlatform(Platform):
         resource_group_name = environment_context.resource_group_name
         # the resource group name is empty when it is not deployed for some reasons,
         # like capability doesn't meet case requirement.
-
+        if not resource_group_name:
+            return
+        assert self._azure_runbook
+        
         if not environment_context.resource_group_is_specified:
             log.info(
                 f"skipped to delete resource group: {resource_group_name}, "
