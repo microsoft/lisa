db3f1332af47f4846b0936ef6256a35e8c0a8745
Umang Francis <umfranci@microsoft.com>
2025-04-09 15:33:00 +0530

    updating rx value for testing 2

--- microsoft/testsuites/performance/common.py
+++ microsoft/testsuites/performance/common.py
@@ -216,9 +216,11 @@ def perf_tcp_pps(
 
     # Setting ethtool rx to 1024
     server_ethtool = server.tools[Ethtool]
-    server.log.debug(f"Ring Buffer Values before: {server_ethtool.get_device_ring_buffer_settings("eth1").device_ring_buffer_settings_raw}")
+    before_val = server_ethtool.get_device_ring_buffer_settings("eth1").device_ring_buffer_settings_raw
+    server.log.debug(f"Ring Buffer Values before: {before_val}")
     server_ethtool.change_device_ring_buffer_settings("eth1", 1024, 256)
-    server.log.debug(f"Ring Buffer Values after: {server_ethtool.get_device_ring_buffer_settings("eth1").device_ring_buffer_settings_raw}")
+    after_val = server_ethtool.get_device_ring_buffer_settings("eth1").device_ring_buffer_settings_raw
+    server.log.debug(f"Ring Buffer Values after: {after_val}")
     if server is not None or client is not None:
         assert server is not None, "server need to be specified, if client is set"
         assert client is not None, "client need to be specified, if server is set"
