deb9c7c0756832c92cda3d3a92f641bc3c7f90ec
Autumn Nash <autumnnash@microsoft.com>
2025-06-02 12:01:59 -0700

    fixed and refractored helper function. made private.

--- microsoft/testsuites/security/openssl.py
+++ microsoft/testsuites/security/openssl.py
@@ -28,30 +28,26 @@ class OpenSSLTestSuite(TestSuite):
     """
     Test suite for OpenSSL functionality.
     """
-    @TestSuiteMetadata(
-        area="security",
-        category="functional",
-        description="""
-        Tests the functionality of OpenSSL, including encryption and decryption
-        operations. Validates that OpenSSL can successfully encrypt plaintext data
-        and decrypt it back to its original form using generated keys and IVs.
-        """,
-    )
-    
+
     @TestCaseMetadata(
-        description="""
-        Tests basic functionality of openssl.
-        """,
-        priority=2,
-    )
+            description="""
+            Tests basic functionality of openssl.
+            """,
+            priority=2,
+        )
+
+    def verify_openssl_basic(self, log: Logger, node: Node) -> None:
+        """
+        Verifies basic OpenSSL encryption and decryption
+        """
+        _openssl_test_encrypt_decrypt(log, node)
     
-    def openssl_test_encrypt_decrypt(self, log: Logger, node: Node) -> None:
+    def _openssl_test_encrypt_decrypt(log: Logger, node: Node) -> None:
         """
         Tests OpenSSL encryption and decryption functionality.
         This function generates a random key and IV, encrypts various types of
         """
-        
-        self.openssl_test_encrypt_decrypt(log, node)
+    
         # Key and IV for encryption and decryption.   
         openssl = node.tools[OpenSSL]
         key_hex = openssl.run("rand -hex 32", expected_exit_code=0).stdout.strip()
@@ -71,9 +67,4 @@ class OpenSSLTestSuite(TestSuite):
             assert_that(plaintext).is_equal_to(decrypted_data)
 
         log.debug("Successfully encrypted and decrypted all test data.")
-
-    def verify_openssl_basic(self, log: Logger, node: Node) -> None:
-        """
-        Verifies basic OpenSSL encryption and decryption
-        """
-        openssl_test_encrypt_decrypt(log, node)
+       
