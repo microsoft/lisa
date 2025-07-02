c4aa9b0d4f215825f58afb341ad1ff5acf2b323e
rabdulfaizy <rabdulfaizy@microsoft.com>
2025-07-02 13:49:54 -0700

    hdd

--- microsoft/testsuites/performance/storageperf.py
+++ microsoft/testsuites/performance/storageperf.py
@@ -147,8 +147,8 @@ class StoragePerformance(TestSuite):
         timeout=TIME_OUT,
         requirement=simple_requirement(
             disk=schema.DiskOptionSettings(
-                data_disk_type=schema.DiskType.PremiumSSDLRS,
-                os_disk_type=schema.DiskType.PremiumSSDLRS,
+                data_disk_type=schema.DiskType.StandardHDDLRS,
+                os_disk_type=schema.DiskType.StandardHDDLRS,
                 data_disk_iops=search_space.IntRange(min=5000),
                 data_disk_count=search_space.IntRange(min=16),
             ),
@@ -165,8 +165,8 @@ class StoragePerformance(TestSuite):
         timeout=TIME_OUT,
         requirement=simple_requirement(
             disk=schema.DiskOptionSettings(
-                data_disk_type=schema.DiskType.PremiumSSDLRS,
-                os_disk_type=schema.DiskType.PremiumSSDLRS,
+                data_disk_type=schema.DiskType.StandardHDDLRS,
+                os_disk_type=schema.DiskType.StandardHDDLRS,
                 data_disk_iops=search_space.IntRange(min=5000),
                 data_disk_count=search_space.IntRange(min=16),
             ),
