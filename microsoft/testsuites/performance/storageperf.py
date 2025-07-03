e619679fcbb2faf7b24c1485edef063a5426553f
rabdulfaizy <rabdulfaizy@microsoft.com>
2025-07-03 10:08:21 -0700

    added hdd and ssd perf cases

--- microsoft/testsuites/performance/storageperf.py
+++ microsoft/testsuites/performance/storageperf.py
@@ -154,7 +154,7 @@ class StoragePerformance(TestSuite):
             ),
         ),
     )
-    def perf_premium_datadisks_4k(self, node: Node, result: TestResult) -> None:
+    def perf_hdd_datadisks_4k(self, node: Node, result: TestResult) -> None:
         self._perf_premium_datadisks(node, result)
 
     @TestCaseMetadata(
@@ -172,7 +172,43 @@ class StoragePerformance(TestSuite):
             ),
         ),
     )
-    def perf_premium_datadisks_1024k(self, node: Node, result: TestResult) -> None:
+    def perf_hdd_datadisks_1024k(self, node: Node, result: TestResult) -> None:
+        self._perf_premium_datadisks(node, result, block_size=1024)
+
+    @TestCaseMetadata(
+        description="""
+        This test case uses fio to test data disk performance with 4K block size.
+        """,
+        priority=3,
+        timeout=TIME_OUT,
+        requirement=simple_requirement(
+            disk=schema.DiskOptionSettings(
+                data_disk_type=schema.DiskType.StandardSSDLRS,
+                os_disk_type=schema.DiskType.StandardSSDLRS,
+                data_disk_iops=search_space.IntRange(min=500),
+                data_disk_count=search_space.IntRange(min=16),
+            ),
+        ),
+    )
+    def perf_ssd_datadisks_4k(self, node: Node, result: TestResult) -> None:
+        self._perf_premium_datadisks(node, result)
+
+    @TestCaseMetadata(
+        description="""
+        This test case uses fio to test data disk performance using 1024K block size.
+        """,
+        priority=3,
+        timeout=TIME_OUT,
+        requirement=simple_requirement(
+            disk=schema.DiskOptionSettings(
+                data_disk_type=schema.DiskType.StandardSSDLRS,
+                os_disk_type=schema.DiskType.StandardSSDLRS,
+                data_disk_iops=search_space.IntRange(min=500),
+                data_disk_count=search_space.IntRange(min=16),
+            ),
+        ),
+    )
+    def perf_ssd_datadisks_1024k(self, node: Node, result: TestResult) -> None:
         self._perf_premium_datadisks(node, result, block_size=1024)
 
     @TestCaseMetadata(
