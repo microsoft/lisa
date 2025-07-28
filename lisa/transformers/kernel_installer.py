0b41d7210861d8f57103bed4284e1562626423b9
SrikanthMyakam <m.srikanth.m@gmail.com>
2025-07-28 09:02:30 +0530

    Update kernel_installer.py

--- lisa/transformers/kernel_installer.py
+++ lisa/transformers/kernel_installer.py
@@ -290,7 +290,7 @@ class RepoInstaller(BaseInstaller):
         ubuntu.install_packages(full_package_name)
 
         # kernel_version = self._get_kernel_version(runbook.source, node)
-        kernel_version = "6.14.0-1009"
+        kernel_version = "6.14.0-1010"
         return kernel_version
 
     def _get_kernel_version(self, source: str, node: Node) -> str:
