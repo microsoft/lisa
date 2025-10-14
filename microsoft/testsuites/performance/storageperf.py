fcb80c8996d42a6393e505b067bf804d3dc2b0a3
Aditya Nagesh <adityanagesh@microsoft.com>
2025-10-14 11:46:40 +0000

    Increase min disk count

--- microsoft/testsuites/performance/storageperf.py
+++ microsoft/testsuites/performance/storageperf.py
@@ -152,7 +152,7 @@ class StoragePerformance(TestSuite):
                 data_disk_type=schema.DiskType.PremiumSSDLRS,
                 os_disk_type=schema.DiskType.PremiumSSDLRS,
                 data_disk_iops=search_space.IntRange(min=5000),
-                data_disk_count=search_space.IntRange(min=16),
+                data_disk_count=search_space.IntRange(min=40),
             ),
         ),
     )
@@ -169,8 +169,8 @@ class StoragePerformance(TestSuite):
             disk=schema.DiskOptionSettings(
                 data_disk_type=schema.DiskType.PremiumSSDLRS,
                 os_disk_type=schema.DiskType.PremiumSSDLRS,
-                data_disk_iops=search_space.IntRange(min=5000),
-                data_disk_count=search_space.IntRange(min=16),
+                data_disk_iops=search_space.IntRange(min=10000),
+                data_disk_count=search_space.IntRange(min=40),
             ),
         ),
     )
