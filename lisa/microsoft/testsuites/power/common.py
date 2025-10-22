d7de7e1d460d0215cfd7a305a461afaa6793c5ca
Johnson George <johgeorg@microsoft.com>
2025-10-21 23:05:49 -0700

    Network test cases enhancements
    Handle enable/disable test cases
    Handle PCI NIC count

--- lisa/microsoft/testsuites/power/common.py
+++ lisa/microsoft/testsuites/power/common.py
@@ -189,7 +189,7 @@ def _verify_common_hibernation_requirements(
 
     node_nic = node.nics
     node_nic.initialize()
-    lower_nics_after_hibernation = node_nic.get_lower_nics()
+    lower_nics_after_hibernation = node_nic.get_pci_nics()
     upper_nics_after_hibernation = node_nic.get_nic_names()
 
     assert_that(len(lower_nics_after_hibernation)).described_as(
@@ -212,7 +212,7 @@ def verify_hibernation_by_tool(
     then verifies hibernation through tool-specific logs and metrics.
     """
     node_nic = node.nics
-    lower_nics_before_hibernation = node_nic.get_lower_nics()
+    lower_nics_before_hibernation = node_nic.get_pci_nics()
     upper_nics_before_hibernation = node_nic.get_nic_names()
 
     hibernation_setup_tool = node.tools[HibernationSetup]
@@ -316,7 +316,7 @@ def verify_hibernation_by_vm_extension(
         raise
 
     node_nic = node.nics
-    lower_nics_before_hibernation = node_nic.get_lower_nics()
+    lower_nics_before_hibernation = node_nic.get_pci_nics()
     upper_nics_before_hibernation = node_nic.get_nic_names()
 
     # Perform hibernation cycle
