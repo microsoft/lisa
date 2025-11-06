99d7277a6d56ff1adfbdd2945d2d27a136920074
Rachel Menge <rachelmenge@microsoft.com>
2025-11-06 09:44:51 -0800

    Remove the unused functions in dmsetup

--- lisa/tools/dmsetup.py
+++ lisa/tools/dmsetup.py
@@ -1,8 +1,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from typing import Optional
-
 from lisa.executable import Tool
 
 
@@ -42,57 +40,6 @@ class Dmsetup(Tool):
         """
         result = self.node.execute(
             f"dmsetup table {device_name}", sudo=True, expected_exit_code=0
-        )
-        return result.stdout
-
-    def info(self, device_name: Optional[str] = None) -> str:
-        """
-        Get information about device mapper devices.
-
-        Args:
-            device_name: Optional name of a specific device mapper device
-
-        Returns:
-            The info output
-        """
-        cmd = "dmsetup info"
-        if device_name:
-            cmd += f" {device_name}"
-
-        result = self.node.execute(cmd, sudo=True, expected_exit_code=0)
-        return result.stdout
-
-    def ls(self) -> str:
-        """
-        List all device mapper devices.
-
-        Returns:
-            The list of devices
-        """
-        result = self.node.execute("dmsetup ls", sudo=True, expected_exit_code=0)
-        return result.stdout
-
-    def message(
-        self,
-        device_name: str,
-        sector: str,
-        message: str,
-    ) -> str:
-        """
-        Send a message to a device mapper device.
-
-        Args:
-            device_name: Name of the device mapper device
-            sector: Sector number
-            message: Message to send
-
-        Returns:
-            The command output
-        """
-        result = self.node.execute(
-            f"dmsetup message {device_name} {sector} {message}",
-            sudo=True,
-            expected_exit_code=0,
         )
         return result.stdout
 
