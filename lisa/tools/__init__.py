d5fc40e46de6885ef07e83187ac16bcbab055849
Aditya Nagesh <adityanagesh@microsoft.com>
2025-11-11 09:49:18 +0000

    Use ReadLink in NIC.py

--- lisa/tools/__init__.py
+++ lisa/tools/__init__.py
@@ -114,6 +114,7 @@ from .pvremove import Pvremove
 from .python import Pip, Python
 from .qemu import Qemu
 from .qemu_img import QemuImg
+from .readlink import Readlink
 from .reboot import Reboot
 from .remote_copy import RemoteCopy
 from .resize_partition import ResizePartition
@@ -271,6 +272,7 @@ __all__ = [
     "Qemu",
     "QemuImg",
     "Reboot",
+    "Readlink",
     "RemoteCopy",
     "ResizePartition",
     "Rpm",
