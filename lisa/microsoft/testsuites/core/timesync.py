94fecd7527999bf31c67eb34824be6d43db9ae61
Sebastian Heid <8442432+s4heid@users.noreply.github.com>
2025-11-24 05:53:04 +0000

    Scan chrony drop-in replacement files for verify_timesync_ptp test (#4120)

--- lisa/microsoft/testsuites/core/timesync.py
+++ lisa/microsoft/testsuites/core/timesync.py
@@ -28,6 +28,7 @@ from lisa.tools import (
     Chrony,
     Dmesg,
     Echo,
+    Find,
     Hwclock,
     Ls,
     Lscpu,
@@ -75,7 +76,8 @@ class TimeSync(TestSuite):
     chrony_path = [
         "/etc/chrony.conf",
         "/etc/chrony/chrony.conf",
-        "/etc/chrony.d/azure.conf",
+        "/etc/chrony.d/*.conf",
+        "/etc/chrony/conf.d/*.conf",
     ]
     current_clocksource = (
         "/sys/devices/system/clocksource/clocksource0/current_clocksource"
@@ -146,13 +148,20 @@ class TimeSync(TestSuite):
 
         # 4. Chrony should be configured to use the symlink /dev/ptp_hyperv
         #  instead of /dev/ptp0 or /dev/ptp1.
+        find = node.tools[Find]
+        config_files = find.find_files(
+            node.get_pure_path("/etc"),
+            path_pattern=self.chrony_path,
+            file_type="f",
+            sudo=True,
+            ignore_not_exist=True,
+            force_run=True,
+        )
+
         all_chrony_configs: str = ""
-        for chrony_config in self.chrony_path:
-            if node.shell.exists(PurePosixPath(chrony_config)):
-                config_data = cat.run(
-                    f"{chrony_config}", sudo=True, shell=True, force_run=True
-                )
-                all_chrony_configs = all_chrony_configs + config_data.stdout + "\n"
+        for config_file in config_files:
+            config_data = cat.run(config_file, sudo=True, shell=True, force_run=True)
+            all_chrony_configs = all_chrony_configs + config_data.stdout + "\n"
         assert_that(all_chrony_configs).described_as(
             "Chrony config file should use the symlink /dev/ptp_hyperv."
         ).contains(self.hyperv_ptp_udev_rule)
