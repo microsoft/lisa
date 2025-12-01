cc3f40b480963abbcce5efb4fe8269ba65dc066b
rabdulfaizy <rabdulfaizy@microsoft.com>
2025-12-01 10:31:37 -0800

    15nics

--- lisa/microsoft/testsuites/network/synthetic.py
+++ lisa/microsoft/testsuites/network/synthetic.py
@@ -56,7 +56,7 @@ class Synthetic(TestSuite):
         """,
         priority=2,
         requirement=simple_requirement(
-            min_nic_count=8,
+            min_nic_count=15,
             network_interface=schema.NetworkInterfaceOptionSettings(
                 data_path=schema.NetworkDataPath.Synthetic,
             ),
@@ -82,7 +82,7 @@ class Synthetic(TestSuite):
         """,
         priority=2,
         requirement=simple_requirement(
-            min_nic_count=8,
+            min_nic_count=15,
             network_interface=schema.NetworkInterfaceOptionSettings(
                 data_path=schema.NetworkDataPath.Synthetic,
             ),
@@ -109,7 +109,7 @@ class Synthetic(TestSuite):
         """,
         priority=2,
         requirement=simple_requirement(
-            min_nic_count=8,
+            min_nic_count=15,
             network_interface=schema.NetworkInterfaceOptionSettings(
                 data_path=schema.NetworkDataPath.Synthetic,
             ),
@@ -140,7 +140,7 @@ class Synthetic(TestSuite):
         requirement=simple_requirement(
             network_interface=schema.NetworkInterfaceOptionSettings(
                 data_path=schema.NetworkDataPath.Synthetic,
-                max_nic_count=8,
+                max_nic_count=15,
             ),
         ),
     )
@@ -173,7 +173,7 @@ class Synthetic(TestSuite):
         requirement=simple_requirement(
             network_interface=schema.NetworkInterfaceOptionSettings(
                 data_path=schema.NetworkDataPath.Synthetic,
-                max_nic_count=8,
+                max_nic_count=15,
             ),
         ),
     )
