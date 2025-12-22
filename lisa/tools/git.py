5726dac32c6e10dadf4db7677317027a5dcddcd0
Piyush Sachdeva <psachdeva@microsoft.com>
2025-12-22 17:40:38 +0530

    tools: git: Optimise git-fetch to fetch from a remote if provided
    Signed-off-by: Piyush Sachdeva <psachdeva@microsoft.com>
    Signed-off-by: Piyush Sachdeva <s.piyush1024@gmail.com>

--- lisa/tools/git.py
+++ lisa/tools/git.py
@@ -176,9 +176,12 @@ class Git(Tool):
         )
         result.assert_exit_code(message=f"failed to pull code. {result.stdout}")
 
-    def fetch(self, cwd: pathlib.PurePath) -> None:
+    def fetch(self, cwd: pathlib.PurePath, remote: str = "") -> None:
+        cmd = "fetch -p"
+        if remote:
+            cmd += f" {remote}"
         result = self.run(
-            "fetch -p",
+            cmd,
             force_run=True,
             cwd=cwd,
             no_info_log=True,
