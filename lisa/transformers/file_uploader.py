15442caa7e26e8dce552385d985fe808968823e3
Piyush Sachdeva <psachdeva@microsoft.com>
2025-12-24 17:28:31 +0530

    file_uploader: Add new `file_transfer` transformer
    The new file_transfer transformer uses `copy_between_remotes` method to scp
    file[s] from source to destination node. Its validate method verifies the
    existence of files on the source node and also has the functionality to transfer
    all files from source directory on source node using `files: ["*"]`.
    In case the `copy_between_remotes` fails, there is also a provision to first
    save files from source node to local, and then upload from the local node to
    destination. For this to work, the user should set the `local_path` variable.
    
    Signed-off-by: Piyush Sachdeva <psachdeva@microsoft.com>
    Signed-off-by: Piyush Sachdeva <s.piyush1024@gmail.com>

--- lisa/transformers/file_uploader.py
+++ lisa/transformers/file_uploader.py
@@ -3,16 +3,18 @@
 import os
 from dataclasses import dataclass, field
 from pathlib import PurePath
-from typing import Any, Dict, List, Type
+from typing import Any, Dict, List, Optional, Type
 
 from dataclasses_json import dataclass_json
 
 from lisa import schema
+from lisa.node import RemoteNode, quick_connect
 from lisa.tools import Ls, Mkdir, RemoteCopy
 from lisa.transformers.deployment_transformer import (
     DeploymentTransformer,
     DeploymentTransformerSchema,
 )
+from lisa.util import field_metadata
 
 FILE_UPLOADER = "file_uploader"
 UPLOADED_FILES = "uploaded_files"
@@ -108,4 +110,137 @@ class FileUploaderTransformer(DeploymentTransformer):
             uploaded_files.append(name)
 
         result[UPLOADED_FILES] = uploaded_files
+        return result
+
+
+FILE_TRANSFER = "file_transfer"
+TRANSFERRED_FILES = "transferred_files"
+
+
+@dataclass_json
+@dataclass
+class FileTransferTransformerSchema(FileUploaderTransformerSchema):
+    # optional source node information, use schema.RemoteNode
+    source_node: schema.RemoteNode = field(
+        default_factory=schema.RemoteNode,
+        metadata=field_metadata(required=True),
+    )
+    # optional local path for temporary storage during file transfers
+    local_path: Optional[str] = ""
+    # flag to skip scp attempts
+    try_scp: Optional[bool] = True
+
+
+class FileTransferTransformer(FileUploaderTransformer):
+    """
+    This transformer transfers files between two remotes. It should be used when
+    environment is connected.
+    """
+
+    @classmethod
+    def type_name(cls) -> str:
+        return FILE_TRANSFER
+
+    @classmethod
+    def type_schema(cls) -> Type[schema.TypedSchema]:
+        return FileTransferTransformerSchema
+
+    @property
+    def _output_names(self) -> List[str]:
+        return [TRANSFERRED_FILES]
+
+    def _validate(self) -> None:
+        runbook: FileTransferTransformerSchema = self.runbook
+        source = runbook.source
+
+        # Cache the connected source node on the instance to avoid reconnecting.
+        self._source_node = quick_connect(runbook.source_node, runbook.name)
+        src_node = self._source_node
+
+        ls = src_node.tools[Ls]
+        if not ls.path_exists(source):
+            raise ValueError(f"source {source} doesn't exist on remote node.")
+
+        self._runbook_files: List[str] = runbook.files
+        if self._runbook_files == ["*"]:
+            self._runbook_files = []
+            files = ls.list(source, sudo=True)
+            if len(files) == 0:
+                self._log.debug("No files to transfer.")
+            for file in files:
+                self._runbook_files.append(PurePath(file).name)
+
+        for file in self._runbook_files:
+            assert ls.path_exists(
+                f"{source}/{file}"
+            ), f"Node does not contain file: {file}"
+
+    def _initialize(self, *args: Any, **kwargs: Any) -> None:
+        super()._initialize(*args, **kwargs)
+        runbook: FileTransferTransformerSchema = self.runbook
+        if not runbook.source_node:
+            raise ValueError("'source_node' must be provided.")
+
+    def _internal_run(self) -> Dict[str, Any]:
+        runbook: FileTransferTransformerSchema = self.runbook
+        result: Dict[str, Any] = dict()
+        dest_copy = self._node.tools[RemoteCopy]
+        transferred_files: List[str] = []
+
+        src_purepath = PurePath(runbook.source)
+        dest_node = self._node
+        dest_purepath = PurePath(runbook.destination)
+
+        # checking destination existence
+        self._check_dest_dir(runbook.destination)
+
+        src_node = self._source_node
+        src_copy = src_node.tools[RemoteCopy]
+        local_purepath = None
+        if runbook.local_path:
+            if not os.path.exists(runbook.local_path):
+                os.mkdir(runbook.local_path)
+
+            local_path = f"{runbook.local_path}/{src_purepath.name}"
+            if not os.path.exists(local_path):
+                os.mkdir(local_path)
+
+            local_purepath = PurePath(local_path)
+
+        scp_result: Optional[int] = 0
+        if not runbook.try_scp:
+            scp_result = -1
+
+        for file_name in self._runbook_files:
+            if scp_result is not None and scp_result == 0:
+                self._log.info(
+                    f"remote-to-remote: '{file_name}' to '{str(dest_purepath)}'"
+                )
+                scp_result = dest_copy.copy_between_remotes(
+                    src_node=src_node,
+                    src_path=src_purepath / file_name,
+                    dest_node=dest_node,
+                    dest_path=dest_purepath / file_name,
+                    recurse=False,
+                )
+            else:
+                assert local_purepath, "local path doesn't exist"
+                self._log.info(f"downloading '{file_name}' from '{str(src_purepath)}")
+                src_copy.copy_to_local(
+                    src=src_purepath / file_name,
+                    dest=local_purepath,
+                    recurse=False,
+                    sudo=False,
+                )
+                self._log.info(f"uploading '{file_name}' to '{str(dest_purepath)}'")
+                dest_copy.copy_to_remote(
+                    src=local_purepath / file_name,
+                    dest=dest_purepath,
+                    recurse=False,
+                    sudo=False,
+                )
+            transferred_files.append(file_name)
+
+        self._log.info(f"files transferred: {transferred_files}")
+        result[TRANSFERRED_FILES] = transferred_files
         return result
