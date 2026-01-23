d7aca13095de0a8c4b98cf417593198109cc6086
rabdulfaizy <rabdulfaizy@microsoft.com>
2026-01-22 16:31:39 -0800

    16000 iops

--- lisa/microsoft/testsuites/performance/storageperf.py
+++ lisa/microsoft/testsuites/performance/storageperf.py
@@ -174,7 +174,7 @@ class StoragePerformance(TestSuite):
             disk=schema.DiskOptionSettings(
                 data_disk_type=schema.DiskType.PremiumSSDLRS,
                 os_disk_type=schema.DiskType.PremiumSSDLRS,
-                data_disk_iops=search_space.IntRange(min=5000),
+                data_disk_iops=search_space.IntRange(min=16000),
                 data_disk_count=search_space.IntRange(min=16),
             ),
         ),
