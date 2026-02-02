psachdeva-ms/azfiles-lisa

package_installer: New DEBPkgInstallerTransformer using dpkg tool
Signed-off-by: Piyush Sachdeva <psachdeva@microsoft.com>
Signed-off-by: Piyush Sachdeva <s.piyush1024@gmail.com>

lisa/transformers/package_installer.py
@@ -7,8 +7,8 @@ from typing import Any, Dict, List, Type
 from dataclasses_json import dataclass_json
 
 from lisa import schema
-from lisa.operating_system import RPMDistro
-from lisa.tools import Rpm
+from lisa.operating_system import RPMDistro, Debian
+from lisa.tools import Dpkg, Rpm, Uname
 from lisa.tools.ls import Ls
 from lisa.transformers.deployment_transformer import (
     DeploymentTransformer,
@@ -130,3 +130,33 @@ class RPMPackageInstallerTransformer(PackageInstaller):
 
     def _install_package(self, file: str) -> None:
         self._node.tools[Rpm].install_local_package(file, force=True, nodeps=True)
+
+
+class DEBPackageInstallerTransformer(PackageInstaller):
+    @classmethod
+    def type_name(cls) -> str:
+        return "deb_package_installer"
+
+    @classmethod
+    def type_schema(cls) -> Type[schema.TypedSchema]:
+        return PackageInstallerSchema
+
+    @property
+    def _output_names(self) -> List[str]:
+        return []
+
+    def _validate(self) -> None:
+        if not isinstance(self._node.os, Debian):
+            raise UnsupportedDistroException(
+                self._node.os,
+                f"'{self.type_name()}' transformer only supports Debian-based Distros.",
+            )
+        super()._validate()
+
+    def _validate_package(self, file: str) -> None:
+        assert self._node.tools[Dpkg].is_valid_package(
+            file
+        ), f"Provided file {file} is not a deb"
+
+    def _install_package(self, file: str) -> None:
+        self._node.tools[Dpkg].install_local_package(file, force=True)

microsoft/lisa

lisa/transformers/package_installer.py
@@ -7,9 +7,8 @@ from typing import Any, Dict, List, Type
 from dataclasses_json import dataclass_json
 
 from lisa import schema
-from lisa.operating_system import RPMDistro
-from lisa.tools import Rpm
-from lisa.tools.ls import Ls
+from lisa.operating_system import Debian, RPMDistro
+from lisa.tools import Dpkg, Ls, Rpm, Uname
 from lisa.transformers.deployment_transformer import (
     DeploymentTransformer,
     DeploymentTransformerSchema,
@@ -122,3 +121,33 @@ class RPMPackageInstallerTransformer(PackageInstaller):
 
     def _install_package(self, file: str) -> None:
         self._node.tools[Rpm].install_local_package(file, force=True, nodeps=True)
+
+
+class DEBPackageInstallerTransformer(PackageInstaller):
+    @classmethod
+    def type_name(cls) -> str:
+        return "deb_package_installer"
+
+    @classmethod
+    def type_schema(cls) -> Type[schema.TypedSchema]:
+        return PackageInstallerSchema
+
+    @property
+    def _output_names(self) -> List[str]:
+        return []
+
+    def _validate(self) -> None:
+        if not isinstance(self._node.os, Debian):
+            raise UnsupportedDistroException(
+                self._node.os,
+                f"'{self.type_name()}' transformer only supports Debian-based Distros.",
+            )
+        super()._validate()
+
+    def _validate_package(self, file: str) -> None:
+        assert self._node.tools[Dpkg].is_valid_package(
+            file
+        ), f"Provided file {file} is not a deb"
+
+    def _install_package(self, file: str) -> None:
+        self._node.tools[Dpkg].install_local_package(file, force=True)