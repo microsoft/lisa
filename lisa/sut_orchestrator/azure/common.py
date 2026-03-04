c68df0d0c40011c2163ba0a2b043c4343fb1bb5b
Matthew McGovern (LINUX) <mamcgove@microsoft.com>
2026-03-04 13:04:02 -0800

    cleanup platform changes, still needs a lock

--- lisa/sut_orchestrator/azure/common.py
+++ lisa/sut_orchestrator/azure/common.py
@@ -1711,26 +1711,39 @@ def get_virtual_networks(
         ]
     return virtual_network_dict
 
-def remove_vnet_peering(platform: "AzurePlatform", resource_group_name:str)-> None:
+
+def remove_vnet_peerings(platform: "AzurePlatform", resource_group_name: str) -> None:
     # delete both the vnet peering for a resource group and the corresponding link
     # in the remote group.
     network_client = get_network_client(platform)
     vnets = network_client.virtual_networks.list(
-            resource_group_name=resource_group_name
-        )
+        resource_group_name=resource_group_name
+    )
     for vnet in vnets:
-        peerings = network_client.virtual_network_peerings.get(resource_group_name, name=vnet.name)
+        peerings = network_client.virtual_network_peerings.get(
+            resource_group_name, name=vnet.name
+        )
         for peering in peerings:
-            network_client.virtual_network_peerings.begin_delete(resource_group_name, vnet.name, peering.name).wait()
-            
+            network_client.virtual_network_peerings.begin_delete(
+                resource_group_name, vnet.name, peering.name
+            ).wait()
+
             if not delete_op.done():
-                platform._log.debug(f"Delete peering operation failed for peering: {peering.name}")
-            
-            remote_peering = self._rm_client.get(id: peering.remote_virtual_network)
-            remote_peering = network_client.virtual_network_peerings.begin_delete(resource_group_name, peering.remote_virtual_network.split('/')[-1], peering.name).wait()
+                platform._log.debug(
+                    f"Delete peering operation failed for peering: {peering.name}"
+                )
+
+            remote_peering = self._rm_client.get(peering.remote_virtual_network)
+            remote_peering = network_client.virtual_network_peerings.begin_delete(
+                resource_group_name,
+                peering.remote_virtual_network.split("/")[-1],
+                peering.name,
+            ).wait()
         for subnet in vnet.subnets:
-            network_client.subnets.begin_delete(resource_group_name, vnet.name, subnet.name).wait()
-        
+            network_client.subnets.begin_delete(
+                resource_group_name, vnet.name, subnet.name
+            ).wait()
+
 
 def get_network_client(platform: "AzurePlatform") -> NetworkManagementClient:
     return NetworkManagementClient(
