186e5a1c531b8f7af1c99a79ad3991385253093b
Umang Francis <umfranci@microsoft.com>
2026-03-05 16:29:33 +0530

    increasing the min throughput and data disks for perf tests

--- lisa/microsoft/testsuites/performance/storageperf.py
+++ lisa/microsoft/testsuites/performance/storageperf.py
@@ -155,8 +155,8 @@ class StoragePerformance(TestSuite):
             disk=schema.DiskOptionSettings(
                 data_disk_type=schema.DiskType.PremiumSSDLRS,
                 os_disk_type=schema.DiskType.PremiumSSDLRS,
-                data_disk_iops=search_space.IntRange(min=5000),
-                data_disk_count=search_space.IntRange(min=16),
+                data_disk_iops=search_space.IntRange(min=10000),
+                data_disk_count=search_space.IntRange(min=64),
             ),
         ),
     )
@@ -174,8 +174,8 @@ class StoragePerformance(TestSuite):
             disk=schema.DiskOptionSettings(
                 data_disk_type=schema.DiskType.PremiumSSDLRS,
                 os_disk_type=schema.DiskType.PremiumSSDLRS,
-                data_disk_iops=search_space.IntRange(min=5000),
-                data_disk_count=search_space.IntRange(min=16),
+                data_disk_iops=search_space.IntRange(min=10000),
+                data_disk_count=search_space.IntRange(min=64),
             ),
         ),
     )
