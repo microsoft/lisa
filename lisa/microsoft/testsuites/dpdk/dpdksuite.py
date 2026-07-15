d993709f97c9ae2703c0e29161eee1cd1c8c5f37
Matthew McGovern (LINUX) <mamcgove@microsoft.com>
2026-07-14 22:46:22 -0700

    dpdk: deprecate start_testpmd_concurrent
    Gets rid of old abstraction that didn't allow direct monitoring
    of testpmd processes.
    
    It is replaced with new functions specifically for each version of the
    test. We are violating the DRY ethos here in favor of readability and
    maintaining direct control over the processes.
    
    This allows implementing the hotplug tests in a cleaner way,
    which also ends up being faster and more reliable in practice.
    
    remove burst stats flag (not needed)

--- lisa/microsoft/testsuites/dpdk/dpdksuite.py
+++ lisa/microsoft/testsuites/dpdk/dpdksuite.py
@@ -25,7 +25,7 @@ from microsoft.testsuites.dpdk.dpdkutil import (
     init_nodes_concurrent,
     initialize_node_resources,
     run_dpdk_symmetric_mp,
-    run_testpmd_concurrent,
+    run_testpmd_hotplug,
     verify_dpdk_build,
     verify_dpdk_l3fwd_ntttcp_tcp,
     verify_dpdk_mutliple_ports,
@@ -518,10 +518,13 @@ class Dpdk(TestSuite):
         receiver.switch_sriov = True
         sender.switch_sriov = False
 
-        kit_cmd_pairs = generate_send_receive_run_info(pmd, sender, receiver)
-
-        run_testpmd_concurrent(
-            kit_cmd_pairs, DPDK_VF_REMOVAL_MAX_TEST_TIME, log, hotplug_sriov=True
+        kit_cmd_pairs = generate_send_receive_run_info(
+            pmd, sender, receiver, multiple_queues=(False, True), stats_period=5
+        )
+        run_testpmd_hotplug(
+            kit_cmd_pairs=kit_cmd_pairs,
+            sender=sender,
+            receiver=receiver,
         )
 
         hotplug_pps_set = receiver.testpmd.get_mean_rx_pps_sriov_hotplug()
@@ -547,9 +550,7 @@ class Dpdk(TestSuite):
             test_kit: testpmd_cmd,
         }
 
-        run_testpmd_concurrent(
-            kit_cmd_pairs, DPDK_VF_REMOVAL_MAX_TEST_TIME, log, hotplug_sriov=True
-        )
+        run_testpmd_hotplug(kit_cmd_pairs=kit_cmd_pairs, sender=test_kit)
 
         hotplug_pps_set = testpmd.get_mean_tx_pps_sriov_hotplug()
         self._check_rx_or_tx_pps_sriov_hotplug("TX", hotplug_pps_set)
