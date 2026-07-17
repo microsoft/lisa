eefdd062b3214486d1dfa4d086915390d67efd9d
Matthew McGovern (LINUX) <mamcgove@microsoft.com>
2026-07-17 11:13:37 -0700

    add retry for httpresponseerror to switch_sriov

--- lisa/sut_orchestrator/azure/features.py
+++ lisa/sut_orchestrator/azure/features.py
@@ -988,6 +988,7 @@ class NetworkInterface(AzureFeatureMixin, features.NetworkInterface):
                     f"into status [{enable}]"
                 ).is_equal_to(enable)
 
+    @retry(HttpResponseError, tries=3, delay=30)
     def switch_sriov(
         self, enable: bool, wait: bool = True, reset_connections: bool = True
     ) -> None:
