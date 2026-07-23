c995e398172449b2ab3ae24f17462cebb58b64a5
Matthew McGovern (LINUX) <mamcgove@microsoft.com>
2026-07-23 06:52:49 -0700

    dpdk: don't infer pmd flags, pass Pmd enum instead

--- lisa/microsoft/testsuites/dpdk/dpdkutil.py
+++ lisa/microsoft/testsuites/dpdk/dpdkutil.py
@@ -300,6 +300,7 @@ def generate_send_receive_run_info(
         [snd_nic],
         0,
         "txonly",
+        pmd=pmd,
         extra_args=f"--tx-ip={snd_nic.ip_addr},{rcv_nic.ip_addr}",
         multiple_queues=snd_mq,
         service_cores=use_service_cores,
@@ -311,6 +312,7 @@ def generate_send_receive_run_info(
         [rcv_nic],
         0,
         "rxonly",
+        pmd=pmd,
         multiple_queues=rcv_mq,
         service_cores=use_service_cores,
         mtu=set_mtu,
@@ -385,6 +387,7 @@ def generate_testpmd_multiple_port_command(
             [sender_nic],
             0,
             "txonly",
+            pmd=pmd,
             extra_args=f"--tx-ip={sender_nic.ip_addr},{receiver_nic.ip_addr}",
             multiple_queues=multiple_queues,
             service_cores=use_service_cores,
@@ -395,7 +398,7 @@ def generate_testpmd_multiple_port_command(
         kit_cmd_pairs[sender] = snd_cmd
         # receiver needs multiple ports, so only generate the include.
         receiver_include = receiver.testpmd.generate_testpmd_include(
-            receiver_nics[sender_subnet], i
+            receiver_nics[sender_subnet], i, pmd=pmd
         )
         # and save it
         receiver_includes += [receiver_include]
@@ -405,6 +408,7 @@ def generate_testpmd_multiple_port_command(
         list([receiver_nics[key] for key in receiver_nics]),
         0,
         "rxonly",
+        pmd=pmd,
         multiple_queues=multiple_queues,
         service_cores=use_service_cores,
         mtu=set_mtu,
@@ -674,7 +678,7 @@ def verify_dpdk_build(
     test_nic = node.nics.get_secondary_nic()
 
     testpmd_cmd = testpmd.generate_testpmd_command(
-        [test_nic], 0, "txonly", multiple_queues=multiple_queues
+        [test_nic], 0, "txonly", pmd=pmd, multiple_queues=multiple_queues
     )
     testpmd.run_for_n_seconds(testpmd_cmd, 10)
     tx_pps = testpmd.get_mean_tx_pps()
@@ -1288,10 +1292,10 @@ def verify_dpdk_l3fwd_ntttcp_tcp(
     # generate the dpdk include arguments to add to our commandline
     include_devices = [
         fwd_kit.testpmd.generate_testpmd_include(
-            subnet_a_nics[forwarder], dpdk_port_a, force_netvsc=True
+            subnet_a_nics[forwarder], dpdk_port_a, pmd=Pmd.NETVSC
         ),
         fwd_kit.testpmd.generate_testpmd_include(
-            subnet_b_nics[forwarder], dpdk_port_b, force_netvsc=True
+            subnet_b_nics[forwarder], dpdk_port_b, pmd=Pmd.NETVSC
         ),
     ]
 
