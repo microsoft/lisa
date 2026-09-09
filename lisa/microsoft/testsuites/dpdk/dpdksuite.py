3e2a1f38f6988c9cdfe239120048f55824b46064
Matthew McGovern (LINUX) <mamcgove@microsoft.com>
2026-09-09 08:19:57 -0700

    dpdk-ovs: fix issues with OVS test and support MANA
    OVS test hadn't been updated since 2022 so would always fail on MANA.
    
    Add MANA support to the OVS test and add a scheme for parsing the
    documentation to resolve which ovs verison to use for a given dpdk
    version.

--- lisa/microsoft/testsuites/dpdk/dpdksuite.py
+++ lisa/microsoft/testsuites/dpdk/dpdksuite.py
@@ -240,51 +240,12 @@ class Dpdk(TestSuite):
             ),
         ),
     )
-    def verify_dpdk_ovs(
+    def verify_dpdk_ovs_netvsc(
         self, node: Node, log: Logger, variables: Dict[str, Any]
     ) -> None:
         # initialize DPDK first, OVS requires it built from source before configuring.
-        if node.tools[Lscpu].get_architecture() == CpuArchitecture.ARM64:
-            raise SkippedException("OVS test not supported on ARM64")
-
         force_dpdk_default_source(variables)
-        try:
-            test_kit = initialize_node_resources(
-                node, log, variables, Pmd.NETVSC, HugePageSize.HUGE_2MB
-            )
-        except (NotEnoughMemoryException, UnsupportedOperationException) as err:
-            raise SkippedException(err)
-
-        # checkout OpenVirtualSwitch
-        ovs: DpdkOvs = node.tools[DpdkOvs]
-
-        # check for runbook variable to skip dpdk version check
-        use_latest_ovs = variables.get("use_latest_ovs", False)
-        # provide ovs build with DPDK tool info and build
-        ovs.build_with_dpdk(test_kit.testpmd, use_latest_ovs=use_latest_ovs)
-
-        # enable hugepages needed for dpdk EAL
-        hugepages = node.tools[Hugepages]
-        try:
-            hugepages.init_hugepages(HugePageSize.HUGE_2MB)
-        except (NotEnoughMemoryException, UnsupportedOperationException) as err:
-            raise SkippedException(err)
-
-        try:
-            # run OVS tests, providing OVS with the NIC info needed for DPDK init
-            ovs.setup_ovs(node.nics.get_secondary_nic().pci_slot)
-
-            # validate if OVS was able to initialize DPDK
-            node.execute(
-                "ovs-vsctl get Open_vSwitch . dpdk_initialized",
-                sudo=True,
-                expected_exit_code=0,
-                expected_exit_code_failure_message=(
-                    "OVS repoted that DPDK EAL failed to initialize."
-                ),
-            )
-        finally:
-            ovs.stop_ovs()
+        run_ovs_test(node, log, variables, Pmd.NETVSC)
 
     @TestCaseMetadata(
         description="""
@@ -1548,3 +1509,51 @@ class Dpdk(TestSuite):
     def after_case(self, log: Logger, **kwargs: Any) -> None:
         environment: Environment = kwargs.pop("environment")
         do_parallel_cleanup(environment)
+
+
+def run_ovs_test(node: Node, log: Logger, variables: Dict[str, Any], pmd: Pmd) -> None:
+    if node.tools[Lscpu].get_architecture() == CpuArchitecture.ARM64:
+        raise SkippedException("OVS test not supported on ARM64")
+
+    test_nics = [node.nics.get_nic_by_subnet("10.0.1.0/24")]
+    try:
+        test_kit = initialize_node_resources(
+            node,
+            log,
+            variables,
+            pmd,
+            HugePageSize.HUGE_2MB,
+            test_nics=test_nics,
+        )
+    except (NotEnoughMemoryException, UnsupportedOperationException) as err:
+        raise SkippedException(err)
+
+    # checkout OpenVirtualSwitch
+    ovs: DpdkOvs = node.tools.create(DpdkOvs)
+
+    # check for runbook variable to skip dpdk version check
+    use_latest_ovs = variables.get("use_latest_ovs", False)
+    # provide ovs build with DPDK tool info and build
+    ovs.build_with_dpdk(test_kit.testpmd, use_latest_ovs=use_latest_ovs)
+    # enable hugepages needed for dpdk EAL
+    hugepages = node.tools[Hugepages]
+    try:
+        hugepages.init_hugepages(HugePageSize.HUGE_2MB)
+    except (NotEnoughMemoryException, UnsupportedOperationException) as err:
+        raise SkippedException(err)
+
+    try:
+        # run OVS tests, providing OVS with the NIC info needed for DPDK init
+        ovs.setup_ovs(test_nics, test_kit.testpmd)
+
+        # validate if OVS was able to initialize DPDK
+        node.execute(
+            "ovs-vsctl get Open_vSwitch . dpdk_initialized",
+            sudo=True,
+            expected_exit_code=0,
+            expected_exit_code_failure_message=(
+                "OVS repoted that DPDK EAL failed to initialize."
+            ),
+        )
+    finally:
+        ovs.stop_ovs()
