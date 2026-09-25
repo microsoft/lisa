4cd23f54511d7c8a70a8217f070465609d8c89dd
Matthew McGovern (LINUX) <mamcgove@microsoft.com>
2026-09-24 20:55:18 -0700

    ip: force run operations

--- lisa/tools/ip.py
+++ lisa/tools/ip.py
@@ -176,6 +176,7 @@ class Ip(Tool):
             f"ip -br link show {nic_name}",
             shell=True,
             sudo=True,
+            force_run=True,
             expected_exit_code=0,
             expected_exit_code_failure_message=(
                 f"ip show could not get info for device {nic_name}"
@@ -194,6 +195,7 @@ class Ip(Tool):
     def addr_flush(self, nic_name: str) -> None:
         self.node.execute(
             f"ip addr flush dev {nic_name}",
+            force_run=True,
             shell=True,
             sudo=True,
             expected_exit_code=0,
@@ -205,6 +207,7 @@ class Ip(Tool):
     def add_ipv4_address(self, nic_name: str, ip: str, persist: bool = True) -> None:
         self.run(
             f"addr add {ip} dev {nic_name}",
+            force_run=True,
             sudo=True,
             expected_exit_code=0,
             expected_exit_code_failure_message=(
@@ -580,6 +583,7 @@ class Ip(Tool):
         details = self.run(
             f"-d link show {interface}",
             shell=True,
+            force_run=True,
             expected_exit_code=0,
             expected_exit_code_failure_message=(
                 "Could not fetch interface details with 'ip -d link show'"
